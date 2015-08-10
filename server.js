269ccb8878f11531b71df3dbb1d990317b2a275b
thisisbrianhan <bthan@us.ibm.com>
2015-08-10 12:57:30 -0500

    Add process killer for controlling shutdown of nodemon When manually starting and stopping `npm run dev` script, which includes nodemon, the nodemon process doesn't always exit cleanly. This code (I think) controls the shutdown process so that there's always a clean exit

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require('express');
 var port = process.env.PORT || 3333;
 var app = express();
 
+
+// Controlling shutdown of nodemon
+// https://github.com/remy/nodemon#controlling-shutdown-of-your-script
+process.once('SIGUSR2', function () {
+  process.kill(process.pid, 'SIGUSR2');
+});
+
 app.use(express.static(__dirname + '/dev/patterns'));
 
 app.get('/', function (req, res) {
