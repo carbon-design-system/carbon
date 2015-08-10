30461a6abe9608d24ac1c8b1bb051a78107ac118
thisisbrianhan <bthan@us.ibm.com>
2015-08-10 13:56:01 -0500

    Create a gulp task for controlling shutdown of server.js when any file changes

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var reload = browserSync.reload;
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var scsslint = require('gulp-scss-lint');
+var exec = require('child_process').exec;
 
 var BROWSERS = [
   "> 5%",
@@ -63,9 +64,20 @@ gulp.task('sass', function() {
     .pipe(reload({ stream: true })); // Reload the browser (but stream when possible)
 });
 
+// Controls shutdown of nodemon
+// https://github.com/remy/nodemon#controlling-shutdown-of-your-script
+gulp.task('clean-server', function() {
+  exec('node clean-server.js', function(err, stdout, stderr) {
+    if (err) throw err;
+    console.log(stdout);
+    console.log(stderr);
+  });
+});
+
 gulp.task('watch', function() {
-  gulp.watch(['dev/*.scss', 'dev/patterns/**/*.scss'], ['sass', 'scss-lint', 'dist']); // watch for changes on these scss files
-  gulp.watch(['*.json', '*.js', 'dist/patterns/**/*.json'], ['jslint']);
+  gulp.watch('dev/patterns/**/*.md', ['clean-server']);
+  gulp.watch(['dev/*.scss', 'dev/patterns/**/*.scss'], ['sass', 'scss-lint', 'dist', 'clean-server']); // watch for changes on these scss files
+  gulp.watch(['*.json', '*.js', 'dist/patterns/**/*.json'], ['jslint', 'clean-server']);
 });
 
 gulp.task('default', ['browser-sync', 'sass', 'dist', 'watch']);
--- server.js
+++ server.js
@@ -2,13 +2,6 @@ var express = require('express');
 var port = process.env.PORT || 3333;
 var app = express();
 
-
-// Controlling shutdown of nodemon
-// https://github.com/remy/nodemon#controlling-shutdown-of-your-script
-process.once('SIGUSR2', function () {
-  process.kill(process.pid, 'SIGUSR2');
-});
-
 app.use(express.static(__dirname + '/dev/patterns'));
 
 app.get('/', function (req, res) {
