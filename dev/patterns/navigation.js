fc2bc2e14fb69c534707b52e16b6d20097c92a50
thisisbrianhan <bthan@us.ibm.com>
2015-08-12 16:21:01 -0500

    Update navigation.js

--- dev/patterns/navigation.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  var menuToggle = document.querySelector('#menu-toggle');
-  var nav = document.querySelector('ul.nav__links');
-  
-  menuToggle.addEventListener('click', function (event) {
-    nav.classList.toggle('visible');
-  });
-})();
--- dev/patterns/navigation/js/navigation.js
+++ dev/patterns/navigation/js/navigation.js
@@ -1,4 +1,8 @@
 (function(){
-  var menu = document.querySelector('#menu-toggle');
-  console.log(menu);    
+  var menuToggle = document.querySelector('#menu-toggle');
+  var nav = document.querySelector('ul.nav__links');
+  
+  menuToggle.addEventListener('click', function (event) {
+    nav.classList.toggle('visible');
+  });
 })();
