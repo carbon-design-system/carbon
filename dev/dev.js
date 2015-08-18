e7bb006d73a57c0778133f4ca44b2c3ea452b592
thisisbrianhan <bthan@us.ibm.com>
2015-08-18 16:39:52 -0400

    Append modal.html to div#modal

--- dev/dev.js
+++ dev/dev.js
@@ -43,23 +43,31 @@ $(document).ready(function() {
 
   // Loop over each property in patterns object
   for (var prop in patterns) {
+    var patternArray = patterns[prop];
 
     // Handle the nav patterns differently
     if (prop === 'nav') {
-      var navArray = patterns[prop];
-
-      for (var i = 0; i < navArray.length; i++ ) {
-        $.get(navArray[i], function (data) {
+      for (var i = 0; i < patternArray.length; i++ ) {
+        $.get(patternArray[i], function (data) {
 
           // Append nav patterns to #nav because bluemix nav needs to be at the top of the viewport in order for the styles to work responsively.
           $('#nav').append(data);
-        })
+        });
       }
     }
+
+    else if (prop === 'modals') {
+      for (var i = 0; i < patternArray.length; i++ ) {
+        $.get(patternArray[i], function (data) {
+
+          // Append modal pattern to #modal so that it can display with margins specifc for modals
+          $('#modal').append(data);
+        });
+      }
+    }
+
     // For all other patterns, append them to #patterns.
     else {
-      var patternArray = patterns[prop];
-
       for (var i = 0; i < patternArray.length; i++) {
         $.get(patternArray[i], function (data) {
           $('#patterns').append('<div>' + data + '</div>');
