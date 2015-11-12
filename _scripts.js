dfdf8794440a39e7ef64e715288c9ae23c7cf388
Mari Johannessen <mari.l.johannessen@gmail.com>
2015-11-12 10:55:07 -0600

    fix all this stuff

--- _scripts.js
+++ _scripts.js
@@ -1,13 +1,2 @@
 console.log('test');
 
-window.onhashchange = function() { 
-	var current = document.querySelector("#current");
-	var hash = document.location.hash;
-	if (hash == '') {
-		current.innerHTML = "Home";
-	} else {
-	var currentHash = hash.substring(1, hash.length);
-	current.innerHTML = currentHash.charAt(0).toUpperCase() + currentHash.slice(1);
-	}
-	console.log(current.innerHTML);
-}
