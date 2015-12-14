614ee2b87ecc9d79bd9fb568eb17e92d33ee9868
Chris Dhanaraj <adhanaraj@gmail.com>
2015-12-14 15:45:27 -0600

    bring in webpack, update current javascript

--- components/floating-add-button/_fab.js
+++
@@ -1,7 +0,0 @@
-(function() {
-		var fabButton = document.querySelector('.fab');
-		fabButton.addEventListener('click', function(e) {
-			e.preventDefault();
-			fabButton.classList.toggle('fab--close');
-		});
-})();
