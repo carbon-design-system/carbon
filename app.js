8c33e68b004e9d55a6b50899a13a38f99f421547
Akira Sudoh <asudoh@jp.ibm.com>
2016-01-20 14:42:24 +0900

    Add import statement for overflow-menu.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import Fab from './components/floating-action-button/fab';
 import FileUploader from './base-elements/inputs/file/file';
 import Tab from './components/tabs-nav/tabs-nav';
 import ContentSwitcher from './components/content-switcher/content-switcher';
+import OverflowMenu from './components/overflow-menu/overflow-menu';
 
 Fab();
 Tab();
