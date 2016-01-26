cfd0a222b4ee95a4b70193ea243ba4129d801ed4
Akira Sudoh <asudoh@jp.ibm.com>
2016-01-27 04:28:06 +0900

    Fixes to template/create directory.

--- templates/create/app/js/app.js
+++ templates/create/app/js/app.js
@@ -1,3 +1,5 @@
+import fileUploader from '../../../../base-elements/inputs/file/file';
 import contentSwitcher from '../../../../components/content-switcher/content-switcher';
 
+fileUploader();
 contentSwitcher();
