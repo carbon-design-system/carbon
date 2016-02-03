2d84d3fcc39e198f31ed641aa4b77da4cc91aee1
Rizchel Dayao <rdayao@us.ibm.com>
2016-02-03 11:32:02 -0600

    Update base elements and components in create template #562
    Updated base elements and components in create template and resolved merge conflict #562
    
    Update create template for base elements and components
    
    fixed global header create new issue for svgs not showing
    
    Changed JSON file for correct dependencies structure
    
    Fixed dependencies

--- templates/create/app/js/app.js
+++ templates/create/app/js/app.js
@@ -1,8 +1,22 @@
+import 'svgxuse';
 import '../../../../global/js/array-from';
 
 import FileUploader from '../../../../base-elements/inputs/file/file';
+import Tab from '../../../../components/tabs-nav/tabs-nav';
+import HeaderNav from '../../../../components/header/header';
 
 document.addEventListener('DOMContentLoaded', () => {
   [... document.querySelectorAll('[data-file-input]')]
     .forEach((element) => new FileUploader(element));
+  [... document.querySelectorAll('[data-tabs]')].forEach((element) => new Tab(element));
+  [... document.querySelectorAll('[data-nav-target]')].forEach((element) => {
+  [... document.querySelectorAll(element.getAttribute('data-nav-target'))].forEach((target) => {
+      target.addEventListener('header-beingselected', (e) => {
+        // In demo, don't follow the link in nav
+        e.detail.initiatingEvent.preventDefault();
+      });
+    });
+    HeaderNav.hook(element);
+  });
+  [... document.querySelectorAll('[data-list-icons-search-action-target]')].forEach((element) => new Toolbars(element));
 });
