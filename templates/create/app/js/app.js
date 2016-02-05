4fbb2b789cc59d53be9187b830e16cdec42b3c36
Colby Williams <colby@colbycheeze.com>
2016-02-05 16:36:08 -0600

    Refactored content-switcher to css only via radios   - Semantic and tabbable.   - No need for js any more.   - From a consume standpoint, just query for the checked property
    Added content-switcher to tabs-nav folder since it is a dependancy
      - naming could be changed, but for now nothing is broken
    
    Updated template folder to use updated markup / remove js

--- templates/create/app/js/app.js
+++ templates/create/app/js/app.js
@@ -1,11 +1,8 @@
 import '../../../../global/js/array-from';
 
 import FileUploader from '../../../../base-elements/inputs/file/file';
-import ContentSwitcher from '../../../../components/content-switcher/content-switcher';
 
 document.addEventListener('DOMContentLoaded', () => {
   [... document.querySelectorAll('[data-file-input]')]
     .forEach((element) => new FileUploader(element));
-  [... document.querySelectorAll('[data-content-switcher]')]
-    .forEach((element) => new ContentSwitcher(element));
 });
