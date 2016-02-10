3b914cb1515bbb7d9c1bdbc842691181356ea0d6
UNA A. KRAVETS <uakravet@us.ibm.com>
2016-02-10 11:09:55 -0600

    This is a combination of 2 commits.
    closing all menus when clicking another menu open
    
    upated to fix tests

--- components/overflow-menu/overflow-menu.js
+++ components/overflow-menu/overflow-menu.js
@@ -1,8 +1,11 @@
+import '../../global/js/array-from';
+
 export default class OverflowMenu {
   constructor(element) {
     if (!element || element.nodeType !== Node.ELEMENT_NODE) {
       throw new TypeError('DOM element should be given to initialize this widget.');
     }
+
     this.element = element;
     this.element.addEventListener('click', (event) => this.openMenu(event));
   }
@@ -11,6 +14,11 @@ export default class OverflowMenu {
     if (event.currentTarget.tagName === 'A' || event.currentTarget.querySelector('a')) {
       event.preventDefault();
     }
+
+    [... this.element.ownerDocument.querySelectorAll('[data-overflow-menu].open')].forEach((element) => {
+      element.classList.remove('open');
+    });
+
     this.element.classList.toggle('open');
   }
 }
