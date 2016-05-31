8d0448cfdde8db1b6e245b36e886e4474bd58f99
Akira Sudoh <ASUDOH@jp.ibm.com>
2016-06-01 08:32:57 +0900

    Add missing tests to number input, refs #569. (#1179)

--- tests/spec/number-input_spec.js
+++ tests/spec/number-input_spec.js
@@ -18,24 +18,81 @@ describe('Test Number Input', function () {
   });
 
   describe('Adding and Subtracting', function () {
-    // let input;
-    // let element;
+    let input;
+    let element;
+    let upArrowNode;
+    let downArrowNode;
+    let inputNode;
 
-    // before(function () {
-    //   element = document.createElement('input');
-    //   input = new NumberInput(element);
-    //   element.value = 2;
-    //   document.body.appendChild(element);
-    // });
+    before(function () {
+      element = document.createElement('div');
+      upArrowNode = element.appendChild(document.createElement('div'));
+      upArrowNode.classList.add('bx--number__arrow--icon-up');
+      downArrowNode = element.appendChild(document.createElement('div'));
+      downArrowNode.classList.add('bx--number__arrow--icon-down');
+      inputNode = element.appendChild(document.createElement('input'));
+      inputNode.type = 'number';
+      inputNode.classList.add('bx--number__input');
+      input = new NumberInput(element);
+      document.body.appendChild(element);
+    });
 
-    // it(`Should increase the value`, function () {
-    //   element.dispatchEvent(new CustomEvent('click'));
-    //   expect(element.value).to.equal('3');
-    // });
-    //
-    // it(`Should decrease the value`, function () {
-    //   element.dispatchEvent(new CustomEvent('click'));
-    //   expect(element.value).to.equal('1');
-    // });
+    beforeEach(function () {
+      inputNode.value = '2';
+    });
+
+    it(`Should increase the value`, function () {
+      upArrowNode.dispatchEvent(new CustomEvent('click', { bubbles: true }));
+      expect(inputNode.value).to.equal('3');
+    });
+
+    it(`Should decrease the value`, function () {
+      downArrowNode.dispatchEvent(new CustomEvent('click', { bubbles: true }));
+      expect(inputNode.value).to.equal('1');
+    });
+
+    after(function () {
+      document.body.removeChild(element);
+      input.release();
+    });
+  });
+
+  describe('Managing instances', function () {
+    let element;
+
+    before(function () {
+      element = document.createElement('div');
+    });
+
+    it('Should prevent creating duplicate instances', function () {
+      let first;
+      let second;
+      try {
+        first = NumberInput.create(element);
+        second = NumberInput.create(element);
+        expect(first).to.equal(second);
+      } finally {
+        first && first.release();
+        if (first !== second) {
+          second && second.release();
+        }
+      }
+    });
+
+    it('Should let create a new instance for an element if an earlier one has been released', function () {
+      let first;
+      let second;
+      try {
+        first = NumberInput.create(element);
+        first.release();
+        second = NumberInput.create(element);
+        expect(first).not.to.equal(second);
+      } finally {
+        first && first.release();
+        if (first !== second) {
+          second && second.release();
+        }
+      }
+    });
   });
 });
