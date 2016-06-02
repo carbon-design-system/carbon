058cbde682bd8f6a592efc159a22b4748b3e521f
Akira Sudoh <ASUDOH@jp.ibm.com>
2016-06-03 05:20:18 +0900

    JSDoc comments for file uploader, refs #564. (#1263)

--- consumables/js/es2015/file-uploader.js
+++ consumables/js/es2015/file-uploader.js
@@ -1,4 +1,11 @@
 export default class FileUploader {
+  /**
+   * File uploader.
+   * @implements Component
+   * @param {HTMLElement} element The element working as a file uploader.
+   * @param {Object} [options] The component options.
+   * @param {string} [options.labelSelector] The CSS selector to find the label for the file name.
+   */
   constructor(element, options = {}) {
     if (!element || element.nodeType !== Node.ELEMENT_NODE) {
       throw new TypeError('DOM element should be given to initialize this widget.');
@@ -14,6 +21,24 @@ export default class FileUploader {
     element.addEventListener('change', (event) => this.updateLabel(event));
   }
 
+  /**
+   * Instantiates file uploader of the given element.
+   * @param {HTMLElement} element The element working as a file uploader.
+   * @param {Object} [options] The component options.
+   * @param {string} [options.labelSelector] The CSS selector to find the label for the file name.
+   */
+  static create(element, options) {
+    return this.components.get(element) || new this(element, options);
+  }
+
+  /**
+   * Instantiates file uploader in the given node.
+   * If the given element indicates that it's an file uploader (having `data-file-uploader` attribute), instantiates it.
+   * Otherwise, instantiates file uploader by searching for file uploader in the given node.
+   * @param {HTMLElement} element The element working as a file uploader.
+   * @param {Object} [options] The component options.
+   * @param {string} [options.labelSelector] The CSS selector to find the label for the file name.
+   */
   static init(target = document, options) {
     if (target.nodeType !== Node.ELEMENT_NODE && target.nodeType !== Node.DOCUMENT_NODE) {
       throw new Error('DOM document or DOM element should be given to search for and initialize this widget.');
@@ -25,6 +50,10 @@ export default class FileUploader {
     }
   }
 
+  /**
+   * Updates the label for the file name upon file selection.
+   * @param {Event} event The event triggering this method.
+   */
   updateLabel(event) {
     let fileName = '';
     const element = this.element;
@@ -43,10 +72,16 @@ export default class FileUploader {
   release() {
     this.constructor.components.delete(this.element);
   }
-
-  static create(element, options) {
-    return this.components.get(element) || new this(element, options);
-  }
 }
 
+/**
+ * The component options.
+ * @member {Object} FileUploader#options
+ * @property {string} [labelSelector] The CSS selector to find the label for the file name.
+ */
+
+/**
+ * The map associating DOM element and file uploader instance.
+ * @type {WeakMap}
+ */
 FileUploader.components = new WeakMap();
