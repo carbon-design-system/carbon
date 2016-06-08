88d550b56714e72924097e18ba85b91768e7724f
Akira Sudoh <ASUDOH@jp.ibm.com>
2016-06-09 06:49:18 +0900

    JSDoc comments for drop down, refs #564. (#1264)
    * JSDoc comments for drop down, refs #564.
    
    * More description on drop down item.

--- consumables/js/es2015/dropdown.js
+++ consumables/js/es2015/dropdown.js
@@ -3,6 +3,19 @@ import '../polyfills/object-assign';
 import '../polyfills/custom-event';
 
 export default class Dropdown {
+  /**
+   * A selector with drop downs.
+   * @implements Component
+   * @param {HTMLElement} element The element working as a selector.
+   * @param {Object} [options] The component options.
+   * @param {string} [options.selectorItem] The CSS selector to find clickable areas in dropdown items.
+   * @param {string} [options.selectorItemSelected] The CSS selector to find the clickable area in the selected dropdown item.
+   * @param {string} [options.classSelected] The CSS class for the selected dropdown item.
+   * @param {string} [options.eventBeforeSelected]
+   *   The name of the custom event fired before a drop down item is selected.
+   *   Cancellation of this event stops selection of drop down item.
+   * @param {string} [options.eventAfterSelected] The name of the custom event fired after a drop down item is selected.
+   */
   constructor(element, options = {}) {
     if (!element || element.nodeType !== Node.ELEMENT_NODE) {
       throw new TypeError('DOM element should be given to initialize this widget.');
@@ -26,10 +39,36 @@ export default class Dropdown {
     this.element.addEventListener('click', (event) => this.selected(event));
   }
 
+  /**
+   * Instantiates selector of the given element.
+   * @param {HTMLElement} element The element working as a selector.
+   * @param {Object} [options] The component options.
+   * @param {string} [options.selectorItem] The CSS selector to find clickable areas in dropdown items.
+   * @param {string} [options.selectorItemSelected] The CSS selector to find the clickable area in the selected dropdown item.
+   * @param {string} [options.classSelected] The CSS class for the selected dropdown item.
+   * @param {string} [options.eventBeforeSelected]
+   *   The name of the custom event fired before a drop down item is selected.
+   *   Cancellation of this event stops selection of drop down item.
+   * @param {string} [options.eventAfterSelected] The name of the custom event fired after a drop down item is selected.
+   */
   static create(element, options) {
     return this.components.get(element) || new this(element, options);
   }
 
+  /**
+   * Instantiates selectors in the given node.
+   * If the given element indicates that it's an selector (having `data-dropdown` attribute), instantiates it.
+   * Otherwise, instantiates selectors by searching for selectors in the given node.
+   * @param {Node} target The DOM node to instantiate selectors in. Should be a document or an element.
+   * @param {Object} [options] The component options.
+   * @param {string} [options.selectorItem] The CSS selector to find clickable areas in dropdown items.
+   * @param {string} [options.selectorItemSelected] The CSS selector to find the clickable area in the selected dropdown item.
+   * @param {string} [options.classSelected] The CSS class for the selected dropdown item.
+   * @param {string} [options.eventBeforeSelected]
+   *   The name of the custom event fired before a drop down item is selected.
+   *   Cancellation of this event stops selection of drop down item.
+   * @param {string} [options.eventAfterSelected] The name of the custom event fired after a drop down item is selected.
+   */
   static init(target = document, options) {
     if (target.nodeType !== Node.ELEMENT_NODE && target.nodeType !== Node.DOCUMENT_NODE) {
       throw new Error('DOM document or DOM element should be given to search for and initialize this widget.');
@@ -45,7 +84,10 @@ export default class Dropdown {
     this.constructor.components.delete(this.element);
   }
 
-  // Open and close dropdown menu
+  /**
+   * Opens and closes the dropdown menu.
+   * @param {Event} event The event triggering this method.
+   */
   toggle(event) {
     const isOfSelf = this.element.contains(event.target);
 
@@ -56,10 +98,13 @@ export default class Dropdown {
     }
   }
 
-  // Handles clicking on dropdown options.
-  // * Change Dropdown text to selected option.
-  // * Remove selected option from options when selected.
-  // * Emit custom events.
+  /**
+   * Handles clicking on the dropdown options, doing the following:
+   * * Change Dropdown text to selected option.
+   * * Remove selected option from options when selected.
+   * * Emit custom events.
+   * @param {Event} event The event triggering this method.
+   */
   selected(event) {
     const activatedElement = event.target;
     if (activatedElement.parentElement.dataset.option !== undefined) {
@@ -91,4 +136,21 @@ export default class Dropdown {
   }
 }
 
+
+/**
+ * The component options.
+ * @member {Object} Dropdown#options
+ * @property {string} [selectorItem] The CSS selector to find clickable areas in dropdown items.
+ * @property {string} [selectorItemSelected] The CSS selector to find the clickable area in the selected dropdown item.
+ * @property {string} [classSelected] The CSS class for the selected dropdown item.
+ * @property {string} [eventBeforeSelected]
+ *   The name of the custom event fired before a drop down item is selected.
+ *   Cancellation of this event stops selection of drop down item.
+ * @property {string} [eventAfterSelected] The name of the custom event fired after a drop down item is selected.
+ */
+
+/**
+ * The map associating DOM element and selector instance.
+ * @type {WeakMap}
+ */
 Dropdown.components = new WeakMap();
