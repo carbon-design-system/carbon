497e14f6cd3ce17b06142ffb26662842b8fb9656
Akira Sudoh <ASUDOH@jp.ibm.com>
2016-06-09 06:48:51 +0900

    JSDoc comments for content switcher, refs #564. (#1281)

--- consumables/js/es2015/content-switcher.js
+++ consumables/js/es2015/content-switcher.js
@@ -4,6 +4,19 @@ import '../polyfills/object-assign';
 import toggleClass from '../polyfills/toggle-class';
 
 export default class ContentSwitcher {
+  /**
+   * Set of content switcher buttons.
+   * @implements Component
+   * @param {HTMLElement} element The element working as a set of content switcher buttons.
+   * @param {Object} [options] The component options.
+   * @param {string} [options.selectorButton] The CSS selector to find switcher buttons.
+   * @param {string} [options.selectorButtonSelected] The CSS selector to find the selected switcher button.
+   * @param {string} [options.classActive] The CSS class for switcher button's selected state.
+   * @param {string} [options.eventBeforeSelected]
+   *   The name of the custom event fired before a switcher button is selected.
+   *   Cancellation of this event stops selection of content switcher button.
+   * @param {string} [options.eventAfterSelected] The name of the custom event fired after a switcher button is selected.
+   */
   constructor(element, options = {}) {
     if (!element || element.nodeType !== Node.ELEMENT_NODE) {
       throw new TypeError('DOM element should be given to initialize this widget.');
@@ -28,6 +41,36 @@ export default class ContentSwitcher {
     });
   }
 
+  /**
+   * Instantiates a set of content switcher buttons of the given element.
+   * @param {HTMLElement} element The element working as a set of content switcher buttons.
+   * @param {Object} [options] The component options.
+   * @param {string} [options.selectorButton] The CSS selector to find switcher buttons.
+   * @param {string} [options.selectorButtonSelected] The CSS selector to find the selected switcher button.
+   * @param {string} [options.classActive] The CSS class for switcher button's selected state.
+   * @param {string} [options.eventBeforeSelected]
+   *   The name of the custom event fired before a switcher button is selected.
+   *   Cancellation of this event stops selection of content switcher button.
+   * @param {string} [options.eventAfterSelected] The name of the custom event fired after a switcher button is selected.
+   */
+  static create(element, options) {
+    return this.components.get(element) || new this(element, options);
+  }
+
+  /**
+   * Instantiates content switcher button sets in the given node.
+   * If the given element indicates that it's an content switcher button set (having `data-content-switcher` attribute), instantiates it.
+   * Otherwise, instantiates content switcher button sets by searching for content switcher button sets in the given node.
+   * @param {Node} target The DOM node to instantiate content switcher button sets in. Should be a document or an element.
+   * @param {Object} [options] The component options.
+   * @param {string} [options.selectorButton] The CSS selector to find switcher buttons.
+   * @param {string} [options.selectorButtonSelected] The CSS selector to find the selected switcher button.
+   * @param {string} [options.classActive] The CSS class for switcher button's selected state.
+   * @param {string} [options.eventBeforeSelected]
+   *   The name of the custom event fired before a switcher button is selected.
+   *   Cancellation of this event stops selection of content switcher button.
+   * @param {string} [options.eventAfterSelected] The name of the custom event fired after a switcher button is selected.
+   */
   static init(target = document, options) {
     if (target.nodeType !== Node.ELEMENT_NODE && target.nodeType !== Node.DOCUMENT_NODE) {
       throw new Error('DOM document or DOM element should be given to search for and initialize this widget.');
@@ -39,6 +82,11 @@ export default class ContentSwitcher {
     }
   }
 
+  /**
+   * Handles click on content switcher button set.
+   * If the click is on a content switcher button, activates it.
+   * @param {Event} event The event triggering this method.
+   */
   handleClick(event) {
     const button = eventMatches(event, this.options.selectorButton);
     if (button) {
@@ -46,6 +94,11 @@ export default class ContentSwitcher {
     }
   }
 
+  /**
+   * Internal method of {@linkcode ContentSwitcher#setActive .setActive()}, to select a content switcher button.
+   * @private
+   * @param {HTMLElement} item The button to be selected.
+   */
   _changeActive(item) {
     // `options.selectorLink` is not defined in this class itself, code here primary is for inherited classes
     const itemLink = item.querySelector(this.options.selectorLink);
@@ -68,6 +121,27 @@ export default class ContentSwitcher {
     [... item.ownerDocument.querySelectorAll(item.dataset.target)].forEach(element => element.removeAttribute('hidden'));
   }
 
+  /**
+   * Error thrown when selection (upon clicking on a content switcher button, etc.) is canceled.
+   * @typedef {Object} ContentSwitcher~cancelError
+   * @property {boolean} canceled `true` always.
+   * @property {boolean} item The content switcher button that was about to be selected.
+   */
+
+  /**
+   * The callback called once switching is finished or is canceled.
+   * @callback ContentSwitcher~setActiveCallback
+   * @param {ContentSwitcher~cancelError} error Error thrown when selection (upon clicking on a content switcher button, etc.) is canceled.
+   * @param {HTMLElement} item The newly selected button. `null` if when selection is canceled.
+   */
+
+  /**
+   * Selects a content switcher button.
+   * If the selected button has `data-target` attribute, DOM elements it points to as a CSS selector will be shown.
+   * DOM elements associated with unselected buttons in the same way will be hidden.
+   * @param {HTMLElement} item The button to be selected.
+   * @param {ContentSwitcher~setActiveCallback} callback The callback called once selection is finished or is canceled.
+   */
   setActive(item, callback) {
     const eventStart = new CustomEvent(this.options.eventBeforeSelected, {
       bubbles: true,
@@ -99,10 +173,22 @@ export default class ContentSwitcher {
   release() {
     this.constructor.components.delete(this.element);
   }
-
-  static create(element, options) {
-    return this.components.get(element) || new this(element, options);
-  }
 }
 
+/**
+ * The component options.
+ * @member {Object} ContentSwitcher#options
+ * @property {string} [selectorButton] The CSS selector to find switcher buttons.
+ * @property {string} [selectorButtonSelected] The CSS selector to find the selected switcher button.
+ * @property {string} [classActive] The CSS class for switcher button's selected state.
+ * @property {string} [eventBeforeSelected]
+ *   The name of the custom event fired before a switcher button is selected.
+ *   Cancellation of this event stops selection of content switcher button.
+ * @property {string} [eventAfterSelected] The name of the custom event fired after a switcher button is selected.
+ */
+
+/**
+ * The map associating DOM element and content switcher set instance.
+ * @type {WeakMap}
+ */
 ContentSwitcher.components = new WeakMap();
