3a42fa98e6732bb237f4e6e3be0f6a96b0e7774b
bthan <bthan@us.ibm.com>
2016-07-13 14:01:41 -0500

    Update bluemix-components with PR branch

--- elements/PrimaryButton.js
+++ elements/PrimaryButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
-import '@console/bluemix-components/consumables/scss/base-elements/buttons/buttons.scss';
+import '@console/bluemix-components/consumables/scss/base-elements/buttons/button--primary.scss';
 
 
 export default class PrimaryButton extends React.Component {
