c740853d659140cc13b06a717ab8e39d169759f8
bthan <bthan@us.ibm.com>
2016-07-14 15:06:18 -0500

    Only show AppContainer with default state (darkTheme)

--- stories/containers/AppContainer.js
+++ stories/containers/AppContainer.js
@@ -4,9 +4,8 @@ import { storiesOf, action } from '@kadira/storybook';
 import AppContainer from '../../containers/AppContainer';
 
 storiesOf('AppContainer', module)
-  .add('Dark UI', () => (
-    <AppContainer>body</AppContainer>
-  ))
-  .add('Light UI', () => (
-    <AppContainer className="bx--global-light-ui">body</AppContainer>
+  .add('Default', () => (
+    <AppContainer>
+      Wraps all content inside body to add Global styles and toggle Dark/Light UIs with state
+    </AppContainer>
   ))
