c1bc7a3264b74db7b839570d307fcf3c52f7c27e
BRIAN T. HAN <bthan@us.ibm.com>
2016-08-01 11:32:53 -0500

    [6.x - minor] Content Switcher - js:void + button and anchor markup (#1490)
    * Replace # with javascript:void(0) for content-switcher
    
    * Separate radio specific styles
    
    * Change args in content-switcher mixin for using buttons
    
    * Separate default styles into their own file
    
    * Update JS and Tests to accept class-only selector for selectorButton

--- consumables/js/es2015/content-switcher.js
+++ consumables/js/es2015/content-switcher.js
@@ -25,7 +25,7 @@ export default class ContentSwitcher {
     this.element = element;
 
     this.options = Object.assign({
-      selectorButton: 'input[type="radio"], a.bx--content-switcher__btn',
+      selectorButton: 'input[type="radio"], .bx--content-switcher__btn',
       selectorButtonSelected: 'input[type="radio"].bx--content-switcher--selected',
       classActive: 'bx--content-switcher--selected',
       eventBeforeSelected: 'content-switcher-beingselected',
