d7b47883ecc32ee16e9732f6922bf5345cd6eced
Nicholas Sandonato <nsandona@us.ibm.com>
2016-10-31 14:19:13 -0400

    Fix properties for TextInput and SelectItem. Fixes Bluemix/bluemix-components-react#188 (#189)
    👍

--- .storybook/components/ModalWrapperStory.js
+++ .storybook/components/ModalWrapperStory.js
@@ -64,13 +64,13 @@ storiesOf('ModalWrapper', module)
       modalHeading="Modal with Inputs"
       handleSubmit={action('onSubmit')}
     >
-      <TextInput id="test2" placeholder="Hint text here">Text Input:</TextInput>
+      <TextInput id="test2" placeholder="Hint text here" label="Text Input:" />
       <br />
       <Select id="select-1" labelText="Select">
-        <SelectItem disabled hidden value="placeholder-item" selectItemText="Pick an option" />
-        <SelectItem value="option-1" selectItemText="Option 1" />
-        <SelectItem value="option-2" selectItemText="Option 2" />
-        <SelectItem value="option-3" selectItemText="Option 3" />
+        <SelectItem disabled hidden value="placeholder-item" text="Pick an option" />
+        <SelectItem value="option-1" text="Option 1" />
+        <SelectItem value="option-2" text="Option 2" />
+        <SelectItem value="option-3" text="Option 3" />
       </Select>
       <br />
       <RadioButtonGroup name="radio-button-group" defaultSelected="default-selected">
