cdff66f5f78ed5b20b7ff9506c2c0643dffeba77
ALEX E. WEIDNER <aeweidne@us.ibm.com>
2016-11-01 13:40:08 -0400

    make deploy not clobber remote coverage stats (#202)
    * make deploy not clobber remote coverage stats
    
    * only use travis email/user on Travis CI, comment for clarity
    
    * smooth travis experience

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -1,26 +1,40 @@
-/**
-  * This script is used to deploy GitHub Pages form Travis CI.
-  * That is why the name and email are specified in options.user
-  **/
+/* eslint-disable no-console */
+
+// Script for deploying React Storybook and Coverage information
+// to GitHub pages
 
 const ghpages = require('gh-pages');
 const path = require('path');
+const sh = require('shelljs');
+
+const pagesFolder = path.join(__dirname, '..', '.gh-pages');
+
+// always run coverage if not on travis to prevent clobbering
+// or pushing stale coverage stats
+if (!process.env.TRAVIS) {
+  console.log('Running locally, going to generate coverage statistics');
+  sh.exec('npm run coverage');
+}
 
 const options = {
   remote: 'origin',
   logger: (message) => {
-    console.log(message); // eslint-disable-line no-console
-  },
-  user: {
-    name: 'travis',
-    email: 'travis',
+    console.log(message);
   },
 };
 
-ghpages.publish(path.join(__dirname, '..', '.gh-pages'), options, (err) => {
+// provide user and email if the script is invoked on Travis CI
+if (process.env.TRAVIS) {
+  options.user = {
+    name: 'travis',
+    email: 'travis',
+  };
+}
+
+ghpages.publish(pagesFolder, options, (err) => {
   if (err) {
     throw err;
   } else {
-    console.log('Published to GitHub Pages'); // eslint-disable-line no-console
+    console.log('Published to GitHub Pages');
   }
 });
