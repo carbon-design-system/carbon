7443a31e4b67497f255947999ab3884e7ef921bc
mjohannessen <mjohannessen@us.ibm.com>
2017-04-17 13:57:23 -0500

    refactor(interior-left-nav): started updating

--- src/components/interior-left-nav/interior-left-nav.js
+++ src/components/interior-left-nav/interior-left-nav.js
@@ -1,14 +1,16 @@
 import mixin from '../../globals/js/misc/mixin';
 import createComponent from '../../globals/js/mixins/create-component';
-import initComponentBySearch from '../../globals/js/mixins/init-component-by-search';
+import initComponentBySearch
+  from '../../globals/js/mixins/init-component-by-search';
 import eventMatches from '../../globals/js/misc/event-matches';
+import toggleClass from '../../globals/js/misc/svg-toggle-class';
 
 class InteriorLeftNav extends mixin(createComponent, initComponentBySearch) {
   /**
-   * Spinner indicating loading state.
+   * Interior left nav.
    * @extends CreateComponent
    * @extends InitComponentBySearch
-   * @param {HTMLElement} element The element working as a spinner.
+   * @param {HTMLElement} element The element working as a interior left nav.
    * @param {Object} options The component options.
    */
   constructor(element, options) {
@@ -17,13 +19,29 @@ class InteriorLeftNav extends mixin(createComponent, initComponentBySearch) {
     this.hookListItemsEvents();
   }
 
-  hookListItemsEvents() {
-    const leftNavList = this.element.querySelector(this.options.selectorLeftNavList);
-    leftNavList.addEventListener('click', (evt) => {
-      const leftNavItem = eventMatches(evt, this.options.selectorLeftNavListItem);
+  hookListItemsEvents = () => {
+    this.element.addEventListener('click', evt => {
+      const leftNavItem = eventMatches(
+        evt,
+        this.options.selectorLeftNavListItem
+      );
+      const collapseEl = eventMatches(
+        evt,
+        this.options.selectorLeftNavCollapse
+      );
+      const collapsedBar = eventMatches(
+        evt,
+        `.${this.options.classLeftNavCollapsed}`
+      );
+
       if (leftNavItem) {
-        const childItem = eventMatches(evt, this.options.selectorLeftNavNestedListItem);
-        const hasChildren = leftNavItem.classList.contains('left-nav-list__item--has-children');
+        const childItem = eventMatches(
+          evt,
+          this.options.selectorLeftNavNestedListItem
+        );
+        const hasChildren = leftNavItem.classList.contains(
+          'left-nav-list__item--has-children'
+        );
         if (childItem) {
           this.addActiveListItem(childItem);
         } else if (hasChildren) {
@@ -32,24 +50,44 @@ class InteriorLeftNav extends mixin(createComponent, initComponentBySearch) {
           this.addActiveListItem(leftNavItem);
         }
       }
+
+      if (collapseEl || collapsedBar) {
+        evt.preventDefault();
+        this.toggleLeftNav();
+      }
     });
-    [...this.element.querySelectorAll(this.options.selectorLeftNavListItem)].forEach((item) => {
-      item.addEventListener('keydown', (evt) => {
-        const leftNavItemWithChildren = eventMatches(evt, this.options.selectorLeftNavListItemHasChildren);
-        if (leftNavItemWithChildren && evt.which === 13) {
-          this.handleNestedListClick(leftNavItemWithChildren, evt);
-        }
-      });
+
+    this.element.addEventListener('keydown', evt => {
+      const leftNavItemWithChildren = eventMatches(
+        evt,
+        this.options.selectorLeftNavListItemHasChildren
+      );
+      const leftNavItem = eventMatches(
+        evt,
+        this.options.selectorLeftNavListItem
+      );
+
+      if (leftNavItemWithChildren && evt.which === 13) {
+        this.handleNestedListClick(leftNavItemWithChildren, evt);
+      } else if (leftNavItem && evt.which === 13) {
+        this.addActiveListItem(leftNavItem);
+      }
     });
-  }
+  };
 
   addActiveListItem(item) {
-    [...this.element.querySelectorAll(this.options.selectorLeftNavListItem)].forEach((currentItem) => {
+    [
+      ...this.element.querySelectorAll(this.options.selectorLeftNavListItem),
+    ].forEach(currentItem => {
       if (!(item === currentItem)) {
         currentItem.classList.remove(this.options.classActiveLeftNavListItem);
       }
     });
-    [...this.element.querySelectorAll(this.options.selectorLeftNavNestedListItem)].forEach((currentItem) => {
+    [
+      ...this.element.querySelectorAll(
+        this.options.selectorLeftNavNestedListItem
+      ),
+    ].forEach(currentItem => {
       if (!(item === currentItem)) {
         currentItem.classList.remove(this.options.classActiveLeftNavListItem);
       }
@@ -64,22 +102,93 @@ class InteriorLeftNav extends mixin(createComponent, initComponentBySearch) {
    * @param {Event} event The event triggering this method.
    */
   handleNestedListClick(listItem, evt) {
-    const isOpen = listItem.classList.contains(this.options.classExpandedLeftNavListItem);
-    if (!('leftNavItemLink' in evt.target.dataset)) {
-      listItem.classList.toggle(this.options.classExpandedLeftNavListItem, !isOpen);
+    const allNestedItems = [
+      ...document.querySelectorAll(
+        this.options.selectorLeftNavListItemHasChildren
+      ),
+    ];
+    const isOpen = listItem.classList.contains(
+      this.options.classExpandedLeftNavListItem
+    );
+    allNestedItems.forEach(currentItem => {
+      if (currentItem !== listItem) {
+        toggleClass(
+          currentItem,
+          this.options.classExpandedLeftNavListItem,
+          false
+        );
+      }
+    });
+    if (!('InteriorLeftNavItemLink' in evt.target.dataset)) {
+      toggleClass(listItem, this.options.classExpandedLeftNavListItem, !isOpen);
     }
     const list = listItem.querySelector(this.options.selectorLeftNavNestedList);
-    const listItems = [...list.querySelectorAll(this.options.selectorLeftNavNestedListItem)];
-    listItems.forEach((item) => {
+    const listItems = [
+      ...list.querySelectorAll(this.options.selectorLeftNavNestedListItem),
+    ];
+    listItems.forEach(item => {
       if (isOpen) {
         // eslint-disable-next-line no-param-reassign
-        item.querySelector(this.options.selectorLeftNavListItemLink).tabIndex = -1;
+        item.querySelector(
+          this.options.selectorLeftNavListItemLink
+        ).tabIndex = -1;
       } else {
         // eslint-disable-next-line no-param-reassign
-        item.querySelector(this.options.selectorLeftNavListItemLink).tabIndex = 0;
+        item.querySelector(
+          this.options.selectorLeftNavListItemLink
+        ).tabIndex = 0;
       }
     });
   }
+
+  toggleLeftNav = () => {
+    const collapsed = this.element.dataset.collapsed === 'true';
+    const eventStart = new CustomEvent(this.options.eventBeforeLeftNavToggled, {
+      bubbles: true,
+      cancelable: true,
+      detail: { collapsed: !collapsed }, // shows where the toggle is going, not where it is
+    });
+
+    if (this.element.dispatchEvent(eventStart)) {
+      if (!collapsed) {
+        this.element.dataset.collapsed = true;
+        this.element.classList.add(this.options.classLeftNavCollapsing);
+
+        window.setTimeout(
+          () => {
+            this.element.classList.remove(this.options.classLeftNavCollapsing);
+            this.element.classList.add(this.options.classLeftNavCollapsed);
+            this.element.dispatchEvent(
+              new CustomEvent(this.options.eventAfterLeftNavToggled, {
+                bubbles: true,
+                cancelable: true,
+                detail: { collapsed: true },
+              })
+            );
+          },
+          250
+        );
+      } else {
+        this.element.dataset.collapsed = false;
+        this.element.classList.remove(this.options.classLeftNavCollapsed);
+        this.element.classList.add(this.options.classLeftNavExpanding);
+
+        window.setTimeout(
+          () => {
+            this.element.classList.remove(this.options.classLeftNavExpanding);
+            this.element.dispatchEvent(
+              new CustomEvent(this.options.eventAfterLeftNavToggled, {
+                bubbles: true,
+                cancelable: true,
+                detail: { collapsed: false },
+              })
+            );
+          },
+          250
+        );
+      }
+    }
+  };
 
   /**
    * The map associating DOM element and spinner instance.
@@ -95,20 +204,27 @@ class InteriorLeftNav extends mixin(createComponent, initComponentBySearch) {
    * properties in this object are overriden for the instance being create and how {@linkcode InteriorLeftNav.init .init()} works.
    * @member InteriorLeftNav.options
    * @type {Object}
-   * @property {string} selectorInit The CSS selector to find inline left navs.
+   * @property {string} selectorInit The CSS selector to find interior left navs.
    */
   static options = {
-    selectorInit: '[data-inline-left-nav]',
+    selectorInit: '[data-interior-left-nav]',
     // Data Attribute selectors
-    selectorLeftNavList: '[data-inline-left-nav-list]',
-    selectorLeftNavNestedList: '[data-inline-left-nav-nested-list]',
-    selectorLeftNavListItem: '[data-inline-left-nav-item]',
-    selectorLeftNavListItemLink: '[data-inline-left-nav-item-link]',
-    selectorLeftNavNestedListItem: '[data-inline-left-nav-nested-item]',
-    selectorLeftNavListItemHasChildren: '[data-inline-left-nav-with-children]',
+    selectorLeftNavList: '[data-interior-left-nav-list]',
+    selectorLeftNavNestedList: '[data-interior-left-nav-nested-list]',
+    selectorLeftNavListItem: '[data-interior-left-nav-item]',
+    selectorLeftNavListItemLink: '[data-interior-left-nav-item-link]',
+    selectorLeftNavNestedListItem: '[data-interior-left-nav-nested-item]',
+    selectorLeftNavListItemHasChildren: '[data-interior-left-nav-with-children]',
+    selectorLeftNavCollapse: '[data-interior-left-nav-collapse]',
     // CSS Class Selectors
     classActiveLeftNavListItem: 'left-nav-list__item--active',
     classExpandedLeftNavListItem: 'left-nav-list__item--expanded',
+    classLeftNavCollapsing: 'bx--interior-left-nav--collapsing',
+    classLeftNavCollapsed: 'bx--interior-left-nav--collapsed',
+    classLeftNavExpanding: 'bx--interior-left-nav--expanding',
+    // Event
+    eventBeforeLeftNavToggled: 'left-nav-beingtoggled',
+    eventAfterLeftNavToggled: 'left-nav-toggled',
   };
 }
 
