f308110520fbf05f7abcafe902fedb34beaa2d57
Ian Fleming <ianfleming@us.ibm.com>
2017-05-01 15:30:35 -0500

    fix(FloatingMenu): fix ownerDocument bug (#588)

--- internal/FloatingMenu.js
+++ internal/FloatingMenu.js
@@ -5,18 +5,19 @@ class FloatingMenu extends React.Component {
   static propTypes = {
     children: PropTypes.any,
     menuPosition: PropTypes.object.isRequired,
-    menuDirection: PropTypes.oneOf(['left', 'top', 'right', 'bottom']).isRequired,
+    menuDirection: PropTypes.oneOf(['left', 'top', 'right', 'bottom'])
+      .isRequired,
     menuOffset: PropTypes.object.isRequired,
-    extraStyles: PropTypes.object
-  }
+    extraStyles: PropTypes.object,
+  };
 
   static defaultProps = {
     menuPosition: { left: 0, top: 0, right: 0, bottom: 0 },
-    menuDirection: 'bottom'
-  }
+    menuDirection: 'bottom',
+  };
 
   componentDidMount() {
-    this.menu = this.menu.ownerDocument.createElement('div');
+    this.menu = document.createElement('div');
     this.menu.ownerDocument.body.appendChild(this.menu);
     this.renderLayer();
   }
@@ -53,22 +54,22 @@ class FloatingMenu extends React.Component {
     return {
       left: () => ({
         left: refLeft - menuWidth - menuOffset.left,
-        top: ((refCenterVertical - (menuHeight / 2)) + scroll) + menuOffset.top,
+        top: refCenterVertical - menuHeight / 2 + scroll + menuOffset.top,
       }),
       top: () => ({
-        left: (refCenterHorizontal - (menuWidth / 2)) + menuOffset.left,
-        top: ((refTop - menuHeight) + scroll) - menuOffset.top,
+        left: refCenterHorizontal - menuWidth / 2 + menuOffset.left,
+        top: refTop - menuHeight + scroll - menuOffset.top,
       }),
       right: () => ({
         left: refRight + menuOffset.left,
-        top: ((refCenterVertical - (menuHeight / 2)) + scroll) + menuOffset.top,
+        top: refCenterVertical - menuHeight / 2 + scroll + menuOffset.top,
       }),
       bottom: () => ({
-        left: (refCenterHorizontal - (menuWidth / 2)) + menuOffset.left,
+        left: refCenterHorizontal - menuWidth / 2 + menuOffset.left,
         top: refBottom + scroll + menuOffset.top,
       }),
     }[this.props.menuDirection]();
-  }
+  };
 
   renderLayer = () => {
     const pos = this.positionFloatingMenu();
@@ -78,15 +79,14 @@ class FloatingMenu extends React.Component {
       top: `${pos.top}px`,
       position: 'absolute',
       right: 'auto',
-      margin: 0
+      margin: 0,
     };
 
     const style = Object.assign(coreStyles, this.props.extraStyles);
     Object.assign(this.menu.style, style);
 
     ReactDOM.render(this.props.children, this.menu);
-  }
-
+  };
 
   render() {
     return null;
