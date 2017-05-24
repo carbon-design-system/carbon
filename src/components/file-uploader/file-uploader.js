276e6a3427bf177b13974be87726b42a7edc2297
Mari Johannessen <mari.l.johannessen@gmail.com>
2017-05-24 12:36:00 -0500

    Focus states: Tooltip, notification, modal, file uploader (#85)
    * refactor(components): updated focus states for notification, tooltip, file uploader, modal
    
    * fix(file-uploader): added tabindex to close icon
    
    * fix(tests): trying to make a11y pass

--- src/components/file-uploader/file-uploader.js
+++ src/components/file-uploader/file-uploader.js
@@ -48,7 +48,7 @@ class FileUploader extends mixin(createComponent, initComponentBySearch, evented
 
   _closeButtonHTML() {
     return (`
-      <svg class="bx--file-close" viewBox="0 0 16 16" fill-rule="evenodd" width="16" height="16">
+      <svg class="bx--file-close" tabindex="0" viewBox="0 0 16 16" fill-rule="evenodd" width="16" height="16">
         <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm3.5 10.1l-1.4 1.4L8
           9.4l-2.1 2.1-1.4-1.4L6.6 8 4.5 5.9l1.4-1.4L8 6.6l2.1-2.1 1.4 1.4L9.4 8l2.1 2.1z" />
       </svg>`
