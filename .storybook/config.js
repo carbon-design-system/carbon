9708411f1bdad3d7af5c3ad09012a480f4a80979
chrisdhanaraj <adhanaraj@gmail.com>
2017-06-01 14:57:41 -0500

    chore(storybook): update storybook configuration, remove appcontainer

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { configure, setAddon, addDecorator } from '@storybook/react';
 import infoAddon from '@storybook/addon-info';
-import ThemeContainer from './ThemeContainer';
 
 setAddon(infoAddon);
-
-const appContainerDecorator = story => <ThemeContainer story={story} />;
-addDecorator(appContainerDecorator);
 
 const req = require.context('./components', true, /\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(story => (
+  <div
+    style={{
+      padding: '3em',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }}
+  >
+    {story()}
+  </div>
+));
 
 configure(loadStories, module);
