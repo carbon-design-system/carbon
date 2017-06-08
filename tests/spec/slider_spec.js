a423ca7d21739b5e543f841773b1949f26bdbcce
Ian Fleming <fyedka2@gmail.com>
2017-06-08 16:26:12 -0500

    test(slider): WIP fix raf conflict

--- tests/spec/slider_spec.js
+++ tests/spec/slider_spec.js
@@ -3,89 +3,89 @@ import createMockRaf from 'mock-raf';
 import Slider from '../../src/components/slider/slider';
 import SliderHTML from '../../src/components/slider/slider.html';
 
-const mockRaf = createMockRaf();
-sinon.stub(window, 'requestAnimationFrame', mockRaf.raf);
-
 describe('Test slider', function () {
-  // describe('Constructor', function () {
-  //   let slider;
-  //
-  //   it('Should throw if root element is not given', function () {
-  //     expect(() => {
-  //       slider = new Slider();
-  //     }).to.throw(Error);
-  //   });
-  //
-  //   it('Should throw if root element is not a DOM element', function () {
-  //     expect(() => {
-  //       slider = new Slider(document.createTextNode(''));
-  //     }).to.throw(Error);
-  //   });
-  //
-  //   it('Should set default options', function () {
-  //     const container = document.createElement('div');
-  //     container.innerHTML = SliderHTML;
-  //     document.body.appendChild(container);
-  //     slider = new Slider(document.querySelector('[data-slider]'));
-  //
-  //     expect(slider.options).to.deep.equal({
-  //       selectorInit: '[data-slider]',
-  //       selectorTrack: '.bx--slider__track',
-  //       selectorFilledTrack: '.bx--slider__filled-track',
-  //       selectorThumb: '.bx--slider__thumb',
-  //       selectorInput: '.bx--slider__input',
-  //       eventBeforeSliderValueChange: 'slider-before-value-change',
-  //       eventAfterSliderValueChange: 'slider-after-value-change',
-  //       stepMuliplier: 4,
-  //     });
-  //   });
-  //
-  //   afterEach(function () {
-  //     if (slider) {
-  //       slider = slider.release();
-  //     }
-  //   });
-  // });
-  // describe('Programatic change', function () {
-  //   let slider;
-  //   let container;
-  //   beforeEach(function () {
-  //     container = document.createElement('div');
-  //     container.innerHTML = SliderHTML;
-  //     document.body.appendChild(container);
-  //     slider = new Slider(document.querySelector('[data-slider]'));
-  //   });
-  //   it('Should setValue as expected', function () {
-  //     slider.setValue(100);
-  //     expect(slider.getInputProps().value).to.equal('100');
-  //   });
-  //   it('Should stepUp as expected', function () {
-  //     slider.setValue(50);
-  //     slider.stepUp();
-  //     expect(slider.getInputProps().value).to.equal('51');
-  //   });
-  //   it('Should stepDown as expected', function () {
-  //     slider.setValue(50);
-  //     slider.stepDown();
-  //     expect(slider.getInputProps().value).to.equal('49');
-  //   });
-  //   afterEach(function () {
-  //     if (slider) {
-  //       slider = slider.release();
-  //       document.body.innerHTML = '';
-  //     }
-  //   });
-  // });
+  describe('Constructor', function () {
+    let slider;
+
+    it('Should throw if root element is not given', function () {
+      expect(() => {
+        slider = new Slider();
+      }).to.throw(Error);
+    });
+
+    it('Should throw if root element is not a DOM element', function () {
+      expect(() => {
+        slider = new Slider(document.createTextNode(''));
+      }).to.throw(Error);
+    });
+
+    it('Should set default options', function () {
+      const container = document.createElement('div');
+      container.innerHTML = SliderHTML;
+      document.body.appendChild(container);
+      slider = new Slider(document.querySelector('[data-slider]'));
+
+      expect(slider.options).to.deep.equal({
+        selectorInit: '[data-slider]',
+        selectorTrack: '.bx--slider__track',
+        selectorFilledTrack: '.bx--slider__filled-track',
+        selectorThumb: '.bx--slider__thumb',
+        selectorInput: '.bx--slider__input',
+        eventBeforeSliderValueChange: 'slider-before-value-change',
+        eventAfterSliderValueChange: 'slider-after-value-change',
+        stepMuliplier: 4,
+      });
+    });
+
+    afterEach(function () {
+      if (slider) {
+        slider = slider.release();
+      }
+    });
+  });
+  describe('Programatic change', function () {
+    let slider;
+    let container;
+    beforeEach(function () {
+      container = document.createElement('div');
+      container.innerHTML = SliderHTML;
+      document.body.appendChild(container);
+      slider = new Slider(document.querySelector('[data-slider]'));
+    });
+    it('Should setValue as expected', function () {
+      slider.setValue(100);
+      expect(slider.getInputProps().value).to.equal('100');
+    });
+    it('Should stepUp as expected', function () {
+      slider.setValue(50);
+      slider.stepUp();
+      expect(slider.getInputProps().value).to.equal('51');
+    });
+    it('Should stepDown as expected', function () {
+      slider.setValue(50);
+      slider.stepDown();
+      expect(slider.getInputProps().value).to.equal('49');
+    });
+    afterEach(function () {
+      if (slider) {
+        slider = slider.release();
+        document.body.innerHTML = '';
+      }
+    });
+  });
   describe('Keydown on slider', function () {
     let container;
     let slider;
     let thumb;
+    let mockRaf;
     beforeEach(function () {
       container = document.createElement('div');
       container.innerHTML = SliderHTML;
       document.body.appendChild(container);
       slider = new Slider(document.querySelector('[data-slider]'));
       thumb = document.querySelector('.bx--slider__thumb');
+      mockRaf = createMockRaf();
+      sinon.stub(window, 'requestAnimationFrame').callsFake(mockRaf.raf);
       mockRaf.step({ count: 1 });
     });
     it('Should stepUp value on up/right key', function () {
@@ -111,6 +111,10 @@ describe('Test slider', function () {
       expect(slider.getInputProps().value).to.equal('48');
     });
     afterEach(function () {
+      if (mockRaf) {
+        mockRaf.restore();
+        mockRaf = null;
+      }
       if (slider) {
         slider = slider.release();
         document.body.innerHTML = '';
@@ -121,12 +125,15 @@ describe('Test slider', function () {
     let container;
     let slider;
     let track;
+    let mockRaf;
     beforeEach(function () {
       container = document.createElement('div');
       container.innerHTML = SliderHTML;
       document.body.appendChild(container);
       slider = new Slider(document.querySelector('[data-slider]'));
       track = document.querySelector('.bx--slider__track');
+      mockRaf = createMockRaf();
+      sinon.stub(window, 'requestAnimationFrame').callsFake(mockRaf.raf);
       mockRaf.step({ count: 1 });
     });
     it('Should change value on click', function () {
@@ -137,6 +144,10 @@ describe('Test slider', function () {
       expect(slider.getInputProps().value).to.equal('0');
     });
     afterEach(function () {
+      if (mockRaf) {
+        mockRaf.restore();
+        mockRaf = null;
+      }
       if (slider) {
         slider = slider.release();
         document.body.innerHTML = '';
