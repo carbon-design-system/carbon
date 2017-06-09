425c142f1ee73519b368a60fa336f67edf4fff2b
Brian Han <hellobrian@users.noreply.github.com>
2017-06-09 13:24:28 -0500

    feat(structured-list): add structured-list component (#140)
    * chore(descriptive-list): setup files
    
    * fix(descriptive-list): html
    
    * fix(descriptive-list): fix HTML to be a11y nice
    
    * fix(descriptive-list): reorder scss
    
    * refactor(descriptive-list): rough js and more styles
    
    * refactor(descriptive-list): make new html wo radio
    
    * feat(DescriptiveList): add JS class
    
    * fix(descriptive-list.scss): add bottom-borders for rows
    
    * fix(styles.scss): uncomment code
    
    * fix(descriptive-list): add modifier class for border
    
    * fix(descriptive-list): remove extra html
    
    * refactor(structured-list): rename
    
    * docs(structured-list): readme for scss and js
    
    * refactor(structured-list): add condensed modifier
    
    * test(structured-list): add unit tests
    
    * fix(structured-list): add focus border
    
    * refactor(structured-list): use space key for select
    
    * feat(structured-list): use up and down arrow keys
    
    * fix(structured-list): visual review check!
    
    * refactor(structured-list.js): loop focus
    
    * refactor(structured-list): refactor js
    
    * docs(structured-list): update keydown event
    
    * refactor(structured-list): more refactoring for js
    
    * test(structured-list): update tests
    
    * docs(structured-list): update docs for keyboard events
    
    * refactor(structured-list): adjustedNextIndex suggestion
    
    * refactor(structured-list): transitions and better FPO content
    
    * fix(structured-list): import once
    
    * fix(structured-list): _arrowKeyDown works in FF but wo focus
    
    * refactor(structured-list): change conditional
    
    * fix(structured-list): remove commented code
    
    * refactor(structured-list): direction method
    
    * refactor(structured-list): set nextIndex
    
    * refactor(structured-list): use switch case
    
    * test(structured-list): more tests
    
    * test(structured-list): remove some unused code
    
    * docs(structured-list): chrome vs firefox keyboard behavior
    
    * test(structured-list): more test coverage
    
    * fix(structured-list): change border modifier
    
    * refactor(structured-list): allow for configurable gutter sizes between table-cells
    
    * docs(structured-list): add variables and mixins
    
    * fix(structured-list): make padding-right smaller on td:first-child
    
    * fix(structured-list): remove <strong> elements for a11y
    
    * refactor(structured-list): new mixins for padding
    
    * docs(structured-list): add new mixins
    
    * docs(structured-list): add faq about visual styles for text
    
    * fix(structured-list): change th padding-top to 1rem
    
    * docs(structured-list): phrasing!
    
    * fix(structured-list): remove tabindex on div rows
    
    * fix(structured-list): remove ownerDocument
    
    * fix(structured-list): remove this.rows
    
    * style(structured-list): prettier

---
+++ src/components/structured-list/structured-list.js
@@ -0,0 +1,96 @@
+import mixin from '../../globals/js/misc/mixin';
+import createComponent from '../../globals/js/mixins/create-component';
+import initComponentBySearch from '../../globals/js/mixins/init-component-by-search';
+import eventMatches from '../../globals/js/misc/event-matches';
+
+class StructuredList extends mixin(createComponent, initComponentBySearch) {
+  /**
+   * StructuredList
+   * @extends CreateComponent
+   * @extends InitComponentBySearch
+   * @param {HTMLElement} element The root element of tables
+   * @param {Object} [options] the... options
+   * @param {string} [options.selectorInit] selector initialization
+   * @param {string} [options.selectorRow] css selector for selected row
+   */
+  constructor(element, options) {
+    super(element, options);
+    this.element.addEventListener('keydown', (evt) => {
+      if (evt.which === 38 || evt.which === 40) {
+        this._handleKeydownArrow(evt);
+      }
+      if (evt.which === 13 || evt.which === 32) {
+        this._handleKeydownChecked(evt);
+      }
+    });
+  }
+
+  _direction(evt) {
+    return {
+      38: -1, // backward
+      40: 1, // forward
+    }[evt.which];
+  }
+
+  _nextIndex(array, arrayItem, direction) {
+    return array.indexOf(arrayItem) + direction; // returns -1, 0, 1, 2, 3, 4...
+  }
+
+  _getInput(index) {
+    const rows = [...this.element.querySelectorAll(this.options.selectorRow)];
+    const id = `#${rows[index].getAttribute('for')}`;
+    return this.element.ownerDocument.querySelector(`${id}.bx--structured-list-input`);
+  }
+
+  _handleInputChecked(index) {
+    const input = this._getInput(index);
+    input.checked = true;
+  }
+
+  _handleKeydownChecked(evt) {
+    const selectedRow = eventMatches(evt, this.options.selectorRow);
+    if (selectedRow) {
+      const input = this.element.querySelector(
+        `#${selectedRow.getAttribute('for')}.bx--structured-list-input`,
+      );
+      input.checked = true;
+    }
+  }
+
+  _handleKeydownArrow(evt) {
+    const selectedRow = eventMatches(evt, this.options.selectorRow);
+    const direction = this._direction(evt);
+
+    if (direction && selectedRow !== undefined) {
+      const rows = [...this.element.querySelectorAll(this.options.selectorRow)];
+      const firstIndex = 0;
+      const nextIndex = this._nextIndex(rows, selectedRow, direction);
+      const lastIndex = rows.length - 1;
+
+      switch (nextIndex) {
+        case -1:
+          rows[lastIndex].focus();
+          this._handleInputChecked(lastIndex);
+          break;
+        case rows.length:
+          rows[firstIndex].focus();
+          this._handleInputChecked(firstIndex);
+          break;
+        default:
+          rows[nextIndex].focus();
+          this._handleInputChecked(nextIndex);
+          break;
+      }
+    }
+  }
+
+  static components = new WeakMap();
+
+  static options = {
+    selectorInit: '[data-structured-list]',
+    selectorRow:
+      '[data-structured-list] .bx--structured-list-tbody > label.bx--structured-list-row',
+  };
+}
+
+export default StructuredList;
---
+++ tests/spec/structured-list_spec.js
@@ -0,0 +1,296 @@
+import 'core-js/modules/es6.weak-map'; // For PhantomJS
+import StructuredList from '../../src/components/structured-list/structured-list';
+import HTML from '../../src/components/structured-list/structured-list--selection.html';
+
+describe('StructuredList', function () {
+  describe('Constructor', function () {
+    let instance;
+    let element;
+    let wrapper;
+
+    before(function () {
+      wrapper = document.createElement('div');
+      wrapper.innerHTML = HTML;
+      document.body.appendChild(wrapper);
+      element = document.querySelector('[data-structured-list]');
+      instance = new StructuredList(element);
+    });
+
+    it('should throw if root element is not given', function () {
+      expect(() => {
+        new StructuredList();
+      }).to.throw(Error);
+    });
+
+    it('should throw if root element is not a DOM element', function () {
+      expect(() => {
+        new StructuredList(document.createTextNode(''));
+      }).to.throw(Error);
+    });
+
+    it('should set default options', function () {
+      expect(instance.options).to.deep.equal({
+        selectorInit: '[data-structured-list]',
+        selectorRow:
+          '[data-structured-list] .bx--structured-list-tbody > label.bx--structured-list-row',
+      });
+    });
+
+    after(function () {
+      instance.release();
+      document.body.removeChild(wrapper);
+    });
+  });
+
+  describe('_handleKeydownChecked(evt)', function () {
+    let instance;
+    let element;
+    let wrapper;
+    let spy;
+
+    beforeEach(function () {
+      wrapper = document.createElement('div');
+      wrapper.innerHTML = HTML;
+      document.body.appendChild(wrapper);
+      element = document.querySelector('[data-structured-list]');
+      instance = new StructuredList(element);
+    });
+
+    it('should be called on "enter" keydown event', function () {
+      spy = sinon.spy(instance, '_handleKeydownChecked');
+      const event = Object.assign(
+        new CustomEvent('keydown', {
+          bubbles: true,
+        }),
+        { which: 13 },
+      );
+      instance.element.dispatchEvent(event);
+      expect(spy).to.have.been.called;
+    });
+
+    it('should be called on "space" keydown event', function () {
+      spy = sinon.spy(instance, '_handleKeydownChecked');
+      const event = Object.assign(
+        new CustomEvent('keydown', {
+          bubbles: true,
+        }),
+        { which: 32 },
+      );
+      instance.element.dispatchEvent(event);
+      expect(spy).to.have.been.called;
+    });
+
+    it('should not be called with another keydown event', function () {
+      spy = sinon.spy(instance, '_handleKeydownChecked');
+      const event = Object.assign(
+        new CustomEvent('keydown', {
+          bubbles: true,
+        }),
+        { which: 40 },
+      );
+      instance.element.dispatchEvent(event);
+      expect(spy).to.not.have.been.called;
+    });
+
+    afterEach(function () {
+      spy.restore();
+      instance.release();
+      document.body.removeChild(wrapper);
+    });
+  });
+
+  describe('_direction(evt)', function () {
+    let instance;
+    let element;
+    let wrapper;
+    let spy;
+
+    beforeEach(function () {
+      wrapper = document.createElement('div');
+      wrapper.innerHTML = HTML;
+      document.body.appendChild(wrapper);
+      element = document.querySelector('[data-structured-list]');
+      instance = new StructuredList(element);
+    });
+
+    it('should be called on "up" keydown event', function () {
+      spy = sinon.spy(instance, '_direction');
+      const event = Object.assign(
+        new CustomEvent('keydown', {
+          bubbles: true,
+        }),
+        { which: 38 },
+      );
+      instance.element.dispatchEvent(event);
+      expect(spy).to.have.been.called;
+    });
+
+    it('should be called on "down" keydown event', function () {
+      spy = sinon.spy(instance, '_direction');
+      const event = Object.assign(
+        new CustomEvent('keydown', {
+          bubbles: true,
+        }),
+        { which: 40 },
+      );
+      instance.element.dispatchEvent(event);
+      expect(spy).to.have.been.called;
+    });
+
+    it('should return -1 on "up" key', function () {
+      const event = Object.assign(
+        new CustomEvent('keydown', {
+          bubbles: true,
+        }),
+        { which: 38 },
+      );
+      const direction = instance._direction(event);
+      expect(direction).to.equal(-1);
+    });
+
+    it('should return 1 on "down" key', function () {
+      const event = Object.assign(
+        new CustomEvent('keydown', {
+          bubbles: true,
+        }),
+        { which: 40 },
+      );
+      const direction = instance._direction(event);
+      expect(direction).to.equal(1);
+    });
+
+    it('should return undefined on other key presses', function () {
+      const event = Object.assign(
+        new CustomEvent('keydown', {
+          bubbles: true,
+        }),
+        { which: 70 },
+      );
+      const direction = instance._direction(event);
+      expect(direction).to.be.undefined;
+    });
+
+    afterEach(function () {
+      spy.restore();
+      instance.release();
+      document.body.removeChild(wrapper);
+    });
+  });
+
+  describe('_nextIndex()', function () {
+    let instance;
+    let element;
+    let wrapper;
+    let spy;
+
+    beforeEach(function () {
+      wrapper = document.createElement('div');
+      wrapper.innerHTML = HTML;
+      document.body.appendChild(wrapper);
+      element = document.querySelector('[data-structured-list]');
+      instance = new StructuredList(element);
+    });
+
+    it('should be called', function () {
+      spy = sinon.spy(instance, '_nextIndex');
+      instance._nextIndex([0, 1, 2, 3], 0, 1);
+      expect(spy).to.have.been.called;
+    });
+
+    it('should return a number', function () {
+      const array = [0, 1, 2, 3];
+      const result = instance._nextIndex(array, 0, 1);
+      expect(typeof result).to.equal('number');
+    });
+
+    afterEach(function () {
+      spy.restore();
+      instance.release();
+      document.body.removeChild(wrapper);
+    });
+  });
+
+  describe('_handleInputChecked()', function () {
+    let instance;
+    let element;
+    let wrapper;
+    let spy;
+
+    beforeEach(function () {
+      wrapper = document.createElement('div');
+      wrapper.innerHTML = HTML;
+      document.body.appendChild(wrapper);
+      element = document.querySelector('[data-structured-list]');
+      instance = new StructuredList(element);
+    });
+
+    it('should be called', function () {
+      spy = sinon.spy(instance, '_handleInputChecked');
+      instance._handleInputChecked(0);
+      expect(spy).to.have.been.called;
+    });
+
+    afterEach(function () {
+      spy.restore();
+      instance.release();
+      document.body.removeChild(wrapper);
+    });
+  });
+
+  describe('_handleKeydownArrow(evt)', function () {
+    let instance;
+    let element;
+    let wrapper;
+    let spy;
+
+    beforeEach(function () {
+      wrapper = document.createElement('div');
+      wrapper.innerHTML = HTML;
+      document.body.appendChild(wrapper);
+      element = document.querySelector('[data-structured-list]');
+      instance = new StructuredList(element);
+    });
+
+    it('should be called on "up" keydown event', function () {
+      spy = sinon.spy(instance, '_handleKeydownArrow');
+      const event = Object.assign(
+        new CustomEvent('keydown', {
+          bubbles: true,
+        }),
+        { which: 38 },
+      );
+      instance.element.dispatchEvent(event);
+      expect(spy).to.have.been.called;
+    });
+
+    it('should be called on "down" keydown event', function () {
+      spy = sinon.spy(instance, '_handleKeydownArrow');
+      const event = Object.assign(
+        new CustomEvent('keydown', {
+          bubbles: true,
+        }),
+        { which: 40 },
+      );
+      instance.element.dispatchEvent(event);
+      expect(spy).to.have.been.called;
+    });
+
+    it('should not be called with another keydown event', function () {
+      spy = sinon.spy(instance, '_handleKeydownArrow');
+      const event = Object.assign(
+        new CustomEvent('keydown', {
+          bubbles: true,
+        }),
+        { which: 13 },
+      );
+      instance.element.dispatchEvent(event);
+      expect(spy).to.not.have.been.called;
+    });
+
+    afterEach(function () {
+      spy.restore();
+      instance.release();
+      document.body.removeChild(wrapper);
+    });
+  });
+});
