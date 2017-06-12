29012dccf46aeef5d4b265e2ce82572d451feeb7
mjohannessen <mari.l.johannessen@gmail.com>
2017-06-12 15:14:27 -0500

    fix(demo-env): fixed demo environment

--- demo/index.js
+++ demo/index.js
@@ -1,12 +1,10 @@
 import './polyfills/index';
 
-import ThemeSwitcher from './js/theme-switcher';
 import DemoSwitcher from './js/demo-switcher';
 
 export * from '../src/index';
 
 const init = () => {
-  ThemeSwitcher.init();
   DemoSwitcher.init();
 };
 
