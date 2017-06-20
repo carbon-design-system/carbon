9d41b6995f489de000a22a4df9170e77e272dc88
bthan <bthan@us.ibm.com>
2017-06-20 16:20:51 -0500

    fix(structured-list): label > input refactor for better react porting

--- src/components/structured-list/structured-list.js
+++ src/components/structured-list/structured-list.js
@@ -15,13 +15,16 @@ class StructuredList extends mixin(createComponent, initComponentBySearch) {
    */
   constructor(element, options) {
     super(element, options);
-    this.element.addEventListener('keydown', (evt) => {
+    this.element.addEventListener('keydown', evt => {
       if (evt.which === 38 || evt.which === 40) {
         this._handleKeydownArrow(evt);
       }
       if (evt.which === 13 || evt.which === 32) {
         this._handleKeydownChecked(evt);
       }
+    });
+    this.element.addEventListener('click', evt => {
+      this._handleClick(evt);
     });
   }
 
@@ -47,36 +50,56 @@ class StructuredList extends mixin(createComponent, initComponentBySearch) {
     input.checked = true;
   }
 
+  _handleClick(evt) {
+    const selectedRow = eventMatches(evt, this.options.selectorRow);
+    [...this.element.querySelectorAll(this.options.selectorRow)].forEach(row =>
+      row.classList.remove(this.options.classActive)
+    );
+    if (selectedRow) {
+      selectedRow.classList.add(this.options.classActive);
+    }
+  }
+
+  // Handle Enter or Space keydown events for selecting <label> rows
   _handleKeydownChecked(evt) {
     const selectedRow = eventMatches(evt, this.options.selectorRow);
+    [...this.element.querySelectorAll(this.options.selectorRow)].forEach(row =>
+      row.classList.remove(this.options.classActive)
+    );
     if (selectedRow) {
+      selectedRow.classList.add(this.options.classActive);
       const input = this.element.querySelector(
-        `#${selectedRow.getAttribute('for')}.bx--structured-list-input`,
+        `#${selectedRow.getAttribute('for')}.bx--structured-list-input`
       );
       input.checked = true;
     }
   }
 
+  // Handle up and down keydown events for selecting <label> rows
   _handleKeydownArrow(evt) {
     const selectedRow = eventMatches(evt, this.options.selectorRow);
     const direction = this._direction(evt);
 
     if (direction && selectedRow !== undefined) {
       const rows = [...this.element.querySelectorAll(this.options.selectorRow)];
+      rows.forEach(row => row.classList.remove(this.options.classActive));
       const firstIndex = 0;
       const nextIndex = this._nextIndex(rows, selectedRow, direction);
       const lastIndex = rows.length - 1;
 
       switch (nextIndex) {
         case -1:
+          rows[lastIndex].classList.add(this.options.classActive);
           rows[lastIndex].focus();
           this._handleInputChecked(lastIndex);
           break;
         case rows.length:
+          rows[firstIndex].classList.add(this.options.classActive);
           rows[firstIndex].focus();
           this._handleInputChecked(firstIndex);
           break;
         default:
+          rows[nextIndex].classList.add(this.options.classActive);
           rows[nextIndex].focus();
           this._handleInputChecked(nextIndex);
           break;
@@ -90,6 +113,7 @@ class StructuredList extends mixin(createComponent, initComponentBySearch) {
     selectorInit: '[data-structured-list]',
     selectorRow:
       '[data-structured-list] .bx--structured-list-tbody > label.bx--structured-list-row',
+    classActive: 'bx--structured-list-row--selected',
   };
 }
 
