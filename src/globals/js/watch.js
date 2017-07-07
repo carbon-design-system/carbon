961c5b30ad15830a38e9ae513af62d3612db902a
Akira Sudoh <asudoh@gmail.com>
2017-07-07 10:18:24 +0900

    chore(components): hoist mutation observer callback

--- src/globals/js/watch.js
+++ src/globals/js/watch.js
@@ -3,6 +3,37 @@ import { componentClasses } from '../../index';
 
 const forEach = Array.prototype.forEach;
 
+const createAndReleaseComponentsUponDOMMutation = (records, componentClassesForWatchInit, options) => {
+  records.forEach((record) => {
+    forEach.call(record.addedNodes, (node) => {
+      if (node.nodeType === Node.ELEMENT_NODE) {
+        componentClassesForWatchInit.forEach((Clz) => {
+          Clz.init(node, options);
+        });
+      }
+    });
+    forEach.call(record.removedNodes, (node) => {
+      if (node.nodeType === Node.ELEMENT_NODE) {
+        componentClasses.forEach((Clz) => {
+          if (node.matches(Clz.options.selectorInit)) {
+            const instance = Clz.components.get(node);
+            if (instance) {
+              instance.release();
+            }
+          } else {
+            forEach.call(node.querySelectorAll(Clz.options.selectorInit), (element) => {
+              const instance = Clz.components.get(element);
+              if (instance) {
+                instance.release();
+              }
+            });
+          }
+        });
+      }
+    });
+  });
+};
+
 /**
  * Automatically instantiates/destroys components in the given element, by watching for DOM additions/removals.
  * @param {Node} target The DOM node to instantiate components in. Should be a document or an element.
@@ -18,35 +49,9 @@ export default function (target = document, options = {}) {
   handles.push(initCheckbox());
 
   const componentClassesForWatchInit = componentClasses.filter(Clz => !Clz.forLazyInit);
+
   let observer = new MutationObserver((records) => {
-    records.forEach((record) => {
-      forEach.call(record.addedNodes, (node) => {
-        if (node.nodeType === Node.ELEMENT_NODE) {
-          componentClassesForWatchInit.forEach((Clz) => {
-            Clz.init(node, options);
-          });
-        }
-      });
-      forEach.call(record.removedNodes, (node) => {
-        if (node.nodeType === Node.ELEMENT_NODE) {
-          componentClasses.forEach((Clz) => {
-            if (node.matches(Clz.options.selectorInit)) {
-              const instance = Clz.components.get(node);
-              if (instance) {
-                instance.release();
-              }
-            } else {
-              forEach.call(node.querySelectorAll(Clz.options.selectorInit), (element) => {
-                const instance = Clz.components.get(element);
-                if (instance) {
-                  instance.release();
-                }
-              });
-            }
-          });
-        }
-      });
-    });
+    createAndReleaseComponentsUponDOMMutation(records, componentClassesForWatchInit, options);
   });
   observer.observe(target, {
     childList: true,
