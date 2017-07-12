d4ee34f4f400ad162e2f311753f6945b782e2eb7
Brian Han <hellobrian@users.noreply.github.com>
2017-07-12 13:50:44 -0500

    feat(StructuredList): add components for StructuredList (#24)
    * feat(StructuredList): in progress
    
    * feat(StructuredList): all basic components for simple list
    
    * chore(StructuredList): setup storybook
    
    * test(StructuredListWrapper): initial tests
    
    * chore(package.json): use latest carbon-components
    
    * refactor(StructuredListContent): remove
    
    * chore(StructuredListStory): rename file
    
    * feat(StructuredListRow): new variant for selection
    
    * feat(StructuredListInput): make input
    
    * refactor(StructuredList): row can render <label>
    
    * fix(StructuredList): some changes
    
    * chore(StructuredList): welp
    
    * test(StructuredListHead): add tests
    
    * test(StructuredList): tests for input
    
    * test(StructuredList): row tests
    
    * test(StructuredList): tests for body and cell
    
    * refactor(StructuredList): add handleKeyDown prop to children
    
    * chore(package.json): update carbon-components in devDeps to use latest
    
    * refactor(StructuredListRow): render input with prop
    
    * test(StructuredList): update tests
    
    * chore(StructuredList): next input inside row
    
    * test(StructuredList): remove unused mount

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+export { Module, ModuleBody, ModuleHeader } from './components/Module';
+export { ProgressIndicator, ProgressStep } from './components/ProgressIndicator';
 export Accordion from './components/Accordion';
 export AccordionItem from './components/AccordionItem';
 export Breadcrumb from './components/Breadcrumb';
@@ -19,23 +21,19 @@ export DatePickerInput from './components/DatePickerInput';
 export DetailPageHeader from './components/DetailPageHeader';
 export Dropdown from './components/Dropdown';
 export DropdownItem from './components/DropdownItem';
-export FileUploader, {
-  Filename,
-  FileUploaderButton,
-} from './components/FileUploader';
+export FileUploader, { Filename, FileUploaderButton } from './components/FileUploader';
 export Footer from './components/Footer';
 export Form from './components/Form';
 export FormGroup from './components/FormGroup';
 export Icon from './components/Icon';
 export InteriorLeftNav from './components/InteriorLeftNav';
-export InteriorLeftNavList from './components/InteriorLeftNavList';
 export InteriorLeftNavItem from './components/InteriorLeftNavItem';
+export InteriorLeftNavList from './components/InteriorLeftNavList';
 export Link from './components/Link';
 export ListItem from './components/ListItem';
 export Loading from './components/Loading';
 export Modal from './components/Modal';
 export ModalWrapper from './components/ModalWrapper';
-export { Module, ModuleBody, ModuleHeader } from './components/Module';
 export Notification from './components/Notification';
 export NumberInput from './components/NumberInput';
 export OrderedList from './components/OrderedList';
@@ -43,10 +41,6 @@ export OverflowMenu from './components/OverflowMenu';
 export OverflowMenuItem from './components/OverflowMenuItem';
 export Pagination from './components/Pagination';
 export PrimaryButton from './components/PrimaryButton';
-export {
-  ProgressIndicator,
-  ProgressStep,
-} from './components/ProgressIndicator';
 export RadioButton from './components/RadioButton';
 export RadioButtonGroup from './components/RadioButtonGroup';
 export Search from './components/Search';
@@ -54,10 +48,10 @@ export SecondaryButton from './components/SecondaryButton';
 export Select from './components/Select';
 export SelectItem from './components/SelectItem';
 export SelectItemGroup from './components/SelectItemGroup';
+export StructuredList from './components/StructuredList';
 export Switch from './components/Switch';
 export Tab from './components/Tab';
 export TabContent from './components/TabContent';
-export Tabs from './components/Tabs';
 export Table from './components/Table';
 export TableBody from './components/TableBody';
 export TableData from './components/TableData';
@@ -66,6 +60,7 @@ export TableHeader from './components/TableHeader';
 export TableRow from './components/TableRow';
 export TableRowExpanded from './components/TableRowExpanded';
 export Tag, { types } from './components/Tag';
+export Tabs from './components/Tabs';
 export TextArea from './components/TextArea';
 export TextInput from './components/TextInput';
 export TimePicker from './components/TimePicker';
