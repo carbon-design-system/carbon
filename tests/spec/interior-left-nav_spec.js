68d0610fef4d6b15c191d97999a0086b53c96928
chrisdhanaraj <adhanaraj@gmail.com>
2017-08-31 11:17:14 -0500

    feat(ILN): add in keepOpen configuration and tests

--- tests/spec/interior-left-nav_spec.js
+++ tests/spec/interior-left-nav_spec.js
@@ -3,6 +3,7 @@ import InteriorLeftNav
   from '../../src/components/interior-left-nav/interior-left-nav';
 import InteriorLeftNavHtml
   from '../../src/components/interior-left-nav/interior-left-nav.html';
+import KeepOpen from '../../src/components/interior-left-nav/interior-left-nav-keep-open.html';
 
 describe('Test interior left nav', function () {
   describe('Constructor', function () {
@@ -42,6 +43,67 @@ describe('Test interior left nav', function () {
     afterEach(function () {
       instance.release();
       document.body.removeChild(container);
+    });
+  });
+
+  describe('keepOpen', function () {
+    let container;
+    let element;
+    let instance1;
+    let instance2;
+    let instance3;
+
+    beforeEach(function () {
+      container = document.createElement('div');
+      container.innerHTML = InteriorLeftNavHtml;
+      document.body.appendChild(container);
+      element = document.querySelector('[data-interior-left-nav]');
+    });
+
+    it('Should have a default setting of false', function () {
+      instance1 = new InteriorLeftNav(element);
+      expect(instance1.options.keepOpen).to.be.false;
+      document.body.removeChild(container);
+      instance1.release();
+    });
+
+    it('Should accept an option in the constructor to override', function () {
+      instance2 = new InteriorLeftNav(element, {
+        keepOpen: true,
+      });
+      expect(instance2.options.keepOpen).to.be.true;
+      document.body.removeChild(container);
+      instance2.release();
+    });
+
+    it('Should read data-keep-open attribute in the markup to override', function () {
+      document.body.removeChild(container);
+      const container2 = document.createElement('div');
+      container2.innerHTML = KeepOpen;
+      document.body.appendChild(container2);
+      element = document.querySelector('[data-interior-left-nav]');
+      instance3 = new InteriorLeftNav(element);
+      expect(instance3.keepOpen).to.be.true;
+      document.body.removeChild(container2);
+      instance3.release();
+    });
+
+    it('If true, should not remove expanded class from other elements on second open', function () {
+      document.body.removeChild(container);
+      const container2 = document.createElement('div');
+      container2.innerHTML = KeepOpen;
+      document.body.appendChild(container2);
+      element = document.querySelector('[data-interior-left-nav]');
+      instance3 = new InteriorLeftNav(element);
+
+      const nested = document.querySelectorAll('.left-nav-list__item--has-children');
+
+      for (let i = 0; i < nested.length; i += 1) {
+        nested[i].dispatchEvent(new CustomEvent('click', { bubbles: true }));
+      }
+
+      const expanded = document.querySelectorAll('.left-nav-list__item--expanded');
+      expect(expanded.length).to.equal(nested.length);
     });
   });
 
