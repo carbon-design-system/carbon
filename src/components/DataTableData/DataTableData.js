c9fa746457cfb29b4ae834c45fdfdf3618109608
Alison Joseph <alisonejoseph@gmail.com>
2018-01-12 09:57:13 -0600

    fix(data-table): Update checkbox html (#474)

--- src/components/DataTableData/DataTableData.js
+++ src/components/DataTableData/DataTableData.js
@@ -26,18 +26,7 @@ const DataTableData = props => {
         value={value}
         name={id}
       />
-      <label htmlFor={id} className="bx--checkbox-label">
-        <span className="bx--checkbox-appearance">
-          <svg
-            className="bx--checkbox-checkmark"
-            width="12"
-            height="9"
-            viewBox="0 0 12 9"
-            fillRule="evenodd">
-            <path d="M4.1 6.1L1.4 3.4 0 4.9 4.1 9l7.6-7.6L10.3 0z" />
-          </svg>
-        </span>
-      </label>
+      <label htmlFor={id} className="bx--checkbox-label" />
     </span>
   );
 
