18c6632b11c130ccffe12a62609d959ada8e71d0
Akira Sudoh <asudoh@gmail.com>
2018-02-01 05:05:36 +0900

    feat(build): support UMD bundle (#520)

--- scripts/build.js
+++ scripts/build.js
@@ -14,6 +14,9 @@ const rootDir = path.resolve(__dirname, '../');
 const babelPath = path
   .resolve(__dirname, '../node_modules/.bin/babel')
   .replace(/ /g, '\\ ');
+const rollupPath = path
+  .resolve(__dirname, '../node_modules/.bin/rollup')
+  .replace(/ /g, '\\ ');
 const rimrafAsync = promisify(rimraf);
 
 const exec = (command, extraEnv) =>
@@ -26,8 +29,13 @@ const ignoreGlobs = ['**/__tests__/*', '**/*-test.js', '**/*-story.js'].join(
   ','
 );
 
-console.log('Deleting old build folders...');
-Promise.all([rimrafAsync(`${rootDir}/lib`), rimrafAsync(`${rootDir}/es`)])
+console.log('Deleting old build folders...'); // eslint-disable-line no-console
+
+Promise.all([
+  rimrafAsync(`${rootDir}/cjs`),
+  rimrafAsync(`${rootDir}/es`),
+  rimrafAsync(`${rootDir}/umd`),
+])
   .then(() => {
     exec(`${babelPath} src -q -d es --ignore "${ignoreGlobs}"`, {
       BABEL_ENV: 'es',
@@ -35,7 +43,20 @@ Promise.all([rimrafAsync(`${rootDir}/lib`), rimrafAsync(`${rootDir}/es`)])
     exec(`${babelPath} src -q -d lib --ignore "${ignoreGlobs}"`, {
       BABEL_ENV: 'cjs',
     });
+    exec(
+      `${rollupPath} -c scripts/rollup.config.js -o umd/carbon-components-react.js`,
+      {
+        NODE_ENV: 'development',
+      }
+    );
+    exec(
+      `${rollupPath} -c scripts/rollup.config.js -o umd/carbon-components-react.min.js`,
+      {
+        NODE_ENV: 'production',
+      }
+    );
   })
   .catch(error => {
-    throw error;
+    console.error('One of the commands failed:', error.stack); // eslint-disable-line no-console
+    process.exit(1);
   });
