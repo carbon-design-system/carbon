4b791199de9ef6b664f6b6c9db313f1e1b1f5f5d
Akira Sudoh <asudoh@gmail.com>
2018-02-05 09:15:21 +0900

    chore(devenv): improved the logic of release components upon new dev build

--- demo/index.js
+++ demo/index.js
@@ -10,22 +10,20 @@ export * from '../src/bundle';
 if (typeof module !== 'undefined' && module.hot) {
   const forEach = Array.prototype.forEach;
 
-  if (window.oldComponentClasses) {
+  module.hot.dispose(() => {
     // Releases component instances of (old) component classes that have been replaced with new ones by HMR
-    window.oldComponentClasses.forEach(Clz => {
-      forEach.call(document.body.querySelectorAll(Clz.options.selectorInit), element => {
-        const instance = Clz.components.get(element);
-        if (instance) {
-          instance.release();
-        }
+    Object.keys(components)
+      .map(key => components[key])
+      .filter(component => typeof component.init === 'function')
+      .forEach(Clz => {
+        forEach.call(document.body.querySelectorAll(Clz.options.selectorInit), element => {
+          const instance = Clz.components.get(element);
+          if (instance) {
+            instance.release();
+          }
+        });
       });
-    });
-  }
-
-  // Preserves component classe refs, that would be replaced replaced with new ones by HMR, to `window`
-  window.oldComponentClasses = Object.keys(components)
-    .map(key => components[key])
-    .filter(component => typeof component.init === 'function');
+  });
 
   module.hot.accept();
 }
