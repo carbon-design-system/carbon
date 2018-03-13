087604a9b949206d0f9829f5d083acc9a633b489
Josh Black <joshuablack.work@gmail.com>
2018-03-13 15:09:15 -0500

    chore(polyfill): update polyfill list (#706)

--- .storybook/polyfills.js
+++ .storybook/polyfills.js
@@ -2,3 +2,4 @@ import 'core-js/modules/es7.array.includes';
 import 'core-js/modules/es6.array.fill';
 import 'core-js/modules/es6.string.includes';
 import 'core-js/modules/es6.string.trim';
+import 'core-js/modules/es7.object.values';
