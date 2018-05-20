37acad3f3ea142aca1d6a11e1e36d2d04ae9b166
Josh Black <josh@josh.black>
2018-05-19 19:34:46 -0500

    fix(table-toolbar-search): add i18n support (#903)

--- src/components/DataTable/TableToolbarSearch.js
+++ src/components/DataTable/TableToolbarSearch.js
@@ -5,11 +5,20 @@ import Search from '../Search';
 import setupGetInstanceId from './tools/instanceId';
 
 const getInstanceId = setupGetInstanceId();
+const translationKeys = {
+  'carbon.table.toolbar.search.label': 'Filter table',
+  'carbon.table.toolbar.search.placeholder': 'Search',
+};
+
+const translateWithId = id => {
+  return translationKeys[id];
+};
 
 const TableToolbarSearch = ({
   className,
   searchContainerClass,
   onChange,
+  translateWithId: t,
   id = `data-table-search-${getInstanceId()}`,
   ...rest
 }) => {
@@ -24,8 +33,8 @@ const TableToolbarSearch = ({
         {...rest}
         small
         id={id}
-        labelText="Filter table"
-        placeHolderText="Search"
+        labelText={t('carbon.table.toolbar.search.label')}
+        placeHolderText={t('carbon.table.toolbar.search.placeholder')}
         onChange={onChange}
       />
     </div>
@@ -44,12 +53,25 @@ TableToolbarSearch.propTypes = {
    * Provide an optional id for the search container
    */
   id: PropTypes.string,
+
+  /**
+   * Provide an optional className for the overal container of the Search
+   */
   searchContainerClasses: PropTypes.string,
 
   /**
    * Provide an optional hook that is called each time the input is updated
    */
   onChange: PropTypes.func,
+
+  /**
+   * Provide custom text for the component for each translation id
+   */
+  translateWithId: PropTypes.func.isRequired,
+};
+
+TableToolbarSearch.defaultProps = {
+  translateWithId,
 };
 
 export default TableToolbarSearch;
