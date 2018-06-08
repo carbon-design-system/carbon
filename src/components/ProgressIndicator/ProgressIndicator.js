2528d7d0af41ce660b1b94d86c59164ee7db135b
TJ Egan <tw15egan@gmail.com>
2018-06-08 09:02:41 -0500

    fix(progress-indicator): fix svg warning (#979)

--- src/components/ProgressIndicator/ProgressIndicator.js
+++ src/components/ProgressIndicator/ProgressIndicator.js
@@ -24,7 +24,7 @@ export const ProgressStep = ({ ...props }) => {
   const completeSvg = complete && (
     <svg width="16" height="16" viewBox="0 0 16 16">
       <title>{description}</title>
-      <g fill-rule="nonzero">
+      <g fillRule="nonzero">
         <path d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z" />
         <path d="M11.646 5.146l.708.708-5.604 5.603-3.104-3.103.708-.708 2.396 2.397z" />
       </g>
