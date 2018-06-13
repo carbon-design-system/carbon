06f7450cb1ec239e6e7cddd839965bad19bfe469
Rezak OTMANI <rezak.otmani@yahoo.fr>
2018-06-13 17:25:41 +0200

    fix(data-table-v2): fix the undefined method called when press in escape key , resolves #674

--- src/components/data-table-v2/data-table-v2.js
+++ src/components/data-table-v2/data-table-v2.js
@@ -210,6 +210,18 @@ class DataTableV2 extends mixin(createComponent, initComponentBySearch, eventedS
     });
   };
 
+  _batchCancel() {
+    const inputs = [...this.element.querySelectorAll(this.options.selectorCheckbox)];
+
+    inputs.forEach(item => {
+      item.checked = false;
+    });
+
+    this.state.checkboxCount = 0;
+    this._actionBarToggle(false);
+    this.countEl.textContent = this.state.checkboxCount;
+  }
+
   _keydownHandler = evt => {
     if (evt.which === 27) {
       this._batchCancel();
