99419741fdc6ce8ab78f6694d8d9c05c10886163
Dmitri Wolf <dmitri.a.wolf@gmail.com>
2018-06-18 10:08:09 -0500

    fix(datepicker): unknown prop on div tag (#1010)

--- src/components/DatePicker/DatePicker.js
+++ src/components/DatePicker/DatePicker.js
@@ -380,6 +380,7 @@ export default class DatePicker extends Component {
 
   render() {
     const {
+      appendTo, // eslint-disable-line
       children,
       className,
       short,
@@ -389,6 +390,8 @@ export default class DatePicker extends Component {
       maxDate, // eslint-disable-line
       dateFormat, // eslint-disable-line
       onChange, // eslint-disable-line
+      locale, // eslint-disable-line
+      value, // eslint-disable-line
       ...other
     } = this.props;
 
