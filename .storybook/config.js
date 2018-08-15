e1f540d4abf0f7e391d2e83abd20b40bd85b7d35
Vince Picone <vpicone@gmail.com>
2018-08-15 09:52:13 -0500

    fix: Add addon-info global defaults to prevent default functions getting cut-off. (#1198)

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { configure, setAddon, addDecorator } from '@storybook/react';
-import infoAddon from '@storybook/addon-info';
+import infoAddon, { setDefaults } from '@storybook/addon-info';
 import { checkA11y } from 'storybook-addon-a11y';
 import Container from './Container';
 
-// addDecorator(checkA11y);
-addDecorator(story => <Container story={story} />);
+// addon-info defaults
+setDefaults({
+  maxPropStringLength: 200, // Displays the first 200 characters in the default prop string
+});
 setAddon(infoAddon);
+
+addDecorator(story => <Container story={story} />);
+// addDecorator(checkA11y);
 
 function loadStories() {
   const req = require.context('../src/components', true, /\-story\.js$/);
