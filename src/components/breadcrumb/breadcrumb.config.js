caedf8d798db6ad162418b91cadd6f170839b601
Mark Wallace <mark_wallace@ie.ibm.com>
2018-09-17 11:04:39 +0100

    fix(breadcrumb): rotate overflow menu icon 90m degress
    Fixes #1105

--- src/components/breadcrumb/breadcrumb.config.js
+++ src/components/breadcrumb/breadcrumb.config.js
@@ -6,7 +6,7 @@ module.exports = {
       name: 'default',
       label: 'Breadcrumb',
       notes:
-        'Breadcrumb enables users to quickly see their location within a path of navigation and move up to a parent level if desired.',
+        'Breadcrumb enables users to quickly see their location within a path of navigation and move up to a parent level if desired. If there are more than three levels, the home level and the last two levels are visible, with all middle levels being collapsed into ellipses and accessible from a dropdown.',
       context: {
         items: [
           {
