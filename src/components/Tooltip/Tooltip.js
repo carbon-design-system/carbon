b237a46755015daab3ad527025ac505be19620b8
Kornel Dubieniecki <lekterable@gmail.com>
2018-10-02 15:45:32 +0200

    feat(Tooltip): add TabIndex to a Tooltip without an icon (#1378)

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -382,6 +382,8 @@ export default class Tooltip extends Component {
             </div>
           ) : (
             <div
+              // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+              tabIndex="0"
               id={triggerId}
               className={triggerClasses}
               ref={node => {
