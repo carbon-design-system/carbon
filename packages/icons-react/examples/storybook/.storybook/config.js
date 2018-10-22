935240ac3bbac693bb9487de6748626dd8debe52
Jen Downs <hello@jendowns.com>
2018-10-22 11:25:21 -0500

    chore(storybook): add options addon so storybook header can link back to icons-react (#48)
    * chore(storybook): add options addon and repo details
    
    * chore: commit yarn offline mirror changes
    
    * chore: format icons-react storybook config
    
    * chore: commit yarn offline files after cache clean

--- packages/icons-react/examples/storybook/.storybook/config.js
+++ packages/icons-react/examples/storybook/.storybook/config.js
@@ -1,4 +1,11 @@
 import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
+
+setOptions({
+  name: `@carbon/icons-react`,
+  url:
+    'https://github.com/IBM/carbon-elements/tree/master/packages/icons-react',
+});
 
 function loadStories() {
   const req = require.context('../stories', true, /\-story\.js$/);
