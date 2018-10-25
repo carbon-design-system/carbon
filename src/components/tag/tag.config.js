6d803c9fd9e5b4f80ee7bd3eb45d4bb1524d2bb4
Alessandra Davila <adavila91@gmail.com>
2018-10-25 15:47:42 -0500

    feat(tags): change colors to experimental colors (#1316)
    * feat(tags): change colors to new colors
    
    * feat(tags): add new tags markup
    
    * chore(tags): remove focus state leave basic functionality
    
    * chore(tags): remove list

--- src/components/tag/tag.config.js
+++ src/components/tag/tag.config.js
@@ -1,43 +1,88 @@
 'use strict';
 
-const tags = [
-  {
-    type: 'ibm',
-    label: 'IBM',
-  },
-  {
-    type: 'beta',
-    label: 'Beta',
-  },
-  {
-    type: 'third-party',
-    label: 'Third-Party',
-  },
-  {
-    type: 'local',
-    label: 'Local',
-  },
-  {
-    type: 'dedicated',
-    label: 'Dedicated',
-  },
-  {
-    type: 'custom',
-    label: 'Custom',
-  },
-  {
-    type: 'experimental',
-    label: 'Experimental',
-  },
-  {
-    type: 'community',
-    label: 'Community',
-  },
-  {
-    type: 'private',
-    label: 'Private',
-  },
-];
+const { componentsX } = require('../../globals/js/feature-flags');
+
+const tags = !componentsX
+  ? [
+      {
+        type: 'ibm',
+        label: 'IBM',
+      },
+      {
+        type: 'beta',
+        label: 'Beta',
+      },
+      {
+        type: 'third-party',
+        label: 'Third-Party',
+      },
+      {
+        type: 'local',
+        label: 'Local',
+      },
+      {
+        type: 'dedicated',
+        label: 'Dedicated',
+      },
+      {
+        type: 'custom',
+        label: 'Custom',
+      },
+      {
+        type: 'experimental',
+        label: 'Experimental',
+      },
+      {
+        type: 'community',
+        label: 'Community',
+      },
+      {
+        type: 'private',
+        label: 'Private',
+      },
+    ]
+  : [
+      {
+        type: 'basic',
+        label: 'Component',
+      },
+      {
+        type: 'red',
+        label: 'Red',
+      },
+      {
+        type: 'magenta',
+        label: 'Magenta',
+      },
+      {
+        type: 'purple',
+        label: 'Purple',
+      },
+      {
+        type: 'blue',
+        label: 'Blue',
+      },
+      {
+        type: 'cyan',
+        label: 'Cyan',
+      },
+      {
+        type: 'teal',
+        label: 'Teal',
+      },
+      {
+        type: 'green',
+        label: 'Green',
+      },
+      {
+        type: 'cool-gray',
+        label: 'Cool-Gray',
+      },
+      {
+        type: 'warm-gray',
+        label: 'Warm-Gray',
+      },
+    ];
 
 module.exports = {
   variants: [
@@ -46,6 +91,7 @@ module.exports = {
       label: 'Tag',
       context: {
         tags,
+        componentsX,
       },
     },
   ],
