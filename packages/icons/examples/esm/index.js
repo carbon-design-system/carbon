fd044d5368d02d9c9918468cd96ab5c6420f6d56
Josh Black <josh@josh.black>
2018-10-26 17:39:27 -0500

    docs(icons): add support for issue template

--- packages/icons/examples/esm/index.js
+++ packages/icons/examples/esm/index.js
@@ -10,6 +10,7 @@ const App = ({ icons }) => (
         <th>Size</th>
         <th>Preview</th>
         <th>GitHub</th>
+        <th>Report Issue</th>
       </tr>
     </thead>
     <tbody>
@@ -34,8 +35,19 @@ const App = ({ icons }) => (
                 </div>
               </td>
               <td>
-                <a href={getLink(icon)} rel="noopener noreferrer">
+                <a
+                  href={getLink(icon)}
+                  rel="noopener noreferrer"
+                  target="_blank">
                   Source
+                </a>
+              </td>
+              <td>
+                <a
+                  href={getBugTemplate(icon)}
+                  rel="noopener noreferrer"
+                  target="_blank">
+                  Issue Template
                 </a>
               </td>
             </tr>
@@ -66,6 +78,53 @@ function getLink(icon) {
   const baseUrl =
     'https://github.com/IBM/carbon-elements/tree/master/packages/icons/src/svg';
   return `${baseUrl}/${icon.size}/${icon.name}.svg`;
+}
+
+function getBugTemplate(icon) {
+  const url = new URL('https://github.com/IBM/carbon-elements/issues/new');
+  const params = new URLSearchParams();
+  params.append('title', `🔍 Visual bug for the \`${icon.name}\` icon`);
+  params.append(
+    'body',
+    `<!-- Feel free to remove sections that aren't relevant. -->
+
+There is an issue for the ${
+      icon.name
+    } icon when viewing [the elements demo](https://ibm.github.io/carbon-elements/icons/examples/esm/index.html).
+
+The source for this icon is available [here](${getLink(icon)}).
+
+## Detailed description
+
+> Describe in detail the issue you're having.
+
+> Is this a feature request (new component, new icon), a bug, or a general issue?
+
+> Is this issue related to a specific component?
+
+> What did you expect to happen? What happened instead? What would you like to see changed?
+
+> What browser are you working in?
+
+> What version of the Carbon Design System are you using?
+
+> What offering/product do you work on? Any pressing ship or release dates we should be aware of?
+
+## Steps to reproduce the issue
+
+1. Step one
+2. Step two
+3. Step three
+4. etc.
+
+## Additional information
+
+- Screenshots or code
+- Notes
+`
+  );
+  url.search = params;
+  return url.href;
 }
 
 render();
