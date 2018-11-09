728031500d9c5a160bfd056b4cd6fd7874ed30b1
Alison Joseph <alisonejoseph@Gmail.com>
2018-11-09 09:52:41 -0600

    chore: reorder storybook addons to show story first (#1546)

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,9 +1,9 @@
-import '@storybook/addon-actions/register';
+import '@storybook/addon-storysource/register';
 import '@storybook/addon-knobs/register';
+import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-a11y/register';
 import '@storybook/addon-options/register';
-import '@storybook/addon-storysource/register';
 
 // Community addons
 import 'storybook-readme/register';
