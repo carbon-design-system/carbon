3a1c4346c059858510e7675cd80234326ee0f3a2
dakahn <dakahn@protonmail.com>
2018-11-09 13:55:42 -0600

    fix(file-uploader): swap out onreadystatechange for DOMContentLoaded

--- src/components/file-uploader/file-uploader.js
+++ src/components/file-uploader/file-uploader.js
@@ -13,19 +13,17 @@ import on from '../../globals/js/misc/on';
  * order to mimic if it were an input button -- also worth noting is we hide the
  * actual input from tab order in the markup.
  */
-document.onreadystatechange(() => {
-  if (document.readyState === 'complete') {
-    const uploadLabel = document.getElementById('input-button');
-    const uploadInput = document.getElementById('your-file-importer-id-here');
-    uploadLabel.addEventListener('keydown', event => {
-      // Stop propagation of the event so we don't throw open the dialog more than once.
-      event.stopPropagation();
-      // if the user presses the enter key or the spacebar click the upload button.
-      if (event.keyCode === 32 || event.keyCode === 13) {
-        uploadInput.click();
-      }
-    });
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  const uploadLabel = document.getElementById('input-button');
+  const uploadInput = document.getElementById('your-file-importer-id-here');
+  uploadLabel.addEventListener('keydown', event => {
+    // Stop propagation of the event so we don't throw open the dialog more than once.
+    event.stopPropagation();
+    // if the user presses the enter key or the spacebar click the upload button.
+    if (event.keyCode === 32 || event.keyCode === 13) {
+      uploadInput.click();
+    }
+  });
 });
 
 class FileUploader extends mixin(createComponent, initComponentBySearch, eventedState, handles) {
