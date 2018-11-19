b99e88d24071606da3d18df0d552f8c560acd8ef
Akira Sudoh <asudoh@gmail.com>
2018-11-20 03:52:36 +0900

    feat(icon-helpers): export formatAttributes (#84)
    Refs IBM/carbon-components#1452.

--- packages/icon-helpers/src/index.js
+++ packages/icon-helpers/src/index.js
@@ -1,5 +1,5 @@
 import getAttributes from './getAttributes';
-import toString from './toString';
+import toString, { formatAttributes } from './toString';
 import toSVG from './toSVG';
 
-export { getAttributes, toString, toSVG };
+export { getAttributes, formatAttributes, toString, toSVG };
