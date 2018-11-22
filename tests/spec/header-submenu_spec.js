49262c14cd89a7a1b41b4f4d5238595f09afa9be
Andrew Wang <afw5059@gmail.com>
2018-11-21 20:54:43 -0600

    test(header-submenu): handle unknown events

--- tests/spec/header-submenu_spec.js
+++ tests/spec/header-submenu_spec.js
@@ -251,4 +251,116 @@ describe('Header Submenu', function() {
       document.body.removeChild(element1);
     });
   });
+
+  describe('Handle mouse event', function() {
+    let headerSubmenu;
+    let element1;
+    let triggerNode;
+    let itemsContainerNode;
+    let itemNode1;
+    let itemNode2;
+    let itemLinkNode1;
+    let itemLinkNode2;
+    let element2;
+    const mouseEnter = new MouseEvent('mouseenter', { bubbles: true });
+    const mouseLeave = new MouseEvent('mouseleave', { bubbles: true });
+
+    beforeAll(function() {
+      element1 = document.createElement('li');
+      element1.classList.add('bx--header__submenu');
+      triggerNode = document.createElement('a');
+      triggerNode.className = 'bx--header__menu-title';
+      element1.appendChild(triggerNode);
+      itemsContainerNode = document.createElement('ul');
+      itemsContainerNode.className = 'bx--header__menu';
+      itemNode1 = document.createElement('li');
+      itemNode2 = document.createElement('li');
+      itemLinkNode1 = document.createElement('a');
+      itemLinkNode1.className = 'bx--header__menu-item';
+      itemLinkNode2 = document.createElement('a');
+      itemLinkNode2.className = 'bx--header__menu-item';
+      itemNode1.appendChild(itemLinkNode1);
+      itemNode2.appendChild(itemLinkNode2);
+      itemsContainerNode.appendChild(itemNode1);
+      itemsContainerNode.appendChild(itemNode2);
+      element1.appendChild(itemsContainerNode);
+      element2 = element1.cloneNode(true);
+      headerSubmenu = new HeaderSubmenu(element1);
+      document.body.appendChild(element1);
+      document.body.appendChild(element2);
+    });
+
+    it('should close menu on mouseleave', function() {
+      triggerNode.setAttribute('aria-expanded', 'true');
+      triggerNode.dispatchEvent(mouseLeave);
+      expect(triggerNode.getAttribute('aria-expanded')).toBe('false');
+    });
+
+    it('should open menu on mouseenter', function() {
+      triggerNode.setAttribute('aria-expanded', 'false');
+      triggerNode.dispatchEvent(mouseEnter);
+      expect(triggerNode.getAttribute('aria-expanded')).toBe('true');
+    });
+
+    afterEach(function() {
+      triggerNode.setAttribute('aria-expanded', 'false');
+    });
+
+    afterAll(function() {
+      headerSubmenu.release();
+      document.body.removeChild(element1);
+    });
+  });
+
+  describe('Handle unknown events', function() {
+    let headerSubmenu;
+    let element;
+    let triggerNode;
+    let itemLinkNode;
+    let unknownEvent;
+
+    beforeAll(function() {
+      element = document.createElement('li');
+      triggerNode = document.createElement('a');
+      triggerNode.classList.add('bx--header__menu-title');
+      triggerNode.classList.add('bx--header__menu-item');
+      element.appendChild(triggerNode);
+      const itemsContainerNode = document.createElement('bx--header__menu');
+      itemsContainerNode.className = 'bx--header__menu';
+      const itemNode = document.createElement('li');
+      itemLinkNode = document.createElement('a');
+      itemLinkNode.className = 'bx--header__menu-item';
+      itemNode.appendChild(itemLinkNode);
+      itemsContainerNode.appendChild(itemNode);
+      element.appendChild(itemsContainerNode);
+      headerSubmenu = new HeaderSubmenu(element);
+      document.body.appendChild(element);
+      unknownEvent = new CustomEvent('unknown', { bubbles: true });
+    });
+
+    beforeEach(function() {
+      triggerNode.dispatchEvent(unknownEvent);
+    });
+
+    it('should gracefully ignore unknown events', function() {
+      const getAction = headerSubmenu._getAction(unknownEvent);
+      expect(getAction).toEqual(null);
+    });
+
+    it('should not change state upon unknown events', function() {
+      const action = headerSubmenu._getAction(unknownEvent);
+      const state = headerSubmenu._getNewState(action);
+      expect(state).toEqual(false);
+    });
+
+    it('should handle unknown keypress', function() {
+      const unknownKeypress = headerSubmenu._handleKeyDown(unknownEvent);
+      expect(unknownKeypress).toEqual(undefined);
+    });
+
+    afterAll(function() {
+      headerSubmenu.release();
+      document.body.removeChild(element);
+    });
+  });
 });
