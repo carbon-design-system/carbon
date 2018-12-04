89fbf0f9790b329dbddb6b6b61c5f5a213e61e8d
emyarod <afw5059@gmail.com>
2018-12-04 13:44:16 -0600

    feat(ui-shell): add navigation menu component

--- src/globals/js/components.js
+++ src/globals/js/components.js
@@ -33,3 +33,4 @@ export { default as TextInput } from '../../components/text-input/text-input';
 export { default as SideNav } from '../../components/ui-shell/side-nav';
 export { default as HeaderSubmenu } from '../../components/ui-shell/header-submenu';
 export { default as HeaderNav } from '../../components/ui-shell/header-nav';
+export { default as NavigationMenu } from '../../components/ui-shell/navigation-menu';
