11388305874e161e28f71ac7db1337744d2193c7
Josh Black <josh@josh.black>
2018-12-06 13:50:32 -0600

    feat(type): add new token set (#117)
    * feat(type): add new token set
    
    * feat(type): add fluid type support
    
    * fix(type): add support for IBM Plex Sans Italic

--- packages/type/src/index.js
+++ packages/type/src/index.js
@@ -67,49 +67,17 @@ export function getTypeSize(step) {
   return getTypeSize(step - 1) + Math.floor((step - 2) / 4 + 1) * 2;
 }
 
-export const typeScale = Array.from({ length: 16 }, (_, i) =>
+export const typeScale = Array.from({ length: 23 }, (_, i) =>
   getTypeSize(i + 1)
 );
 
 // Tokens
-export const component01 = {
+export const caption01 = {
   fontFamily: fontFamilies.sans,
-  fontSize: typeScale(2),
+  fontSize: typeScale(1),
   fontWeight: fontWeights.regular,
-  lineHeight: rem(18),
-  letterSpacing: px(0.16),
-};
-
-export const componentBody01 = {
-  fontFamily: fontFamilies.sans,
-  fontSize: typeScale(2),
-  fontWeight: fontWeights.regular,
-  lineHeight: rem(20),
-  letterSpacing: px(0.16),
-};
-
-export const componentHeader01 = {
-  fontFamily: fontFamilies.sans,
-  fontSize: typeScale(2),
-  fontWeight: fontWeights.semibold,
-  lineHeight: rem(18),
-  letterSpacing: px(0.16),
-};
-
-export const component02 = {
-  fontFamily: fontFamilies.sans,
-  fontSize: typeScale(3),
-  fontWeight: fontWeights.regular,
-  lineHeight: rem(22),
-  letterSpacing: px(0),
-};
-
-export const componentBody02 = {
-  fontFamily: fontFamilies.sans,
-  fontSize: typeScale(3),
-  fontWeight: fontWeights.regular,
-  lineHeight: rem(24),
-  letterSpacing: px(0),
+  lineHeight: rem(16),
+  letterSpacing: px(0.32),
 };
 
 export const label01 = {
@@ -123,74 +91,401 @@ export const label01 = {
 export const helperText01 = {
   fontFamily: fontFamilies.sans,
   fontSize: typeScale(1),
-  fontWeight: fontWeights.regular,
   fontStyle: 'italic',
   lineHeight: rem(16),
   letterSpacing: px(0.32),
 };
 
+export const bodyShort01 = {
+  fontFamily: fontFamilies.sans,
+  fontSize: typeScale(2),
+  fontWeight: fontWeights.regular,
+  lineHeight: rem(18),
+  letterSpacing: px(0.16),
+};
+
+export const bodyLong01 = {
+  fontFamily: fontFamilies.sans,
+  fontSize: typeScale(2),
+  fontWeight: fontWeights.regular,
+  lineHeight: rem(20),
+  letterSpacing: px(0.16),
+};
+
+export const bodyShort02 = {
+  fontFamily: fontFamilies.sans,
+  fontSize: typeScale(3),
+  fontWeight: fontWeights.regular,
+  lineHeight: rem(22),
+  letterSpacing: 0,
+};
+
+export const bodyLong02 = {
+  fontFamily: fontFamilies.sans,
+  fontSize: typeScale(3),
+  fontWeight: fontWeights.regular,
+  lineHeight: rem(24),
+  letterSpacing: 0,
+};
+
 export const code01 = {
   fontFamily: fontFamilies.mono,
-  fontWeight: fontWeights.regular,
   fontSize: typeScale(1),
+  fontWeight: fontWeights.regular,
   lineHeight: rem(16),
   letterSpacing: px(0.32),
 };
 
 export const code02 = {
   fontFamily: fontFamilies.mono,
-  fontWeight: fontWeights.regular,
   fontSize: typeScale(2),
+  fontWeight: fontWeights.regular,
   lineHeight: rem(20),
   letterSpacing: px(0.32),
 };
 
 export const heading01 = {
   fontFamily: fontFamilies.sans,
-  fontSize: typeScale(5),
-  fontWeight: fontWeights.regular,
-  lineHeight: rem(26),
-  letterSpacing: px(0),
+  fontSize: typeScale(2),
+  fontWeight: fontWeights.semibold,
+  lineHeight: rem(18),
+  letterSpacing: px(0.16),
 };
 
 export const heading02 = {
   fontFamily: fontFamilies.sans,
-  fontSize: typeScale(6),
-  fontWeight: fontWeights.regular,
-  lineHeight: rem(30),
-  letterSpacing: px(0),
+  fontSize: typeScale(3),
+  fontWeight: fontWeights.semibold,
+  lineHeight: rem(22),
+  letterSpacing: 0,
 };
 
 export const heading03 = {
   fontFamily: fontFamilies.sans,
+  fontSize: typeScale(5),
+  fontWeight: fontWeights.regular,
+  lineHeight: rem(26),
+  letterSpacing: 0,
+};
+
+export const productiveHeading04 = {
+  fontFamily: fontFamilies.sans,
+  fontSize: typeScale(6),
+  fontWeight: fontWeights.regular,
+  lineHeight: rem(30),
+  letterSpacing: 0,
+};
+
+export const productiveHeading05 = {
+  fontFamily: fontFamilies.sans,
   fontSize: typeScale(8),
   fontWeight: fontWeights.regular,
   lineHeight: rem(40),
-  letterSpacing: px(0),
+  letterSpacing: 0,
 };
 
-export const body01 = {
+export const expressiveHeading04 = {
   fontFamily: fontFamilies.sans,
+  fontSize: typeScale(5),
   fontWeight: fontWeights.regular,
-  fontSize: typeScale(1),
-  lineHeight: rem(16),
-  letterSpacing: px(0.32),
+  lineHeight: rem(26),
+  letterSpacing: 0,
+  breakpoints: {
+    md: {
+      fontSize: typeScale(5),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.regular,
+      lineHeight: rem(26),
+      letterSpacing: 0,
+    },
+    lg: {
+      fontSize: typeScale(6),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.regular,
+      lineHeight: rem(30),
+      letterSpacing: 0,
+    },
+    xlg: {
+      fontSize: typeScale(7),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.regular,
+      lineHeight: rem(36),
+      letterSpacing: 0,
+    },
+    max: {
+      fontSize: typeScale(8),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.regular,
+      lineHeight: rem(40),
+      letterSpacing: 0,
+    },
+  },
 };
 
-export const body02 = {
+export const expressiveHeading05 = {
   fontFamily: fontFamilies.sans,
-  fontWeight: fontWeights.regular,
-  fontSize: typeScale(2),
-  lineHeight: rem(20),
-  letterSpacing: px(0.16),
+  fontSize: typeScale(7),
+  fontWeight: fontWeights.light,
+  lineHeight: rem(36),
+  letterSpacing: 0,
+  breakpoints: {
+    md: {
+      fontSize: typeScale(9),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(44),
+      letterSpacing: 0,
+    },
+    lg: {
+      fontSize: typeScale(10),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(50),
+      letterSpacing: 0,
+    },
+    xlg: {
+      fontSize: typeScale(11),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(56),
+      letterSpacing: 0,
+    },
+    max: {
+      fontSize: typeScale(13),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(70),
+      letterSpacing: 0,
+    },
+  },
 };
 
-export const body03 = {
-  fontFamily: fontFamilies.sans,
+export const quotation01 = {
+  fontFamily: fontFamilies.serif,
+  fontSize: typeScale(5),
   fontWeight: fontWeights.regular,
-  fontSize: typeScale(3),
-  lineHeight: rem(24),
-  letterSpacing: px(0),
+  lineHeight: rem(26),
+  letterSpacing: 0,
+  breakpoints: {
+    md: {
+      fontSize: typeScale(5),
+      fontFamily: fontFamilies.serif,
+      fontWeight: fontWeights.regular,
+      lineHeight: rem(26),
+      letterSpacing: 0,
+    },
+    lg: {
+      fontSize: typeScale(6),
+      fontFamily: fontFamilies.serif,
+      fontWeight: fontWeights.regular,
+      lineHeight: rem(30),
+      letterSpacing: 0,
+    },
+    xlg: {
+      fontSize: typeScale(7),
+      fontFamily: fontFamilies.serif,
+      fontWeight: fontWeights.regular,
+      lineHeight: rem(36),
+      letterSpacing: 0,
+    },
+    max: {
+      fontSize: typeScale(8),
+      fontFamily: fontFamilies.serif,
+      fontWeight: fontWeights.regular,
+      lineHeight: rem(40),
+      letterSpacing: 0,
+    },
+  },
+};
+
+export const quotation02 = {
+  fontFamily: fontFamilies.serif,
+  fontSize: typeScale(7),
+  fontWeight: fontWeights.light,
+  lineHeight: rem(36),
+  letterSpacing: 0,
+  breakpoints: {
+    md: {
+      fontSize: typeScale(9),
+      fontFamily: fontFamilies.serif,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(44),
+      letterSpacing: 0,
+    },
+    lg: {
+      fontSize: typeScale(10),
+      fontFamily: fontFamilies.serif,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(50),
+      letterSpacing: 0,
+    },
+    xlg: {
+      fontSize: typeScale(11),
+      fontFamily: fontFamilies.serif,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(56),
+      letterSpacing: 0,
+    },
+    max: {
+      fontSize: typeScale(13),
+      fontFamily: fontFamilies.serif,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(70),
+      letterSpacing: 0,
+    },
+  },
+};
+
+export const display01 = {
+  fontFamily: fontFamilies.sans,
+  fontSize: typeScale(10),
+  fontWeight: fontWeights.light,
+  lineHeight: rem(50),
+  letterSpacing: 0,
+  breakpoints: {
+    md: {
+      fontSize: typeScale(10),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(50),
+      letterSpacing: 0,
+    },
+    lg: {
+      fontSize: typeScale(12),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(64),
+      letterSpacing: 0,
+    },
+    xlg: {
+      fontSize: typeScale(13),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(70),
+      letterSpacing: 0,
+    },
+    max: {
+      fontSize: typeScale(15),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(86),
+      letterSpacing: 0,
+    },
+  },
+};
+
+export const display02 = {
+  fontFamily: fontFamilies.sans,
+  fontSize: typeScale(10),
+  fontWeight: fontWeights.semibold,
+  lineHeight: rem(50),
+  letterSpacing: 0,
+  breakpoints: {
+    md: {
+      fontSize: typeScale(10),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.semibold,
+      lineHeight: rem(50),
+      letterSpacing: 0,
+    },
+    lg: {
+      fontSize: typeScale(12),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.semibold,
+      lineHeight: rem(64),
+      letterSpacing: 0,
+    },
+    xlg: {
+      fontSize: typeScale(13),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.emiBold,
+      lineHeight: rem(70),
+      letterSpacing: 0,
+    },
+    max: {
+      fontSize: typeScale(15),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.semibold,
+      lineHeight: rem(86),
+      letterSpacing: 0,
+    },
+  },
+};
+
+export const display03 = {
+  fontFamily: fontFamilies.sans,
+  fontSize: typeScale(10),
+  fontWeight: fontWeights.light,
+  lineHeight: rem(50),
+  letterSpacing: 0,
+  breakpoints: {
+    md: {
+      fontSize: typeScale(14),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(78),
+      letterSpacing: 0,
+    },
+    lg: {
+      fontSize: typeScale(17),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(102),
+      letterSpacing: px(-0.64),
+    },
+    xlg: {
+      fontSize: typeScale(20),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(130),
+      letterSpacing: px(-0.64),
+    },
+    max: {
+      fontSize: typeScale(23),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.light,
+      lineHeight: rem(164),
+      letterSpacing: px(-0.96),
+    },
+  },
+};
+
+export const display04 = {
+  fontFamily: fontFamilies.sans,
+  fontSize: typeScale(10),
+  fontWeight: fontWeights.semibold,
+  lineHeight: rem(50),
+  letterSpacing: 0,
+  breakpoints: {
+    md: {
+      fontSize: typeScale(14),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.semibold,
+      lineHeight: rem(78),
+      letterSpacing: 0,
+    },
+    lg: {
+      fontSize: typeScale(17),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.semibold,
+      lineHeight: rem(102),
+      letterSpacing: px(-0.64),
+    },
+    xlg: {
+      fontSize: typeScale(20),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.semibold,
+      lineHeight: rem(130),
+      letterSpacing: px(-0.64),
+    },
+    max: {
+      fontSize: typeScale(23),
+      fontFamily: fontFamilies.sans,
+      fontWeight: fontWeights.semibold,
+      lineHeight: rem(164),
+      letterSpacing: px(-0.96),
+    },
+  },
 };
 
 // Spacing
