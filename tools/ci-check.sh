b152a890e9a102dcc48b7266921b070d3c01b040
Akira Sudoh <asudoh@gmail.com>
2018-12-13 11:44:02 +0900

    chore(build): a test for ES build
    Refs #1552.

--- tools/ci-check.sh
+++ tools/ci-check.sh
@@ -5,6 +5,6 @@ set -e
 yarn build
 yarn lint
 yarn test:unit -- -b ChromeHeadless_Travis -b Firefox
-yarn test:styles
+yarn test:build
 if [[ -n "$RUN_EACH" ]]; then find tests/spec -name "*.js" ! -name left-nav_spec.js -print0 | xargs -0 -n 1 -P 1 yarn test:unit -- -d -f; fi
 if [[ -n "$AAT_TOKEN" ]]; then yarn test:a11y; fi
