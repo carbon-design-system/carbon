b7971d48a4421b73cca80e63bdb1015c46482e37
emyarod <emyarod@users.noreply.github.com>
2019-01-02 16:54:56 -0600

    feat(CopyButton): support new icon strategy (#1674)
    * feat(CopyButton): support new icon strategy
    
    * fix(CopyButton): use icon direct import path
    
    * chore: bump bundle size threshold

--- src/components/CopyButton/CopyButton.js
+++ src/components/CopyButton/CopyButton.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 import { settings } from 'carbon-components';
 import { iconCopy } from 'carbon-icons';
+// TODO: import { Copy16 } from '@carbon/icons-react';
+import Copy16 from '@carbon/icons-react/lib/copy/16';
 import Icon from '../Icon';
+import { componentsX } from '../../internal/FeatureFlags';
 
 const { prefix } = settings;
 
@@ -86,11 +89,19 @@ export default class CopyButton extends Component {
         className={classNames}
         onClick={this.handleClick}
         {...other}>
-        <Icon
-          className={`${prefix}--snippet__icon`}
-          icon={iconCopy}
-          description={iconDescription}
-        />
+        {componentsX ? (
+          <Copy16
+            className={`${prefix}--snippet__icon`}
+            aria-label={iconDescription}
+            alt={iconDescription}
+          />
+        ) : (
+          <Icon
+            className={`${prefix}--snippet__icon`}
+            icon={iconCopy}
+            description={iconDescription}
+          />
+        )}
         <div className={feedbackClassNames} data-feedback={feedback} />
       </button>
     );
