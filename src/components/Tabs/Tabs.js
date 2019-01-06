17fb3dde47b600492e6d05e23521394aa95df98d
Zack Grossbart <zack@grossbart.com>
2019-01-06 16:56:17 -0500

    fix: Set a tab index on the anchors for the tab components so it won'… (#1701)
    * fix: Set a tab index on the anchors for the tab components so it won't be zero, resolves #1548.
    
    * fix: We are now setting the correct tab index, resolves 1548
    
    * fix: we are now setting the tab index only in the tab panel case, resolves 1701.
    
    * fix: Restructured the code a little to make the linter happy, resolves 1701.

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -185,12 +185,27 @@ export default class Tabs extends React.Component {
       ...other
     } = this.props;
 
+    /**
+     * The tab panel acts like a tab panel when the screen is wider, but acts
+     * like a select list when the screen is narrow.  In the wide case we want
+     * to allow the user to use the tab key to set the focus in the tab panel
+     * and then use the left and right arrow keys to navigate the tabs.  In the
+     * narrow case we want to use the tab key to select different options in
+     * the list.
+     *
+     * We set the tab index based on the different states so the browser will treat
+     * the whole tab panel as a single focus component when it looks like a tab
+     * panel and separate components when it looks like a select list.
+     */
     const tabsWithProps = this.getTabs().map((tab, index) => {
+      const tabPanelIndex = index === this.state.selected ? 0 : -1;
+      const tabIndex = !this.state.dropdownHidden ? 0 : tabPanelIndex;
       const newTab = React.cloneElement(tab, {
         index,
         selected: index === this.state.selected,
         handleTabClick: this.handleTabClick(onSelectionChange),
         handleTabAnchorFocus: this.handleTabAnchorFocus(onSelectionChange),
+        tabIndex,
         ref: e => {
           this.setTabAt(index, e);
         },
