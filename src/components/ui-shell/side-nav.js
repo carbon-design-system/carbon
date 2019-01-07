969930f01fc11469536eb49314eff55c71861843
emyarod <emyarod@users.noreply.github.com>
2019-01-07 10:20:32 -0600

    feat(ui-shell): add side nav component (#1527)
    * feat(ui-shell): add side nav component
    
    * feat(ui-shell): use carbon-icons partials to insert svg
    
    * refactor(ui-shell): remove data attributes from side nav SVG icons
    
    * chore: upgrade karma-jasmine
    chore: update lockfile

--- src/components/ui-shell/side-nav.js
+++ src/components/ui-shell/side-nav.js
@@ -1,14 +1,80 @@
 import mixin from '../../globals/js/misc/mixin';
 import createComponent from '../../globals/js/mixins/create-component';
+import settings from '../../globals/js/settings';
 import initComponentBySearch from '../../globals/js/mixins/init-component-by-search';
+import handles from '../../globals/js/mixins/handles';
+import on from '../../globals/js/misc/on';
+import eventMatches from '../../globals/js/misc/event-matches';
 
-class SideNav extends mixin(createComponent, initComponentBySearch) {
+const { prefix } = settings;
+
+export default class SideNav extends mixin(createComponent, initComponentBySearch, handles) {
   /**
    * The map associating DOM element and copy button UI instance.
    * @member SideNav.components
    * @type {WeakMap}
    */
   static components /* #__PURE_CLASS_PROPERTY__ */ = new WeakMap();
+  constructor(element, options) {
+    super(element, options);
+    this.manage(on(element, 'click', this._handleClick));
+  }
+
+  /**
+   * Enum for toggling side nav visibility
+   * @readonly
+   * @member SideNav.state
+   * @type {Object}
+   * @property {string} EXPANDED Opening/visible
+   * @property {string} COLLAPSED Closing/hidden
+   */
+  static state = {
+    EXPANDED: 'expanded',
+    COLLAPSED: 'collapsed',
+  };
+
+  /**
+   * @returns {boolean} `true` if the nav is expanded.
+   */
+  isNavExpanded() {
+    return this.element.classList.contains(this.options.classSideNavExpanded);
+  }
+
+  /**
+   * Changes the expanded/collapsed state.
+   */
+  changeState(state) {
+    this.element.classList.toggle(this.options.classSideNavExpanded, state === this.constructor.state.EXPANDED);
+  }
+
+  _handleClick = evt => {
+    const matchesToggle = eventMatches(evt, this.options.selectorSideNavToggle);
+    const matchesNavSubmenu = eventMatches(evt, this.options.selectorSideNavSubmenu);
+    const matchesSideNavLink = eventMatches(evt, this.options.selectorSideNavLink);
+    if (!matchesToggle && !matchesNavSubmenu && !matchesSideNavLink) {
+      return;
+    }
+    if (matchesToggle) {
+      this.changeState(!this.isNavExpanded() ? this.constructor.state.EXPANDED : this.constructor.state.COLLAPSED);
+      return;
+    }
+    if (matchesNavSubmenu) {
+      const isSubmenuExpanded = matchesNavSubmenu.getAttribute('aria-expanded') === 'true';
+      matchesNavSubmenu.setAttribute('aria-expanded', `${!isSubmenuExpanded}`);
+      return;
+    }
+    if (matchesSideNavLink) {
+      [...this.element.querySelectorAll(this.options.selectorSideNavLinkCurrent)].forEach(el => {
+        el.classList.remove(this.options.classSideNavItemActive, this.options.classSideNavLinkCurrent);
+        el.removeAttribute('aria-current');
+      });
+      matchesSideNavLink.classList.add(this.options.classSideNavLinkCurrent);
+      const closestSideNavItem = matchesSideNavLink.closest(this.options.selectorSideNavItem);
+      if (closestSideNavItem) {
+        closestSideNavItem.classList.add(this.options.classSideNavItemActive);
+      }
+    }
+  };
 
   /**
    * The component options.
@@ -20,7 +86,13 @@ class SideNav extends mixin(createComponent, initComponentBySearch) {
    */
   static options /* #__PURE_CLASS_PROPERTY__ */ = {
     selectorInit: '[data-side-nav]',
+    selectorSideNavToggle: `.${prefix}--side-nav__toggle`,
+    selectorSideNavSubmenu: `.${prefix}--side-nav__submenu`,
+    selectorSideNavItem: `.${prefix}--side-nav__item`,
+    selectorSideNavLink: `.${prefix}--side-nav__link`,
+    selectorSideNavLinkCurrent: `[aria-current="page"],.${prefix}--side-nav__link--current,.${prefix}--side-nav__item--active`,
+    classSideNavExpanded: `${prefix}--side-nav--expanded`,
+    classSideNavItemActive: `${prefix}--side-nav__item--active`,
+    classSideNavLinkCurrent: `${prefix}--side-nav__link--current`,
   };
 }
-
-export default SideNav;
