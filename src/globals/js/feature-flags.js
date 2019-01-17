e4da4a57542a42f7c6e0a90e91aac9b7e0fcaf02
emyarod <emyarod@users.noreply.github.com>
2019-01-17 15:30:29 -0600

    chore: add copyright headers (#1641)

--- src/globals/js/feature-flags.js
+++ src/globals/js/feature-flags.js
@@ -1,4 +1,11 @@
 /**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
  * This file contains the list of the default values of compile-time feature flags.
  *
  * Build toolchain can replace variable here and/or the references
