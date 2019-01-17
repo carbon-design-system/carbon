93e94b2783bb4e7817df8bfe15a3a02a4da25739
emyarod <afw5059@gmail.com>
2019-01-17 13:58:10 -0600

    chore: add copyright headers

--- src/globals/js/misc/resize.js
+++ src/globals/js/misc/resize.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // mdn resize function
 
 const optimizedResize = /* #__PURE__ */ (function optimizedResize() {
