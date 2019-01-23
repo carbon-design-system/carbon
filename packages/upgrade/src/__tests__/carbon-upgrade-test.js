47ae3932896be9692ec4d0e27aa04acd36dfc9b9
Josh Black <josh@josh.black>
2019-01-22 18:26:13 -0600

    chore(project): setup project files (#1)
    * chore(project): update LICENSE
    
    * chore(project): setup project files
    
    * chore(project): add github files

---
+++ packages/upgrade/src/__tests__/carbon-upgrade-test.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright IBM Corp. 2019, 2019
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @jest-environment node
+ */
+
+'use strict';
+
+test('should work', () => {
+  // ...
+});
---
+++ packages/upgrade/src/index.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright IBM Corp. 2019, 2019
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+async function main() {
+  console.log('Hello world');
+}
+
+module.exports = main;
