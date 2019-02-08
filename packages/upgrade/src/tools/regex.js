60f1803405d36eb9f8c1617c0392c6fec257339e
Josh Black <josh@josh.black>
2019-02-07 18:59:40 -0600

    feat(regex): add mixin regex support

--- packages/upgrade/src/tools/regex.js
+++ packages/upgrade/src/tools/regex.js
@@ -23,11 +23,28 @@ function createFunctionRegex(name) {
   return new RegExp(parts.join(''), 'gm');
 }
 
+function createMixinRegex(name) {
+  const parts = [
+    // Make sure there is a space before the group, useful for things that might
+    // intersect
+    '(?<=[ \\(])',
+    // Positive lookahead for the function definition
+    // Support one-line, `my-function()` and multi-line `my-function(\n`
+    `(?=${name}\\(.*[\)\n])?`,
+    // Negative lookahead for checking if already migrated
+    '(?<!carbon--)',
+    // Capture the name of the function itself to change
+    `(${name})`,
+  ];
+  return new RegExp(parts.join(''), 'gm');
+}
+
 function createVariableRegex(name) {
   return new RegExp(`\\$${name}`, 'gm');
 }
 
 module.exports = {
   createFunctionRegex,
+  createMixinRegex,
   createVariableRegex,
 };
