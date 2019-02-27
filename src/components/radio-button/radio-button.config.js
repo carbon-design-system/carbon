8a3027d60200647f87eb3214297e06199950880d
Akira Sudoh <asudoh@gmail.com>
2019-02-27 11:51:48 +0900

    fix(radio-button): support combinations of group/label
    The earlier commit (#1722) had styling problems with:
    
    * Label positioned at left
    * `.bx--radio-button-wrapper` used with the vertical radio group
      feature we introduced in `v10`
    
    Also:
    
    * Introduced a corresponding set of style definitions for `v10`
    * Given `v9` doesn't have a support for vertical radio group, added
      a change to hide vertical radio group in `v9` dev env
    
    Refs #1721.

--- src/components/radio-button/radio-button.config.js
+++ src/components/radio-button/radio-button.config.js
@@ -43,9 +43,84 @@ module.exports = {
       },
     },
     {
+      name: 'horizontal-bottom',
+      label: 'Radio button group (Label at bottom)',
+      context: {
+        direction: 'bottom',
+        selectedValue: 'red',
+        group: 'radio-button',
+        items,
+      },
+    },
+    {
+      name: 'horizontal-left',
+      label: 'Radio button group (Label at left)',
+      context: {
+        direction: 'left',
+        selectedValue: 'red',
+        group: 'radio-button',
+        items,
+      },
+    },
+    {
+      name: 'horizontal-top',
+      label: 'Radio button group (Label at top)',
+      context: {
+        direction: 'top',
+        selectedValue: 'red',
+        group: 'radio-button',
+        items,
+      },
+    },
+    {
       name: 'vertical',
       label: 'Vertical radio button group',
+      meta: {
+        xVersionOnly: true,
+      },
       context: {
+        selectedValue: 'red',
+        group: 'radio-button--vertical',
+        vertical: true,
+        items,
+      },
+    },
+    {
+      name: 'vertical-bottom',
+      label: 'Vertical radio button group (Label at bottom)',
+      meta: {
+        xVersionOnly: true,
+      },
+      context: {
+        direction: 'bottom',
+        selectedValue: 'red',
+        group: 'radio-button--vertical',
+        vertical: true,
+        items,
+      },
+    },
+    {
+      name: 'vertical-left',
+      label: 'Vertical radio button group (Label at left)',
+      meta: {
+        xVersionOnly: true,
+      },
+      context: {
+        direction: 'left',
+        selectedValue: 'red',
+        group: 'radio-button--vertical',
+        vertical: true,
+        items,
+      },
+    },
+    {
+      name: 'vertical-top',
+      label: 'Vertical radio button group (Label at top)',
+      meta: {
+        xVersionOnly: true,
+      },
+      context: {
+        direction: 'top',
         selectedValue: 'red',
         group: 'radio-button--vertical',
         vertical: true,
