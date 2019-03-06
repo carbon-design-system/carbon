174eb5e87ca436afcf34bcb3e65509d1d9035217
Alessandra Davila <aledavila@ibm.com>
2019-03-06 13:10:39 -0600

    chore(radio-button): remove if statement from variant use meta data

--- src/components/radio-button/radio-button.config.js
+++ src/components/radio-button/radio-button.config.js
@@ -8,7 +8,6 @@
 'use strict';
 
 const { prefix } = require('../../globals/js/settings');
-const { componentsX } = require('../../globals/js/feature-flags');
 
 const items = () => {
   const groupId = Math.random()
@@ -38,94 +37,78 @@ module.exports = {
   context: {
     prefix,
   },
-  variants: !componentsX
-    ? [
-        {
-          name: 'default',
-          label: 'Radio button group',
-          context: {
-            selectedValue: 'red',
-            group: 'radio-button',
-            items: items(),
-          },
-        },
-        {
-          name: 'horizontal-bottom',
-          label: 'Radio button group (Label at bottom)',
-          context: {
-            direction: 'bottom',
-            selectedValue: 'red',
-            group: 'radio-button',
-            items: items(),
-          },
-        },
-        {
-          name: 'horizontal-left',
-          label: 'Radio button group (Label at left)',
-          context: {
-            direction: 'left',
-            selectedValue: 'red',
-            group: 'radio-button',
-            items: items(),
-          },
-        },
-        {
-          name: 'horizontal-top',
-          label: 'Radio button group (Label at top)',
-          context: {
-            direction: 'top',
-            selectedValue: 'red',
-            group: 'radio-button',
-            items: items(),
-          },
-        },
-      ]
-    : [
-        {
-          name: 'default',
-          label: 'Radio button group',
-          context: {
-            selectedValue: 'red',
-            group: 'radio-button',
-            items: items(),
-          },
-        },
-        {
-          name: 'horizontal-left',
-          label: 'Radio button group (Label at left)',
-          context: {
-            direction: 'left',
-            selectedValue: 'red',
-            group: 'radio-button',
-            items: items(),
-          },
-        },
-        {
-          name: 'vertical',
-          label: 'Vertical radio button group',
-          meta: {
-            xVersionOnly: true,
-          },
-          context: {
-            selectedValue: 'red',
-            group: 'radio-button--vertical',
-            vertical: true,
-            items: items(),
-          },
-        },
-        {
-          name: 'vertical-left',
-          label: 'Vertical radio button group (Label at left)',
-          meta: {
-            xVersionOnly: true,
-          },
-          context: {
-            direction: 'left',
-            selectedValue: 'red',
-            group: 'radio-button--vertical',
-            vertical: true,
-            items: items(),
-          },
-        },
-      ],
+  variants: [
+    {
+      name: 'default',
+      label: 'Radio button group',
+      context: {
+        selectedValue: 'red',
+        group: 'radio-button',
+        items: items(),
+      },
+    },
+    {
+      name: 'horizontal-bottom',
+      label: 'Radio button group (Label at bottom)',
+      meta: {
+        xVersionNotSupported: true,
+      },
+      context: {
+        direction: 'bottom',
+        selectedValue: 'red',
+        group: 'radio-button',
+        items: items(),
+      },
+    },
+    {
+      name: 'horizontal-left',
+      label: 'Radio button group (Label at left)',
+      context: {
+        direction: 'left',
+        selectedValue: 'red',
+        group: 'radio-button',
+        items: items(),
+      },
+    },
+    {
+      name: 'horizontal-top',
+      label: 'Radio button group (Label at top)',
+      meta: {
+        xVersionNotSupported: true,
+      },
+      context: {
+        direction: 'top',
+        selectedValue: 'red',
+        group: 'radio-button',
+        items: items(),
+      },
+    },
+    {
+      name: 'vertical',
+      label: 'Vertical radio button group',
+      meta: {
+        xVersionOnly: true,
+      },
+      context: {
+        selectedValue: 'red',
+        group: 'radio-button--vertical',
+        vertical: true,
+        items: items(),
+      },
+    },
+    {
+      name: 'vertical-left',
+      label: 'Vertical radio button group (Label at left)',
+      meta: {
+        xVersionOnly: true,
+      },
+      context: {
+        direction: 'left',
+        selectedValue: 'red',
+        group: 'radio-button--vertical',
+        vertical: true,
+        items: items(),
+      },
+    },
+  ],
 };
