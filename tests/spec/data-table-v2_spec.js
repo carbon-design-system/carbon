47c1cadfd2208cd1dcfd45c8f1c338ce91b746f9
Josefina Mancilla <josefinanoemi4@gmail.com>
2019-03-18 11:39:45 -0500

    feat: add v10 search js tests

--- tests/spec/data-table-v2_spec.js
+++ tests/spec/data-table-v2_spec.js
@@ -1,9 +1,11 @@
+import { componentsX } from '../../src/globals/js/feature-flags';
 import EventManager from '../utils/event-manager';
+import flattenOptions from '../utils/flatten-options';
 import DataTableV2 from '../../src/components/data-table-v2/data-table-v2';
 import HTML from '../../html/data-table-v2/data-table-v2.html';
 import ExpandableHTML from '../../html/data-table-v2/data-table-v2--expandable.html';
 
-describe('Dropdown', function() {
+describe('DataTableV2', function() {
   describe('Constructor', function() {
     it('Should throw if root element is not given', function() {
       expect(() => {
@@ -15,6 +17,48 @@ describe('Dropdown', function() {
       expect(() => {
         new DataTableV2(document.createTextNode(''));
       }).toThrowError(TypeError, 'DOM element should be given to initialize this widget.');
+    });
+
+    it('Should set default options', function() {
+      const table = new DataTableV2(document.createElement('div'));
+      expect(flattenOptions(table.options)).toEqual({
+        selectorInit: '[data-table-v2]',
+        selectorToolbar: '.bx--table--toolbar',
+        selectorActions: '.bx--batch-actions',
+        selectorCount: '[data-items-selected]',
+        selectorActionCancel: '.bx--batch-summary__cancel',
+        selectorCheckbox: '.bx--checkbox',
+        selectorExpandCells: '.bx--table-expand-v2',
+        selectorExpandableRows: '.bx--expandable-row-v2',
+        selectorParentRows: '.bx--parent-row-v2',
+        selectorChildRow: '[data-child-row]',
+        selectorTableStickyHeader: '.bx--data-table-v2--sticky-header',
+        selectorTableBody: 'tbody',
+        selectorTableThead: 'thead',
+        selectorTableSort: '.bx--table-sort-v2',
+        selectorTableSelected: '.bx--data-table-v2--selected',
+        selectorToolbarSearchContainer: '.bx--toolbar-search-container-hidden',
+        selectorTableTruncated: '.bx--data-table-v2--overflow-truncate',
+        selectorHeaderLabels: '.bx--table-header-label',
+        selectorTableCellContent: '.bx--table-cell-content',
+        selectorSearchMagnifier: '.bx--search-magnifier',
+        selectorSearchInput: '.bx--search-input',
+        classExpandableRow: 'bx--expandable-row-v2',
+        classExpandableRowHidden: 'bx--expandable-row--hidden-v2',
+        classExpandableRowHover: 'bx--expandable-row--hover-v2',
+        classTableSortAscending: 'bx--table-sort-v2--ascending',
+        classTableSortActive: 'bx--table-sort-v2--active',
+        classActionBarActive: 'bx--batch-actions--active',
+        classTableSelected: 'bx--data-table-v2--selected',
+        classToolbarSearchActive: 'bx--toolbar-search-container-active',
+        classTooltipActive: 'bx--data-table-v2--truncated',
+        eventBeforeExpand: 'data-table-v2-beforetoggleexpand',
+        eventAfterExpand: 'data-table-v2-aftertoggleexpand',
+        eventBeforeSort: 'data-table-v2-beforetogglesort',
+        eventAfterSort: 'data-table-v2-aftertogglesort',
+        eventTrigger: '[data-event]',
+        eventParentContainer: '[data-parent-row]',
+      });
     });
   });
 
@@ -175,6 +219,76 @@ describe('Dropdown', function() {
     afterAll(function() {
       document.body.removeChild(container);
       table.release();
+    });
+  });
+
+  describe('Toggle active search bar', function() {
+    let table;
+    let container;
+    let dt;
+
+    beforeEach(function() {
+      /* istanbul ignore if */
+      if (componentsX) {
+        container = document.createElement('div');
+        container.innerHTML = HTML;
+        document.body.appendChild(container);
+        dt = document.querySelector('.bx--data-table-v2');
+        table = new DataTableV2(container);
+      }
+    });
+
+    it('Should open search bar on click', function() {
+      /* istanbul ignore if */
+      if (componentsX) {
+        const search = document.querySelector('.bx--toolbar-search-container-hidden');
+        const magnifier = document.querySelector('.bx--search-magnifier');
+        magnifier.dispatchEvent(new CustomEvent('click', { bubbles: true }));
+        expect(search.classList.contains('bx--toolbar-search-container-active')).toBe(true);
+      }
+    });
+
+    it('Should close search bar on click', function() {
+      /* istanbul ignore if */
+      if (componentsX) {
+        const search = document.querySelector('.bx--toolbar-search-container-hidden');
+        search.classList.add('bx--toolbar-search-container-active');
+        dt.dispatchEvent(new CustomEvent('click', { bubbles: true }));
+        expect(search.classList.contains('bx--toolbar-search-container-active')).toBe(false);
+      }
+    });
+
+    it('Should open search bar on keydown', function() {
+      /* istanbul ignore if */
+      if (componentsX) {
+        const search = document.querySelector('.bx--toolbar-search-container-hidden');
+        const magnifier = document.querySelector('.bx--search-magnifier');
+        const event = new CustomEvent('keydown', { bubbles: true });
+        event.which = 13;
+        magnifier.dispatchEvent(event);
+        expect(search.classList.contains('bx--toolbar-search-container-active')).toBe(true);
+      }
+    });
+
+    it('Should close search bar on keydown', function() {
+      /* istanbul ignore if */
+      if (componentsX) {
+        const search = document.querySelector('.bx--toolbar-search-container-hidden');
+        const btn = document.querySelector('.bx--btn');
+        search.classList.add('bx--toolbar-search-container-active');
+        const event = new CustomEvent('keydown', { bubbles: true });
+        event.which = 27;
+        btn.dispatchEvent(event);
+        expect(search.classList.contains('bx--toolbar-search-container-active')).toBe(false);
+      }
+    });
+
+    afterEach(function() {
+      /* istanbul ignore if */
+      if (componentsX) {
+        document.body.removeChild(container);
+        table.release();
+      }
     });
   });
 });
