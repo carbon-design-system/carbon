9b9d181efc4e1afa5f2da79836d61818aec874b9
Josefina Mancilla <josefinanoemi4@gmail.com>
2019-03-19 10:36:55 -0500

    fix: tests

--- src/components/data-table-v2/data-table-v2.config.js
+++ src/components/data-table-v2/data-table-v2.config.js
@@ -10,11 +10,11 @@
 const { prefix } = require('../../globals/js/settings');
 const { componentsX } = require('../../globals/js/feature-flags');
 
-// const iconAddSolid = `
-//   <svg class="${prefix}--btn__icon" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-//     <path d="M7 7H4v2h3v3h2V9h3V7H9V4H7v3zm1 9A8 8 0 1 1 8 0a8 8 0 0 1 0 16z" fill-rule="evenodd" />
-//   </svg>
-// `;
+const iconAddSolid = `
+  <svg class="${prefix}--btn__icon" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7 7H4v2h3v3h2V9h3V7H9V4H7v3zm1 9A8 8 0 1 1 8 0a8 8 0 0 1 0 16z" fill-rule="evenodd" />
+  </svg>
+`;
 
 const iconDownload = `
   <svg class="${prefix}--toolbar-action__icon"
@@ -147,20 +147,20 @@ const menuItems = componentsX
       },
     ];
 
-// const batchActions = [
-//   {
-//     label: 'Ghost',
-//     icon: iconAddSolid,
-//   },
-//   {
-//     label: 'Ghost',
-//     icon: iconAddSolid,
-//   },
-//   {
-//     label: 'Ghost',
-//     icon: iconAddSolid,
-//   },
-// ];
+const batchActions = [
+  {
+    label: 'Ghost',
+    icon: iconAddSolid,
+  },
+  {
+    label: 'Ghost',
+    icon: iconAddSolid,
+  },
+  {
+    label: 'Ghost',
+    icon: iconAddSolid,
+  },
+];
 
 const toolbarActions = [
   {
@@ -200,44 +200,44 @@ const toolbarActionsXDisabled = [
 ];
 
 const columns = [
-  // {
-  //   name: 'select',
-  //   title: 'Label name',
-  //   checkbox: true,
-  //   checkboxId: `${prefix}--checkbox-20`,
-  //   checkboxName: 'checkbox-20',
-  //   checkboxValue: 'green',
-  // },
+  {
+    name: 'select',
+    title: 'Label name',
+    checkbox: true,
+    checkboxId: `${prefix}--checkbox-20`,
+    checkboxName: 'checkbox-20',
+    checkboxValue: 'green',
+  },
   {
     name: 'name',
     title: 'Name',
-    // sortable: true,
+    sortable: true,
   },
   {
     name: 'protocol',
     title: 'Protocol',
-    // sortable: true,
+    sortable: true,
   },
   {
     name: 'port',
     title: 'Port',
-    // sortable: true,
+    sortable: true,
     numerical: true,
   },
   {
     name: 'rule',
     title: 'Rule',
-    // sortable: true,
+    sortable: true,
   },
   {
     name: 'attachedGroups',
     title: 'Attached Groups',
-    // sortable: true,
+    sortable: true,
   },
   {
     name: 'status',
     title: 'Status',
-    // sortable: true,
+    sortable: true,
   },
   {
     name: 'menu',
@@ -245,63 +245,63 @@ const columns = [
   },
 ];
 
-// const columnsExpandable = [
-//   {
-//     name: 'section',
-//     section: true,
-//   },
-//   {
-//     name: 'name',
-//     title: 'Name',
-//     sortable: true,
-//   },
-//   {
-//     name: 'protocol',
-//     title: 'Protocol',
-//     sortable: true,
-//   },
-//   {
-//     name: 'port',
-//     title: 'Ports',
-//     sortable: true,
-//   },
-//   {
-//     name: 'rule',
-//     title: 'Rule',
-//     sortable: true,
-//   },
-//   {
-//     name: 'attachedGroups',
-//     title: 'Attached Groups',
-//     sortable: true,
-//   },
-//   {
-//     name: 'status',
-//     title: 'Status',
-//     sortable: true,
-//   },
-// ];
+const columnsExpandable = [
+  {
+    name: 'section',
+    section: true,
+  },
+  {
+    name: 'name',
+    title: 'Name',
+    sortable: true,
+  },
+  {
+    name: 'protocol',
+    title: 'Protocol',
+    sortable: true,
+  },
+  {
+    name: 'port',
+    title: 'Ports',
+    sortable: true,
+  },
+  {
+    name: 'rule',
+    title: 'Rule',
+    sortable: true,
+  },
+  {
+    name: 'attachedGroups',
+    title: 'Attached Groups',
+    sortable: true,
+  },
+  {
+    name: 'status',
+    title: 'Status',
+    sortable: true,
+  },
+];
 
-// const columnsEditable = columns.slice(1, 7).map((column, i) => ({
-//   ...column,
-//   editable: true,
-//   editing: i === 0,
-// }));
+const columnsEditable = columns.slice(1, 7).map((column, i) => ({
+  ...column,
+  editable: true,
+  editing: i === 0,
+}));
 
-// const columnsSmall = columns.slice(1, -1).map(column => ({
-//   ...column,
-//   sortable: false,
-// }));
+const columnsSmall = columns.slice(1, -1).map(column => ({
+  ...column,
+  sortable: false,
+}));
 
 const rows = [
   {
     id: 'row-id-13',
-    // select: {
-    //   id: `${prefix}--checkbox-13`,
-    //   name: 'checkbox-13',
-    //   value: 'green',
-    //   label: 'Label name',
-    // },
+    select: {
+      id: `${prefix}--checkbox-13`,
+      name: 'checkbox-13',
+      value: 'green',
+      label: 'Label name',
+    },
     name: 'Load Balancer 1',
     protocol: 'HTTP',
     port: '80',
@@ -317,12 +317,12 @@ const rows = [
   {
     id: 'row-id-14',
     disabled: true,
-    // select: {
-    //   id: `${prefix}--checkbox-14`,
-    //   name: 'checkbox-14',
-    //   value: 'green',
-    //   label: 'Label name',
-    // },
+    select: {
+      id: `${prefix}--checkbox-14`,
+      name: 'checkbox-14',
+      value: 'green',
+      label: 'Label name',
+    },
     name: 'Load Balancer 5',
     protocol: 'HTTP',
     port: '80',
@@ -337,12 +337,12 @@ const rows = [
   },
   {
     id: 'row-id-15',
-    // select: {
-    //   id: `${prefix}--checkbox-15`,
-    //   name: 'checkbox-15',
-    //   value: 'green',
-    //   label: 'Label name',
-    // },
+    select: {
+      id: `${prefix}--checkbox-15`,
+      name: 'checkbox-15',
+      value: 'green',
+      label: 'Label name',
+    },
     name: 'Load Balancer 5',
     protocol: 'HTTP',
     port: '80',
@@ -357,12 +357,12 @@ const rows = [
   },
   {
     id: 'row-id-11',
-    // select: {
-    //   id: `${prefix}--checkbox-11`,
-    //   name: 'checkbox-11',
-    //   value: 'green',
-    //   label: 'Label name',
-    // },
+    select: {
+      id: `${prefix}--checkbox-11`,
+      name: 'checkbox-11',
+      value: 'green',
+      label: 'Label name',
+    },
     name: 'Load Balancer 5',
     protocol: 'HTTP',
     port: '80',
@@ -377,12 +377,12 @@ const rows = [
   },
   {
     id: 'row-id-12',
-    // select: {
-    //   id: `${prefix}--checkbox-12`,
-    //   name: 'checkbox-12',
-    //   value: 'green',
-    //   label: 'Label name',
-    // },
+    select: {
+      id: `${prefix}--checkbox-12`,
+      name: 'checkbox-12',
+      value: 'green',
+      label: 'Label name',
+    },
     name: 'Load Balancer 5',
     protocol: 'HTTP',
     port: '80',
@@ -397,79 +397,79 @@ const rows = [
   },
 ];
 
-// const rowsExpandable = [
-//   {
-//     sectionContent: `
-//       <h4>
-//         <strong>Harry Potter</strong>
-//       </h4>
-//       <p>Harry James Potter (b. 31 July, 1980) was a half-blood wizard, the only child and son of the late James and Lily
-//         Potter (née Evans), and one of the most famous and powerful wizards of modern times. In what proved to be a vain
-//         attempt to circumvent a prophecy that stated that a boy born at the end of July of 1980 could be able to defeat
-//         him, Lord Voldemort tried to murder him when he was a year and three months old. Voldemort murdered Harry's parents
-//         as they tried to protect him, shortly before attacking Harry.</p>
-//     `,
-//     section: true,
-//     name: 'Load Balancer 1',
-//     protocol: 'HTTP',
-//     port: '80',
-//     rule: 'Round Robin',
-//     attachedGroups: "Maureen's VM Groups",
-//     status: 'Active',
-//   },
-//   {
-//     sectionContent: `
-//       <h4>
-//         <strong>Harry Potter</strong>
-//       </h4>
-//       <p>Harry James Potter (b. 31 July, 1980) was a half-blood wizard, the only child and son of the late James and Lily
-//         Potter (née Evans), and one of the most famous and powerful wizards of modern times. In what proved to be a vain
-//         attempt to circumvent a prophecy that stated that a boy born at the end of July of 1980 could be able to defeat
-//         him, Lord Voldemort tried to murder him when he was a year and three months old. Voldemort murdered Harry's parents
-//         as they tried to protect him, shortly before attacking Harry.</p>
-//     `,
-//     section: true,
-//     name: 'Load Balancer 1',
-//     protocol: 'HTTP',
-//     port: '80',
-//     rule: 'Round Robin',
-//     attachedGroups: "Maureen's VM Groups",
-//     status: 'Active',
-//   },
-// ];
+const rowsExpandable = [
+  {
+    sectionContent: `
+      <h4>
+        <strong>Harry Potter</strong>
+      </h4>
+      <p>Harry James Potter (b. 31 July, 1980) was a half-blood wizard, the only child and son of the late James and Lily
+        Potter (née Evans), and one of the most famous and powerful wizards of modern times. In what proved to be a vain
+        attempt to circumvent a prophecy that stated that a boy born at the end of July of 1980 could be able to defeat
+        him, Lord Voldemort tried to murder him when he was a year and three months old. Voldemort murdered Harry's parents
+        as they tried to protect him, shortly before attacking Harry.</p>
+    `,
+    section: true,
+    name: 'Load Balancer 1',
+    protocol: 'HTTP',
+    port: '80',
+    rule: 'Round Robin',
+    attachedGroups: "Maureen's VM Groups",
+    status: 'Active',
+  },
+  {
+    sectionContent: `
+      <h4>
+        <strong>Harry Potter</strong>
+      </h4>
+      <p>Harry James Potter (b. 31 July, 1980) was a half-blood wizard, the only child and son of the late James and Lily
+        Potter (née Evans), and one of the most famous and powerful wizards of modern times. In what proved to be a vain
+        attempt to circumvent a prophecy that stated that a boy born at the end of July of 1980 could be able to defeat
+        him, Lord Voldemort tried to murder him when he was a year and three months old. Voldemort murdered Harry's parents
+        as they tried to protect him, shortly before attacking Harry.</p>
+    `,
+    section: true,
+    name: 'Load Balancer 1',
+    protocol: 'HTTP',
+    port: '80',
+    rule: 'Round Robin',
+    attachedGroups: "Maureen's VM Groups",
+    status: 'Active',
+  },
+];
 
-// const rowsEditable = [
-//   {
-//     id: 'row-id-15',
-//     name: 'Load Balancer 3',
-//     protocol: 'HTTP',
-//     port: '3000',
-//     rule: 'Round Robin',
-//     attachedGroups: 'Kevins VM Groups',
-//     status: 'Disabled',
-//   },
-//   {
-//     id: 'row-id-11',
-//     name: 'Load Balancer 1',
-//     protocol: 'HTTP',
-//     port: '443',
-//     rule: 'Round Robin',
-//     attachedGroups: 'Maureens VM Groups',
-//     status: 'Starting',
-//   },
-//   {
-//     id: 'row-id-10',
-//     name: 'Load Balancer 2',
-//     protocol: 'HTTP',
-//     port: '80',
-//     rule: 'DNS delegation',
-//     attachedGroups: 'Andrews VM Groups',
-//     status: 'Active',
-//   },
-// ].map((row, i) => ({
-//   ...row,
-//   editable: i === 0,
-// }));
+const rowsEditable = [
+  {
+    id: 'row-id-15',
+    name: 'Load Balancer 3',
+    protocol: 'HTTP',
+    port: '3000',
+    rule: 'Round Robin',
+    attachedGroups: 'Kevins VM Groups',
+    status: 'Disabled',
+  },
+  {
+    id: 'row-id-11',
+    name: 'Load Balancer 1',
+    protocol: 'HTTP',
+    port: '443',
+    rule: 'Round Robin',
+    attachedGroups: 'Maureens VM Groups',
+    status: 'Starting',
+  },
+  {
+    id: 'row-id-10',
+    name: 'Load Balancer 2',
+    protocol: 'HTTP',
+    port: '80',
+    rule: 'DNS delegation',
+    attachedGroups: 'Andrews VM Groups',
+    status: 'Active',
+  },
+].map((row, i) => ({
+  ...row,
+  editable: i === 0,
+}));
 
 module.exports = {
   label: 'Data Table V2',
@@ -508,45 +508,45 @@ module.exports = {
         noSort: true,
       },
     },
-    // {
-    //   name: 'expandable',
-    //   label: 'Expandable',
-    //   context: {
-    //     title: 'Table title',
-    //     state: 'default',
-    //     toolbarActions,
-    //     toolbarActionsX,
-    //     columns: columnsExpandable,
-    //     rows: rowsExpandable,
-    //     searchInputId: 'search__input-2',
-    //     searchLabelId: 'search-input-label-1',
-    //     searchLabel: 'Search',
-    //     clearSearchLabel: 'Clear search input',
-    //     hasToolbar: true,
-    //   },
-    // },
-    // {
-    //   name: 'editable',
-    //   label: 'Inline Edit',
-    //   context: {
-    //     title: 'Table title',
-    //     columns: columnsEditable,
-    //     rows: rowsEditable,
-    //     cancelLabel: 'Cancel',
-    //     saveLabel: 'Save',
-    //     sortLabel: 'Sort rows by this header in descending order',
-    //     zebra: true,
-    //   },
-    // },
-    // {
-    //   name: 'small',
-    //   label: 'Small',
-    //   context: {
-    //     small: true,
-    //     columns: columnsSmall,
-    //     rows,
-    //   },
-    // },
+    {
+      name: 'expandable',
+      label: 'Expandable',
+      context: {
+        title: 'Table title',
+        state: 'default',
+        toolbarActions,
+        toolbarActionsX,
+        columns: columnsExpandable,
+        rows: rowsExpandable,
+        searchInputId: 'search__input-2',
+        searchLabelId: 'search-input-label-1',
+        searchLabel: 'Search',
+        clearSearchLabel: 'Clear search input',
+        hasToolbar: true,
+      },
+    },
+    {
+      name: 'editable',
+      label: 'Inline Edit',
+      context: {
+        title: 'Table title',
+        columns: columnsEditable,
+        rows: rowsEditable,
+        cancelLabel: 'Cancel',
+        saveLabel: 'Save',
+        sortLabel: 'Sort rows by this header in descending order',
+        zebra: true,
+      },
+    },
+    {
+      name: 'small',
+      label: 'Small',
+      context: {
+        small: true,
+        columns: columnsSmall,
+        rows,
+      },
+    },
     {
       name: 'with-pager',
       label: 'Pagination',
@@ -555,7 +555,7 @@ module.exports = {
         title: 'Table title',
         state: 'persistent-search',
         toolbarActionsX,
-        // batchActions,
+        batchActions,
         toolbarActions,
         columns,
         rows,
