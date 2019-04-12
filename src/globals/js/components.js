5f9024cc337e66a76f208fc8686baca1b36c4058
Josh Black <josh@josh.black>
2019-04-12 13:01:34 -0500

    feat(data-table): add data-table paths and aliases (#2292)
    * feat(data-table): add data-table paths and aliases
    
    * chore(data-table): rename to v2, add hidden config for v2 alias

--- src/globals/js/components.js
+++ src/globals/js/components.js
@@ -16,6 +16,7 @@ export { default as InlineLoading } from '../../components/inline-loading/inline
 export { default as Dropdown } from '../../components/dropdown/dropdown';
 export { default as NumberInput } from '../../components/number-input/number-input';
 export { default as DataTableV2 } from '../../components/data-table-v2/data-table-v2';
+export { default as DataTable } from '../../components/data-table/data-table';
 export { default as DatePicker } from '../../components/date-picker/date-picker';
 export { default as Pagination } from '../../components/pagination/pagination';
 export { default as Search } from '../../components/search/search';
