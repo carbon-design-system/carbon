f94797cbf896d0dc7f8ba35c2dfd1873afb9fd94
Josh Black <josh@josh.black>
2019-04-14 10:17:51 -0500

    chore(project): add clean task (#2215)
    * chore(project): add clean task
    
    * chore(yarn): sync yarn.lock file

--- scripts/build.js
+++ scripts/build.js
@@ -3,21 +3,17 @@
 const { execSync } = require('child_process');
 const { inInstall } = require('in-publish');
 const path = require('path');
-const rimraf = require('rimraf');
-const { promisify } = require('util');
 
 if (inInstall()) {
   process.exit(0);
 }
 
-const rootDir = path.resolve(__dirname, '../');
 const babelPath = path
   .resolve(__dirname, '../node_modules/.bin/babel')
   .replace(/ /g, '\\ ');
 const rollupPath = path
   .resolve(__dirname, '../node_modules/.bin/rollup')
   .replace(/ /g, '\\ ');
-const rimrafAsync = promisify(rimraf);
 
 const exec = (command, extraEnv) =>
   execSync(command, {
@@ -29,34 +25,26 @@ const ignoreGlobs = ['**/__tests__/*', '**/*-test.js', '**/*-story.js'].join(
   ','
 );
 
-console.log('Deleting old build folders...'); // eslint-disable-line no-console
-
-Promise.all([
-  rimrafAsync(`${rootDir}/lib`),
-  rimrafAsync(`${rootDir}/es`),
-  rimrafAsync(`${rootDir}/umd`),
-])
-  .then(() => {
-    exec(`${babelPath} src -q -d es --ignore "${ignoreGlobs}"`, {
-      BABEL_ENV: 'es',
-    });
-    exec(`${babelPath} src -q -d lib --ignore "${ignoreGlobs}"`, {
-      BABEL_ENV: 'cjs',
-    });
-    exec(
-      `${rollupPath} -c scripts/rollup.config.js -o umd/carbon-components-react.js`,
-      {
-        NODE_ENV: 'development',
-      }
-    );
-    exec(
-      `${rollupPath} -c scripts/rollup.config.js -o umd/carbon-components-react.min.js`,
-      {
-        NODE_ENV: 'production',
-      }
-    );
-  })
-  .catch(error => {
-    console.error('One of the commands failed:', error.stack); // eslint-disable-line no-console
-    process.exit(1);
+try {
+  exec(`${babelPath} src -q -d es --ignore "${ignoreGlobs}"`, {
+    BABEL_ENV: 'es',
   });
+  exec(`${babelPath} src -q -d lib --ignore "${ignoreGlobs}"`, {
+    BABEL_ENV: 'cjs',
+  });
+  exec(
+    `${rollupPath} -c scripts/rollup.config.js -o umd/carbon-components-react.js`,
+    {
+      NODE_ENV: 'development',
+    }
+  );
+  exec(
+    `${rollupPath} -c scripts/rollup.config.js -o umd/carbon-components-react.min.js`,
+    {
+      NODE_ENV: 'production',
+    }
+  );
+} catch (error) {
+  console.error('One of the commands failed:', error.stack); // eslint-disable-line no-console
+  process.exit(1);
+}
