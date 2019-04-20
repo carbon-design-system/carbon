2335c441de076f8d45c369ae26e6db64c3473af6
Akira Sudoh <asudoh@gmail.com>
2019-04-19 19:03:30 -0500

    fix(Tabs): avoid stale tab ref cache (#2224)
    The problem happens when `selected` and `children` changes at the same
    time.
    
    Fixes #2204.

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -115,9 +115,10 @@ export default class Tabs extends React.Component {
     return React.Children.map(this.props.children, tab => tab);
   }
 
-  getTabAt = index => {
+  getTabAt = (index, useFresh) => {
     return (
-      this[`tab${index}`] || React.Children.toArray(this.props.children)[index]
+      (!useFresh && this[`tab${index}`]) ||
+      React.Children.toArray(this.props.children)[index]
     );
   };
 
@@ -251,7 +252,7 @@ export default class Tabs extends React.Component {
       }),
     };
 
-    const selectedTab = this.getTabAt(this.state.selected);
+    const selectedTab = this.getTabAt(this.state.selected, true);
     const selectedLabel = selectedTab ? selectedTab.props.label : '';
 
     return (
