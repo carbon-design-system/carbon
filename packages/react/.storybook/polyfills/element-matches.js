58c201f7257d46589c3a291bb46c8e4a2ab12d6d
Josh Black <josh@josh.black>
2019-05-08 13:22:49 -0500

    Revert "refactor(project): add react package (#2417)"
    This reverts commit a94c30586a3da7e99f7a8ba4c2903dcc55b60dbe.

--- packages/react/.storybook/polyfills/element-matches.js
+++
@@ -1,9 +0,0 @@
-const matchesFuncName = [
-  'matches',
-  'webkitMatchesSelector',
-  'msMatchesSelector',
-].filter(name => typeof Element.prototype[name] === 'function')[0];
-
-if (matchesFuncName !== 'matches') {
-  Element.prototype.matches = Element.prototype[matchesFuncName];
-}
--- packages/react/docs/migration/README.md
+++
@@ -1,5 +0,0 @@
-# Migration Guides
-
-Available guides:
-
-- [v6 to v7](./migrate-to-7.x.md)
