b8016ed6bedffcaf5309fff837bf5a1d0f320f5d
Akira Sudoh <asudoh@gmail.com>
2019-05-10 18:22:48 +0900

    feat(tooltip): introduce center/right alignment
    Fixes #2604.

--- packages/react/src/components/TooltipDefinition/TooltipDefinition-story.js
+++ packages/react/src/components/TooltipDefinition/TooltipDefinition-story.js
@@ -16,8 +16,19 @@ const directions = {
   'Top (top)': 'top',
 };
 
+const alignments = {
+  'Left (left)': 'left',
+  'Center (center)': 'center',
+  'Right (right)': 'right',
+};
+
 const props = () => ({
   direction: select('Tooltip direction (direction)', directions, 'bottom'),
+  alignment: select(
+    'Tooltip alignment to trigger button (alignment)',
+    alignments,
+    'left'
+  ),
   tooltipText: text(
     'Tooltip content (tooltipText)',
     'Brief description of the dotted, underlined word above.'
