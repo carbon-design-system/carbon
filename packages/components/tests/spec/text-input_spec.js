57e8d31405e3870c3d168b6cdf0c1ae88df4bd9d
emyarod <afw5059@gmail.com>
2019-05-14 17:11:09 -0500

    test(text-input): add character counter test

--- packages/components/tests/spec/text-input_spec.js
+++ packages/components/tests/spec/text-input_spec.js
@@ -1,6 +1,7 @@
 // only test JS class since it is only instantiated with password fields
 import TextInput from '../../src/components/text-input/text-input';
 import PasswordVisibilityHTML from '../../html/text-input/text-input--password.html';
+import CharacterCounterHTML from '../../html/text-input/text-input--character-counter.html';
 import flattenOptions from '../utils/flatten-options';
 import { prefix } from '../../src/globals/js/settings';
 
@@ -88,6 +89,57 @@ describe('Test text input', () => {
       expect(
         textInput.classList.contains(`${prefix}--text-input--password-visible`)
       ).toBe(false);
+    });
+
+    afterAll(() => {
+      if (document.body.contains(container)) {
+        document.body.removeChild(container);
+      }
+    });
+  });
+
+  describe('Character counter', () => {
+    let textInput;
+    let charCounter;
+    const container = document.createElement('div');
+    container.innerHTML = CharacterCounterHTML;
+    const aKeyInput = new InputEvent('input', {
+      inputType: 'insertText',
+      type: 'input',
+      bubbles: true,
+      data: 'a',
+    });
+    const deleteKeyInput = new InputEvent('input', {
+      inputType: 'deleteContentBackward',
+      type: 'input',
+      bubbles: true,
+      data: null,
+    });
+
+    beforeAll(() => {
+      document.body.appendChild(container);
+      new TextInput(document.querySelector('[data-text-input]'));
+      textInput = document.querySelector('[data-text-input] input');
+      charCounter = document.querySelector(
+        `.${prefix}--text-input--character-counter--length`
+      );
+    });
+
+    beforeEach(() => {
+      textInput.value = '';
+    });
+
+    it('Should increment character counter on input value length increase', () => {
+      textInput.value += aKeyInput.data;
+      textInput.dispatchEvent(aKeyInput);
+      expect(textInput.value).toBe('a');
+      expect(charCounter.textContent).toBe('1');
+    });
+
+    it('Should decrement character counter on input value length decrease', () => {
+      textInput.dispatchEvent(deleteKeyInput);
+      expect(textInput.value).toBe('');
+      expect(charCounter.textContent).toBe('0');
     });
 
     afterAll(() => {
