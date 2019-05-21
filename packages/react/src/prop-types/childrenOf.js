f26ab184ad0332d62623dbe3465e4ee40a1e0b6e
Edward Fink <finken@us.ibm.com>
2019-05-21 10:22:07 -0400

    check React.createElement instead of displayname

--- packages/react/src/prop-types/childrenOf.js
+++ packages/react/src/prop-types/childrenOf.js
@@ -5,6 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import React from 'react';
 import { Children } from 'react';
 import createChainableTypeChecker from './tools/createChainableTypeChecker';
 import getDisplayName from './tools/getDisplayName';
@@ -21,6 +22,9 @@ const childrenOf = expectedChildTypes => {
   const expectedDisplayNames = expectedChildTypes
     .map(child => getDisplayName(child.type || child))
     .join(', ');
+  const expectedChildElementTypes = expectedChildTypes.map(
+    child => React.createElement(child).type
+  );
 
   const validate = (props, propName, componentName) => {
     Children.forEach(props[propName], child => {
@@ -30,7 +34,7 @@ const childrenOf = expectedChildTypes => {
       const childDisplayName = getDisplayName(child.type || child);
       if (
         !expectedChildTypes.includes(child.type) &&
-        !expectedDisplayNames.includes(childDisplayName)
+        !expectedChildElementTypes.includes(child.type)
       ) {
         throw new Error(
           `Invalid prop \`children\` of type \`${childDisplayName}\` ` +
