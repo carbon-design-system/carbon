2c4a28bb650a96523d6bf0cb52600cc963fe3efa
TJ Egan <tw15egan@gmail.com>
2019-05-21 14:24:52 -0700

    feat(ui-shell): more design changes to fixed side nav

--- packages/react/src/components/UIShell/UIShell-story.js
+++ packages/react/src/components/UIShell/UIShell-story.js
@@ -41,6 +41,45 @@ import {
   SwitcherItemLink,
 } from '../UIShell';
 
+/**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import Search20 from '@carbon/icons-react/lib/search/20';
+import Notification20 from '@carbon/icons-react/lib/notification/20';
+import AppSwitcher20 from '@carbon/icons-react/lib/app-switcher/20';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import React from 'react';
+import { withReadme } from 'storybook-readme';
+import readme from './README.md';
+
+import {
+  Content,
+  Header,
+  HeaderMenuButton,
+  HeaderName,
+  HeaderNavigation,
+  HeaderMenu,
+  HeaderMenuItem,
+  HeaderGlobalBar,
+  HeaderGlobalAction,
+  SkipToContent,
+  SideNav,
+  SideNavHeader,
+  SideNavDetails,
+  SideNavSwitcher,
+  SideNavItems,
+  SideNavLink,
+  SideNavMenu,
+  SideNavMenuItem,
+} from '../UIShell';
+
 const Fade16 = () => (
   <svg
     width="16"
@@ -53,63 +92,17 @@ const Fade16 = () => (
 );
 
 const StoryContent = () => {
-  const content = (
-    <div className="bx--grid">
-      <div className="bx--row">
-        <div className="bx--offset-lg-3 bx--col-lg-13">
-          <h2 style={{ fontWeight: '800', margin: '30px 0', fontSize: '20px' }}>
-            Purpose and function
-          </h2>
-          <p style={{ lineHeight: '20px' }}>
-            The shell is perhaps the most crucial piece of any UI built with
-            Carbon. It contains the shared navigation framework for the entire
-            design system and ties the products in IBM’s portfolio together in a
-            cohesive and elegant way. The shell is the home of the topmost
-            navigation, where users can quickly and dependably gain their
-            bearings and move between pages.
-            <br />
-            <br />
-            The shell was designed with maximum flexibility built in, to serve
-            the needs of a broad range of products and users. Adopting the shell
-            ensures compliance with IBM design standards, simplifies development
-            efforts, and provides great user experiences. All IBM products built
-            with Carbon are required to use the shell’s header.
-            <br />
-            <br />
-            To better understand the purpose and function of the UI shell,
-            consider the “shell” of MacOS, which contains the Apple menu,
-            top-level navigation, and universal, OS-level controls at the top of
-            the screen, as well as a universal dock along the bottom or side of
-            the screen. The Carbon UI shell is roughly analogous in function to
-            these parts of the Mac UI. For example, the app switcher portion of
-            the shell can be compared to the dock in MacOS.
-          </p>
-          <h2 style={{ fontWeight: '800', margin: '30px 0', fontSize: '20px' }}>
-            Header responsive behavior
-          </h2>
-          <p style={{ lineHeight: '20px' }}>
-            As a header scales down to fit smaller screen sizes, headers with
-            persistent side nav menus should have the side nav collapse into
-            “hamburger” menu. See the example to better understand responsive
-            behavior of the header.
-          </p>
-          <h2 style={{ fontWeight: '800', margin: '30px 0', fontSize: '20px' }}>
-            Secondary navigation
-          </h2>
-          <p style={{ lineHeight: '20px' }}>
-            The side-nav contains secondary navigation and fits below the
-            header. It can be configured to be either fixed-width or flexible,
-            with only one level of nested items allowed. Both links and category
-            lists can be used in the side-nav and may be mixed together. There
-            are several configurations of the side-nav, but only one
-            configuration should be used per product section. If tabs are needed
-            on a page when using a side-nav, then the tabs are secondary in
-            hierarchy to the side-nav.
-          </p>
-        </div>
-      </div>
+  const content = Array.from({ length: 10 }, (_, i) => (
+    <div key={i}>
+      <h2>Title</h2>
+      <p>
+        Elit dolores reiciendis sit id consequuntur facere! Recusandae rerum
+        sequi possimus soluta sit Facilis quidem minima sit ipsa consequuntur
+        Maiores facilis dolorum suscipit velit soluta unde. Aliquam consequuntur
+        cum eum
+      </p>
     </div>
-  );
+  ));
   return <Content id="main-content">{content}</Content>;
 };
 
@@ -236,97 +229,6 @@ storiesOf('[Experimental] UI Shell', module)
     ))
   )
   .add(
-    'Header Base w/ Actions and Right Panel',
-    withReadme(readme, () => (
-      <Header aria-label="IBM Platform Name">
-        <HeaderName href="#" prefix="IBM">
-          [Platform]
-        </HeaderName>
-        <HeaderGlobalBar>
-          <HeaderGlobalAction
-            aria-label="Search"
-            onClick={action('search click')}>
-            <Search20 />
-          </HeaderGlobalAction>
-          <HeaderGlobalAction
-            aria-label="Notifications"
-            isActive
-            onClick={action('notification click')}>
-            <Notification20 />
-          </HeaderGlobalAction>
-          <HeaderGlobalAction
-            aria-label="App Switcher"
-            onClick={action('app-switcher click')}>
-            <AppSwitcher20 />
-          </HeaderGlobalAction>
-        </HeaderGlobalBar>
-        <HeaderPanel expanded />
-      </Header>
-    ))
-  )
-  .add(
-    'Header Base w/ Actions and Switcher',
-    withReadme(readme, () => (
-      <Header aria-label="IBM Platform Name">
-        <HeaderName href="#" prefix="IBM">
-          [Platform]
-        </HeaderName>
-        <HeaderGlobalBar>
-          <HeaderGlobalAction
-            aria-label="Search"
-            onClick={action('search click')}>
-            <Search20 />
-          </HeaderGlobalAction>
-          <HeaderGlobalAction
-            aria-label="Notifications"
-            onClick={action('notification click')}>
-            <Notification20 />
-          </HeaderGlobalAction>
-          <HeaderGlobalAction
-            aria-label="App Switcher"
-            isActive
-            onClick={action('app-switcher click')}>
-            <AppSwitcher20 />
-          </HeaderGlobalAction>
-        </HeaderGlobalBar>
-        <HeaderPanel expanded>
-          <Switcher>
-            <SwitcherItem>
-              <SwitcherItemLink isSelected href="javascript:void(0)">
-                Link
-              </SwitcherItemLink>
-            </SwitcherItem>
-            <SwitcherItem>
-              <SwitcherItemLink href="javascript:void(0)">
-                Link
-              </SwitcherItemLink>
-            </SwitcherItem>
-            <SwitcherItem>
-              <SwitcherItemLink href="javascript:void(0)">
-                Link
-              </SwitcherItemLink>
-            </SwitcherItem>
-            <SwitcherItem>
-              <SwitcherItemLink href="javascript:void(0)">
-                Link
-              </SwitcherItemLink>
-            </SwitcherItem>
-            <SwitcherItem>
-              <SwitcherItemLink href="javascript:void(0)">
-                Link
-              </SwitcherItemLink>
-            </SwitcherItem>
-            <SwitcherItem>
-              <SwitcherItemLink href="javascript:void(0)">
-                Link
-              </SwitcherItemLink>
-            </SwitcherItem>
-          </Switcher>
-        </HeaderPanel>
-      </Header>
-    ))
-  )
-  .add(
     'Header Base w/ Navigation and Actions',
     withReadme(readme, () => (
       <Header aria-label="IBM Platform Name">
@@ -367,11 +269,7 @@ storiesOf('[Experimental] UI Shell', module)
     'Fixed SideNav',
     withReadme(readme, () => (
       <>
-<<<<<<< HEAD
         <SideNav isFixedNav expanded={true} aria-label="Side navigation">
-=======
-        <SideNav isFixedNav defaultExpanded aria-label="Side navigation">
->>>>>>> feat(ui-shell): add sid nav + header variation
           <SideNavItems>
             <SideNavMenu title="L0 menu">
               <SideNavMenuItem href="javascript:void(0)">
@@ -418,11 +316,7 @@ storiesOf('[Experimental] UI Shell', module)
     'Fixed SideNav w/ Icons',
     withReadme(readme, () => (
       <>
-<<<<<<< HEAD
         <SideNav isFixedNav expanded={true} aria-label="Side navigation">
-=======
-        <SideNav isFixedNav defaultExpanded aria-label="Side navigation">
->>>>>>> feat(ui-shell): add sid nav + header variation
           <SideNavItems>
             <SideNavMenu renderIcon={Fade16} title="Category title">
               <SideNavMenuItem href="javascript:void(0)">Link</SideNavMenuItem>
@@ -466,11 +360,7 @@ storiesOf('[Experimental] UI Shell', module)
             <SkipToContent />
             <HeaderMenuButton
               aria-label="Open menu"
-<<<<<<< HEAD
               onClick={action('Menu clicked')}
-=======
-              onClick={action('menu clicked')}
->>>>>>> feat(ui-shell): add sid nav + header variation
             />
             <HeaderName href="#" prefix="IBM">
               [Platform]
@@ -502,7 +392,6 @@ storiesOf('[Experimental] UI Shell', module)
                 <AppSwitcher20 />
               </HeaderGlobalAction>
             </HeaderGlobalBar>
-<<<<<<< HEAD
           </Header>
           <SideNav isFixedNav expanded={true} aria-label="Side navigation">
             <SideNavItems>
@@ -547,62 +436,82 @@ storiesOf('[Experimental] UI Shell', module)
               </SideNavLink>
             </SideNavItems>
           </SideNav>
-=======
-            <SideNav isFixedNav defaultExpanded aria-label="Side navigation">
-              <SideNavItems>
-                <SideNavMenu renderIcon={Fade16} title="Category title">
-                  <SideNavMenuItem href="javascript:void(0)">
-                    Link
-                  </SideNavMenuItem>
-                  <SideNavMenuItem
-                    aria-current="page"
-                    href="javascript:void(0)">
-                    Link
-                  </SideNavMenuItem>
-                  <SideNavMenuItem href="javascript:void(0)">
-                    Link
-                  </SideNavMenuItem>
-                </SideNavMenu>
-                <SideNavMenu renderIcon={Fade16} title="Category title">
-                  <SideNavMenuItem href="javascript:void(0)">
-                    Link
-                  </SideNavMenuItem>
-                  <SideNavMenuItem
-                    aria-current="page"
-                    href="javascript:void(0)">
-                    Link
-                  </SideNavMenuItem>
-                  <SideNavMenuItem href="javascript:void(0)">
-                    Link
-                  </SideNavMenuItem>
-                </SideNavMenu>
-                <SideNavMenu renderIcon={Fade16} title="Category title">
-                  <SideNavMenuItem href="javascript:void(0)">
-                    Link
-                  </SideNavMenuItem>
-                  <SideNavMenuItem
-                    aria-current="page"
-                    href="javascript:void(0)">
-                    Link
-                  </SideNavMenuItem>
-                  <SideNavMenuItem href="javascript:void(0)">
-                    Link
-                  </SideNavMenuItem>
-                </SideNavMenu>
-                <SideNavLink renderIcon={Fade16} href="javascript:void(0)">
-                  Link
-                </SideNavLink>
-                <SideNavLink renderIcon={Fade16} href="javascript:void(0)">
-                  Link
-                </SideNavLink>
-              </SideNavItems>
-            </SideNav>
-          </Header>
->>>>>>> feat(ui-shell): add sid nav + header variation
           <StoryContent />
         </>
       );
     })
+  )
+  .add(
+    'SideNav w/ Header',
+    withReadme(readme, () => (
+      <>
+        <Header aria-label="IBM Platform Name">
+          <SkipToContent />
+          <HeaderMenuButton
+            aria-label="Open menu"
+            onClick={action('Menu clicked')}
+          />
+          <HeaderName href="#" prefix="IBM">
+            [Platform]
+          </HeaderName>
+          <HeaderNavigation aria-label="IBM [Platform]">
+            <HeaderMenuItem href="#">Link 1</HeaderMenuItem>
+            <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
+            <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
+            <HeaderMenu aria-label="Link 4">
+              <HeaderMenuItem href="#">Sub-link 1</HeaderMenuItem>
+              <HeaderMenuItem href="#">Sub-link 2</HeaderMenuItem>
+              <HeaderMenuItem href="#">Sub-link 3</HeaderMenuItem>
+            </HeaderMenu>
+          </HeaderNavigation>
+          <HeaderGlobalBar>
+            <HeaderGlobalAction
+              aria-label="Search"
+              onClick={action('search click')}>
+              <Search20 />
+            </HeaderGlobalAction>
+            <HeaderGlobalAction
+              aria-label="Notifications"
+              onClick={action('notification click')}>
+              <Notification20 />
+            </HeaderGlobalAction>
+            <HeaderGlobalAction
+              aria-label="App Switcher"
+              onClick={action('app-switcher click')}>
+              <AppSwitcher20 />
+            </HeaderGlobalAction>
+          </HeaderGlobalBar>
+        </Header>
+        <SideNav isSideRail aria-label="Side navigation">
+          <SideNavHeader renderIcon={Fade16}>
+            <SideNavDetails title="Side navigation title">
+              <SideNavSwitcher
+                labelText="Switcher"
+                onChange={action('switcher changed')}
+                options={['Option 1', 'Option 2', 'Option 3']}
+              />
+            </SideNavDetails>
+          </SideNavHeader>
+          <SideNavItems>
+            <SideNavLink renderIcon={Fade16} href="javascript:void(0)">
+              Link
+            </SideNavLink>
+            <SideNavMenu
+              defaultExpanded
+              renderIcon={Fade16}
+              isActive
+              title="Category title">
+              <SideNavMenuItem href="javascript:void(0)">Link</SideNavMenuItem>
+              <SideNavMenuItem aria-current="page" href="javascript:void(0)">
+                Link
+              </SideNavMenuItem>
+              <SideNavMenuItem href="javascript:void(0)">Link</SideNavMenuItem>
+            </SideNavMenu>
+          </SideNavItems>
+        </SideNav>
+        <StoryContent />
+      </>
+    ))
   )
   .add(
     'SideNav w/ Header',
