bb0fdf9dc0ee48a8c7245cd241702bc719c46d35
Josh Black <josh@josh.black>
2019-05-21 11:41:57 -0500

    chore: check-in work

--- packages/sketch/src/icons.js
+++ packages/sketch/src/icons.js
@@ -46,10 +46,11 @@ export function render(context) {
 
   const page = findOrCreatePage(document, 'Icons');
   page.selected = true;
+
   const icons = normalize(meta);
   const iconNames = Object.keys(icons);
   const start = 0;
-  const length = 10;
+  const length = iconNames.length;
 
   ReactSketch.render(
     <View name="rendered">
@@ -94,16 +95,12 @@ export function render(context) {
   let X_OFFSET = 0;
   let Y_OFFSET = 0;
 
-  // iconNames.slice(start, end).forEach((name, i) => {
-  // });
-
-  for (let i = 0; i < length; i++) {
-    const sizes = icons[iconNames[i]];
+  iconNames.slice(start, length).forEach((name, i) => {
+    const sizes = icons[name];
     const sizesLayer = rendered.layers[i];
     let maxSize = -Infinity;
 
-    for (let j = 0; j < sizes.length; j++) {
-      const icon = sizes[j];
+    sizes.forEach((icon, j) => {
       const size = parseInt(icon.size, 10);
       const sizeLayer = sizesLayer.layers[j];
       const paths = sizeLayer.layers[0].layers[0].layers
@@ -132,14 +129,28 @@ export function render(context) {
         }
       }
 
+      const [pathsToFill, innerPaths] = split(paths, path => {
+        return path.style.opacity;
+      });
+
       const deltaX = (icon.size - maxWidth) / 2;
       const deltaY = (icon.size - maxHeight) / 2;
       const fillShape = new Shape({
         name: 'Fill',
         frame: new Rectangle(0, 0, size, size),
-        layers: paths,
+        layers: pathsToFill,
         style: sharedStyle.style,
         sharedStyleId: sharedStyle.id,
+      });
+      const innerPathShape = new Shape({
+        name: 'Inner path',
+        frame: new Rectangle(0, 0, size, size),
+        style: {
+          ...sharedStyle.style,
+          opacity: 0,
+        },
+        sharedStyleId: sharedStyle.id,
+        layers: innerPaths,
       });
 
       const group = new Group({
@@ -150,35 +161,35 @@ export function render(context) {
           icon.original || size,
           icon.original || size
         ),
-        layers: [fillShape],
+        layers: [fillShape, innerPathShape],
       });
 
       if (icon.original) {
         group.frame.scale(size / icon.original);
       }
 
-      let name =
+      let symbolName =
         sizes.length !== 1
-          ? `category/${icon.basename}/${icon.size}`
-          : `category/${icon.basename}`;
+          ? `category/${name}/${icon.size}`
+          : `category/${name}`;
 
       if (icon.original) {
-        name = `${name}*`;
+        symbolName = `${symbolName}*`;
       }
 
       const artboard = new SymbolMaster({
-        name,
+        name: symbolName,
         frame: new Rectangle(X_OFFSET, Y_OFFSET, size, size),
         parent: page,
         layers: [group],
       });
 
       X_OFFSET = X_OFFSET + size + 8;
-    }
+    });
 
     X_OFFSET = 0;
     Y_OFFSET = Y_OFFSET + maxSize + 32;
-  }
+  });
 
   rendered.remove();
 
@@ -198,15 +209,17 @@ function normalize(icons) {
     if (!icon.size) {
       return acc;
     }
-    if (acc[icon.basename]) {
+    // Drop size from prefix
+    const name = [...icon.prefix.slice(1), icon.basename].join('/');
+    if (acc[name]) {
       return {
         ...acc,
-        [icon.basename]: acc[icon.basename].concat(icon).sort(sortBySize),
+        [name]: acc[name].concat(icon).sort(sortBySize),
       };
     }
     return {
       ...acc,
-      [icon.basename]: [icon],
+      [name]: [icon],
     };
   }, {});
 
@@ -215,4 +228,16 @@ function normalize(icons) {
 
 function sortBySize(a, b) {
   return b.size - a.size;
+}
+
+function split(collection, sorter) {
+  const result = [[], []];
+  for (const element of collection) {
+    if (sorter(element)) {
+      result[0].push(element);
+    } else {
+      result[1].push(element);
+    }
+  }
+  return result;
 }
