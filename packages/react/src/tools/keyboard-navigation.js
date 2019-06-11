b104310109929ab5c808e26c5fe19c111eebbd76
D.A. Kahn <dakahn@protonmail.com>
2019-06-10 19:09:43 -0500

    fix(content-switcher): add copyright

--- packages/react/src/tools/keyboard-navigation.js
+++ packages/react/src/tools/keyboard-navigation.js
@@ -1,4 +1,11 @@
 /**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
  * Various utilities to help with a11y work
  */
 
