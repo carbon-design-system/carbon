fc7ebc5377b7e67447bbad4b34f017a2a0e6f1a3
Josh Black <josh@josh.black>
2019-06-14 14:44:07 -0500

    chore(icons-react): update storybook example

--- packages/icons-react/examples/storybook/stories/index.stories.js
+++ packages/icons-react/examples/storybook/stories/index.stories.js
@@ -1,8 +1,10 @@
-import { CheckmarkFilled16 } from '../../../es';
+import { CheckmarkFilled32 } from '../../../es';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import React from 'react';
 
 storiesOf('Icon', module)
-  .add('with text', () => <CheckmarkFilled16 aria-label="yo" tabIndex="0" />)
+  .add('default', () => <CheckmarkFilled32 />)
+  .add('with aria-label', () => <CheckmarkFilled32 aria-label="Label" />)
+  .add('with focus', () => <CheckmarkFilled32 aria-label="Label" tabIndex="0" />);
