4747f83655ade43be9210597aeece365c94d6915
Josefina Mancilla <josefinanoemi4@gmail.com>
2019-06-18 15:40:45 -0500

    feat: create ref

--- packages/react/src/components/UIShell/HeaderContainer.js
+++ packages/react/src/components/UIShell/HeaderContainer.js
@@ -1,7 +1,11 @@
 import PropTypes from 'prop-types';
 import React, { useState, useCallback } from 'react';
 
-const HeaderContainer = ({ isSideNavExpanded, render: Children }) => {
+const HeaderContainer = ({
+  isSideNavExpanded,
+  headerNavRef,
+  render: Children,
+}) => {
   const [isSideNavExpandedState, setIsSideNavExpandedState] = useState(
     isSideNavExpanded
   );
@@ -10,10 +14,13 @@ const HeaderContainer = ({ isSideNavExpanded, render: Children }) => {
     setIsSideNavExpandedState(!isSideNavExpandedState);
   }, [isSideNavExpandedState, setIsSideNavExpandedState]);
 
+  headerNavRef = React.createRef();
+
   return (
     <Children
       isSideNavExpanded={isSideNavExpandedState}
       onClickSideNavExpand={handleHeaderMenuButtonClick}
+      headerNavRef={headerNavRef}
     />
   );
 };
@@ -23,6 +30,7 @@ HeaderContainer.propTypes = {
    * Optionally provide a custom class name that is applied to the underlying <header>
    */
   isSideNavExpanded: PropTypes.bool,
+  headerNavRef: PropTypes.object,
 };
 
 HeaderContainer.defaultProps = {
