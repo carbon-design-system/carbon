e1a0ca31dbf483807e89b2a9ca48311f3a86ddd8
Josh Black <josh@josh.black>
2019-06-20 14:17:54 -0500

    chore: check-in work

--- packages/sketch/src/commands/colors/sync.js
+++ packages/sketch/src/commands/colors/sync.js
@@ -9,9 +9,59 @@ import { Document } from 'sketch/dom';
 import { command } from '../command';
 import { syncColorStyles } from '../../sharedStyles/colors';
 
+// tmp
+import { Style } from 'sketch/dom';
+
+function findLayerStyle(document, name) {
+  const sharedStyle = document.sharedLayerStyles.find(
+    sharedLayerStyle => sharedLayerStyle.name === name
+  );
+  return sharedStyle;
+}
+
+function updateLayerStyle(document, name, style) {
+  const sharedStyle = findLayerStyle(document, name);
+  if (!sharedStyle) {
+    return;
+  }
+
+  // Otherwise, we'll go and update values of the sharedStyle with the given
+  // style if the values are different
+  Object.keys(style).forEach(key => {
+    if (sharedStyle.style[key] !== style[key]) {
+      sharedStyle.style[key] = style[key];
+    }
+  });
+
+  return sharedStyle;
+}
+
+function syncLayerStyle(sharedStyle) {
+  for (const layer of Array.from(sharedStyle.getAllInstancesLayers())) {
+    layer.style.syncWithSharedStyle(sharedStyle);
+  }
+
+  return sharedStyle;
+}
+
 export function sync() {
   command('commands/colors/sync', () => {
     const document = Document.getSelectedDocument();
-    syncColorStyles(document);
+    const [sharedStyle] = document.sharedLayerStyles;
+    console.log(sharedStyle.style);
+
+    sharedStyle.style.fills = [
+      {
+        color: '#000000',
+        fillType: Style.FillType.Color,
+      },
+    ];
+
+    for (const layer of Array.from(sharedStyle.getAllInstancesLayers())) {
+      layer.style.syncWithSharedStyle(sharedStyle);
+    }
+
+    // const document = Document.getSelectedDocument();
+    // syncColorStyles(document);
   });
 }
