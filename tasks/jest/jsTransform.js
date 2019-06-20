6329e8fe7885966bdc87e04a010e557f0c933352
Josh Black <josh@josh.black>
2019-06-19 19:20:48 -0500

    chore(project): update jsTransform at top-level

--- tasks/jest/jsTransform.js
+++ tasks/jest/jsTransform.js
@@ -24,6 +24,7 @@ const babelOptions = {
   plugins: [
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-export-namespace-from',
     [
       '@babel/plugin-transform-runtime',
       {
