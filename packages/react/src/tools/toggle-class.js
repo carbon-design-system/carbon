1bc44d0253008b4f1aa271803849eb8c524e3051
Akira Sudoh <asudoh@gmail.com>
2019-07-02 07:02:38 +0900

    chore: guard for SSR

--- packages/react/src/tools/toggle-class.js
+++ packages/react/src/tools/toggle-class.js
@@ -1,12 +1,14 @@
 // TODO: Move to polyfills in next major release
-const missingNativeDOMTokenListToggleForce = (() => {
-  const elem = document.createElement('div');
-  const randomClass = `_random_class_${Math.random()
-    .toString(36)
-    .slice(2)}`;
-  elem.classList.toggle(randomClass, false);
-  return elem.classList.contains(randomClass);
-})();
+const missingNativeDOMTokenListToggleForce =
+  typeof document !== 'undefined' &&
+  (() => {
+    const elem = document.createElement('div');
+    const randomClass = `_random_class_${Math.random()
+      .toString(36)
+      .slice(2)}`;
+    elem.classList.toggle(randomClass, false);
+    return elem.classList.contains(randomClass);
+  })();
 if (missingNativeDOMTokenListToggleForce) {
   (() => {
     const origToggle = DOMTokenList.prototype.toggle;
