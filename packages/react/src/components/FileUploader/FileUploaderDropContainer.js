4e4888b8842f8153d2b029ddac7ad900c40dafd2
emyarod <afw5059@gmail.com>
2019-07-08 11:01:44 -0500

    refactor(file-uploader): remove file container

--- packages/react/src/components/FileUploader/FileUploaderDropContainer.js
+++ packages/react/src/components/FileUploader/FileUploaderDropContainer.js
@@ -93,7 +93,6 @@ export default function FileUploaderDropContainer(props) {
           }}
         />
       </div>
-      <div className={`${prefix}--file-container`}>{props.children}</div>
     </div>
   );
 }
