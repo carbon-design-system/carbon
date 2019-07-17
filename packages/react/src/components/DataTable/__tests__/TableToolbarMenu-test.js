7dbd8bf10e083a80fd9cfe529cc8d22550c15d0c
Harrison Shockley <hshockley@rocketsoftware.com>
2019-07-17 15:39:51 -0400

    fix: updated test import

--- packages/react/src/components/DataTable/__tests__/TableToolbarMenu-test.js
+++ packages/react/src/components/DataTable/__tests__/TableToolbarMenu-test.js
@@ -7,7 +7,7 @@
 
 import React from 'react';
 import { mount } from 'enzyme';
-import { Download16 } from '@carbon/icons-react';
+import { Download16 } from '@rocketsoftware/icons-react';
 import { TableToolbarMenu } from '..';
 
 describe('DataTable.TableToolbarMenu', () => {
