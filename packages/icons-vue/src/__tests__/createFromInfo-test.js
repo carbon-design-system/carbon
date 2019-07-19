7618629acd100101cd56cb7ca0cc575d587555d1
Harrison Shockley <hshockley@rocketsoftware.com>
2019-07-19 13:40:43 -0400

    adding vue icons package to npm

--- packages/icons-vue/src/__tests__/createFromInfo-test.js
+++ packages/icons-vue/src/__tests__/createFromInfo-test.js
@@ -20,7 +20,7 @@ const Vue = require('vue/dist/vue');
 
 async function getModuleFromString(
   string,
-  { external = ['@carbon/icon-helpers'], name = '<MockIconModule>' } = {}
+  { external = ['@rocketsoftware/icon-helpers'], name = '<MockIconModule>' } = {}
 ) {
   const bundle = await rollup({
     input: '__entrypoint__',
