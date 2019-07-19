f04a2e67ad94cdcde2e13899ce8d12ee6bc278b5
Josh Black <josh@josh.black>
2019-07-19 09:52:29 -0500

    Update and rename v9 to v9.js

--- packages/themes/src/v9.js
+++ packages/themes/src/v9.js
@@ -1,4 +1,10 @@
-MASTER
+/**
+ * Copyright IBM Corp. 2018, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+ 
 import {
   blue60,
   gray100,
