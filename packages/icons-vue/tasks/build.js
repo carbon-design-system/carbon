3392d93f983ea19a4826bd747834779a25751a6a
D.A. Kahn <dakahn@protonmail.com>
2019-08-02 19:26:23 -0500

    refactor(icons-vue): consolidate modules, remove prettier

--- packages/icons-vue/tasks/build.js
+++ packages/icons-vue/tasks/build.js
@@ -12,8 +12,114 @@ const meta = require('@carbon/icons/build-info.json');
 const fs = require('fs-extra');
 const path = require('path');
 const { rollup } = require('rollup');
-const { createModuleFromInfo } = require('./createFromInfo');
-const createIconStory = require('./createIconStory');
+
+const MODULE_IMPORTS = `
+import { getAttributes } from '@carbon/icon-helpers';
+`;
+
+function createModuleFromInfo(info) {
+  const source = `${MODULE_IMPORTS}
+export default ${createComponentFromInfo(info)};`;
+  return source;
+}
+
+function createComponentFromInfo(info) {
+  const { descriptor, moduleName } = info;
+  const { attrs, content } = descriptor;
+  return `{
+  name: '${moduleName}',
+  functional: true,
+  // We use title as the prop name as it is not a valid attribute for an SVG
+  // HTML element
+  props: ['title'],
+  render(createElement, context) {
+    const { children, data, listeners, props } = context;
+    const attrs = getAttributes({
+      width: '${attrs.width}',
+      height: '${attrs.height}',
+      viewBox: '${attrs.viewBox}',
+      preserveAspectRatio: 'xMidYMid meet',
+      xmlns: 'http://www.w3.org/2000/svg',
+      // Special case here, we need to coordinate that we are using title,
+      // potentially, to get the right focus attributes
+      title: props.title,
+      ...data.attrs,
+    });
+    const svgData = {
+      attrs,
+      on: listeners,
+    };
+
+    if (data.staticClass) {
+      svgData.class = {
+        [data.staticClass]: true,
+      };
+    }
+
+    if (data.class) {
+      svgData.class[data.class] = true;
+    }
+
+    return createElement('svg', svgData, [
+      props.title && createElement('title', null, props.title),
+      ${content.map(toString).join(', ')},
+      children,
+    ]);
+  },
+};`;
+}
+
+function toString(descriptor) {
+  const { elem, attrs = {}, content = [] } = descriptor;
+  if (content.length === 0) {
+    return `createElement('${elem}', { attrs: ${JSON.stringify(attrs)} })`;
+  }
+  return `createElement('${elem}', { attrs: ${JSON.stringify(
+    attrs
+  )} }, [${content.map(toString).join(', ')}])`;
+}
+
+function createIconStory(info) {
+  const { moduleName, outputOptions } = info;
+  return `import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+import ${moduleName} from '../../../${outputOptions.file}';
+
+storiesOf('${moduleName}', module)
+  .add('default', () => ({
+    components: {
+      icon: ${moduleName},
+    },
+    template: \`<icon></icon>\`,
+  }))
+  .add('with title', () => ({
+    components: {
+      icon: ${moduleName},
+    },
+    template: \`<icon><title slot="title">${moduleName}</title></icon>\`,
+  }))
+  .add('with aria-label and tabindex', () => ({
+    components: {
+      icon: ${moduleName},
+    },
+    template: \`<icon aria-label="${moduleName}" tabindex="0"></icon>\`,
+  }))
+  .add('with event handler', () => ({
+    components: {
+      icon: ${moduleName},
+    },
+    template: \`<icon v-on:click="action"></icon>\`,
+    methods: {
+      action: action('clicked'),
+    },
+  }))
+  .add('with custom classes', () => ({
+    components: {
+      icon: ${moduleName},
+    },
+    template: \`<icon class="custom classes" :class="'dynamic classes_2'"></icon>\`,
+  }));`;
+}
 
 const STORYBOOK_DIR = path.resolve(__dirname, '../examples/storybook/stories');
 
