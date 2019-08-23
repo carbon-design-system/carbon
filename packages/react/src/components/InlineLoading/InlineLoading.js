fa7fda0021f7938afc06a149de610c8d3a1582a8
Akira Sudoh <asudoh@jp.ibm.com>
2019-08-23 12:40:44 +0900

    chore(InlineLoading): fix broken code

--- packages/react/src/components/InlineLoading/InlineLoading.js
+++ packages/react/src/components/InlineLoading/InlineLoading.js
@@ -58,9 +58,7 @@ export default function InlineLoading({
     <p className={`${prefix}--inline-loading__text`}>{description}</p>
   );
   const loading = getLoading();
-  const loadingAnimation = loading && <div className={`${prefix}--inline-loading__animation`}>{loading}</div>
-    undefined
-  ) : (
+  const loadingAnimation = loading && (
     <div className={`${prefix}--inline-loading__animation`}>{loading}</div>
   );
   return (
