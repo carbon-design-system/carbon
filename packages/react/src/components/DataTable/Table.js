6e4673a3305f90e45ddc95499b7c30bf66de108e
gmoehler <moehler@de.ibm.com>
2019-08-30 16:33:14 +0200

    test(carbon-components-react): add test

--- packages/react/src/components/DataTable/Table.js
+++ packages/react/src/components/DataTable/Table.js
@@ -39,7 +39,7 @@ export const Table = ({
     </table>
   );
   return stickyHeader ? (
-    <section className="bx--data-table_inner-container">{table}</section>
+    <section className="${prefix}--data-table_inner-container">{table}</section>
   ) : (
     table
   );
