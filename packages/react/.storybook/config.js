71873d58756f36e878f51ecb150a7286d0d51899
emyarod <afw5059@gmail.com>
2019-09-13 11:34:06 -0500

    chore: add license headers

--- packages/react/.storybook/config.js
+++ packages/react/.storybook/config.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright IBM Corp. 2019
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import React from 'react';
 import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
--- packages/react/.storybook/webpack.config.js
+++ packages/react/.storybook/webpack.config.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright IBM Corp. 2019
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
