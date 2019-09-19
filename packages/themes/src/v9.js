94b73c8651c118ca1e272b4dfa26b1761b4fe488
dakahn <dakahn@protonmail.com>
2019-09-19 15:48:58 -0500

    fix(button): add danger token to v9 theme

--- packages/themes/src/v9.js
+++ packages/themes/src/v9.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { white } from '@carbon/colors';
+import { white, red60 } from '@carbon/colors';
 
 export const interactive01 = '#3d70b2';
 export const interactive02 = '#5a6872';
@@ -50,6 +50,8 @@ export const inverseSupport03 = '#FDD600';
 export const inverseSupport04 = '#5aaafa';
 
 export const overlay01 = 'rgba(223, 227, 230, 0.5)';
+
+export const danger = red60;
 
 // Interaction states
 export const focus = '#3d70b2';
