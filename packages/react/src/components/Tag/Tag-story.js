7479500dd323b064e95dd8a13d9cbaf0f2f489d5
Ankit <ankitpanchal0404@gmail.com>
2019-10-02 12:48:04 +0530

    fix(Tag): added new prop titleText to specify title on clear

--- packages/react/src/components/Tag/Tag-story.js
+++ packages/react/src/components/Tag/Tag-story.js
@@ -27,6 +27,7 @@ const props = {
     ),
     disabled: boolean('Disabled (disabled)', false),
     role: 'listitem',
+    titleText: 'Clear Selection',
   }),
   filter() {
     return { ...this.regular(), onClick: action('onClick') };
