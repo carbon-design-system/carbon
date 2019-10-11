0e37066a2b38ac10ad0c82be9c71a5d5eac27eb0
Jen Downs <jen.downs@ibm.com>
2019-10-11 11:06:54 -0500

    fix(checkbox-skeleton): convert class to fc, change label to span

--- packages/react/src/components/Checkbox/Checkbox.Skeleton.js
+++ packages/react/src/components/Checkbox/Checkbox.Skeleton.js
@@ -10,19 +10,10 @@ import { settings } from 'carbon-components';
 
 const { prefix } = settings;
 
-export default class CheckboxSkeleton extends React.Component {
-  render() {
-    const { id } = this.props;
-    return (
-      <div className={`${prefix}--form-item ${prefix}--checkbox-wrapper`}>
-        {
-          // eslint-disable-next-line jsx-a11y/label-has-for,jsx-a11y/label-has-associated-control
-          <label
-            className={`${prefix}--checkbox-label ${prefix}--skeleton`}
-            htmlFor={id}
-          />
-        }
-      </div>
-    );
-  }
-}
+const CheckboxSkeleton = () => (
+  <div className={`${prefix}--form-item ${prefix}--checkbox-wrapper`}>
+    <span className={`${prefix}--checkbox-label ${prefix}--skeleton`} />
+  </div>
+);
+
+export default CheckboxSkeleton;
