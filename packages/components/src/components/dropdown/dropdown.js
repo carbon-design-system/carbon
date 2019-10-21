e7344598d44e2c24b7dfd20e4bf9b35d225e859b
liz.judd <liz.judd@ibm.com>
2019-10-21 12:26:46 -0400

    feat(dropdown): navigation for show selected modifier

--- packages/components/src/components/dropdown/dropdown.js
+++ packages/components/src/components/dropdown/dropdown.js
@@ -251,6 +251,9 @@ class Dropdown extends mixin(
         handleUnderflow(handleOverflow(index, items.length), items.length)
       ];
     };
+    const isShowSelected = this.element.classList.contains(
+      this.options.classShowSelected
+    );
     for (
       let current = getNextItem(start);
       current && current !== start;
@@ -259,7 +262,9 @@ class Dropdown extends mixin(
       if (
         !current.matches(this.options.selectorItemHidden) &&
         !current.parentNode.matches(this.options.selectorItemHidden) &&
-        !current.parentElement.matches(this.options.selectorItemSelected)
+        (isShowSelected ||
+          (!isShowSelected &&
+            !current.parentElement.matches(this.options.selectorItemSelected)))
       ) {
         // Using the latest semantic markup structure where trigger is a button
         // @todo remove conditional once legacy structure is depreciated
@@ -359,6 +364,7 @@ class Dropdown extends mixin(
    *   Used to skip dropdown items for keyboard navigation.
    * @property {string} [selectorItemSelected] The CSS selector to find the clickable area in the selected dropdown item.
    * @property {string} [selectorItemFocused] The CSS selector to find the clickable area in the focused dropdown item.
+   * @property {string} [classShowSelected] The CSS class for the show selected modifier of the dropdown.
    * @property {string} [classSelected] The CSS class for the selected dropdown item.
    * @property {string} [classFocused] The CSS class for the focused dropdown item.
    * @property {string} [classOpen] The CSS class for the open state.
@@ -380,6 +386,7 @@ class Dropdown extends mixin(
       selectorItemSelected: `.${prefix}--dropdown--selected`,
       selectorItemFocused: `.${prefix}--dropdown--focused`,
       selectorItemHidden: `[hidden],[aria-hidden="true"]`,
+      classShowSelected: `${prefix}--dropdown--show-selected`,
       classSelected: `${prefix}--dropdown--selected`,
       classFocused: `${prefix}--dropdown--focused`,
       classOpen: `${prefix}--dropdown--open`,
