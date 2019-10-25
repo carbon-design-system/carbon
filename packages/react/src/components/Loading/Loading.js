3701841087c0403a0ba3af8568be39d889b0d1d0
Abbey Hart <abbeyhrt@gmail.com>
2019-10-25 15:53:07 -0500

    chore: format comment lines

--- packages/react/src/components/Loading/Loading.js
+++ packages/react/src/components/Loading/Loading.js
@@ -77,13 +77,17 @@ export default class Loading extends React.Component {
     const loadingId = `loading-id-${this.instanceId}`;
     const spinnerRadius = small ? '26.8125' : '37.5';
 
-    /**  Various screenreaders (JAWS, VoiceOver, NVDA...) interpret live regions differently
-     *  and change their interpretations over time. All the aria on the div and the label
+    /**
+     * Various screenreaders (JAWS, VoiceOver, NVDA...) interpret live regions differently
+     * and change their interpretations over time. The aria on the div and the label
      * associated with the div are currently necessary for the loading state to be properly
-     * read by all screenreaders. https://developer.paciellogroup.com/blog/2014/03/screen-reader-support-aria-live-regions/ .
+     * read by all screenreaders. [0]
+     *
      * JAWS does not read the loading state unless aria-atomic is set to true and the visually
-     *  hidden label is required for the loading state to be read in VoiceOver on iOS. Please
+     * hidden label is required for the loading state to be read in VoiceOver on iOS. Please
      * do not remove without testing on these platforms.
+     *
+     * [0] https://developer.paciellogroup.com/blog/2014/03/screen-reader-support-aria-live-regions/
      * */
     const loading = (
       <div
