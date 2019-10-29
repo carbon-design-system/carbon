677409682a4992790f1e81f60ddc25dd27ad6f2a
Jen Downs <hello@jendowns.com>
2019-10-29 14:28:00 -0500

    fix(table-toolbar): add proptypes docs, add arialabel prop with default

--- packages/react/src/components/DataTable/TableToolbar.js
+++ packages/react/src/components/DataTable/TableToolbar.js
@@ -4,16 +4,33 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+import PropTypes from 'prop-types';
 import React from 'react';
 import { settings } from 'carbon-components';
 
 const { prefix } = settings;
 
-const TableToolbar = ({ children }) => (
-  <section
-    aria-label="data table toolbar"
-    className={`${prefix}--table-toolbar`}>
+const TableToolbar = ({ ariaLabel, children }) => (
+  <section aria-label={ariaLabel} className={`${prefix}--table-toolbar`}>
     {children}
   </section>
 );
+
+TableToolbar.propTypes = {
+  /**
+   * The `aria-label` attribute for the TableToolbar
+   */
+  ariaLabel: PropTypes.string,
+
+  /**
+   * Pass in the children that will be rendered inside the TableToolbar
+   */
+  children: PropTypes.node,
+};
+
+TableToolbar.defaultProps = {
+  ariaLabel: 'data table toolbar',
+};
+
 export default TableToolbar;
