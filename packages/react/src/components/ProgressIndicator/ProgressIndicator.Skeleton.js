61ef49ca499135507efc0f244ded1d958d4039e5
Jen Downs <hello@jendowns.com>
2019-10-29 13:59:48 -0500

    fix(progress-indicator-skeleton): convert to functional component

--- packages/react/src/components/ProgressIndicator/ProgressIndicator.Skeleton.js
+++ packages/react/src/components/ProgressIndicator/ProgressIndicator.Skeleton.js
@@ -10,33 +10,33 @@ import { settings } from 'carbon-components';
 
 const { prefix } = settings;
 
-export default class ProgressIndicatorSkeleton extends React.Component {
-  render() {
-    const currentSvg = (
-      <svg>
-        <path d="M 7, 7 m -7, 0 a 7,7 0 1,0 14,0 a 7,7 0 1,0 -14,0" />
-      </svg>
-    );
+const ProgressIndicatorSkeleton = () => {
+  const currentSvg = (
+    <svg>
+      <path d="M 7, 7 m -7, 0 a 7,7 0 1,0 14,0 a 7,7 0 1,0 -14,0" />
+    </svg>
+  );
 
-    const step = (
-      <li
-        className={`${prefix}--progress-step ${prefix}--progress-step--incomplete`}>
-        <div
-          className={`${prefix}--progress-step-button ${prefix}--progress-step-button--unclickable`}>
-          {currentSvg}
-          <p className={`${prefix}--progress-label`} />
-          <span className={`${prefix}--progress-line`} />
-        </div>
-      </li>
-    );
+  const step = (
+    <li
+      className={`${prefix}--progress-step ${prefix}--progress-step--incomplete`}>
+      <div
+        className={`${prefix}--progress-step-button ${prefix}--progress-step-button--unclickable`}>
+        {currentSvg}
+        <p className={`${prefix}--progress-label`} />
+        <span className={`${prefix}--progress-line`} />
+      </div>
+    </li>
+  );
 
-    return (
-      <ul className={`${prefix}--progress ${prefix}--skeleton`}>
-        {step}
-        {step}
-        {step}
-        {step}
-      </ul>
-    );
-  }
-}
+  return (
+    <ul className={`${prefix}--progress ${prefix}--skeleton`}>
+      {step}
+      {step}
+      {step}
+      {step}
+    </ul>
+  );
+};
+
+export default ProgressIndicatorSkeleton;
