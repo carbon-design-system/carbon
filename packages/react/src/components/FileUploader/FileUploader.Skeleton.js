939efd749c480867f041a8f6359efd2ac90b683d
Jen Downs <hello@jendowns.com>
2019-10-29 14:02:56 -0500

    fix(file-uploader-skeleton): convert to functional component

--- packages/react/src/components/FileUploader/FileUploader.Skeleton.js
+++ packages/react/src/components/FileUploader/FileUploader.Skeleton.js
@@ -12,17 +12,12 @@ import ButtonSkeleton from '../Button/Button.Skeleton';
 
 const { prefix } = settings;
 
-export default class FileUploaderSkeleton extends React.Component {
-  render() {
-    return (
-      <div className={`${prefix}--form-item`}>
-        <SkeletonText heading width="100px" />
-        <SkeletonText
-          width="225px"
-          className={`${prefix}--label-description`}
-        />
-        <ButtonSkeleton />
-      </div>
-    );
-  }
-}
+const FileUploaderSkeleton = () => (
+  <div className={`${prefix}--form-item`}>
+    <SkeletonText heading width="100px" />
+    <SkeletonText width="225px" className={`${prefix}--label-description`} />
+    <ButtonSkeleton />
+  </div>
+);
+
+export default FileUploaderSkeleton;
