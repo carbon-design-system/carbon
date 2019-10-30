1c830b225c3afb7aa156df1a1b74854344b81452
Jen Downs <hello@jendowns.com>
2019-10-30 17:42:09 -0500

    fix(skeleton-components): convert Breadcrumb, Fileuploader, ProgressIndicator, RadioButton, Tabs, and Tag skeletons to function declarations (#4484)

--- packages/react/src/components/FileUploader/FileUploader.Skeleton.js
+++ packages/react/src/components/FileUploader/FileUploader.Skeleton.js
@@ -12,17 +12,14 @@ import ButtonSkeleton from '../Button/Button.Skeleton';
 
 const { prefix } = settings;
 
-export default class FileUploaderSkeleton extends React.Component {
-  render() {
-    return (
-      <div className={`${prefix}--form-item`}>
-        <SkeletonText heading width="100px" />
-        <SkeletonText
-          width="225px"
-          className={`${prefix}--label-description`}
-        />
-        <ButtonSkeleton />
-      </div>
-    );
-  }
+function FileUploaderSkeleton() {
+  return (
+    <div className={`${prefix}--form-item`}>
+      <SkeletonText heading width="100px" />
+      <SkeletonText width="225px" className={`${prefix}--label-description`} />
+      <ButtonSkeleton />
+    </div>
+  );
 }
+
+export default FileUploaderSkeleton;
--- packages/react/src/components/RadioButton/RadioButton.Skeleton.js
+++ packages/react/src/components/RadioButton/RadioButton.Skeleton.js
@@ -10,20 +10,13 @@ import { settings } from 'carbon-components';
 
 const { prefix } = settings;
 
-export default class RadioButtonSkeleton extends React.Component {
-  render() {
-    const { id } = this.props;
-    return (
-      <div className={`${prefix}--radio-button-wrapper`}>
-        <div className={`${prefix}--radio-button ${prefix}--skeleton`} />
-        {
-          /* eslint-disable jsx-a11y/label-has-for,jsx-a11y/label-has-associated-control */
-          <label
-            className={`${prefix}--radio-button__label ${prefix}--skeleton`}
-            htmlFor={id}
-          />
-        }
-      </div>
-    );
-  }
+function RadioButtonSkeleton() {
+  return (
+    <div className={`${prefix}--radio-button-wrapper`}>
+      <div className={`${prefix}--radio-button ${prefix}--skeleton`} />
+      <span className={`${prefix}--radio-button__label ${prefix}--skeleton`} />
+    </div>
+  );
 }
+
+export default RadioButtonSkeleton;
