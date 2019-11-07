d7883cc6a38f3ca7e401319e5b7c472f97c62c45
Jen Downs <hello@jendowns.com>
2019-11-07 11:33:36 -0600

    fix(textarea-skeleton): add className prop, allow extra props

--- packages/react/src/components/TextArea/TextArea.Skeleton.js
+++ packages/react/src/components/TextArea/TextArea.Skeleton.js
@@ -7,12 +7,17 @@
 
 import PropTypes from 'prop-types';
 import React from 'react';
+import cx from 'classnames';
 import { settings } from 'carbon-components';
 
 const { prefix } = settings;
 
-const TextAreaSkeleton = ({ hideLabel }) => (
-  <div className={`${prefix}--form-item`}>
+const TextAreaSkeleton = ({
+  hideLabel,
+  className: customClassName,
+  ...rest
+}) => (
+  <div className={cx(`${prefix}--form-item`, customClassName)} {...rest}>
     {!hideLabel && <span className={`${prefix}--label ${prefix}--skeleton`} />}
     <div className={`${prefix}--skeleton ${prefix}--text-area`} />
   </div>
@@ -23,6 +28,11 @@ TextAreaSkeleton.propTypes = {
    * Specify whether the label should be hidden, or not
    */
   hideLabel: PropTypes.bool,
+
+  /**
+   * Specify an optional className to add to the form item wrapper.
+   */
+  className: PropTypes.string,
 };
 
 export default TextAreaSkeleton;
