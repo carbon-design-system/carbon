6ece744b18e3c6eb2b0acb62e22b3ca6f5f85190
Jen Downs <hello@jendowns.com>
2019-11-08 10:13:01 -0600

    fix(skeleton-components): add className prop to all, allow extra props (#4602)
    * fix(accordion-skeleton): add classname prop, allow extra props
    
    * fix(breadcrumb-skeleton): add classname prop, allow extra props
    
    * fix(button-skeleton): add className prop, allow extra props
    
    * fix(checkbox-skeleton): add className prop, allow extra props
    
    * fix(data-table-skeleton): add className prop, allow extra props
    
    * fix(datepicker-skeleton): add className prop, allow extra props
    
    * fix(dropdown): add className prop, allow extra props
    
    * fix(fileuploader-skeleton): add className prop, allow extra props
    
    * fix(icon-skeleton): add className prop, allow extra props
    
    * fix(number-input-skeleton): add className prop, allow extra props
    
    * fix(pagination-skeleton): className + extra props, use func declaration
    
    * fix(progress-indicator-skeleton): className prop, allow extra props
    
    * fix(radiobutton-skeleton): add className prop, allow extra props
    
    * fix(search-skeleton): add className prop, allow extra props
    
    * fix(select-skeleton): add className prop, allow extra props
    
    * fix(slider-skeleton): add className prop, allow extra props
    
    * fix(structured-list-skeleton): add className prop, allow extra props
    
    * fix(tabs-skeleton): add className, allow extra props
    
    * fix(tag-skeleton): add className prop, allow extra props
    
    * fix(textarea-skeleton): add className prop, allow extra props
    
    * fix(textinput-skeleton): add className prop, allow extra props
    
    * fix(toggle-skeleton): add className prop, allow extra props
    
    * fix(toggle-small-skeleton): add className prop, allow extra props
    
    * fix(skeleton-components): apply PR feedback for classnames
    
    * fix(button-skeleton): more custom className in button classes

--- packages/react/src/components/FileUploader/FileUploader.Skeleton.js
+++ packages/react/src/components/FileUploader/FileUploader.Skeleton.js
@@ -5,21 +5,30 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import PropTypes from 'prop-types';
 import React from 'react';
+import cx from 'classnames';
 import { settings } from 'carbon-components';
 import SkeletonText from '../SkeletonText';
 import ButtonSkeleton from '../Button/Button.Skeleton';
 
 const { prefix } = settings;
 
-function FileUploaderSkeleton() {
+function FileUploaderSkeleton({ className, ...rest }) {
   return (
-    <div className={`${prefix}--form-item`}>
+    <div className={cx(`${prefix}--form-item`, className)} {...rest}>
       <SkeletonText heading width="100px" />
       <SkeletonText width="225px" className={`${prefix}--label-description`} />
       <ButtonSkeleton />
     </div>
   );
 }
+
+FileUploaderSkeleton.propTypes = {
+  /**
+   * Specify an optional className to add.
+   */
+  className: PropTypes.string,
+};
 
 export default FileUploaderSkeleton;
