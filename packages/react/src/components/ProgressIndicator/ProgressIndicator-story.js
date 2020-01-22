17fb59db2f65c96fbf33003ac0506f6007d1d7f0
TJ Egan <tw15egan@gmail.com>
2020-01-22 15:32:23 -0800

    feat(progress-indicator): add vertical progress indicator to react

--- packages/react/src/components/ProgressIndicator/ProgressIndicator-story.js
+++ packages/react/src/components/ProgressIndicator/ProgressIndicator-story.js
@@ -85,6 +85,73 @@ storiesOf('ProgressIndicator', module)
     }
   )
   .add(
+    'Vertical',
+    () => (
+      <ProgressIndicator
+        vertical
+        currentIndex={number('Current progress (currentIndex)', 1)}>
+        <ProgressStep
+          label="First step"
+          description="Step 1: Getting started with Carbon Design System"
+          secondaryLabel="Optional label"
+        />
+        <ProgressStep
+          label="Second step with tooltip"
+          description="Step 2: Getting started with Carbon Design System"
+          renderLabel={() => (
+            <Tooltip
+              direction="bottom"
+              showIcon={false}
+              triggerClassName={`${prefix}--progress-label`}
+              triggerText={'Second step with tooltip'}
+              tooltipId="tooltipId-0">
+              <p>Overflow tooltip content.</p>
+            </Tooltip>
+          )}
+        />
+        <ProgressStep
+          label="Third step with tooltip"
+          description="Step 3: Getting started with Carbon Design System"
+          renderLabel={() => (
+            <Tooltip
+              direction="bottom"
+              showIcon={false}
+              triggerClassName={`${prefix}--progress-label`}
+              triggerText={'Third step with tooltip'}
+              tooltipId="tooltipId-1">
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi
+                consequuntur hic ratione aliquid cupiditate, nesciunt saepe iste
+                blanditiis cumque maxime tenetur veniam est illo deserunt sint
+                quae pariatur. Laboriosam, consequatur.
+              </p>
+            </Tooltip>
+          )}
+        />
+        <ProgressStep
+          label="Fourth step"
+          description="Step 4: Getting started with Carbon Design System"
+          invalid
+          secondaryLabel="Example invalid step"
+        />
+        <ProgressStep
+          label="Fifth step"
+          description="Step 5: Getting started with Carbon Design System"
+          disabled
+        />
+      </ProgressIndicator>
+    ),
+    {
+      info: {
+        text: `
+            For React usage, ProgressIndicator holds the currentIndex state to indicate which ProgressStep is the current step. The ProgressIndicator component should always be used with ProgressStep components as its children. Changing currentIndex prop will automatically set the ProgressStep components props (complete, incomplete, current).
+            For general usage, Progress Indicators display steps in a process. It should indicate when steps have been complete, the active step,
+            and the steps to come.
+          `,
+      },
+    }
+  )
+  .add(
     'interactive',
     () => (
       <ProgressIndicator
