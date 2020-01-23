4364be6b9b0836e6a35bced2f68394c78e1f1be2
sunilkotiyaibm <sunil.kotiya@ibm.com>
2020-01-23 13:05:56 +0530

    feat(table): 3715-Datatable-Column-Resizer
    #5047

--- packages/react/src/components/DataTable/TableHeader.js
+++ packages/react/src/components/DataTable/TableHeader.js
@@ -57,13 +57,19 @@ const TableHeader = React.forwardRef(function TableHeader(
     scope,
     sortDirection,
     translateWithId: t,
+    thWidth,
     ...rest
   },
   ref
 ) {
   if (!isSortable) {
     return (
-      <th {...rest} className={headerClassName} scope={scope} ref={ref}>
+      <th
+        {...rest}
+        className={headerClassName}
+        scope={scope}
+        ref={ref}
+        style={{ width: thWidth }}>
         <span className={`${prefix}--table-header-label`}>{children}</span>
       </th>
     );
@@ -83,7 +89,8 @@ const TableHeader = React.forwardRef(function TableHeader(
       scope={scope}
       className={headerClassName}
       aria-sort={ariaSort}
-      ref={ref}>
+      ref={ref}
+      style={{ width: thWidth }}>
       <button className={className} onClick={onClick} {...rest}>
         <span className={`${prefix}--table-header-label`}>{children}</span>
         <Arrow
@@ -155,12 +162,18 @@ TableHeader.propTypes = {
    * this component.
    */
   translateWithId: PropTypes.func,
+
+  /**
+   * Pass in width that will be applied in the table header column for resize the column width
+   */
+  thWidth: PropTypes.number,
 };
 
 TableHeader.defaultProps = {
   isSortable: false,
   scope: 'col',
   translateWithId,
+  thWidth: undefined,
 };
 
 TableHeader.translationKeys = Object.values(translationKeys);
