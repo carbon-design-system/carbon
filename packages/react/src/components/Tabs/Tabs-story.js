2b9e4e6ef81a040de0a3507404475b38fcf94040
Jen Downs <hello@jendowns.com>
2020-02-04 12:57:26 -0600

    docs(tabs): adjust indentation of code snippet example

--- packages/react/src/components/Tabs/Tabs-story.js
+++ packages/react/src/components/Tabs/Tabs-story.js
@@ -55,26 +55,24 @@ const CustomLabel = ({ text }) => <>{text}</>;
 const CodeSnippetExample = () => (
   <CodeSnippet type="multi">
     {`@mixin grid-container {
-      width: 100%;
-      padding-right: padding(mobile);
-      padding-left: padding(mobile);
-    
-      @include breakpoint(bp--xs--major) {
-        padding-right: padding(xs);
-        padding-left: padding(xs);
-      }
-    }
-    
-    $z-indexes: (
-      modal : 9000,
-      overlay : 8000,
-      dropdown : 7000,
-      header : 6000,
-      footer : 5000,
-      hidden : - 1,
-      overflowHidden: - 1,
-      floating: 10000
-    );`}
+  width: 100%;
+  padding-right: padding(mobile);
+  padding-left: padding(mobile);
+  @include breakpoint(bp--xs--major) {
+    padding-right: padding(xs);
+    padding-left: padding(xs);
+  }
+}
+$z-indexes: (
+  modal : 9000,
+  overlay : 8000,
+  dropdown : 7000,
+  header : 6000,
+  footer : 5000,
+  hidden : - 1,
+  overflowHidden: - 1,
+  floating: 10000
+);`}
   </CodeSnippet>
 );
 
