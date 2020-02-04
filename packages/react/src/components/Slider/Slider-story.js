dbce79476c1cf2e74e5c7407090db1a81f3f73e7
Abbey Hart <abbeyhrt@gmail.com>
2020-02-04 11:22:30 -0600

    fix(slider): change prop to stepMultiplier and deprecate stepMuliplier (#5251)
    * fix(slider): change prop to stepMultiplier and deprecate stepuliplier
    
    * fix(slider): adjust how to support stepMuliplier
    
    Co-authored-by: TJ Egan <tw15egan@gmail.com>

--- packages/react/src/components/Slider/Slider-story.js
+++ packages/react/src/components/Slider/Slider-story.js
@@ -28,9 +28,9 @@ const props = () => ({
   min: number('The minimum value (min)', 0),
   max: number('The maximum value (max)', 100),
   step: number('The step (step)', 1),
-  stepMuliplier: number(
-    'The step factor for Shift+arrow keys (stepMuliplier)',
-    4
+  stepMultiplier: number(
+    'The step factor for Shift+arrow keys (stepMultiplier)',
+    5
   ),
   labelText: text('Label text (labelText)', 'Slider Label'),
   minLabel: text('Label for minimum value (minLabel)', ''),
