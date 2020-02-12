e26b7fe2b411280f83bb039f2684add451dafe15
Abbey Hart <abbeyhrt@gmail.com>
2020-02-12 12:11:36 -0600

    docs(search): remove unnecessary changes

--- packages/react/src/components/Search/Search-story.js
+++ packages/react/src/components/Search/Search-story.js
@@ -40,23 +40,15 @@ const props = () => ({
 
 storiesOf('Search', module)
   .addDecorator(withKnobs)
-  .add(
-    'Default',
-    () => (
-      <div>
-        <Search {...props()} id="search-1" />
-      </div>
-    ),
-    {
-      info: {
-        text: `
+  .add('Default', () => <Search {...props()} id="search-1" />, {
+    info: {
+      text: `
             Search enables users to specify a word or a phrase to find particular relevant pieces of content
             without the use of navigation. Search can be used as the primary means of discovering content,
             or as a filter to aid the user in finding content.
           `,
-      },
-    }
-  )
+    },
+  })
   .add(
     '[Deprecated] custom buttons',
     () => (
