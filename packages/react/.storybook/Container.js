a78262570702300415c6315d4bc073885908c75f
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-02-18 15:25:10 -0600

    chore: upgrade min node version

--- packages/react/.storybook/Container.js
+++ packages/react/.storybook/Container.js
@@ -15,6 +15,7 @@ const { prefix } = settings;
 
 function Container({ story }) {
   useEffect(() => {
+    console.log(window);
     const originalDirection = document.documentElement.dir;
     if (process.env.CARBON_REACT_STORYBOOK_USE_RTL === 'true') {
       document.documentElement.dir = 'rtl';
