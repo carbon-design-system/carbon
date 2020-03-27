95ee9abca61f073b6b0fb42d88d09f115870cc19
Alessandra Davila <aledavila@ibm.com>
2020-03-27 14:10:13 -0500

    chore(release): matching master code

--- packages/react/src/components/Tag/Tag.js
+++ packages/react/src/components/Tag/Tag.js
@@ -38,8 +38,8 @@ const Tag = ({
   onClose,
   ...other
 }) => {
-  const tagClass = `${prefix}--tag--${type}`;
-  const tagClasses = classNames(`${prefix}--tag`, tagClass, className, {
+  const tagId = id || `tag-${getInstanceId()}`;
+  const tagClasses = classNames(`${prefix}--tag`, className, {
     [`${prefix}--tag--disabled`]: disabled,
     [`${prefix}--tag--filter`]: filter,
     [`${prefix}--tag--${type}`]: type,
