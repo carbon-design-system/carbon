0e35e947c34cdd0ea7a104e2aaf989eef89304f2
emyarod <afw5059@gmail.com>
2020-04-01 10:20:21 -0500

    test(sketch): update tests with shared style type

--- packages/sketch/src/commands/test/sync-shared-styles.js
+++ packages/sketch/src/commands/test/sync-shared-styles.js
@@ -42,13 +42,13 @@ export function testSyncSharedStyles() {
     /**
      * Testing shared layer styles
      */
-    const sharedStyle = syncColorStyle(document, 'black', '#000000');
+    const sharedStyle = syncColorStyle(document, 'black', '#000000', 'fill');
 
     if (document.sharedLayerStyles.length !== 1) {
       throw new Error('Expected sync command to generate a shared layer style');
     }
 
-    syncColorStyle(document, 'black', '#000000');
+    syncColorStyle(document, 'black', '#000000', 'fill');
 
     if (document.sharedLayerStyles.length !== 1) {
       throw new Error(
@@ -118,7 +118,7 @@ export function testSyncSharedStyles() {
       throw new Error('The layer is not in sync with the shared style');
     }
 
-    syncColorStyle(document, 'black', '#dedede');
+    syncColorStyle(document, 'black', '#dedede', 'fill');
 
     if (getLayerFillColor() !== '#dededeff') {
       throw new Error('The layer did not update to the new shared style');
