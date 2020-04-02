435859c9d3fc60ce9acdf0d30021f8cb36c15667
D.A. Kahn <dakahn@protonmail.com>
2020-04-02 14:30:58 -0500

    fix(tools): add banner to createPropAdapter

--- packages/react/src/tools/createPropAdapter.js
+++ packages/react/src/tools/createPropAdapter.js
@@ -1,4 +1,11 @@
 /**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
  * Create an adapter that converts an object of props with potentially deprecated
  * prop names to the replacement prop names in a newer version. Useful for guarding
  * against breaking changes when a prop has been renamed
