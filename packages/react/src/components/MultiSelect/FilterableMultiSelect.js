6b265fc1877e59218212964991744fe75e9a7bc8
D.A. Kahn <dakahn@protonmail.com>
2020-04-15 18:36:06 -0500

    fix(multiselect): change field type to fix broken labeling

--- packages/react/src/components/MultiSelect/FilterableMultiSelect.js
+++ packages/react/src/components/MultiSelect/FilterableMultiSelect.js
@@ -376,7 +376,7 @@ export default class FilterableMultiSelect extends React.Component {
                   isOpen={isOpen}
                   size={size}
                   {...getRootProps()}>
-                  <ListBox.Field
+                  <input
                     id={id}
                     disabled={disabled}
                     aria-labelledby={labelId}
@@ -390,7 +390,7 @@ export default class FilterableMultiSelect extends React.Component {
                         disabled={disabled}
                       />
                     )}
-                    <input
+                    <Field
                       className={inputClasses}
                       aria-controls={`${id}__menu`}
                       aria-autocomplete="list"
@@ -417,7 +417,7 @@ export default class FilterableMultiSelect extends React.Component {
                       isOpen={isOpen}
                       translateWithId={translateWithId}
                     />
-                  </ListBox.Field>
+                  </input>
                   {isOpen && (
                     <ListBox.Menu
                       role="group"
