e93c282ce1f2df57048d9a87c526b6af1c21726f
Josh Black <josh@josh.black>
2020-04-17 10:30:48 -0500

    feat(icon-build-helpers): add option support for extensions (#5874)
    * chore: check-in work
    
    * feat(icon-build-helpers): add option support for extensions
    
    * chore(icon-build-helpers): remove unused export

--- packages/icon-build-helpers/src/metadata/extensions/deprecated.js
+++ packages/icon-build-helpers/src/metadata/extensions/deprecated.js
@@ -17,46 +17,48 @@ const Joi = require('joi');
 //
 // In the future, we may want to include a reason for the deprecation, or a
 // notice for what icon to use instead.
-const deprecated = {
-  name: 'deprecated',
+const deprecated = () => {
+  return {
+    name: 'deprecated',
 
-  schema: Joi.object().keys({
-    deprecated: Joi.array()
-      .items(
-        Joi.object().keys({
-          name: Joi.string().required(),
-          reason: Joi.string(),
-        })
-      )
-      .required(),
-  }),
+    schema: Joi.object().keys({
+      deprecated: Joi.array()
+        .items(
+          Joi.object().keys({
+            name: Joi.string().required(),
+            reason: Joi.string(),
+          })
+        )
+        .required(),
+    }),
 
-  extend(metadata, data) {
-    const { deprecated } = data;
+    extend(metadata, data) {
+      const { deprecated } = data;
 
-    for (const icon of metadata.icons) {
-      const entry = deprecated.find(({ name }) => name === icon.name);
-      if (entry) {
-        icon.deprecated = true;
-        if (entry.reason) {
-          icon.reason = entry.reason;
+      for (const icon of metadata.icons) {
+        const entry = deprecated.find(({ name }) => name === icon.name);
+        if (entry) {
+          icon.deprecated = true;
+          if (entry.reason) {
+            icon.reason = entry.reason;
+          }
         }
       }
-    }
-  },
+    },
 
-  validate(registry, data) {
-    for (const icon of data.deprecated) {
-      const entry = registry.has(icon.name);
-      if (!entry) {
-        throw new Error(
-          `Expected the deprecated icon \`${icon.name}\` to exist. Either ` +
-            `this icon does not exist, or is not available in the given SVG ` +
-            `directory`
-        );
+    validate(registry, data) {
+      for (const icon of data.deprecated) {
+        const entry = registry.has(icon.name);
+        if (!entry) {
+          throw new Error(
+            `Expected the deprecated icon \`${icon.name}\` to exist. Either ` +
+              `this icon does not exist, or is not available in the given SVG ` +
+              `directory`
+          );
+        }
       }
-    }
-  },
+    },
+  };
 };
 
 module.exports = deprecated;
