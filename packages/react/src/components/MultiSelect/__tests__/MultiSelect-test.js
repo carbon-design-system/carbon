8672a1ef4e55d9c45c628a55be9e4448a20366c3
DAK <40970507+dakahn@users.noreply.github.com>
2020-04-29 11:01:11 -0500

    Update packages/react/src/components/MultiSelect/__tests__/MultiSelect-test.js
    Co-Authored-By: Josh Black <josh@josh.black>

--- packages/react/src/components/MultiSelect/__tests__/MultiSelect-test.js
+++ packages/react/src/components/MultiSelect/__tests__/MultiSelect-test.js
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import 'carbon-components/src/components/multi-select/_multi-select.scss';
 import { getByText, isElementVisible } from '@carbon/test-utils/dom';
 import { pressEnter, pressSpace, pressTab } from '@carbon/test-utils/keyboard';
 import { render, cleanup } from '@carbon/test-utils/react';
