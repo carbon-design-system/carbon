725f049941421f6ed7ef100dc8a5db981e791986
DAK <40970507+dakahn@users.noreply.github.com>
2020-04-29 11:01:03 -0500

    Update packages/react/src/components/MultiSelect/__tests__/MultiSelect-test.js
    Co-Authored-By: Josh Black <josh@josh.black>

--- packages/react/src/components/MultiSelect/__tests__/MultiSelect-test.js
+++ packages/react/src/components/MultiSelect/__tests__/MultiSelect-test.js
@@ -3,7 +3,6 @@
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
- *
  */
 
 import 'carbon-components/src/components/multi-select/_multi-select.scss';
