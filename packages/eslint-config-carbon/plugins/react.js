9f77f6832609592b35c4bd431f2e2f7ccd57a435
Josh Black <josh@josh.black>
2020-05-29 19:31:46 -0500

    feat(eslint): add support for overrides

--- packages/eslint-config-carbon/plugins/react.js
+++ packages/eslint-config-carbon/plugins/react.js
@@ -52,4 +52,46 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    // Sometimes we'll want to define a quick component in a story to use as a
+    // wrapper for a component we're documenting. For example:
+    //
+    // function DemoComponent({ children }) {
+    //   return <p>{children}</p>;
+    // }
+    //
+    // In these cases, we don't need to handle prop type validation like we
+    // would for code we ship to users.
+    {
+      files: ['*-story.js'],
+      rules: {
+        'react/prop-types': 0,
+      },
+    },
+
+    // When writing fixtures, we tend to focus on the specific component and
+    // don't require React in scope as we never end up executing the code. The
+    // fixtures are often used for asserting transformations on the file
+    {
+      files: ['**/fixtures/**/*.js'],
+      rules: {
+        'react/react-in-jsx-scope': 0,
+      },
+    },
+
+    {
+      files: ['*-test.js'],
+      rules: {
+        // We often write inline functions for certain types of props, typically
+        // `render*` props. For example, `renderIcon={() => <div>test</div>}`
+        'react/display-name': 0,
+
+        // While writing tests, we often write helper components that are meant
+        // to emulate a specific situation or behavior. These do not require prop
+        // types as they are not shipped to end-users and are not valuable for
+        // test authors.
+        'react/prop-types': 0,
+      },
+    },
+  ],
 };
