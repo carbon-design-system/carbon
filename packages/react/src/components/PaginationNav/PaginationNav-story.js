7feac7acc67ab0f9bd10857ccae0cf7f9179f0ee
Jan Hassel <jan.hassel@ibm.com>
2020-06-04 09:39:59 +0200

    fix(pagination-nav): fix prettier issues

--- packages/react/src/components/PaginationNav/PaginationNav-story.js
+++ packages/react/src/components/PaginationNav/PaginationNav-story.js
@@ -9,19 +9,20 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import {
-  withKnobs,
-  boolean,
-  number,
-  text,
-} from '@storybook/addon-knobs';
+import { withKnobs, boolean, number, text } from '@storybook/addon-knobs';
 import PaginationNav from '../PaginationNav';
 
 const props = () => ({
-  loop: boolean('Allow user to loop through the items when reaching first / last (loop)', false),
+  loop: boolean(
+    'Allow user to loop through the items when reaching first / last (loop)',
+    false
+  ),
   page: number('The current page (page)', 0),
   totalItems: number('Total number of items (totalItems)', 10),
-  itemsShown: number('Number of items to be shown (minimum 4) (itemsShown)', 10),
+  itemsShown: number(
+    'Number of items to be shown (minimum 4) (itemsShown)',
+    10
+  ),
   backwardText: text(
     'The description for the backward icon (backwardText)',
     'Previous'
--- packages/react/src/components/PaginationNav/PaginationNav.js
+++ packages/react/src/components/PaginationNav/PaginationNav.js
@@ -31,7 +31,7 @@ const getCuts = (totalItems, itemsThatFit, page) => {
   // remove reserveration for first item when only 4 items can be displayed
   if (itemsThatFit < 5) reservedSpots -= 1;
   // remove one overflow reservation when current page would hide it
-  if (page < 2 || page > (totalItems - 3)) reservedSpots -= 1;
+  if (page < 2 || page > totalItems - 3) reservedSpots -= 1;
 
   const shownItems = itemsThatFit - reservedSpots;
   const hiddenItems = totalItems - shownItems - (itemsThatFit < 5 ? 1 : 2);
@@ -40,14 +40,16 @@ const getCuts = (totalItems, itemsThatFit, page) => {
   let backHidden = Math.ceil(hiddenItems / 2);
 
   if (itemsThatFit > 4) {
-    if (page <= frontHidden) { // if current page would be in the front overflow
+    if (page <= frontHidden) {
+      // if current page would be in the front overflow
       let newFrontHidden = frontHidden - (frontHidden - page) - 1;
       newFrontHidden = newFrontHidden >= 0 ? newFrontHidden : 0;
       const delta = frontHidden - newFrontHidden;
 
       frontHidden -= delta;
       backHidden += delta;
-    } else if (page >= (totalItems - backHidden - 1)) { // if the current page would be in the back overflow
+    } else if (page >= totalItems - backHidden - 1) {
+      // if the current page would be in the back overflow
       let newBackHidden = totalItems - 2 - page;
       newBackHidden = newBackHidden >= 0 ? newBackHidden : 0;
       const delta = backHidden - newBackHidden;
@@ -67,7 +69,7 @@ const getCuts = (totalItems, itemsThatFit, page) => {
     front: frontHidden,
     back: backHidden,
   };
-}
+};
 
 export default class PaginationNav extends Component {
   constructor(props) {
@@ -145,10 +147,7 @@ export default class PaginationNav extends Component {
   };
 
   static getDerivedStateFromProps({ page, totalItems, itemsShown }, state) {
-    const {
-      page: currentPage,
-      prevPage,
-    } = state;
+    const { page: currentPage, prevPage } = state;
 
     const itemsThatFit = itemsShown >= 4 ? itemsShown : 4;
     const pageChanged = page !== prevPage;
@@ -156,8 +155,8 @@ export default class PaginationNav extends Component {
 
     return !pageChanged
       ? {
-        cuts,
-      }
+          cuts,
+        }
       : {
           page: (pageChanged && page) || currentPage,
           prevPage: page,
@@ -208,25 +207,20 @@ export default class PaginationNav extends Component {
   }
 
   renderDirectionButton(direction) {
-    const {
-      loop,
-      totalItems,
-      backwardText,
-      forwardText,
-    } = this.props;
+    const { loop, totalItems, backwardText, forwardText } = this.props;
     const { page } = this.state;
 
-    const disabled = !loop && (
-      (direction === 'forward' && page === totalItems - 1) || (direction === 'backward' && page === 0)
-    );
+    const disabled =
+      !loop &&
+      ((direction === 'forward' && page === totalItems - 1) ||
+        (direction === 'backward' && page === 0));
     const label = direction === 'forward' ? forwardText : backwardText;
     const icon = direction === 'forward' ? CaretRight16 : CaretLeft16;
-    const onClick = direction === 'forward' ? this.jumpToNext : this.jumpToPrevious;
+    const onClick =
+      direction === 'forward' ? this.jumpToNext : this.jumpToPrevious;
 
     return (
-      <li
-        className={`${prefix}--pagination-nav__list-item`}
-      >
+      <li className={`${prefix}--pagination-nav__list-item`}>
         <Button
           disabled={disabled}
           renderIcon={icon}
@@ -258,16 +252,14 @@ export default class PaginationNav extends Component {
     return (
       <li
         key={`item-${index}`}
-        className={`${prefix}--pagination-nav__list-item`}
-      >
+        className={`${prefix}--pagination-nav__list-item`}>
         <button
           className={classnames(`${prefix}--pagination-nav__page`, {
             [`${prefix}--pagination-nav__page--active`]: isActive,
           })}
           onClick={() => {
             this.jumpToItem(index);
-          }}
-        >
+          }}>
           <span className={`${prefix}--pagination-nav__accessibility-label`}>
             {itemLabel}
           </span>
@@ -284,25 +276,28 @@ export default class PaginationNav extends Component {
       return (
         <li className={`${prefix}--pagination-nav__list-item`}>
           <div className={`${prefix}--pagination-nav__select`}>
-            { /* eslint-disable-next-line jsx-a11y/no-onchange */ }
+            {/* eslint-disable-next-line jsx-a11y/no-onchange */}
             <select
               className={`${prefix}--pagination-nav__page ${prefix}--pagination-nav__page--select`}
               aria-label={`Select ${itemLabel} number`}
-              onChange={(e) => {
+              onChange={e => {
                 const index = Number(e.target.value);
                 this.jumpToItem(index);
-              }}
-            >
-              { /* eslint-disable-next-line jsx-a11y/control-has-associated-label */ }
+              }}>
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
               <option value="" hidden />
-              { [...Array(count)].map((e, i) => (
-                <option value={(fromIndex + i).toString()} key={`overflow-${fromIndex + i}`}>
-                  { fromIndex + i + 1 }
+              {[...Array(count)].map((e, i) => (
+                <option
+                  value={(fromIndex + i).toString()}
+                  key={`overflow-${fromIndex + i}`}>
+                  {fromIndex + i + 1}
                 </option>
-              )) }
+              ))}
             </select>
             <div className={`${prefix}--pagination-nav__select-icon-wrapper`}>
-              <OverflowMenuHorizontal16 className={`${prefix}--pagination-nav__select-icon`} />
+              <OverflowMenuHorizontal16
+                className={`${prefix}--pagination-nav__select-icon`}
+              />
             </div>
           </div>
         </li>
@@ -328,10 +323,7 @@ export default class PaginationNav extends Component {
       ...other
     } = this.props;
 
-    const {
-      page,
-      cuts,
-    } = this.state;
+    const { page, cuts } = this.state;
 
     const classNames = classnames(`${prefix}--pagination-nav`, className);
 
@@ -340,39 +332,30 @@ export default class PaginationNav extends Component {
     const startOffset = itemsShown < 5 && page > 1 ? 0 : 1;
 
     return (
-      <nav
-        className={classNames}
-        {...other}
-        aria-label="pagination"
-        key={page}
-      >
+      <nav className={classNames} {...other} aria-label="pagination" key={page}>
         <ul className={`${prefix}--pagination-nav__list`}>
-          { this.renderPreviousButton() }
-          {
-            // render first item if at least 5 items can be displayed
-            // or the current page is first or second item
-            (itemsShown >= 5 || page <= 1) && this.renderItem(0)
-          }
-          {
-            // render first overflow
-            this.renderOverflow(startOffset, cuts.front)
-          }
-          {
-            // render items between overflows
-            [...Array(totalItems)]
-              .map((e, i) => i)
-              .slice(startOffset + cuts.front, (1 + cuts.back) * -1)
-              .map((item) => this.renderItem(item))
-          }
-          {
-            // render second overflow
-            this.renderOverflow(totalItems - cuts.back - 1, cuts.back)
-          }
-          {
-            // render last item unless there is only one in total
-            totalItems > 1 && this.renderItem(totalItems - 1)
-          }
-          { this.renderNextButton() }
+          {this.renderPreviousButton()}
+
+          {// render first item if at least 5 items can be displayed
+          // or the current page is first or second item
+          (itemsShown >= 5 || page <= 1) && this.renderItem(0)}
+
+          {// render first overflow
+          this.renderOverflow(startOffset, cuts.front)}
+
+          {// render items between overflows
+          [...Array(totalItems)]
+            .map((e, i) => i)
+            .slice(startOffset + cuts.front, (1 + cuts.back) * -1)
+            .map(item => this.renderItem(item))}
+
+          {// render second overflow
+          this.renderOverflow(totalItems - cuts.back - 1, cuts.back)}
+
+          {// render last item unless there is only one in total
+          totalItems > 1 && this.renderItem(totalItems - 1)}
+
+          {this.renderNextButton()}
         </ul>
       </nav>
     );
