50ef990aaf55a67fb51c9c4f4f3c9a6b3cdf5407
Josh Black <josh@josh.black>
2020-07-09 11:59:18 -0500

    fix(combobox): use initialSelectedItem instead of defaultSelectedItem

--- packages/react/src/components/ComboBox/ComboBox.js
+++ packages/react/src/components/ComboBox/ComboBox.js
@@ -340,7 +340,7 @@ export default class ComboBox extends React.Component {
         onStateChange={this.handleOnStateChange}
         inputValue={this.state.inputValue || ''}
         itemToString={itemToString}
-        defaultSelectedItem={initialSelectedItem}
+        initialSelectedItem={initialSelectedItem}
         inputId={id}
         selectedItem={selectedItem}>
         {({
