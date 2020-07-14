82400ea5e59c3acee9a178ef4b6f7d259e6c9a54
TJ Egan <tw15egan@gmail.com>
2020-07-14 12:06:52 -0700

    fix(SideNav): a11y fixes for SideNavMenu, MenuItem

--- packages/react/src/components/UIShell/SideNavMenuItem.js
+++ packages/react/src/components/UIShell/SideNavMenuItem.js
@@ -23,8 +23,8 @@ const SideNavMenuItem = React.forwardRef(function SideNavMenuItem(props, ref) {
   });
 
   return (
-    <li className={className} role="none">
-      <Link {...rest} className={linkClassName} role="menuitem" ref={ref}>
+    <li className={className}>
+      <Link {...rest} className={linkClassName} ref={ref}>
         <SideNavLinkText>{children}</SideNavLinkText>
       </Link>
     </li>
