a8fd1d0eeaa395deaa433f827cc268806106828d
Josh Black <josh@josh.black>
2020-07-28 11:14:28 -0500

    fix(icon-build-helpers): update react builder to split up entrypoint

--- packages/icon-build-helpers/src/builders/react/builder.js
+++ packages/icon-build-helpers/src/builders/react/builder.js
@@ -44,20 +44,56 @@ const babelConfig = {
 };
 
 async function builder(metadata, { output }) {
+  // Using the metadata, we can build up a list of modules that we want included
+  // in the different bundles we ship.
+  //
+  // For each module, we need to create two ways of consuming it. The first way
+  // is as a dedicated module, the second as a single function that we could
+  // export in an entrypoint.
   const modules = metadata.icons.flatMap((icon) => {
     return icon.output.map((size) => {
-      const { source } = createIconComponent(
-        size.moduleName,
-        size.descriptor,
-        icon.deprecated
-      );
       return {
-        source,
+        source: createIconFlatExport(
+          size.moduleName,
+          size.descriptor,
+          icon.deprecated
+        ),
+        entrypoint: createIconEntrypoint(
+          size.moduleName,
+          size.descriptor,
+          icon.deprecated
+        ),
         filepath: size.filepath,
         moduleName: size.moduleName,
       };
     });
   });
+
+  // Given the current size of our icons (~5000 modules) we need to split up the
+  // entrypoint into chunks. In the past, we tried re-exporting from the source
+  // but this ended up causing slowdowns due to module resolution.
+  //
+  // As a result, we sort our list of modules into buckets and generate
+  // entrypoints based on those. Then, the root `index.js` file re-exports from
+  // these entrypoints. This is a hybrid approach between a single entrypoint
+  // that is too big for most tooling, and a re-export strategy that takes too
+  // long due to the number of modules along with the time it takes tooling to
+  // process them.
+  const BUCKET_SIZE = 250;
+  const BUCKET_COUNT = Math.ceil(modules.length / BUCKET_SIZE);
+  const buckets = Array(BUCKET_COUNT);
+
+  for (let i = 0; i < buckets.length; i++) {
+    const start = BUCKET_SIZE * i;
+    const end = Math.min(start + BUCKET_SIZE, modules.length) - 1;
+    const bucket = Array(end - start + 1);
+
+    for (let j = start; j <= end; j++) {
+      bucket[j - start] = modules[j];
+    }
+
+    buckets[i] = bucket;
+  }
 
   const files = {
     'index.js': `${BANNER}\n\nexport { default as Icon } from './Icon.js';`,
@@ -66,18 +102,26 @@ async function builder(metadata, { output }) {
     'index.js': 'index.js',
   };
   for (const m of modules) {
-    const file = `${BANNER}
-      import React from 'react';
-      import Icon from './Icon.js';
-      ${m.source}
-      export default ${m.moduleName};
-    `;
-
-    files[m.filepath] = file;
+    files[m.filepath] = m.entrypoint;
     input[m.filepath] = m.filepath;
-    files[
-      'index.js'
-    ] += `\nexport { default as ${m.moduleName} } from '${m.filepath}';`;
+  }
+
+  for (let i = 0; i < buckets.length; i++) {
+    const bucket = buckets[i];
+    const filename = `__generated__/bucket-${i}.js`;
+
+    input[filename] = filename;
+    files[filename] = `${BANNER}
+
+import React from 'react';
+import Icon from './Icon.js';
+
+const didWarnAboutDeprecation = {};`;
+
+    for (const m of bucket) {
+      files[filename] += `export ${m.source}`;
+      files['index.js'] += `export { ${m.moduleName} } from '${filename}';`;
+    }
   }
 
   const bundle = await rollup({
@@ -145,61 +189,6 @@ async function builder(metadata, { output }) {
 }
 
 /**
- * Generate an icon component, which in our case is the string representation
- * of the component, from a given moduleName and icon descriptor.
- * @param {string} moduleName
- * @param {object} descriptor
- * @param {boolean} [isDeprecated]
- * @returns {object}
- */
-function createIconComponent(moduleName, descriptor, isDeprecated = false) {
-  const { attrs, content } = descriptor;
-  const { width, height, viewBox, ...rest } = attrs;
-
-  const deprecatedTopLevel = isDeprecated
-    ? 'let didWarnAboutDeprecation = false;'
-    : '';
-  const deprecatedBlock = isDeprecated
-    ? `
-    if (__DEV__) {
-      if (!didWarnAboutDeprecation) {
-        didWarnAboutDeprecation = true;
-        console.warn(
-          \`The ${moduleName} component has been deprecated and will be \` +
-          \`removed in the next major version of @carbon/icons-react.\`
-        );
-      }
-    }
-    `
-    : '';
-
-  const source = `
-    ${deprecatedTopLevel}
-    const ${moduleName} = /*#__PURE__*/ React.forwardRef(
-      function ${moduleName}({ children, ...rest }, ref) {
-        ${deprecatedBlock}
-        return (
-          <Icon
-            width={${width}}
-            height={${height}}
-            viewBox="${viewBox}"
-            ${formatAttributes(rest)}
-            ref={ref}
-            {...rest}>
-            ${content.map(convertToJSX).join('\n')}
-            {children}
-          </Icon>
-        );
-      }
-    );
-  `;
-
-  return {
-    source,
-  };
-}
-
-/**
  * Convert the given node to a JSX string source
  * @param {object} node
  * @returns {string}
@@ -237,6 +226,81 @@ function formatAttributes(attrs) {
     }
     return acc + ' ' + attribute;
   }, '');
+}
+
+function createIconFlatExport(moduleName, descriptor, isDeprecated = false) {
+  const deprecatedBlock = isDeprecated
+    ? `
+    if (__DEV__) {
+      if (!didWarnAboutDeprecation['${moduleName}']) {
+        didWarnAboutDeprecation['${moduleName}'] = true;
+        console.warn(
+          \`The ${moduleName} component has been deprecated and will be \` +
+          \`removed in the next major version of @carbon/icons-react.\`
+        );
+      }
+    }
+    `
+    : '';
+  return createIconSource(moduleName, descriptor, deprecatedBlock);
+}
+
+function createIconEntrypoint(moduleName, descriptor, isDeprecated = false) {
+  const deprecatedPreamble = isDeprecated
+    ? 'let didWarnAboutDeprecation = false;'
+    : '';
+  const deprecatedBlock = isDeprecated
+    ? `
+    if (__DEV__) {
+      if (!didWarnAboutDeprecation) {
+        didWarnAboutDeprecation = true;
+        console.warn(
+          \`The ${moduleName} component has been deprecated and will be \` +
+          \`removed in the next major version of @carbon/icons-react.\`
+        );
+      }
+    }
+    `
+    : '';
+  const source = createIconSource(moduleName, descriptor, deprecatedBlock);
+  return `${BANNER}
+import React from 'react';
+import Icon from './Icon.js';
+${deprecatedPreamble}
+${source}
+export default ${moduleName};
+`;
+}
+
+/**
+ * Generate an icon component, which in our case is the string representation
+ * of the component, from a given moduleName and icon descriptor.
+ * @param {string} moduleName
+ * @param {object} descriptor
+ * @param {string} customBlock
+ */
+function createIconSource(moduleName, descriptor, customBlock = '') {
+  const { attrs, content } = descriptor;
+  const { width, height, viewBox, ...rest } = attrs;
+  return `
+    const ${moduleName} = /*#__PURE__*/ React.forwardRef(
+      function ${moduleName}({ children, ...rest }, ref) {
+        ${customBlock}
+        return (
+          <Icon
+            width={${width}}
+            height={${height}}
+            viewBox="${viewBox}"
+            ${formatAttributes(rest)}
+            ref={ref}
+            {...rest}>
+            ${content.map(convertToJSX).join('\n')}
+            {children}
+          </Icon>
+        );
+      }
+    );
+  `;
 }
 
 module.exports = builder;
