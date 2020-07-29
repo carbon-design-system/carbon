319588beee014039d40ba25ba99515c21ae0f799
Josh Black <josh@josh.black>
2020-07-29 10:48:32 -0500

    docs(accordion): update default example

--- packages/react/src/components/Accordion/Accordion-story.js
+++ packages/react/src/components/Accordion/Accordion-story.js
@@ -58,15 +58,14 @@ export const accordion = () => (
       </p>
     </AccordionItem>
     <AccordionItem title="Section 3 title">
-      <Button>This is a button.</Button>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
     </AccordionItem>
-    <AccordionItem
-      title={
-        <span>
-          Section 4 title (<em>the title can be a node</em>)
-        </span>
-      }
-      {...props}>
+    <AccordionItem title="Section 4 title">
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
@@ -77,11 +76,11 @@ export const accordion = () => (
   </Accordion>
 );
 
-export const skeleton = () => (
-  <div style={{ width: '500px' }}>
-    <AccordionSkeleton open count={4} />
-  </div>
-);
+export const skeleton = () => <AccordionSkeleton open count={4} />;
+
+skeleton.story = {
+  decorators: [(story) => <div style={{ width: '500px' }}>{story()}</div>],
+};
 
 const props = {
   onClick: action('onClick'),
