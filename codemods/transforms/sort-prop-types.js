b28524f95f2474afff634f916885f250f5d3e9d4
Josh Black <josh@josh.black>
2020-08-05 17:29:31 -0500

    feat(project): add codemods package

---
+++ codemods/transforms/sort-prop-types.js
@@ -0,0 +1,87 @@
+/**
+ * Copyright IBM Corp. 2016, 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+function transform(fileInfo, api, options) {
+  const printOptions = options.printOptions || {
+    quote: 'single',
+    trailingComma: true,
+  };
+  const j = api.jscodeshift;
+  const root = j(fileInfo.source);
+
+  root
+    .find(j.AssignmentExpression, {
+      left: {
+        type: 'MemberExpression',
+        property: {
+          name: 'propTypes',
+        },
+      },
+    })
+    .forEach((path) => {
+      path.node.right.properties.sort((a, b) => {
+        if (a.type === 'Property' && b.type === 'Property') {
+          if (getPropName(a) > getPropName(b)) {
+            return 1;
+          }
+          return -1;
+          return getPropName(a).localeCompare(getPropName(b));
+        }
+
+        if (a.type === 'SpreadElement' && b.type === 'SpreadElement') {
+          if (getPropName(a) > getPropName(b)) {
+            return 1;
+          }
+          return -1;
+        }
+
+        if (a.type === 'SpreadElement') {
+          return -1;
+        }
+
+        return 1;
+      });
+    });
+
+  root
+    .find(j.ClassProperty, {
+      key: {
+        name: 'propTypes',
+      },
+    })
+    .forEach((path) => {
+      path.node.value.properties.sort((a, b) => {
+        if (getPropName(a) > getPropName(b)) {
+          return 1;
+        }
+        return -1;
+      });
+    });
+
+  function getPropName(node) {
+    switch (node.type) {
+      case 'SpreadElement':
+        return node.argument.name;
+      case 'Property':
+        if (node.key.type === 'Identifier') {
+          return node.key.name;
+        }
+        if (node.key.type === 'Literal') {
+          return node.key.value;
+        }
+        console.log(node);
+      default:
+        throw new Error(`Unknown node of type: ${node.type}`);
+    }
+  }
+
+  return root.toSource(printOptions);
+}
+
+module.exports = transform;
