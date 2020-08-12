aa3a62767305dadb27aa1c15b40f758fb1c76684
Josh Black <josh@josh.black>
2020-08-12 14:12:15 -0500

    feat(project): add accessibility-react package

---
+++ packages/accessibility-react/.storybook/main.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright IBM Corp. 2016, 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/preset-scss',
+  ],
+};
---
+++ packages/accessibility-react/.storybook/preview.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright IBM Corp. 2016, 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import './styles.scss';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
