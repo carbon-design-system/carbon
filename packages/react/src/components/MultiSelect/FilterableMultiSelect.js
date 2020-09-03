0ecddd6f82c7fd79c966e8aa14d043bd88cfaa93
emyarod <emyarod@users.noreply.github.com>
2020-09-03 08:19:56 -0500

    fix(FilterableMultiSelect): set light text input styles with light prop (#6740)
    Co-authored-by: TJ Egan <tw15egan@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/react/src/components/MultiSelect/FilterableMultiSelect.js
+++ packages/react/src/components/MultiSelect/FilterableMultiSelect.js
@@ -315,6 +315,7 @@ export default class FilterableMultiSelect extends React.Component {
     ) : null;
     const inputClasses = cx(`${prefix}--text-input`, {
       [`${prefix}--text-input--empty`]: !this.state.inputValue,
+      [`${prefix}--text-input--light`]: light,
     });
     const input = (
       <Selection
