8232e38ed1a89abaf687be263f4dc64459bde210
Dominik Brugger <dbrugger@de.ibm.com>
2020-10-17 03:55:34 +0200

    chore: avoid circular dependencies in DataTable and FileUploader

--- packages/react/src/components/FileUploader/FileUploaderItem.js
+++ packages/react/src/components/FileUploader/FileUploaderItem.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2016, 2018
+ * Copyright IBM Corp. 2016, 2020
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -9,7 +9,7 @@ import { settings } from 'carbon-components';
 import cx from 'classnames';
 import PropTypes from 'prop-types';
 import React, { useRef } from 'react';
-import { Filename } from './';
+import Filename from './Filename';
 import { keys, matches } from '../../internal/keyboard';
 import uid from '../../tools/uniqueId';
 
