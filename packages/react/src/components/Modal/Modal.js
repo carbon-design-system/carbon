a8ea55cbaf66c9d53c28f3f29c67d8c53ce0a9dd
TJ Egan <tw15egan@gmail.com>
2020-11-10 11:08:18 -0800

    fix(wrapFocus): update selectorTabbable list to omit disabled items

--- packages/react/src/components/Modal/Modal.js
+++ packages/react/src/components/Modal/Modal.js
@@ -435,7 +435,6 @@ export default class Modal extends Component {
             <Button
               kind={danger ? 'danger' : 'primary'}
               disabled={primaryButtonDisabled}
-              tabIndex={primaryButtonDisabled ? -1 : 0}
               onClick={onRequestSubmit}
               ref={this.button}>
               {primaryButtonText}
