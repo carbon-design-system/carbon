e55821d9a03bd18311a2f4c362a55cc383144b72
Josh Black <josh@josh.black>
2020-11-10 13:20:00 -0600

    chore: check-in work

---
+++ packages/utility-css/examples/preview/pages/_app.js
@@ -0,0 +1,5 @@
+import '../scss/styles.scss';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ packages/utility-css/examples/preview/pages/index.js
@@ -0,0 +1,10 @@
+export default function IndexPage() {
+  return (
+    <>
+      <section>
+        <h2>Spacing</h2>
+        <div className="bg-ui-04 pt-5">pt-5</div>
+      </section>
+    </>
+  );
+}
