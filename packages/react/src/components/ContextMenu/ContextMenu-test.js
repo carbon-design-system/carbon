e5ad6405503bb8c16af95f97faea2cb2d574e004
Jan Hassel <jan.hassel@ibm.com>
2020-12-08 18:11:04 +0100

    chore: run prettier

--- packages/react/src/components/ContextMenu/ContextMenu-test.js
+++ packages/react/src/components/ContextMenu/ContextMenu-test.js
@@ -51,67 +51,107 @@ describe('ContextMenu', () => {
       it('renders props.label', () => {
         const wrapper = mount(<ContextMenuOption label="Copy" />);
 
-        expect(wrapper.find(`span.${prefix}--context-menu-option__label`).text()).toBe('Copy');
-        expect(wrapper.find(`span.${prefix}--context-menu-option__label`).prop('title')).toBe('Copy');
+        expect(
+          wrapper.find(`span.${prefix}--context-menu-option__label`).text()
+        ).toBe('Copy');
+        expect(
+          wrapper
+            .find(`span.${prefix}--context-menu-option__label`)
+            .prop('title')
+        ).toBe('Copy');
       });
 
       it('renders props.renderIcon when provided', () => {
-        const wrapper = mount(<ContextMenuOption label="Copy" renderIcon={Copy16} indented />);
+        const wrapper = mount(
+          <ContextMenuOption label="Copy" renderIcon={Copy16} indented />
+        );
 
-        expect(wrapper.find(`div.${prefix}--context-menu-option__icon`).length).toBeGreaterThan(0);
+        expect(
+          wrapper.find(`div.${prefix}--context-menu-option__icon`).length
+        ).toBeGreaterThan(0);
       });
 
       it('renders props.shortcut when provided', () => {
-        const wrapper = mount(<ContextMenuOption label="Copy" shortcut="⌘C" shortcutText="command c" />);
+        const wrapper = mount(
+          <ContextMenuOption
+            label="Copy"
+            shortcut="⌘C"
+            shortcutText="command c"
+          />
+        );
 
-        expect(wrapper.find(`div.${prefix}--context-menu-option__info`).length).toBeGreaterThan(0);
-        expect(wrapper.find(`div.${prefix}--context-menu-option__info`).text()).toBe('⌘C');
-        expect(wrapper.find(`li.${prefix}--context-menu-option`).prop('aria-label')).toContain('command c');
+        expect(
+          wrapper.find(`div.${prefix}--context-menu-option__info`).length
+        ).toBeGreaterThan(0);
+        expect(
+          wrapper.find(`div.${prefix}--context-menu-option__info`).text()
+        ).toBe('⌘C');
+        expect(
+          wrapper.find(`li.${prefix}--context-menu-option`).prop('aria-label')
+        ).toContain('command c');
       });
 
       it('respects props.disabled', () => {
         const wrapper = mount(<ContextMenuOption label="Copy" disabled />);
-        const content = wrapper.find(`button.${prefix}--context-menu-option__content`);
+        const content = wrapper.find(
+          `button.${prefix}--context-menu-option__content`
+        );
 
         expect(content.prop('disabled')).toBe(true);
-        expect(content.hasClass(`${prefix}--context-menu-option__content--disabled`)).toBe(true);
-        expect(wrapper.find(`li.${prefix}--context-menu-option`).prop('aria-disabled')).toBe(true);
+        expect(
+          content.hasClass(`${prefix}--context-menu-option__content--disabled`)
+        ).toBe(true);
+        expect(
+          wrapper
+            .find(`li.${prefix}--context-menu-option`)
+            .prop('aria-disabled')
+        ).toBe(true);
       });
 
       it('renders props.children as submenu', () => {
-        const wrapper = mount((
+        const wrapper = mount(
           <ContextMenu>
             <ContextMenuOption label="Format">
               <ContextMenuOption label="Bold" />
               <ContextMenuOption label="Italic" />
             </ContextMenuOption>
           </ContextMenu>
-        ));
+        );
 
         const level1 = wrapper.find(`li.${prefix}--context-menu-option`).at(0);
 
         expect(level1.prop('aria-label')).toBe('Format');
-        expect(level1.find(`ul.${prefix}--context-menu`).length).toBeGreaterThan(0);
+        expect(
+          level1.find(`ul.${prefix}--context-menu`).length
+        ).toBeGreaterThan(0);
       });
     });
 
     describe('radiogroup', () => {
       it('receives the expected classes', () => {
-        const wrapper = mount(<ContextMenuRadioGroup label="Share with" items={['None', 'All']} />);
+        const wrapper = mount(
+          <ContextMenuRadioGroup label="Share with" items={['None', 'All']} />
+        );
         const container = wrapper.childAt(0);
 
-        expect(container.hasClass(`${prefix}--context-menu-radio-group`)).toBe(true);
+        expect(container.hasClass(`${prefix}--context-menu-radio-group`)).toBe(
+          true
+        );
       });
 
       it('has role "radiogroup"', () => {
-        const wrapper = mount(<ContextMenuRadioGroup label="Share with" items={['None', 'All']} />);
+        const wrapper = mount(
+          <ContextMenuRadioGroup label="Share with" items={['None', 'All']} />
+        );
         const container = wrapper.childAt(0);
 
         expect(container.prop('role')).toBe('radiogroup');
       });
 
       it('children have role "radio"', () => {
-        const wrapper = mount(<ContextMenuRadioGroup label="Share with" items={['None', 'All']} />);
+        const wrapper = mount(
+          <ContextMenuRadioGroup label="Share with" items={['None', 'All']} />
+        );
         const options = wrapper.find(`li.${prefix}--context-menu-option`);
 
         expect(options.every('li[role="radio"]')).toBe(true);
@@ -132,7 +172,9 @@ describe('ContextMenu', () => {
         const wrapper = mount(<ContextMenuDivider />);
         const container = wrapper.childAt(0);
 
-        expect(container.hasClass(`${prefix}--context-menu-divider`)).toBe(true);
+        expect(container.hasClass(`${prefix}--context-menu-divider`)).toBe(
+          true
+        );
       });
 
       it('has role "separator"', () => {
