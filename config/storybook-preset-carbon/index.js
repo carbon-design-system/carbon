d1e2dd7231f28a535be1fd28cab9c3f5952f09f9
Alessandra Davila <aledavila@ibm.com>
2021-01-11 15:32:11 -0600

    feat(storybook-config): add custom panel

--- config/storybook-preset-carbon/index.js
+++ config/storybook-preset-carbon/index.js
@@ -30,6 +30,10 @@ module.exports = {
     '@storybook/addon-docs',
     '@storybook/addon-notes/register',
     'storybook-readme/register',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "./carbon-theme-addon/src/preset.js"
 
     // Phase 3: port over custom panels/add-ons
   ],
