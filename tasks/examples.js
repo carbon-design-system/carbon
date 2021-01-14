a389ed005231a893a77e07fed831f96510f4dcf6
Josh Black <josh@josh.black>
2021-01-14 17:36:05 -0600

    chore(examples): debug example script

--- tasks/examples.js
+++ tasks/examples.js
@@ -87,70 +87,74 @@ async function main() {
     (pkg) => Array.isArray(pkg.examples) && pkg.examples.length !== 0
   );
 
-  await Promise.all(
-    packagesWithExamples.map(async (pkg) => {
-      reporter.info(`Building examples in package \`${pkg.name}\``);
+  // await Promise.all(
+  // packagesWithExamples.map(async (pkg) => {
+  for (const pkg of packagesWithExamples) {
+    reporter.info(`Building examples in package \`${pkg.name}\``);
 
-      const { examples, name } = pkg;
-      const packageDir = path.join(BUILD_DIR, name, 'examples');
+    const { examples, name } = pkg;
+    const packageDir = path.join(BUILD_DIR, name, 'examples');
 
-      await fs.ensureDir(packageDir);
+    await fs.ensureDir(packageDir);
 
-      await Promise.all(
-        examples.map(async (example) => {
-          reporter.info(
-            `Building example \`${example.name}\` in package \`${pkg.name}\``
-          );
-
-          const exampleDir = path.join(packageDir, example.name);
-          const exampleBuildDir = path.join(example.filepath, 'build');
-          const packageJsonPath = path.join(example.filepath, 'package.json');
-          const packageJson = await fs.readJson(packageJsonPath);
-
-          await fs.ensureDir(exampleDir);
-
-          if (packageJson.scripts.build) {
-            spawn.sync('yarn', ['install'], {
-              stdio: 'ignore',
-              cwd: example.filepath,
-            });
-            spawn.sync('yarn', ['build'], {
-              stdio: 'ignore',
-              cwd: example.filepath,
-            });
-          }
-
-          console.log(example.name, exampleBuildDir);
-
-          if (await fs.pathExists(exampleBuildDir)) {
-            console.log('build dir path exists');
-            await fs.copy(exampleBuildDir, exampleDir);
-            return;
-          }
-
-          console.log('build dir does not exist');
-
-          await fs.copy(example.filepath, exampleDir, {
-            filter(src) {
-              const relativePath = path.relative(example.filepath, src);
-              if (relativePath.includes('node_modules')) {
-                return false;
-              }
-              if (relativePath[0] === '.') {
-                return false;
-              }
-              return true;
-            },
-          });
-          reporter.success(
-            `Built example \`${example.name}\` in package \`${pkg.name}\``
-          );
-        })
+    // await Promise.all(
+    // examples.map(async (example) => {
+    for (const example of examples) {
+      reporter.info(
+        `Building example \`${example.name}\` in package \`${pkg.name}\``
       );
 
-      reporter.success(`Built examples in package \`${pkg.name}\``);
-    })
-  );
+      const exampleDir = path.join(packageDir, example.name);
+      const exampleBuildDir = path.join(example.filepath, 'build');
+      const packageJsonPath = path.join(example.filepath, 'package.json');
+      const packageJson = await fs.readJson(packageJsonPath);
+
+      await fs.ensureDir(exampleDir);
+
+      if (packageJson.scripts.build) {
+        spawn.sync('yarn', ['install'], {
+          stdio: 'ignore',
+          cwd: example.filepath,
+        });
+        spawn.sync('yarn', ['build'], {
+          stdio: 'inherit',
+          cwd: example.filepath,
+        });
+      }
+
+      console.log(example.name, exampleBuildDir);
+
+      if (await fs.pathExists(exampleBuildDir)) {
+        console.log('build dir path exists');
+        await fs.copy(exampleBuildDir, exampleDir);
+        return;
+      }
+
+      console.log('build dir does not exist');
+
+      await fs.copy(example.filepath, exampleDir, {
+        filter(src) {
+          const relativePath = path.relative(example.filepath, src);
+          if (relativePath.includes('node_modules')) {
+            return false;
+          }
+          if (relativePath[0] === '.') {
+            return false;
+          }
+          return true;
+        },
+      });
+      reporter.success(
+        `Built example \`${example.name}\` in package \`${pkg.name}\``
+      );
+    }
+    // })
+    // );
+
+    reporter.success(`Built examples in package \`${pkg.name}\``);
+  }
+  // })
+  // );
 
   const links = packagesWithExamples.reduce((html, pkg) => {
     const links = pkg.examples.reduce((acc, example) => {
