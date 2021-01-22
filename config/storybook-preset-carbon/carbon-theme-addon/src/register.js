0d8322526a4775d10eb2e5ad2a3224a944253f1c
Alessandra Davila <aledavila@ibm.com>
2021-01-21 18:54:39 -0600

    chore(storybook-config): remove merge messages

--- config/storybook-preset-carbon/carbon-theme-addon/src/register.js
+++ config/storybook-preset-carbon/carbon-theme-addon/src/register.js
@@ -1,9 +1,5 @@
 /**
-<<<<<<< HEAD
- * Copyright IBM Corp. 2016, 2018
-=======
  * Copyright IBM Corp. 2021, 2021
->>>>>>> ccbb482a864620d7a10bf8593864efc15a13896f
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
