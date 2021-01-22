be3b939491ccd9e337a42fc83ba467c57e907db3
Andrea N. Cardona <andreancardona@gmail.com>
2021-01-21 20:06:44 -0700

    chore(release): v10.27.0-rc.3

--- packages/elements/src/index.js
+++ packages/elements/src/index.js
@@ -162,6 +162,37 @@ export {
   spacing11,
   spacing12,
 } from '@carbon/layout';
+export {
+  caption01,
+  label01,
+  helperText01,
+  bodyShort01,
+  bodyShort02,
+  bodyLong02,
+  code01,
+  code02,
+  heading01,
+  productiveHeading01,
+  heading02,
+  productiveHeading02,
+  productiveHeading03,
+  productiveHeading04,
+  productiveHeading05,
+  productiveHeading06,
+  productiveHeading07,
+  expressiveHeading01,
+  expressiveHeading02,
+  expressiveHeading03,
+  expressiveHeading04,
+  expressiveHeading05,
+  expressiveHeading06,
+  expressiveParagraph01,
+  quotation01,
+  quotation02,
+  display01,
+  display02,
+  display03,
+  display04,
+} from '@carbon/type';
 export * from '@carbon/themes';
 export * from '@carbon/motion';
-export * from '@carbon/type';
