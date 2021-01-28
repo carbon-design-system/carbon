0f26f4b87cce2ec1a324061cad3a2785b83de4c7
Alessandra Davila <aledavila@ibm.com>
2021-01-28 14:47:33 -0600

    chore(storybook-config): adds clean script

--- config/storybook-preset-carbon/carbon-theme-addon/src/components/Panel.js
+++ config/storybook-preset-carbon/carbon-theme-addon/src/components/Panel.js
@@ -5,6 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+'use strict';
+
 import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form } from '@storybook/components';
