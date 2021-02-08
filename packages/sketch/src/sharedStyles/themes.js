625c2fb755c303fcd197a77d211d9c41e7c01395
emyarod <afw5059@gmail.com>
2021-02-08 13:26:20 -0600

    fix(sketch): remove styleType param from syncThemeColorStyles

--- packages/sketch/src/sharedStyles/themes.js
+++ packages/sketch/src/sharedStyles/themes.js
@@ -21,10 +21,9 @@ const { colors } = tokens;
 /**
  * Sync theme color shared styles to the given document and return the result
  * @param {Document} document
- * @param {string} styleType
  * @returns {Array<SharedStyle>}
  */
-export function syncThemeColorStyles(document, styleType) {
+export function syncThemeColorStyles(document) {
   const themes = {
     'White theme': white,
     'Gray 10 theme': g10,
@@ -48,7 +47,6 @@ export function syncThemeColorStyles(document, styleType) {
           document,
           name,
           value: themes[theme][token],
-          type: styleType,
         });
       });
   });
