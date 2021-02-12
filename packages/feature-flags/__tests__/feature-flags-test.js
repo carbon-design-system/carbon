507ad0661e07dc1a39e061379b382c85f502c27e
Josh Black <josh@josh.black>
2021-02-12 15:47:12 -0600

    feat(feature-flags): rename exports, add merge, and update tests

--- packages/feature-flags/__tests__/feature-flags-test.js
+++ packages/feature-flags/__tests__/feature-flags-test.js
@@ -5,36 +5,100 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import {
-  addFeatureFlag,
-  enableFeatureFlag,
-  disableFeatureFlag,
-  featureFlagEnabled,
-} from '../src';
-
 describe('@carbon/feature-flags', () => {
-  it('should let the user check if a feature flag is enabled', () => {
-    addFeatureFlag('test', false);
-    expect(featureFlagEnabled('test')).toBe(false);
+  let FeatureFlag;
 
-    enableFeatureFlag('test');
-    expect(featureFlagEnabled('test')).toBe(true);
-
-    disableFeatureFlag('test');
-    expect(featureFlagEnabled('test')).toBe(false);
+  beforeEach(() => {
+    jest.resetModules();
+    FeatureFlag = require('../src');
   });
 
-  it('should throw an error if a flag does not exist', () => {
-    expect(() => {
-      enableFeatureFlag('does-not-exist');
-    }).toThrow();
+  describe('add', () => {
+    it('should add the given flag and set whether its enabled', () => {
+      FeatureFlag.add('flag-a', true);
+      FeatureFlag.add('flag-b', false);
 
-    expect(() => {
-      disableFeatureFlag('does-not-exist');
-    }).toThrow();
+      expect(FeatureFlag.enabled('flag-a')).toBe(true);
+      expect(FeatureFlag.enabled('flag-b')).toBe(false);
+    });
 
-    expect(() => {
-      featureFlagEnabled('does-not-exist');
-    }).toThrow();
+    it('should throw if a duplicate flag name is given', () => {
+      FeatureFlag.add('flag-a', true);
+
+      expect(() => {
+        FeatureFlag.add('flag-a', true);
+      }).toThrow();
+    });
+  });
+
+  describe('enable', () => {
+    it('should enable the given feature flag', () => {
+      FeatureFlag.add('flag-a', false);
+      expect(FeatureFlag.enabled('flag-a')).toBe(false);
+
+      FeatureFlag.enable('flag-a');
+      expect(FeatureFlag.enabled('flag-a')).toBe(true);
+    });
+
+    it('should throw if the given flag does not exist', () => {
+      expect(() => {
+        FeatureFlag.enable('flag-a');
+      }).toThrow();
+    });
+  });
+
+  describe('disable', () => {
+    it('should disable the given feature flag', () => {
+      FeatureFlag.add('flag-a', true);
+      expect(FeatureFlag.enabled('flag-a')).toBe(true);
+
+      FeatureFlag.disable('flag-a');
+      expect(FeatureFlag.enabled('flag-a')).toBe(false);
+    });
+
+    it('should throw if the given flag does not exist', () => {
+      expect(() => {
+        FeatureFlag.disable('flag-a');
+      }).toThrow();
+    });
+  });
+
+  describe('enabled', () => {
+    it('should return whether a flag is enabled or disabled', () => {
+      FeatureFlag.add('flag-a', true);
+      FeatureFlag.add('flag-b', false);
+
+      expect(FeatureFlag.enabled('flag-a')).toBe(true);
+      expect(FeatureFlag.enabled('flag-b')).toBe(false);
+    });
+
+    it('should throw if the given flag does not exist', () => {
+      expect(() => {
+        FeatureFlag.enabled('flag-a');
+      }).toThrow();
+    });
+  });
+
+  describe('merge', () => {
+    it('should set each feature flag given', () => {
+      FeatureFlag.merge({
+        'flag-a': true,
+        'flag-b': false,
+      });
+
+      expect(FeatureFlag.enabled('flag-a')).toBe(true);
+      expect(FeatureFlag.enabled('flag-b')).toBe(false);
+    });
+
+    it('should override duplicate keys with the given flag', () => {
+      FeatureFlag.add('flag-b', true);
+      FeatureFlag.merge({
+        'flag-a': true,
+        'flag-b': false,
+      });
+
+      expect(FeatureFlag.enabled('flag-a')).toBe(true);
+      expect(FeatureFlag.enabled('flag-b')).toBe(false);
+    });
   });
 });
