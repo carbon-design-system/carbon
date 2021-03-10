59bfae095fe61213e98ebc31b3165b876f8d579d
Jan Hassel <jan.hassel@ibm.com>
2021-03-10 17:58:50 +0100

    feat: add context-menu (#7350)
    * chore(ContextMenu): scaffold new component
    
    * feat(context-menu): build basic structure and styling
    
    * feat(context-menu): build basic nesting support
    
    * feat(context-menu): use button markup
    
    * feat(context-menu): add support for disabled options
    
    * feat(context-menu): add support fort shortcuts
    
    * feat(context-menu): add support for icons
    
    * feat(context-menu): add divider component
    
    * feat(context-menu): add support for selectable option
    
    * fix(context-menu): also indent options when sibling is selectable
    
    * feat(context-menu): add support for radio groups
    
    * fix(context-menu): add support for initialSelectedItem in radio groups
    
    * fix(context-menu): also indent options when sibling is radio
    
    * feat(context-menu): add support for keyboard navigation
    
    * feat(context-menu): dynamically reverse menu direction
    
    * feat(context-menu): add props.onClose
    
    * chore(context-menu): add inline notification to story
    
    * fix(context-menu): fix styling issues
    
    * refactor(context-menu): rename SelectableContextMenuOption to ContextMenuSelectableOption
    
    * fix(context-menu): don't overwrite checkbox or radio role
    
    * fix(context-menu): automatically close menu after item was clicked
    
    * fix(context-menu): improve behaviour with screenreaders
    
    * fix(context-menu): add onChange to radio group and checkbox
    
    * test(context-menu): update public-api and index snapshots
    
    * chore(context-menu): correct formatting
    
    * test(context-menu): add render tests
    
    * chore: run prettier
    
    * fix(context-menu): focus entire menu on open
    
    * fix(context-menu): only open submenu on long hover or right arrow
    
    * feat(context-menu): always keep menu within body boundaries
    
    * fix(context-menu): implement review feedback
    
    * fix(context-menu): don't close when disabled or divider was clicked
    
    * fix(context-menu): prevent menu from closing immediately in Safari
    
    * refactor(context-menu): separate between external and internal component
    
    * refactor(context-menu): mark as unstable
    
    * feat(context-menu): export useContextMenu hook
    
    * test(context-menu): replace Option component with Item component
    
    * test: update react index snapshot
    
    * fix(context-menu): open submenus on enter
    
    * fix(context-menu): fix lint issue
    
    * fix(context-menu): use menuitem* roles for checkbox and radio
    
    * fix(context-menu): remove aria-label from menuitems
    
    * refactor(context-menu): remove button markup
    
    * test(context-menu): update test to reflect markup changes
    
    * fix(context-menu): use ul without role for radio groups
    
    * fix(context-menu): fix aria-haspopup attribute on parent node
    
    * fix(context-menu): add aria-expanded attribute on items with children
    
    * fix(context-menu): add "group" role to radio groups
    
    * fix(context-menu): extract radio items from group when top level
    
    * chore: add story with multiple radio groups
    
    * feat(context-menu): add ContextMenuGroup component
    
    * fix(context-menu): support "space" to activate items
    
    * chore: add context menu story to experimental group
    
    * fix(context-menu): prevents error where user must click twice to open
    
    Co-authored-by: Andrea N. Cardona <andreancardona@gmail.com>
    Co-authored-by: TJ Egan <tw15egan@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ packages/react/src/components/ContextMenu/ContextMenu-story.js
@@ -0,0 +1,154 @@
+/**
+ * Copyright IBM Corp. 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { InlineNotification } from '../Notification';
+
+import ContextMenu, {
+  ContextMenuDivider,
+  ContextMenuGroup,
+  ContextMenuItem,
+  ContextMenuRadioGroup,
+  ContextMenuSelectableItem,
+  useContextMenu,
+} from '../ContextMenu';
+
+export default {
+  title: 'Experimental/unstable_ContextMenu',
+  parameters: {
+    component: ContextMenu,
+  },
+};
+
+const InfoBanners = () => (
+  <>
+    <InlineNotification
+      kind="info"
+      title="Experimental component"
+      subtitle="This component is considered experimental. Its API may change until the stable version is released."
+      lowContrast
+      hideCloseButton
+    />
+    <InlineNotification
+      kind="info"
+      title="Context menu"
+      subtitle="Right-click anywhere on this page to access an example implementation of this component."
+      lowContrast
+      hideCloseButton
+    />
+  </>
+);
+
+const Story = (items) => {
+  const contextMenuProps = useContextMenu();
+
+  function renderItem(item, i) {
+    switch (item.type) {
+      case 'item':
+        return (
+          <ContextMenuItem
+            key={i}
+            label={item.label}
+            shortcut={item.shortcut}
+            disabled={item.disabled}
+            onClick={!item.children ? action('onClick') : null}>
+            {item.children && item.children.map(renderItem)}
+          </ContextMenuItem>
+        );
+      case 'divider':
+        return <ContextMenuDivider key={i} />;
+      case 'selectable':
+        return (
+          <ContextMenuSelectableItem
+            key={i}
+            label={item.label}
+            initialChecked={item.initialChecked}
+            onChange={action('onChange')}
+          />
+        );
+      case 'radiogroup':
+        return (
+          <ContextMenuRadioGroup
+            key={i}
+            label={item.label}
+            items={item.items}
+            initialSelectedItem={item.initialSelectedItem}
+            onChange={action('onChange')}
+          />
+        );
+      case 'group':
+        return (
+          <ContextMenuGroup key={i} label={item.label}>
+            {item.children && item.children.map(renderItem)}
+          </ContextMenuGroup>
+        );
+    }
+  }
+
+  return (
+    <div style={{ height: 'calc(100vh - 6.25rem)' }}>
+      <InfoBanners />
+      <ContextMenu {...contextMenuProps}>{items.map(renderItem)}</ContextMenu>
+    </div>
+  );
+};
+
+export const _ContextMenu = () =>
+  Story([
+    {
+      type: 'item',
+      label: 'Share with',
+      children: [
+        {
+          type: 'radiogroup',
+          label: 'Share with',
+          items: ['None', 'Product team', 'Organization', 'Company'],
+          initialSelectedItem: 'Product team',
+        },
+      ],
+    },
+    { type: 'divider' },
+    { type: 'item', label: 'Cut', shortcut: '⌘X' },
+    { type: 'item', label: 'Copy', shortcut: '⌘C' },
+    { type: 'item', label: 'Copy path', shortcut: '⌥⌘C' },
+    { type: 'item', label: 'Paste', shortcut: '⌘V', disabled: true },
+    { type: 'item', label: 'Duplicate' },
+    { type: 'divider' },
+    { type: 'selectable', label: 'Publish', initialChecked: true },
+    { type: 'divider' },
+    { type: 'item', label: 'Rename', shortcut: '↩︎' },
+    { type: 'item', label: 'Delete', shortcut: '⌘⌫' },
+  ]);
+_ContextMenu.storyName = 'ContextMenu';
+
+export const _MultipleGroups = () =>
+  Story([
+    {
+      type: 'group',
+      label: 'Font style',
+      children: [
+        { type: 'selectable', label: 'Bold' },
+        { type: 'selectable', label: 'Italic' },
+      ],
+    },
+    { type: 'divider' },
+    {
+      type: 'radiogroup',
+      label: 'Text color',
+      items: ['Black', 'Blue', 'Red', 'Green'],
+      initialSelectedItem: 'Black',
+    },
+    { type: 'divider' },
+    {
+      type: 'radiogroup',
+      label: 'Text decoration',
+      items: ['None', 'Overline', 'Line-through', 'Underline'],
+      initialSelectedItem: 'None',
+    },
+  ]);
+_MultipleGroups.storyName = 'MultipleGroups';
