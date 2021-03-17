33af8e79e085666c5c51f1169d84ca2e6b73b135
Josh Black <josh@josh.black>
2021-03-17 13:30:45 -0500

    chore(grid): update example base path

--- packages/grid/examples/css-grid/next.config.js
+++ packages/grid/examples/css-grid/next.config.js
@@ -3,7 +3,7 @@ const { PHASE_PRODUCTION_BUILD } = require('next/constants')
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_PRODUCTION_BUILD) {
     return {
-        basePath: '/grid/examples/preview/css-grid',
+        basePath: '/grid/examples/css-grid',
     }
   }
 
