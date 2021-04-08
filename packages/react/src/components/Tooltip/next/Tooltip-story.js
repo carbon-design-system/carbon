0bed65966e8359298c606830655f983746034aff
Josh Black <josh@josh.black>
2021-04-08 10:05:42 -0500

    chore: check-in work

--- packages/react/src/components/Tooltip/next/Tooltip-story.js
+++ packages/react/src/components/Tooltip/next/Tooltip-story.js
@@ -1,69 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import { useId } from '../../../internal/useId';
+/**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-function Tooltip({ children, label, description }) {
-  // throw warning if there's more than one child
-  const child = React.Children.only(children);
-  const [visible, setVisible] = useState(false);
-  const id = useId('tooltip');
+import { Add24 } from '@carbon/icons-react';
+import React from 'react';
+import { Tooltip } from './Tooltip';
 
-  const triggerProps = {
-    onFocus: () => setVisible(true),
-    onBlur: () => setVisible(false),
-  };
-
-  if (label) {
-    triggerProps['aria-labelledby'] = id;
-  } else {
-    triggerProps['aria-describedby'] = id;
-  }
-
-  useEffect(() => {
-    function listener(event) {
-      if (event.key === 'Escape') {
-        return setVisible(false);
-      }
-    }
-    window.addEventListener('keydown', listener);
-
-    return () => {
-      window.removeEventListener('keydown', listener);
-    };
-  }, []);
-
-  return (
-    <div
-      onMouseEnter={() => setVisible(true)}
-      onMouseLeave={() => setVisible(false)}>
-      {React.cloneElement(child, triggerProps)}
-      <span className="tooltip">
-        <span
-          hidden={!visible}
-          id={id}
-          role="tooltip"
-          aria-hidden="true"
-          className="tooltip-content">
-          {label || description}
-        </span>
-      </span>
-    </div>
-  );
-}
-
-export const Description = () => {
-  return (
-    <Tooltip description="modify account settings">
-      <button type="button">Edit</button>
-    </Tooltip>
-  );
+export default {
+  title: 'Experimental/unstable_Tooltip',
+  component: Tooltip,
 };
 
 export const Default = () => {
   return (
-    <Tooltip label="close">
-      <button type="button">X</button>
+    <Tooltip label="Close">
+      <button type="button">
+        <Add24 />
+      </button>
     </Tooltip>
   );
 };
 
-export default { title: 'Experimental/unstable_Tooltip' };
+export const Description = () => {
+  return (
+    <Tooltip description="Modify account settings">
+      <button type="button">Edit</button>
+    </Tooltip>
+  );
+};
---
+++ packages/react/src/components/Tooltip/next/Tooltip.js
@@ -0,0 +1,138 @@
+/**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { settings } from 'carbon-components';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import React, { useRef, useEffect, useState } from 'react';
+import { Popover, PopoverContent } from '../../Popover';
+import { canUseDOM } from '../../../internal/environment';
+import { useEvent } from '../../../internal/useEvent';
+import { useId } from '../../../internal/useId';
+import { useNoInteractiveChildren } from '../../../internal/useNoInteractiveChildren';
+
+const { prefix } = settings;
+
+function Tooltip({
+  className: customClassName,
+  children,
+  label,
+  description,
+  ...rest
+}) {
+  const containerRef = useRef(null);
+  const tooltipRef = useRef(null);
+  const [open, setOpen] = useState(false);
+  const id = useId('tooltip');
+  const child = React.Children.only(children);
+
+  const triggerProps = {
+    onFocus: () => setOpen(true),
+    onBlur: () => setOpen(false),
+  };
+
+  if (label) {
+    triggerProps['aria-labelledby'] = id;
+  } else {
+    triggerProps['aria-describedby'] = id;
+  }
+
+  if (__DEV__) {
+    useNoInteractiveChildren(
+      tooltipRef,
+      'The Tooltip component must have no interactive content rendered by the' +
+        '`label` or `description` prop'
+    );
+  }
+
+  if (canUseDOM) {
+    useEvent(window, 'keydown', () => {
+      setOpen(false);
+    });
+  }
+
+  return (
+    <div
+      {...rest}
+      className={cx('cds--tooltip', customClassName)}
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+      ref={containerRef}>
+      {React.cloneElement(child, triggerProps)}
+      {open && <MouseArea from={containerRef} to={tooltipRef} />}
+      <Popover align="top" open={open} highContrast>
+        <PopoverContent
+          aria-hidden="true"
+          className={`cds--tooltip-content`}
+          id={id}
+          ref={tooltipRef}
+          role="tooltip">
+          {label || description}
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
+
+function MouseArea({ from: fromRef, to: toRef }) {
+  const [style, setStyle] = useState({});
+
+  useEffect(() => {
+    const { current: from } = fromRef;
+    const { current: to } = toRef;
+    const fromRect = from.getBoundingClientRect();
+    const toRect = to.getBoundingClientRect();
+
+    const height = toRect.top - fromRect.top;
+    const width = Math.max(toRect.width, fromRect.width);
+
+    // console.log(fromRect);
+    // console.log(toRect);
+
+    console.log(width);
+    console.log(height);
+  }, []);
+
+  return <div style={style} />;
+}
+
+Tooltip.propTypes = {
+  /**
+   * Pass in the child to which the tooltip will be applied
+   */
+  children: PropTypes.node,
+
+  /**
+   * Specify an optional className to be applied to the container node
+   */
+  className: PropTypes.string,
+
+  /**
+   * Provide the label to be rendered inside of the Tooltip. The label will use
+   * `aria-labelledby` and will fully describe the child node that is provided.
+   * This means that if you have text in the child node, that it will not be
+   * announced to the screen reader.
+   *
+   * Note: if label and description are both provided, description will not be
+   * used
+   */
+  label: PropTypes.node,
+
+  /**
+   * Provide the description to be rendered inside of the Tooltip. The
+   * description will use `aria-describedby` and will describe the child node
+   * in addition to the text rendered inside of the child. This means that if you
+   * have text in the child node, that it will be announced alongside the
+   * description to the screen reader.
+   *
+   * Note: if label and description are both provided, label will be used and
+   * description will not be used
+   */
+  description: PropTypes.node,
+};
+
+export { Tooltip };
