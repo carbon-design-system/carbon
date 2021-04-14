9673830c2423d4dcf92496baa60170f9d9e8db63
Taylor Jones <taylor.jones826@gmail.com>
2021-04-14 11:19:58 -0500

    fix(storybook): remove fast-sass-loader to support sass modules

--- config/storybook-preset-carbon/index.js
+++ config/storybook-preset-carbon/index.js
@@ -35,40 +35,6 @@ module.exports = {
   ].filter(Boolean),
 
   webpack(config) {
-    const sassLoader = {
-      loader: require.resolve('sass-loader'),
-      options: {
-        additionalData(content) {
-          return `
-            $feature-flags: (
-              ui-shell: true,
-              enable-css-custom-properties: ${CARBON_REACT_STORYBOOK_USE_CUSTOM_PROPERTIES},
-            );
-            ${content}
-          `;
-        },
-        sassOptions: {
-          implementation: require('sass'),
-          includePaths: [path.resolve(__dirname, '..', '..', 'node_modules')],
-        },
-        sourceMap: true,
-      },
-    };
-
-    const fastSassLoader = {
-      loader: require.resolve('fast-sass-loader'),
-      options: {
-        data: `
-          $feature-flags: (
-            ui-shell: true,
-            enable-css-custom-properties: ${CARBON_REACT_STORYBOOK_USE_CUSTOM_PROPERTIES},
-          );
-        `,
-        implementation: require('sass'),
-        includePaths: [path.resolve(__dirname, '..', '..', 'node_modules')],
-      },
-    };
-
     config.module.rules.push({
       test: /-story\.jsx?$/,
       loaders: [
@@ -119,7 +85,27 @@ module.exports = {
             sourceMap: true,
           },
         },
-        NODE_ENV === 'production' ? sassLoader : fastSassLoader,
+        {
+          loader: require.resolve('sass-loader'),
+          options: {
+            additionalData(content) {
+              return `
+                $feature-flags: (
+                  ui-shell: true,
+                  enable-css-custom-properties: ${CARBON_REACT_STORYBOOK_USE_CUSTOM_PROPERTIES},
+                );
+                ${content}
+              `;
+            },
+            sassOptions: {
+              implementation: require('sass'),
+              includePaths: [
+                path.resolve(__dirname, '..', '..', 'node_modules'),
+              ],
+            },
+            sourceMap: true,
+          },
+        },
       ],
     });
 
