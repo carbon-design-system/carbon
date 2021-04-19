f7ec2c4f7165ddcbc665945ecc303bd87cc5af83
TJ Egan <tw15egan@gmail.com>
2021-04-19 11:58:53 -0700

    fix(sass-loader): add extra config

--- config/storybook-preset-carbon/index.js
+++ config/storybook-preset-carbon/index.js
@@ -38,17 +38,19 @@ module.exports = {
 
   webpack(config) {
     const sassLoader = {
-      loader: 'sass-loader',
+      loader: require.resolve('sass-loader'),
       options: {
-        prependData() {
+        additionalData(content) {
           return `
             $feature-flags: (
               ui-shell: true,
               enable-css-custom-properties: ${CARBON_REACT_STORYBOOK_USE_CUSTOM_PROPERTIES},
             );
+            ${content}
           `;
         },
         sassOptions: {
+          implementation: require('sass'),
           includePaths: [path.resolve(__dirname, '..', 'node_modules')],
         },
         sourceMap: true,
@@ -56,7 +58,7 @@ module.exports = {
     };
 
     const fastSassLoader = {
-      loader: 'fast-sass-loader',
+      loader: require.resolve('fast-sass-loader'),
       options: {
         data: `
           $feature-flags: (
@@ -64,6 +66,8 @@ module.exports = {
             enable-css-custom-properties: ${CARBON_REACT_STORYBOOK_USE_CUSTOM_PROPERTIES},
           );
         `,
+        implementation: require('sass'),
+        includePaths: [path.resolve(__dirname, '..', '..', 'node_modules')],
       },
     };
 
