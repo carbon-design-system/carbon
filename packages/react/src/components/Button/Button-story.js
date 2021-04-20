58437a0c5bc18c3a522bf85d523242539a7b69e8
TJ Egan <tw15egan@gmail.com>
2021-04-20 12:09:50 -0700

    feat(Button): finish size prop update

--- packages/react/src/components/Button/Button-story.js
+++ packages/react/src/components/Button/Button-story.js
@@ -151,31 +151,23 @@ _Default.story = {
 export const SizeStory = () => {
   return (
     <>
-      <p>Do we need a xs variant?</p>
-      <Button size="xs">xs</Button>
+      <Button size="sm">sm</Button>&nbsp;
+      <Button size="small">small</Button>
       <br />
       <br />
-      <p>This is correct</p>
-      <Button size="sm">sm</Button>
-      <Button size="small">sm</Button>
+      <Button size="field">field</Button>&nbsp;
+      <Button size="md">md</Button>
       <br />
       <br />
-      <p>This should be md</p>
-      <Button size="field">field</Button>
+      <Button size="default">default</Button>&nbsp;
+      <Button size="lg">lg</Button>&nbsp;
+      <Button></Button>
       <br />
       <br />
-      <p>This should be lg</p>
-      <Button size="default">md - default</Button>
-      <Button size="lg">md - default</Button>
-      <Button>md - default</Button>
-      <br />
-      <br />
-      <p>This should be xl</p>
-      <Button size="lg">lg</Button>
-      <br />
-      <br />
-      <p>This should be 2xl</p>
       <Button size="xl">xl</Button>
+      <br />
+      <br />
+      <Button size="2xl">2xl</Button>
     </>
   );
 };
