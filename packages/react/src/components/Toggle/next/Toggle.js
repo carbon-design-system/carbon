88ecf7971d74c4dab8458cb2f1cc515e8fdce6e0
Jan Hassel <jan.hassel@ibm.com>
2021-04-21 17:23:07 +0200

    refactor(toggle): create v11 toggle basis

---
+++ packages/react/src/components/Toggle/next/Toggle.js
@@ -0,0 +1,134 @@
+/**
+ * Copyright IBM Corp. 2021
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { settings } from 'carbon-components';
+
+const { prefix } = settings;
+
+export function Toggle({
+  className,
+  defaultToggled = false,
+  disabled = false,
+  id,
+  labelA = 'Off',
+  labelB = 'On',
+  labelText,
+  onToggle = () => {},
+  size = 'md',
+}) {
+  const [checked, setChecked] = useState(defaultToggled);
+
+  const isSm = size === 'sm';
+
+  const wrapperClasses = classNames(
+    `${prefix}--toggle`,
+    {
+      [`${prefix}--toggle--disabled`]: disabled,
+    },
+    className
+  );
+
+  const appearanceClasses = classNames(`${prefix}--toggle__appearance`, {
+    [`${prefix}--toggle__appearance--sm`]: isSm,
+  });
+
+  const switchClasses = classNames(`${prefix}--toggle__switch`, {
+    [`${prefix}--toggle__switch--checked`]: checked,
+  });
+
+  return (
+    <div className={wrapperClasses}>
+      <button
+        id={id}
+        tabIndex={0}
+        className={`${prefix}--toggle__button`}
+        role="switch"
+        type="button"
+        aria-checked={checked}
+        disabled={disabled}
+        onClick={() => {
+          const newChecked = !checked;
+
+          setChecked(newChecked);
+          onToggle(newChecked);
+        }}
+      />
+      <label htmlFor={id} className={`${prefix}--toggle__label`}>
+        <span className={`${prefix}--toggle__label-text`}>{labelText}</span>
+        <div className={appearanceClasses}>
+          <div className={switchClasses}>
+            {isSm && (
+              <svg
+                className={`${prefix}--toggle__check`}
+                width="6px"
+                height="5px"
+                viewBox="0 0 6 5">
+                <path d="M2.2 2.7L5 0 6 1 2.2 5 0 2.7 1 1.5z" />
+              </svg>
+            )}
+          </div>
+          <span className={`${prefix}--toggle__text`} aria-hidden="true">
+            {checked ? labelB : labelA}
+          </span>
+        </div>
+      </label>
+    </div>
+  );
+}
+
+Toggle.propTypes = {
+  /**
+   * Specify a custom className to apply to the form-item node
+   */
+  className: PropTypes.string,
+
+  /**
+   * Specify whether the toggle should be on by default
+   */
+  defaultToggled: PropTypes.bool,
+
+  /**
+   * Whether this control should be disabled
+   */
+  disabled: PropTypes.bool,
+
+  /**
+   * Provide an id that unique represents the underlying `<button>`
+   */
+  id: PropTypes.string.isRequired,
+
+  /**
+   * Specify the label for the "off" position
+   */
+  labelA: PropTypes.node.isRequired,
+
+  /**
+   * Specify the label for the "on" position
+   */
+  labelB: PropTypes.node.isRequired,
+
+  /**
+   * Provide the text that will be read by a screen reader when visiting this
+   * control
+   */
+  labelText: PropTypes.node.isRequired,
+
+  /**
+   * Provide an event listener that is called when the control is toggled
+   */
+  onToggle: PropTypes.func,
+
+  /**
+   * Specify the size of the Toggle. Currently only supports 'sm' or 'md' (default)
+   */
+  size: PropTypes.oneOf(['sm', 'md']),
+};
+
+export default Toggle;
