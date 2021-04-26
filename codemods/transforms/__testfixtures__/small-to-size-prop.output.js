8b5d629502f4abb3826eb65a7e1be5e871bfad3f
Josh Black <josh@josh.black>
2021-04-26 11:26:31 -0500

    revert(codemods): revert change to small-to-size-prop fixture

--- codemods/transforms/__testfixtures__/small-to-size-prop.output.js
+++ codemods/transforms/__testfixtures__/small-to-size-prop.output.js
@@ -9,25 +9,3 @@ function Test2() {
     </Button>
   );
 }
-
-const mapped = {
-  default: Add,
-  // prettier-ignore
-  size: React.forwardRef((props, ref) => <Bee ref={ref} size={24} {...props} />),
-  // prettier-ignore
-  lowercase: React.forwardRef((props, ref) => React.createElement(chevron, {
-    ref,
-    size: 16,
-    ...props
-  })),
-};
-
-function RenderIconProp() {
-  // prettier-ignore
-  return (
-    <div>
-      <DefaultSize renderIcon={Add} />
-      <Size renderIcon={props => <Bee size={24} {...props} />} />
-    </div>
-  );
-}
