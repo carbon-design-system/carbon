a1d97f3217d0979cb237eaf9e46547f9ca6d1581
Taylor Jones <taylor.jones826@gmail.com>
2021-04-28 15:53:03 -0500

    chore(wip): push up work

--- packages/react/src/components/Notification/index.js
+++ packages/react/src/components/Notification/index.js
@@ -5,4 +5,61 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import React from 'react';
+import {
+  NotificationActionButton as NotificationActionButtonNext,
+  NotificationTextDetails as NotificationTextDetailsNext,
+  NotificationButton as NotificationButtonNext,
+  ToastNotification as ToastNotificationNext,
+  InlineNotification as InlineNotificationNext,
+} from './next/Notification';
+import {
+  NotificationActionButton as NotificationActionButtonClassic,
+  NotificationTextDetails as NotificationTextDetailsClassic,
+  NotificationButton as NotificationButtonClassic,
+  ToastNotification as ToastNotificationClassic,
+  InlineNotification as InlineNotificationClassic,
+} from './Notification';
+import { useFeatureFlag } from '../FeatureFlags';
+
+export function NotificationActionButton(props) {
+  const enabled = useFeatureFlag('enable-2021-release');
+  if (enabled) {
+    return <NotificationActionButtonNext {...props} />;
+  }
+  return <NotificationActionButtonClassic {...props} />;
+}
+
+export function NotificationTextDetails(props) {
+  const enabled = useFeatureFlag('enable-2021-release');
+  if (enabled) {
+    return <NotificationTextDetailsNext {...props} />;
+  }
+  return <NotificationTextDetailsClassic {...props} />;
+}
+
+export function NotificationButton(props) {
+  const enabled = useFeatureFlag('enable-2021-release');
+  if (enabled) {
+    return <NotificationButtonNext {...props} />;
+  }
+  return <NotificationButtonClassic {...props} />;
+}
+
+export function ToastNotification(props) {
+  const enabled = useFeatureFlag('enable-2021-release');
+  if (enabled) {
+    return <ToastNotificationNext {...props} />;
+  }
+  return <ToastNotificationClassic {...props} />;
+}
+
+export function InlineNotification(props) {
+  const enabled = useFeatureFlag('enable-2021-release');
+  if (!enabled) {
+    return <InlineNotificationNext {...props} />;
+  }
+  return <InlineNotificationClassic {...props} />;
+}
+
 export * from './Notification';
---
+++ packages/react/src/components/Notification/next/Notification.js
@@ -0,0 +1,534 @@
+/**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import PropTypes from 'prop-types';
+import React, { useEffect, useRef, useState } from 'react';
+import cx from 'classnames';
+import { settings } from 'carbon-components';
+import {
+  Close20,
+  ErrorFilled20,
+  CheckmarkFilled20,
+  WarningFilled20,
+  WarningAltFilled20,
+  InformationFilled20,
+  InformationSquareFilled20,
+} from '@carbon/icons-react';
+
+import Button from '../../Button';
+import useIsomorphicEffect from '../../../internal/useIsomorphicEffect';
+
+const { prefix } = settings;
+
+export function NotificationActionButton({
+  children,
+  className: customClassName,
+  onClick,
+  ...rest
+}) {
+  const className = cx(
+    customClassName,
+    `${prefix}--inline-notification__action-button`
+  );
+
+  return (
+    <Button
+      className={className}
+      kind="ghost"
+      onClick={onClick}
+      size="small"
+      {...rest}>
+      {children}
+    </Button>
+  );
+}
+
+NotificationActionButton.propTypes = {
+  /**
+   * Specify the content of the notification action button.
+   */
+  children: PropTypes.node,
+
+  /**
+   * Specify an optional className to be applied to the notification action button
+   */
+  className: PropTypes.string,
+
+  /**
+   * Optionally specify a click handler for the notification action button.
+   */
+  onClick: PropTypes.func,
+};
+
+export function NotificationButton({
+  ariaLabel,
+  className,
+  iconDescription,
+  type,
+  renderIcon: IconTag,
+  name,
+  notificationType,
+  ...rest
+}) {
+  const buttonClassName = cx(className, {
+    [`${prefix}--${notificationType}-notification__close-button`]: notificationType,
+  });
+  const iconClassName = cx({
+    [`${prefix}--${notificationType}-notification__close-icon`]: notificationType,
+  });
+
+  return (
+    // eslint-disable-next-line react/button-has-type
+    <button
+      {...rest}
+      type={type}
+      aria-label={iconDescription}
+      title={iconDescription}
+      className={buttonClassName}>
+      {IconTag && (
+        <IconTag aria-label={ariaLabel} className={iconClassName} name={name} />
+      )}
+    </button>
+  );
+}
+
+NotificationButton.propTypes = {
+  /**
+   * Specify a label to be read by screen readers on the notification button
+   */
+  ariaLabel: PropTypes.string,
+
+  /**
+   * Specify an optional className to be applied to the notification button
+   */
+  className: PropTypes.string,
+
+  /**
+   * Provide a description for "close" icon that can be read by screen readers
+   */
+  iconDescription: PropTypes.string,
+
+  /**
+   * Specify an optional icon for the Button through a string,
+   * if something but regular "close" icon is desirable
+   */
+  name: PropTypes.string,
+
+  /**
+   * Specify the notification type
+   */
+  notificationType: PropTypes.oneOf(['toast', 'inline']),
+
+  /**
+   * Optional prop to allow overriding the icon rendering.
+   * Can be a React component class
+   */
+  renderIcon: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+
+  /**
+   * Optional prop to specify the type of the Button
+   */
+  type: PropTypes.string,
+};
+
+NotificationButton.defaultProps = {
+  ariaLabel: 'close notification', // TODO: deprecate this prop
+  notificationType: 'toast',
+  type: 'button',
+  iconDescription: 'close icon',
+  renderIcon: Close20,
+};
+
+export function NotificationTextDetails({
+  content,
+  notificationType,
+  ...rest
+}) {
+  if (notificationType === 'toast') {
+    return (
+      <div {...rest} className={`${prefix}--toast-notification__details`}>
+        <div className={`${prefix}--toast-notification__content`}>
+          {content}
+        </div>
+      </div>
+    );
+  }
+
+  if (notificationType === 'inline') {
+    return (
+      <p {...rest} className={`${prefix}--inline-notification__content`}>
+        {content}
+      </p>
+    );
+  }
+}
+
+NotificationTextDetails.propTypes = {
+  /**
+   * Specify the content
+   */
+  content: PropTypes.string,
+  /**
+   * Specify the notification type
+   */
+  notificationType: PropTypes.oneOf(['toast', 'inline']),
+};
+
+NotificationTextDetails.defaultProps = {
+  notificationType: 'toast',
+};
+
+const iconTypes = {
+  error: ErrorFilled20,
+  success: CheckmarkFilled20,
+  warning: WarningFilled20,
+  ['warning-alt']: WarningAltFilled20,
+  info: InformationFilled20,
+  ['info-square']: InformationSquareFilled20,
+};
+
+function NotificationIcon({ iconDescription, kind, notificationType }) {
+  const IconForKind = iconTypes[kind];
+  if (!IconForKind) {
+    return null;
+  }
+  return (
+    <IconForKind
+      className={`${prefix}--${notificationType}-notification__icon`}>
+      <title>{iconDescription}</title>
+    </IconForKind>
+  );
+}
+
+NotificationIcon.propTypes = {
+  iconDescription: PropTypes.string.isRequired,
+  kind: PropTypes.oneOf([
+    'error',
+    'success',
+    'warning',
+    'warning-alt',
+    'info',
+    'info-square',
+  ]).isRequired,
+  notificationType: PropTypes.oneOf(['inline', 'toast']).isRequired,
+};
+
+export function ToastNotification({
+  role,
+  notificationType,
+  onClose,
+  onCloseButtonClick,
+  iconDescription,
+  statusIconDescription,
+  className,
+  content,
+  kind,
+  lowContrast,
+  hideCloseButton,
+  timeout,
+  ...rest
+}) {
+  const [isOpen, setIsOpen] = useState(true);
+  const containerClassName = cx(className, {
+    [`${prefix}--toast-notification`]: true,
+    [`${prefix}--toast-notification--low-contrast`]: lowContrast,
+    [`${prefix}--toast-notification--${kind}`]: kind,
+  });
+
+  const handleClose = (evt) => {
+    if (!onClose || onClose(evt) !== false) {
+      setIsOpen(false);
+    }
+  };
+
+  useIsomorphicEffect(() => {
+    document.addEventListener('keydown', handleClose, false);
+
+    return function cleanup() {
+      document.removeEventListener('keydown', handleClose, false);
+    };
+  });
+
+  function handleCloseButtonClick(event) {
+    onCloseButtonClick(event);
+    handleClose(event);
+  }
+
+  const savedOnClose = useRef(onClose);
+
+  useEffect(() => {
+    savedOnClose.current = onClose;
+  });
+
+  useEffect(() => {
+    if (!timeout) {
+      return;
+    }
+
+    const timeoutId = window.setTimeout((event) => {
+      setIsOpen(false);
+      if (savedOnClose.current) {
+        savedOnClose.current(event);
+      }
+    }, timeout);
+    return () => {
+      window.clearTimeout(timeoutId);
+    };
+  }, [timeout]);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div {...rest} role={role} kind={kind} className={containerClassName}>
+      <NotificationIcon
+        notificationType={notificationType}
+        kind={kind}
+        iconDescription={statusIconDescription || `${kind} icon`}
+      />
+      <NotificationTextDetails
+        content={content}
+        notificationType={notificationType}
+      />
+      {!hideCloseButton && (
+        <NotificationButton
+          iconDescription={iconDescription}
+          notificationType={notificationType}
+          onClick={handleCloseButtonClick}
+        />
+      )}
+    </div>
+  );
+}
+
+ToastNotification.propTypes = {
+  /**
+   * Specify an optional className to be applied to the notification box
+   */
+  className: PropTypes.string,
+
+  /**
+   * Specify the content
+   */
+  content: PropTypes.string.isRequired,
+
+  /**
+   * Specify the close button should be disabled, or not
+   */
+  hideCloseButton: PropTypes.bool,
+
+  /**
+   * Provide a description for "close" icon that can be read by screen readers
+   */
+  iconDescription: PropTypes.string,
+
+  /**
+   * Specify what state the notification represents
+   */
+  kind: PropTypes.oneOf([
+    'error',
+    'info',
+    'info-square',
+    'success',
+    'warning',
+    'warning-alt',
+  ]).isRequired,
+
+  /**
+   * Specify whether you are using the low contrast variant of the ToastNotification.
+   */
+  lowContrast: PropTypes.bool,
+
+  /**
+   * By default, this value is "toast". You can also provide an alternate type
+   * if it makes sense for the underlying `<NotificationTextDetails>` and `<NotificationButton>`
+   */
+  notificationType: PropTypes.string,
+
+  /**
+   * Provide a function that is called when menu is closed
+   */
+  onClose: PropTypes.func,
+
+  /**
+   * Provide a function that is called when the close button is clicked
+   */
+  onCloseButtonClick: PropTypes.func,
+
+  /**
+   * By default, this value is "alert". You can also provide an alternate
+   * role if it makes sense from the accessibility-side
+   */
+  role: PropTypes.string.isRequired,
+
+  /**
+   * Provide a description for "status" icon that can be read by screen readers
+   */
+  statusIconDescription: PropTypes.string,
+
+  /**
+   * Specify an optional duration the notification should be closed in
+   */
+  timeout: PropTypes.number,
+};
+
+ToastNotification.defaultProps = {
+  kind: 'error',
+  content: 'provide content',
+  role: 'alert',
+  notificationType: 'toast',
+  iconDescription: 'closes notification',
+  onCloseButtonClick: () => {},
+  hideCloseButton: false,
+  timeout: 0,
+};
+
+export function InlineNotification({
+  content,
+  role,
+  notificationType,
+  onClose,
+  onCloseButtonClick,
+  iconDescription,
+  statusIconDescription,
+  className,
+  kind,
+  lowContrast,
+  hideCloseButton,
+  ...rest
+}) {
+  const [isOpen, setIsOpen] = useState(true);
+  const containerClassName = cx(className, {
+    [`${prefix}--inline-notification`]: true,
+    [`${prefix}--inline-notification--low-contrast`]: lowContrast,
+    [`${prefix}--inline-notification--${kind}`]: kind,
+    [`${prefix}--inline-notification--hide-close-button`]: hideCloseButton,
+  });
+
+  const handleClose = (evt) => {
+    if (!onClose || onClose(evt) !== false) {
+      setIsOpen(false);
+    }
+  };
+
+  useIsomorphicEffect(() => {
+    document.addEventListener('keydown', handleClose, false);
+
+    return function cleanup() {
+      document.removeEventListener('keydown', handleClose, false);
+    };
+  });
+
+  function handleCloseButtonClick(event) {
+    onCloseButtonClick(event);
+    handleClose(event);
+  }
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div {...rest} role={role} kind={kind} className={containerClassName}>
+      <div className={`${prefix}--inline-notification__details`}>
+        <NotificationIcon
+          notificationType={notificationType}
+          kind={kind}
+          iconDescription={statusIconDescription || `${kind} icon`}
+        />
+        <NotificationTextDetails
+          content={content}
+          notificationType={notificationType}
+        />
+      </div>
+
+      {!hideCloseButton && (
+        <NotificationButton
+          iconDescription={iconDescription}
+          notificationType={notificationType}
+          onClick={handleCloseButtonClick}
+        />
+      )}
+    </div>
+  );
+}
+
+InlineNotification.propTypes = {
+  /**
+   * Specify an optional className to be applied to the notification box
+   */
+  className: PropTypes.string,
+
+  /**
+   * Specify the content
+   */
+  content: PropTypes.string.isRequired,
+
+  /**
+   * Specify the close button should be disabled, or not
+   */
+  hideCloseButton: PropTypes.bool,
+
+  /**
+   * Provide a description for "close" icon that can be read by screen readers
+   */
+  iconDescription: PropTypes.string,
+
+  /**
+   * Specify what state the notification represents
+   */
+  kind: PropTypes.oneOf([
+    'error',
+    'info',
+    'info-square',
+    'success',
+    'warning',
+    'warning-alt',
+  ]).isRequired,
+
+  /**
+   * Specify whether you are using the low contrast variant of the InlineNotification.
+   */
+  lowContrast: PropTypes.bool,
+
+  /**
+   * By default, this value is "inline". You can also provide an alternate type
+   * if it makes sense for the underlying `<NotificationTextDetails>` and `<NotificationButton>`
+   */
+  notificationType: PropTypes.string,
+
+  /**
+   * Provide a function that is called when menu is closed
+   */
+  onClose: PropTypes.func,
+
+  /**
+   * Provide a function that is called when the close button is clicked
+   */
+  onCloseButtonClick: PropTypes.func,
+
+  /**
+   * By default, this value is "alert". You can also provide an alternate
+   * role if it makes sense from the accessibility-side
+   */
+  role: PropTypes.string.isRequired,
+
+  /**
+   * Provide a description for "status" icon that can be read by screen readers
+   */
+  statusIconDescription: PropTypes.string,
+};
+
+InlineNotification.defaultProps = {
+  content: 'provide content',
+  role: 'alert',
+  notificationType: 'inline',
+  iconDescription: 'closes notification',
+  onCloseButtonClick: () => {},
+  hideCloseButton: false,
+};
