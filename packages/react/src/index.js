891c41a99934de2bf27a0280cb9f065a62c719b2
Radoslaw Kopec <radek4ec@gmail.com>
2021-05-06 19:24:32 +0200

    fix(feature-popver-export): add popover to the export path

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -225,3 +225,8 @@ export {
   Heading as unstable_Heading,
   Section as unstable_Section,
 } from './components/Heading';
+export {
+  Popover as unstable_Popover,
+  PopoverContent as unstable_PopoverContent,
+} from './components/Popover';
+
