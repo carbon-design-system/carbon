7bc35514edb30ccdf66222f8d900505c09ac2544
TJ Egan <tw15egan@gmail.com>
2021-05-19 19:19:57 -0700

    feat(Plex): add Plex package to carbon-react storybook

--- packages/carbon-react/.storybook/preview.js
+++ packages/carbon-react/.storybook/preview.js
@@ -24,6 +24,11 @@ export const globalTypes = {
           title: 'English',
           value: 'en',
         },
+        {
+          right: '🇵🇸',
+          title: 'Arabic',
+          value: 'ar',
+        },
       ],
     },
   },
@@ -83,11 +88,15 @@ configureActions({
 
 export const decorators = [
   (Story, context) => {
-    const { theme } = context.globals;
+    const { locale, theme } = context.globals;
 
     React.useEffect(() => {
       document.body.setAttribute('data-carbon-theme', theme);
     }, [theme]);
+
+    React.useEffect(() => {
+      document.documentElement.lang = locale;
+    }, [locale]);
 
     return <Story {...context} />;
   },
