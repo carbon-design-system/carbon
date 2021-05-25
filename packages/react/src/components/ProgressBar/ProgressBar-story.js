a3d913c8231390e0cbb567925e398ff0a8664cf4
Jan Hassel <jan.hassel@ibm.com>
2021-05-25 18:33:43 +0200

    docs(progress-bar): add example

--- packages/react/src/components/ProgressBar/ProgressBar-story.js
+++ packages/react/src/components/ProgressBar/ProgressBar-story.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 import ProgressBar from '../ProgressBar';
@@ -36,8 +36,47 @@ export const _ProgressBar = () => (
 );
 _ProgressBar.storyName = 'ProgressBar';
 
-export const _Indeterminate = () => (
+export const Indeterminate = () => (
   <ProgressBar label="Progress bar label" helperText="Optional helper text" />
 );
+
+export const Example = () => {
+  const size = 728;
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const interval = setInterval(() => {
+        setProgress((currentProgress) => {
+          const advancement = Math.random() * 8;
+          if (currentProgress + advancement < size) {
+            return currentProgress + advancement;
+          } else {
+            clearInterval(interval);
+            return size;
+          }
+        });
+      }, 50);
+    }, 3000);
+  }, []);
+
+  const running = progress > 0;
+
+  let helperText = running
+    ? `${progress.toFixed(1)}MB of ${size}MB`
+    : 'Fetching assets...';
+  if (progress >= size) {
+    helperText = 'Done';
+  }
+
+  return (
+    <ProgressBar
+      value={running ? progress : null}
+      max={size}
+      label="Export data"
+      helperText={helperText}
+    />
+  );
+};
 
 export const Playground = () => <ProgressBar {...props()} />;
