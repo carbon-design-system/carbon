65906045af4733f92e32786ac6a510fb6e6760a3
TJ Egan <tw15egan@gmail.com>
2021-06-29 12:38:52 -0400

    chore(storybook): remove actions, knobs

--- packages/carbon-react/src/components/Pagination/Pagination.stories.js
+++ packages/carbon-react/src/components/Pagination/Pagination.stories.js
@@ -6,66 +6,44 @@
  */
 
 import React from 'react';
-import { action } from '@storybook/addon-actions';
-
-import {
-  withKnobs,
-  array,
-  boolean,
-  number,
-  text,
-} from '@storybook/addon-knobs';
 import { Pagination } from 'carbon-components-react';
-
-const props = () => ({
-  disabled: boolean('Disable page inputs (disabled)', false),
-  page: number('The current page (page)', 1),
-  totalItems: number('Total number of items (totalItems)', 103),
-  pagesUnknown: boolean('Total number of items unknown (pagesUnknown)', false),
-  pageInputDisabled: boolean(
-    'Disable page input (pageInputDisabled)',
-    undefined
-  ),
-  pageSizeInputDisabled: boolean(
-    'Disable page size input (pageSizeInputDisabled)',
-    undefined
-  ),
-  backwardText: text(
-    'The description for the backward icon (backwardText)',
-    'Previous page'
-  ),
-  forwardText: text(
-    'The description for the forward icon (forwardText)',
-    'Next page'
-  ),
-  pageSize: number('Number of items per page (pageSize)', 10),
-  pageSizes: array('Choices of `pageSize` (pageSizes)', [10, 20, 30, 40, 50]),
-  itemsPerPageText: text(
-    'Label for `pageSizes` select UI (itemsPerPageText)',
-    'Items per page:'
-  ),
-  onChange: action('onChange'),
-});
 
 export default {
   title: 'Components/Pagination',
-  decorators: [
-    withKnobs,
-    (story) => <div style={{ maxWidth: '800px' }}>{story()}</div>,
-  ],
+  decorators: [(story) => <div style={{ maxWidth: '800px' }}>{story()}</div>],
 
   parameters: {
     component: Pagination,
   },
 };
 
-export const _Default = () => <Pagination {...props()} />;
+export const _Default = () => (
+  <Pagination
+    id="pagination-1"
+    pageSize={10}
+    pageSizes={[10, 20, 30, 40, 50]}
+    itemsPerPageText="Items per page:"
+    totalItems={103}
+  />
+);
 
 export const MultiplePaginationComponents = () => {
   return (
     <div>
-      <Pagination {...props()} />
-      <Pagination {...props()} />
+      <Pagination
+        id="pagination-2"
+        pageSize={10}
+        pageSizes={[10, 20, 30, 40, 50]}
+        itemsPerPageText="Items per page:"
+        totalItems={103}
+      />
+      <Pagination
+        id="pagination-3"
+        pageSize={10}
+        pageSizes={[10, 20, 30, 40, 50]}
+        itemsPerPageText="Items per page:"
+        totalItems={103}
+      />
     </div>
   );
 };
@@ -74,7 +52,6 @@ export const PaginationWithCustomPageSizesLabel = () => {
   return (
     <div>
       <Pagination
-        {...props()}
         pageSizes={[
           { text: 'Ten', value: 10 },
           { text: 'Twenty', value: 20 },
@@ -82,6 +59,8 @@ export const PaginationWithCustomPageSizesLabel = () => {
           { text: 'Fourty', value: 40 },
           { text: 'Fifty', value: 50 },
         ]}
+        itemsPerPageText="Items per page:"
+        totalItems={103}
       />
     </div>
   );
--- packages/carbon-react/src/components/Select/Select.stories.js
+++ packages/carbon-react/src/components/Select/Select.stories.js
@@ -6,9 +6,6 @@
  */
 
 import React from 'react';
-import { action } from '@storybook/addon-actions';
-
-import { withKnobs, boolean, text, select } from '@storybook/addon-knobs';
 import {
   Select,
   SelectItem,
@@ -16,44 +13,9 @@ import {
   SelectSkeleton,
 } from 'carbon-components-react';
 
-const sizes = {
-  'Small  (sm)': 'sm',
-  'Medium (md) - default': undefined,
-  'Large  (lg)': 'lg',
-};
-
-const selectProps = {
-  className: 'some-class',
-  light: boolean('Light variant (light in <Select>)', false),
-  inline: boolean('Put control in-line with label (inline in <Select>)', false),
-  size: select('Field size (size)', sizes, undefined) || undefined,
-  disabled: boolean('Disabled (disabled in <Select>)', false),
-  hideLabel: boolean('No label (hideLabel in <Select>)', false),
-  invalid: boolean('Show form validation UI (invalid in <Select>)', false),
-  invalidText: text(
-    'Form validation UI content (invalidText in <Select>)',
-    'A valid value is required'
-  ),
-  labelText: text('Label text (labelText)', 'Select'),
-  helperText: text('Helper text (helperText)', 'Optional helper text.'),
-  onChange: action('onChange'),
-  warn: boolean('Show warning state (warn)', false),
-  warnText: text(
-    'Warning state text (warnText)',
-    'This will overwrite your current settings'
-  ),
-};
-
-const selectGroupProps = {
-  disabled: boolean('Disabled (disabled in <SelectItemGroup>)', false),
-};
-
 export default {
   title: 'Components/Select',
-  decorators: [
-    withKnobs,
-    (story) => <div style={{ width: '400px' }}>{story()}</div>,
-  ],
+  decorators: [(story) => <div style={{ width: '400px' }}>{story()}</div>],
   parameters: {
     component: Select,
     subcomponents: {
@@ -67,18 +29,50 @@ export default {
 export const _Default = () => {
   return (
     <div>
-      <Select {...selectProps} id="select-1" defaultValue="placeholder-item">
+      <Select
+        id="select-1"
+        defaultValue="placeholder-item"
+        labelText="Select an option"
+        helperText="Optional helper text">
         <SelectItem
           disabled
           hidden
           value="placeholder-item"
           text="Choose an option"
         />
-        <SelectItemGroup label="Category 1" {...selectGroupProps}>
+        <SelectItemGroup label="Category 1">
           <SelectItem value="option-1" text="Option 1" />
           <SelectItem value="option-2" text="Option 2" />
         </SelectItemGroup>
-        <SelectItemGroup label="Category 2" {...selectGroupProps}>
+        <SelectItemGroup label="Category 2">
+          <SelectItem value="option-3" text="Option 3" />
+          <SelectItem value="option-4" text="Option 4" />
+        </SelectItemGroup>
+      </Select>
+    </div>
+  );
+};
+
+export const Inline = () => {
+  return (
+    <div>
+      <Select
+        inline
+        id="select-1"
+        defaultValue="placeholder-item"
+        labelText=""
+        helperText="Optional helper text">
+        <SelectItem
+          disabled
+          hidden
+          value="placeholder-item"
+          text="Choose an option"
+        />
+        <SelectItemGroup label="Category 1">
+          <SelectItem value="option-1" text="Option 1" />
+          <SelectItem value="option-2" text="Option 2" />
+        </SelectItemGroup>
+        <SelectItemGroup label="Category 2">
           <SelectItem value="option-3" text="Option 3" />
           <SelectItem value="option-4" text="Option 4" />
         </SelectItemGroup>
