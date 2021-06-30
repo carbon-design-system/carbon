5e9ad8cea6d739cb30d661a3bb0ddea807b3e9ca
TJ Egan <tw15egan@gmail.com>
2021-06-30 11:40:52 -0400

    chore(storybook): remove actions, knobs

--- packages/carbon-react/src/components/ProgressIndicator/ProgressIndicator.stories.js
+++ packages/carbon-react/src/components/ProgressIndicator/ProgressIndicator.stories.js
@@ -6,12 +6,11 @@
  */
 
 import React from 'react';
-import { number, boolean, text } from '@storybook/addon-knobs';
-import { action } from '@storybook/addon-actions';
 import {
   ProgressIndicator,
   ProgressStep,
   ProgressIndicatorSkeleton,
+  Tooltip,
 } from 'carbon-components-react';
 // import Tooltip from '../Tooltip';
 
@@ -20,44 +19,39 @@ export default {
 };
 
 export const Default = () => (
-  <ProgressIndicator
-    vertical={boolean('Vertical (vertical)', false)}
-    currentIndex={number('Current progress (currentIndex)', 1)}
-    spaceEqually={boolean('Space Equally (spaceEqually)', false)}>
+  <ProgressIndicator>
     <ProgressStep
-      label={text('Label (label)', 'First step')}
+      label="First step"
       description="Step 1: Getting started with Carbon Design System"
       secondaryLabel="Optional label"
     />
     <ProgressStep
       label="Second step with tooltip"
       description="Step 2: Getting started with Carbon Design System"
-      // renderLabel={() => (
-      //     // <Tooltip
-      //     //   direction="bottom"
-      //     //   showIcon={false}
-      //     //   triggerClassName={`bx--progress-label`}
-      //     //   triggerText={'Second step with tooltip'}
-      //     //   tooltipId="tooltipId-0">
-      //     //   <p>Overflow tooltip content.</p>
-      //     // </Tooltip>
-      // )}
-      //
+      renderLabel={() => (
+        <Tooltip
+          direction="bottom"
+          showIcon={false}
+          triggerClassName={`bx--progress-label`}
+          triggerText={'Second step with tooltip'}
+          tooltipId="tooltipId-0">
+          <p>Overflow tooltip content.</p>
+        </Tooltip>
+      )}
     />
     <ProgressStep
       label="Third step with tooltip"
       description="Step 3: Getting started with Carbon Design System"
-      // renderLabel={() => (
-      //     // <Tooltip
-      //     //   direction="bottom"
-      //     //   showIcon={false}
-      //     //   triggerClassName={`bx--progress-label`}
-      //     //   triggerText={'Second step with tooltip'}
-      //     //   tooltipId="tooltipId-0">
-      //     //   <p>Overflow tooltip content.</p>
-      //     // </Tooltip>
-      // )}
-      //
+      renderLabel={() => (
+        <Tooltip
+          direction="bottom"
+          showIcon={false}
+          triggerClassName={`bx--progress-label`}
+          triggerText={'Second step with tooltip'}
+          tooltipId="tooltipId-0">
+          <p>Overflow tooltip content.</p>
+        </Tooltip>
+      )}
     />
     <ProgressStep
       label="Fourth step"
@@ -74,9 +68,7 @@ export const Default = () => (
 );
 
 export const Interactive = () => (
-  <ProgressIndicator
-    currentIndex={number('Current progress (currentIndex)', 1)}
-    onChange={action('onChange')}>
+  <ProgressIndicator currentIndex={1} onChange={() => alert('Clicked')}>
     <ProgressStep
       label="Click me"
       description="Step 1: Register a onChange event"
@@ -88,21 +80,21 @@ export const Interactive = () => (
     <ProgressStep
       label="Tooltip and really long label"
       description="The progress indicator will listen for clicks on the steps"
-      //   renderLabel={() => (
-      //     // <Tooltip
-      //     //   direction="bottom"
-      //     //   showIcon={false}
-      //     //   triggerClassName={`${prefix}--progress-label`}
-      //     //   triggerText="Tooltip and really long label"
-      //     //   tooltipId="tooltipId-1">
-      //     //   <p>
-      //     //     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi
-      //     //     consequuntur hic ratione aliquid cupiditate, nesciunt saepe iste
-      //     //     blanditiis cumque maxime tenetur veniam est illo deserunt sint quae
-      //     //     pariatur. Laboriosam, consequatur.
-      //     //   </p>
-      //     // </Tooltip>
-      //   )}
+      renderLabel={() => (
+        <Tooltip
+          direction="bottom"
+          showIcon={false}
+          triggerClassName={`bx--progress-label`}
+          triggerText="Tooltip and really long label"
+          tooltipId="tooltipId-1">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi
+            consequuntur hic ratione aliquid cupiditate, nesciunt saepe iste
+            blanditiis cumque maxime tenetur veniam est illo deserunt sint quae
+            pariatur. Laboriosam, consequatur.
+          </p>
+        </Tooltip>
+      )}
     />
   </ProgressIndicator>
 );
