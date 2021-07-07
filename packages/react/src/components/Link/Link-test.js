0e2b53b740a5ed852521705ccfe0e1d83e9aab23
Josh Black <josh@josh.black>
2021-07-07 08:25:07 -0500

    fix(react): update radio button group state initialization (#9104)
    * fix(react): update radio button group state initialization
    
    * test(react): add test for RadioButtonGroup
    
    * feat(jest-config): add jest-dom as default matcher
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/react/src/components/Link/Link-test.js
+++ packages/react/src/components/Link/Link-test.js
@@ -10,7 +10,6 @@ import Link from '../Link';
 import { shallow } from 'enzyme';
 import { settings } from 'carbon-components';
 import { cleanup, render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import { describe, document } from 'window-or-global';
 
