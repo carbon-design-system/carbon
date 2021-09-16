6923ccbc845e50a7dc464952ab0448f4f59e74f7
dakahn <dakahn@protonmail.com>
2021-09-16 18:01:48 -0500

    feat(Carbon-React): add usePrefix hook and examples

---
+++ packages/react/src/internal/usePrefix.js
@@ -0,0 +1,8 @@
+import { settings } from 'carbon-components';
+import React from 'react';
+
+export const PrefixContext = React.createContext(settings.prefix);
+
+export function usePrefix() {
+  return React.useContext(PrefixContext);
+}
