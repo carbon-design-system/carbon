83f26589275aa42edb713c17af77c99f3412fa4e
Scott Strubberg <sstrubberg@protonmail.com>
2021-10-13 13:27:38 -0500

    chore(tile): refactor to functional component (#9721)
    * chore(tile): refactor to functional component
    
    * fix(tile): build errors
    
    * fix(tile): working with storybook
    
    * fix(tile): added prefixes
    
    * feat(tile): refactoring-feature-flags
    
    * feat(tile): removed custom styling
    
    * Update packages/react/.storybook/preview.js
    
    Co-authored-by: Josh Black <josh@josh.black>
    
    * Update packages/react/src/components/Tile/next/Tile.js
    
    Co-authored-by: Josh Black <josh@josh.black>
    
    * Update packages/react/src/components/Tile/next/Tile.js
    
    Co-authored-by: Josh Black <josh@josh.black>
    
    * Update packages/react/src/components/Tile/next/Tile.js
    
    Co-authored-by: Josh Black <josh@josh.black>
    
    * Update packages/react/src/components/Tile/next/Tile.js
    
    Co-authored-by: Josh Black <josh@josh.black>
    
    * Update packages/react/src/components/Tile/next/Tile.js
    
    Co-authored-by: Josh Black <josh@josh.black>
    
    * fix(tile): removed useEffect hook
    
    * fix(tile): alphabetized things
    
    * fix(tile): syntax
    
    * fix(Tile): added deprecation warning for the light prop
    
    * feat(tile): added tests
    
    * feat(tile): added rtl
    
    * feat(tile): removed debug
    
    * feat(tile): hammering on testing
    
    * feat(tile): added more tests
    
    * feat(tile): hammering on tests
    
    * feat(tile): fixed an aria-label
    
    Co-authored-by: Josh Black <josh@josh.black>

--- packages/react/src/components/Tile/index.js
+++ packages/react/src/components/Tile/index.js
@@ -5,4 +5,45 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export * from './Tile';
+import * as FeatureFlags from '@carbon/feature-flags';
+import {
+  Tile as TileNext,
+  ClickableTile as ClickableTileNext,
+  SelectableTile as SelectableTileNext,
+  TileAboveTheFoldContent as TileAboveTheFoldContentNext,
+  TileBelowTheFoldContent as TileBelowTheFoldContentNext,
+} from './next/Tile';
+import {
+  Tile as TileClassic,
+  ClickableTile as ClickableTileClassic,
+  SelectableTile as SelectableTileClassic,
+  ExpandableTile,
+  TileAboveTheFoldContent as TileAboveTheFoldContentClassic,
+  TileBelowTheFoldContent as TileBelowTheFoldContentClassic,
+} from './Tile';
+
+export const Tile = FeatureFlags.enabled('enable-v11-release')
+  ? TileNext
+  : TileClassic;
+
+export const ClickableTile = FeatureFlags.enabled('enable-v11-release')
+  ? ClickableTileNext
+  : ClickableTileClassic;
+
+export const SelectableTile = FeatureFlags.enabled('enable-v11-release')
+  ? SelectableTileNext
+  : SelectableTileClassic;
+
+export { ExpandableTile };
+
+export const TileAboveTheFoldContent = FeatureFlags.enabled(
+  'enable-v11-release'
+)
+  ? TileAboveTheFoldContentNext
+  : TileAboveTheFoldContentClassic;
+
+export const TileBelowTheFoldContent = FeatureFlags.enabled(
+  'enable-v11-release'
+)
+  ? TileBelowTheFoldContentNext
+  : TileBelowTheFoldContentClassic;
