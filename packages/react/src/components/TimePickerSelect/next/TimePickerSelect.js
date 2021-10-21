e5e6a79063e0aa6edd493b02ac3afd28d52e2ede
Scott Strubberg <sstrubberg@us.ibm.com>
2021-10-21 16:04:00 -0500

    feat(TimePickerSelect): class -> functional

---
+++ packages/react/src/components/TimePickerSelect/next/TimePickerSelect.js
@@ -0,0 +1,88 @@
+/**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import PropTypes from 'prop-types';
+import React from 'react';
+import cx from 'classnames';
+import { ChevronDown } from '@carbon/react/icons'; // Build errors. What we tell people to use in our docs.
+// import { ChevronDown } from '@carbon/icons-react/next'; //  Build errors.
+// import { ChevronDown16 } from '@carbon/icons-react'; // Old school, still works.
+
+import { usePrefix } from '../../../internal/usePrefix';
+import deprecate from '../../../prop-types/deprecate';
+
+const TimePickerSelect = React.forwardRef(function TimePickerSelect(
+  {
+    ['aria-label']: ariaLabel = 'open list of options',
+    children,
+    id,
+    disabled = false,
+    className,
+    ...rest
+  },
+  ref
+) {
+  const prefix = usePrefix();
+
+  const selectClasses = cx({
+    [`${prefix}--select`]: true,
+    [`${prefix}--time-picker__select`]: true,
+    [className]: className,
+  });
+
+  return (
+    <div className={selectClasses} ref={ref}>
+      <select
+        id={id}
+        className={`${prefix}--select-input`}
+        disabled={disabled}
+        aria-label={ariaLabel}
+        {...rest}>
+        {children}
+      </select>
+      <ChevronDown className={`${prefix}--select__arrow`} aria-hidden="true" />
+    </div>
+  );
+});
+
+TimePickerSelect.propTypes = {
+  /**
+   * Provide the contents of your TimePickerSelect
+   */
+  children: PropTypes.node,
+
+  /**
+   * Specify an optional className to be applied to the node containing the label and the select box
+   */
+  className: PropTypes.string,
+
+  /**
+   * Optionally provide the default value of the `<select>`
+   */
+  defaultValue: PropTypes.any,
+
+  /**
+   * Specify whether the control is disabled
+   */
+  disabled: PropTypes.bool,
+
+  /**
+   * Provide a description for the twistie icon that can be read by screen readers
+   */
+  iconDescription: deprecate(
+    PropTypes.string,
+    'The `iconDescription` prop for `TimePickerSelect` is no longer needed and has ' +
+      'been deprecated. It will be moved in the next major release. Use "aria-label" instead'
+  ),
+
+  /**
+   * Specify a custom `id` for the `<select>`
+   */
+  id: PropTypes.string.isRequired,
+};
+
+export default TimePickerSelect;
