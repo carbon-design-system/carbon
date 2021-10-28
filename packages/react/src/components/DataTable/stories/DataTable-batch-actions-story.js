de51557200baf72e9fd1d1e220a68e4587fa1454
Abbey Hart <abbeyhrt@gmail.com>
2021-10-28 16:24:17 -0500

    fix(react): add display grid to actions list so scroll works

--- packages/react/src/components/DataTable/stories/DataTable-batch-actions-story.js
+++ packages/react/src/components/DataTable/stories/DataTable-batch-actions-story.js
@@ -155,3 +155,133 @@ export const Usage = () => (
     )}
   </DataTable>
 );
+
+export const LotsOfBatchActions = () => (
+  <DataTable rows={rows} headers={headers}>
+    {({
+      rows,
+      headers,
+      getHeaderProps,
+      getRowProps,
+      getSelectionProps,
+      getToolbarProps,
+      getBatchActionProps,
+      onInputChange,
+      selectedRows,
+      getTableProps,
+      getTableContainerProps,
+    }) => (
+      <TableContainer
+        title="DataTable"
+        description="With batch actions"
+        {...getTableContainerProps()}>
+        <TableToolbar {...getToolbarProps()}>
+          <TableBatchActions {...getBatchActionProps()}>
+            <TableBatchAction
+              tabIndex={getBatchActionProps().shouldShowBatchActions ? 0 : -1}
+              renderIcon={Delete}
+              onClick={batchActionClick(selectedRows)}>
+              Item 1
+            </TableBatchAction>
+            <TableBatchAction
+              tabIndex={getBatchActionProps().shouldShowBatchActions ? 0 : -1}
+              renderIcon={Save}
+              onClick={batchActionClick(selectedRows)}>
+              Item 2
+            </TableBatchAction>
+            <TableBatchAction
+              tabIndex={getBatchActionProps().shouldShowBatchActions ? 0 : -1}
+              renderIcon={Save}
+              onClick={batchActionClick(selectedRows)}>
+              Item 3
+            </TableBatchAction>
+            <TableBatchAction
+              tabIndex={getBatchActionProps().shouldShowBatchActions ? 0 : -1}
+              renderIcon={Save}
+              onClick={batchActionClick(selectedRows)}>
+              Item 4
+            </TableBatchAction>
+            <TableBatchAction
+              tabIndex={getBatchActionProps().shouldShowBatchActions ? 0 : -1}
+              renderIcon={Save}
+              onClick={batchActionClick(selectedRows)}>
+              Item 5
+            </TableBatchAction>
+            <TableBatchAction
+              tabIndex={getBatchActionProps().shouldShowBatchActions ? 0 : -1}
+              renderIcon={Download}
+              onClick={batchActionClick(selectedRows)}>
+              Item 6
+            </TableBatchAction>
+            <TableBatchAction
+              tabIndex={getBatchActionProps().shouldShowBatchActions ? 0 : -1}
+              renderIcon={Download}
+              onClick={batchActionClick(selectedRows)}>
+              Item 7
+            </TableBatchAction>
+            <TableBatchAction
+              tabIndex={getBatchActionProps().shouldShowBatchActions ? 0 : -1}
+              renderIcon={Download}
+              onClick={batchActionClick(selectedRows)}>
+              Item 8
+            </TableBatchAction>
+            <TableBatchAction
+              tabIndex={getBatchActionProps().shouldShowBatchActions ? 0 : -1}
+              renderIcon={Download}
+              onClick={batchActionClick(selectedRows)}>
+              Item 9
+            </TableBatchAction>
+          </TableBatchActions>
+          <TableToolbarContent>
+            <TableToolbarSearch
+              persistent="true"
+              tabIndex={getBatchActionProps().shouldShowBatchActions ? -1 : 0}
+              onChange={onInputChange}
+            />
+            <TableToolbarMenu
+              tabIndex={getBatchActionProps().shouldShowBatchActions ? -1 : 0}>
+              <TableToolbarAction onClick={() => alert('Alert 1')}>
+                Action 1
+              </TableToolbarAction>
+              <TableToolbarAction onClick={() => alert('Alert 2')}>
+                Action 2
+              </TableToolbarAction>
+              <TableToolbarAction onClick={() => alert('Alert 3')}>
+                Action 3
+              </TableToolbarAction>
+            </TableToolbarMenu>
+            <Button
+              tabIndex={getBatchActionProps().shouldShowBatchActions ? -1 : 0}
+              onClick={action('Add new row')}
+              size="small"
+              kind="primary">
+              Add new
+            </Button>
+          </TableToolbarContent>
+        </TableToolbar>
+        <Table {...getTableProps()}>
+          <TableHead>
+            <TableRow>
+              <TableSelectAll {...getSelectionProps()} />
+              {headers.map((header, i) => (
+                <TableHeader key={i} {...getHeaderProps({ header })}>
+                  {header.header}
+                </TableHeader>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row, i) => (
+              <TableRow key={i} {...getRowProps({ row })}>
+                <TableSelectRow {...getSelectionProps({ row })} />
+                {row.cells.map((cell) => (
+                  <TableCell key={cell.id}>{cell.value}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    )}
+  </DataTable>
+);
