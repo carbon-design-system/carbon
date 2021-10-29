6ce1b54ee0f0407d1949b3c6375eff6f91cce8c0
Josefina Mancilla <32556167+jnm2377@users.noreply.github.com>
2021-10-29 14:00:37 -0500

    fix(DataTable): fixes overflowing sort button and select column spacing (#9956)
    * fix: add width to checkbox column
    
    * feat: add select/sort story
    
    * feat: add fix to styles package
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/react/src/components/DataTable/stories/DataTable-selection-story.js
+++ packages/react/src/components/DataTable/stories/DataTable-selection-story.js
@@ -124,3 +124,45 @@ export const WithRadioSelection = () => (
     )}
   </DataTable>
 );
+
+export const WithSelectionAndSorting = () => (
+  <DataTable rows={rows} headers={headers} isSortable>
+    {({
+      rows,
+      headers,
+      getHeaderProps,
+      getRowProps,
+      getSelectionProps,
+      getTableProps,
+      getTableContainerProps,
+    }) => (
+      <TableContainer
+        title="DataTable"
+        description="With selection"
+        {...getTableContainerProps()}>
+        <Table {...getTableProps()}>
+          <TableHead>
+            <TableRow>
+              <TableSelectAll {...getSelectionProps()} />
+              {headers.map((header, i) => (
+                <TableHeader key={i} {...getHeaderProps({ header })}>
+                  {header.header}
+                </TableHeader>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row, i) => (
+              <TableRow key={i} {...getRowProps({ row })}>
+                <TableSelectRow {...getSelectionProps({ row })} />
+                {row.cells.map((cell) => (
+                  <TableCell key={cell.id}>{cell.value}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    )}
+  </DataTable>
+);
