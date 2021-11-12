014c2376bf76f7ecaa994bcb82134f5326d1b50f
Taylor Jones <taylor.jones826@gmail.com>
2021-11-11 20:38:06 -0600

    docs(form): use stack for form spacing examples, fix label spacing

--- packages/carbon-react/src/components/Form/Form.stories.js
+++ packages/carbon-react/src/components/Form/Form.stories.js
@@ -21,6 +21,7 @@ import {
   TextArea,
   TextInput,
   Toggle,
+  unstable_Stack as Stack,
 } from 'carbon-components-react';
 
 const checkboxEvents = {
@@ -126,100 +127,102 @@ export default {
 
 export const Default = () => (
   <Form>
-    <FormGroup {...fieldsetCheckboxProps()}>
-      <Checkbox defaultChecked {...checkboxEvents} id="checkbox-0" />
-      <Checkbox {...checkboxEvents} id="checkbox-1" />
-      <Checkbox disabled {...checkboxEvents} id="checkbox-2" />
-    </FormGroup>
+    <Stack gap={6}>
+      <FormGroup {...fieldsetCheckboxProps()}>
+        <Checkbox defaultChecked {...checkboxEvents} id="checkbox-0" />
+        <Checkbox {...checkboxEvents} id="checkbox-1" />
+        <Checkbox disabled {...checkboxEvents} id="checkbox-2" />
+      </FormGroup>
 
-    <NumberInput {...numberInputProps} />
+      <NumberInput {...numberInputProps} />
 
-    <FormGroup {...fieldsetToggleProps}>
-      <Toggle {...toggleProps} id="toggle-1" />
-      <Toggle disabled {...toggleProps} id="toggle-2" />
-    </FormGroup>
+      <FormGroup {...fieldsetToggleProps}>
+        <Toggle {...toggleProps} id="toggle-1" />
+        <Toggle disabled {...toggleProps} id="toggle-2" />
+      </FormGroup>
 
-    <FormGroup {...fieldsetFileUploaderProps}>
-      <FileUploader
-        {...fileUploaderEvents}
-        id="file-1"
-        labelDescription="Choose Files..."
-      />
-    </FormGroup>
-
-    <FormGroup {...fieldsetRadioProps}>
-      <RadioButtonGroup
-        name="radio-button-group"
-        defaultSelected="default-selected">
-        <RadioButton
-          value="standard"
-          id="radio-1"
-          labelText="Standard Radio Button"
-          {...radioProps}
+      <FormGroup {...fieldsetFileUploaderProps}>
+        <FileUploader
+          {...fileUploaderEvents}
+          id="file-1"
+          labelDescription="Choose Files..."
         />
-        <RadioButton
-          value="default-selected"
-          labelText="Default Selected Radio Button"
-          id="radio-2"
-          {...radioProps}
+      </FormGroup>
+
+      <FormGroup {...fieldsetRadioProps}>
+        <RadioButtonGroup
+          name="radio-button-group"
+          defaultSelected="default-selected">
+          <RadioButton
+            value="standard"
+            id="radio-1"
+            labelText="Standard Radio Button"
+            {...radioProps}
+          />
+          <RadioButton
+            value="default-selected"
+            labelText="Default Selected Radio Button"
+            id="radio-2"
+            {...radioProps}
+          />
+          <RadioButton
+            value="blue"
+            labelText="Standard Radio Button"
+            id="radio-3"
+            {...radioProps}
+          />
+          <RadioButton
+            value="disabled"
+            labelText="Disabled Radio Button"
+            id="radio-4"
+            disabled
+            {...radioProps}
+          />
+        </RadioButtonGroup>
+      </FormGroup>
+
+      <FormGroup {...fieldsetSearchProps}>
+        <Search
+          {...searchProps}
+          id="search-1"
+          labelText="Search"
+          placeholder="Search"
         />
-        <RadioButton
-          value="blue"
-          labelText="Standard Radio Button"
-          id="radio-3"
-          {...radioProps}
-        />
-        <RadioButton
-          value="disabled"
-          labelText="Disabled Radio Button"
-          id="radio-4"
+      </FormGroup>
+
+      <Select {...selectProps} id="select-1" defaultValue="placeholder-item">
+        <SelectItem
           disabled
-          {...radioProps}
+          hidden
+          value="placeholder-item"
+          text="Choose an option"
         />
-      </RadioButtonGroup>
-    </FormGroup>
+        <SelectItem value="option-1" text="Option 1" />
+        <SelectItem value="option-2" text="Option 2" />
+        <SelectItem value="option-3" text="Option 3" />
+      </Select>
 
-    <FormGroup {...fieldsetSearchProps}>
-      <Search
-        {...searchProps}
-        id="search-1"
-        labelText="Search"
-        placeholder="Search"
+      <TextInput {...TextInputProps} />
+
+      <TextInput
+        type="password"
+        required
+        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
+        {...PasswordProps}
       />
-    </FormGroup>
 
-    <Select {...selectProps} id="select-1" defaultValue="placeholder-item">
-      <SelectItem
-        disabled
-        hidden
-        value="placeholder-item"
-        text="Choose an option"
+      <TextInput
+        type="password"
+        required
+        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
+        {...InvalidPasswordProps}
       />
-      <SelectItem value="option-1" text="Option 1" />
-      <SelectItem value="option-2" text="Option 2" />
-      <SelectItem value="option-3" text="Option 3" />
-    </Select>
 
-    <TextInput {...TextInputProps} />
+      <TextArea {...textareaProps} />
 
-    <TextInput
-      type="password"
-      required
-      pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
-      {...PasswordProps}
-    />
-
-    <TextInput
-      type="password"
-      required
-      pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
-      {...InvalidPasswordProps}
-    />
-
-    <TextArea {...textareaProps} />
-
-    <Button type="submit" className="some-class" {...buttonEvents}>
-      Submit
-    </Button>
+      <Button type="submit" className="some-class" {...buttonEvents}>
+        Submit
+      </Button>
+    </Stack>
   </Form>
 );
