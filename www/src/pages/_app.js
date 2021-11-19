52185550e9e04f97419d3a7be3f6816d37caa0bb
Josh Black <josh@josh.black>
2021-11-19 14:55:55 -0600

    chore(www): fix eslint violations and remove build task

--- www/src/pages/_app.js
+++ www/src/pages/_app.js
@@ -6,7 +6,9 @@
  */
 
 import '../scss/styles.scss';
+import React from 'react';
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
