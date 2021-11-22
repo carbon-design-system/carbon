11ee5cd88c12044ccd208645219c96b2b0c6cb50
Josh Black <josh@josh.black>
2021-11-22 16:57:12 -0600

    chore: check-in work

---
+++ packages/react/src/components/DatePicker/next/index.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { DatePicker } from './DatePicker';
+import { DatePickerInput } from './DatePickerInput';
+
+export { DatePicker, DatePickerInput };
