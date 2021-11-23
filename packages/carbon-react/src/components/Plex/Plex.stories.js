78c68dd437d88c68b731f6010754ffa5904f945c
Josh Black <josh@josh.black>
2021-11-23 16:23:16 -0600

    chore: check-in work

--- packages/carbon-react/src/components/Plex/Plex.stories.js
+++ packages/carbon-react/src/components/Plex/Plex.stories.js
@@ -4,6 +4,8 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+import './story.scss';
 import React from 'react';
 
 export default {
@@ -40,26 +42,187 @@ export default {
   },
 };
 
-export const English = (args) => {
+export const IBMPlexMono = (args) => {
   return (
-    <p dir="auto" style={args}>
+    <>
+      <code className="text-mono" style={args}>
+        This paragraph is in English and is monospaced.
+      </code>
+      <p
+        className="text-mono"
+        style={{
+          ...args,
+          fontStyle: 'italic',
+        }}>
+        This paragraph is in English and is monospaced and italic.
+      </p>
+    </>
+  );
+};
+
+export const IBMPlexSansArabic = (args) => {
+  return (
+    <p
+      dir="auto"
+      style={{
+        ...args,
+        fontFamily: 'IBM Plex Sans Arabic',
+      }}>
+      المغلوطة حول استنكار النشوة وتمجيد الألم نشأت بالفعل، وسأعرض لك التفاصيل
+      لتكتشف حقيقة وأساس تلك السعادة البشرية، فلا أحد يرفض أو يكره أو يتجنب
+      الشعور بالسعادة، ولكن بفضل هؤلاء الأشخاص الذين لا يدركون بأن السعادة لا بد
+      أن نستشعرها بصورة أكثر عقلانية ومنطقية فيعرضهم هذا لمواجهة الظروف الأليمة،
+      وأكرر بأنه لا يوجد من يرغب في الحب ونيل المنال ويتلذذ بالآلام، الألم هو
+      الألم ولكن نتيجة لظروف ما قد تكمن السعاده فيما نتحمله من كد وأسي.
+    </p>
+  );
+};
+
+export const IBMPlexSansCondensed = (args) => {
+  return (
+    <p
+      dir="auto"
+      style={{
+        ...args,
+        fontFamily: 'IBM Plex Sans Condensed',
+      }}>
       This paragraph is in English and correctly goes left to right.
     </p>
   );
 };
 
-export const Mono = (args) => {
+export const IBMPlexSansDevanagari = (args) => {
   return (
-    <code className="text-mono" style={args}>
-      This paragraph is in English and is monospaced.
-    </code>
+    <p
+      dir="auto"
+      style={{
+        ...args,
+        fontFamily: 'IBM Plex Sans Devanagari',
+      }}>
+      कराना अपने सिद्धांत भेदनक्षमता गुजरना विनिमय पेदा ध्येय संपादक आशाआपस ।क
+      सक्षम नयेलिए सोफ़्टवेर सुस्पश्ट संभव जिसकी औषधिक संपादक प्रतिबध उपलब्धता
+      कैसे पढने विभाग पुर्णता गोपनीयता विवरन दिशामे प्रेरना व्याख्या भाषए ऎसाजीस
+      आधुनिक विकेन्द्रित कैसे बनाति परस्पर मुख्यतह अनुवादक उन्हे बारे लेकिन रखति
+      मानसिक जानकारी विभाग उसके जिम्मे सिद्धांत प्राथमिक समाज सारांश दुनिया
+      आवश्यकत ध्वनि विषय औषधिक ब्रौशर उसीएक् असक्षम आवश्यकत कार्यसिधान्तो देखने
+      जिवन
+    </p>
   );
 };
 
-export const Arabic = (args) => {
+export const IBMPlexSansHebrew = (args) => {
   return (
-    <p dir="auto" style={args}>
-      هذه الفقرة باللغة العربية ، لذا يجب الانتقال من اليمين إلى اليسار.
+    <p
+      dir="auto"
+      style={{
+        ...args,
+        fontFamily: 'IBM Plex Sans Hebrew',
+      }}>
+      גם החלה ואלקטרוניקה שער, כתב החול משפטית אדריכלות אם. אנא אודות חרטומים
+      דת, אחר תחבורה מדריכים דת. ראשי ומהימנה לרפובליקה כדי גם, של בקר מתוך
+      ביוני, ויקימדיה בהיסטוריה אם מלא. הבהרה הספרות האטמוספירה או אחר, את ביוני
+      תאולוגיה אנא, סדר ב לתרום בחירות בלשנות.
     </p>
+  );
+};
+
+export const IBMPlexSansJP = (args) => {
+  return (
+    <p
+      dir="auto"
+      style={{
+        ...args,
+        fontFamily: 'IBM Plex Sans JP',
+      }}>
+      新しい時代のこころを映すタイプフェイスデザイン
+    </p>
+  );
+};
+
+export const IBMPlexSansKR = (args) => {
+  return (
+    <p
+      dir="auto"
+      style={{
+        ...args,
+        fontFamily: 'IBM Plex Sans KR',
+      }}>
+      동해 물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세.
+    </p>
+  );
+};
+
+export const IBMPlexSansThaiLooped = (args) => {
+  return (
+    <p
+      dir="auto"
+      style={{
+        ...args,
+        fontFamily: 'IBM Plex Sans Thai Looped',
+      }}>
+      ลักษณะของตัวพิมพ์แต่ละตัวสามารถแสดงออกได้ถึงระดับเสียง
+    </p>
+  );
+};
+
+export const IBMPlexSansThai = (args) => {
+  return (
+    <p
+      dir="auto"
+      style={{
+        ...args,
+        fontFamily: 'IBM Plex Sans Thai',
+      }}>
+      ลักษณะของตัวพิมพ์แต่ละตัวสามารถแสดงออกได้ถึงระดับเสียง
+    </p>
+  );
+};
+
+export const IBMPlexSansVariable = (args) => {
+  return (
+    <p
+      dir="auto"
+      style={{
+        ...args,
+        fontFamily: 'IBM Plex Sans Variable',
+      }}>
+      This paragraph is in English and correctly goes left to right.
+    </p>
+  );
+};
+
+export const IBMPlexSans = (args) => {
+  return (
+    <>
+      <p
+        dir="auto"
+        style={{
+          ...args,
+          fontFamily: 'IBM Plex Sans',
+        }}>
+        This paragraph is in English and correctly goes left to right.
+      </p>
+      <p style={{ ...args, fontFamily: 'IBM Plex Sans', fontStyle: 'italic' }}>
+        This text is italic
+      </p>
+    </>
+  );
+};
+
+export const IBMPlexSerif = (args) => {
+  return (
+    <>
+      <p
+        dir="auto"
+        style={{
+          ...args,
+          fontFamily: 'IBM Plex Serif',
+        }}>
+        This paragraph is in English and correctly goes left to right.
+      </p>
+      <p style={{ ...args, fontFamily: 'IBM Plex Serif', fontStyle: 'italic' }}>
+        This text is italic
+      </p>
+    </>
   );
 };
---
+++ packages/styles/scss/fonts/README.md
@@ -0,0 +1,121 @@
+# `scss/fonts`
+
+## Usage
+
+By default, you can configure what fonts are loaded in your project with the
+`$fonts` variable:
+
+```scss
+@use '@carbon/styles' with (
+  $fonts: (
+    IBM-Plex-Sans: true,
+  ),
+);
+```
+
+By default, the following fonts are enabled:
+
+- IBM Plex Sans
+- IBM Plex Mono
+- IBM Plex Serif
+
+You can also enable the following fonts from IBM Plex:
+
+- `IBM-Plex-Sans-Arabic`
+- `IBM-Plex-Sans-Condensed`
+- `IBM-Plex-Sans-Devanagari`
+- `IBM-Plex-Sans-Hebrew`
+- `IBM-Plex-Sans-JP`
+- `IBM-Plex-Sans-KR`
+- `IBM-Plex-Sans-Thai-Looped`
+- `IBM-Plex-Sans-Thai`
+- `IBM-Plex-Sans-Variable`
+
+By default, we provide the Light, Regular, and SemiBold font weights for each
+font, where applicable, along with an italic variant if available.
+
+If you would like to customize which font weights and styles are loaded, you can
+use a font entrypoint.
+
+**Disabling a font**
+
+You can disable a specific font from loading by setting it to `false`:
+
+```scss
+@use '@carbon/styles' with (
+  $fonts: (
+    IBM-Plex-Sans: true,
+  ),
+);
+```
+
+**Customizing a font**
+
+```scss
+@use '@carbon/scss/fonts/sans';
+@use '@carbon/scss/fonts/serif';
+
+@include sans.regular(normal);
+@include serif.default();
+```
+
+**Variable font**
+
+**Unicode ranges**
+
+**Japanese and Korean**
+
+How to deal with large file sizes?
+
+**Font entrypoints**
+
+| Entrypoint                    | styles         | complete | split | hinted | default | all | thin | extra-light | light | regular | text | medium | semibold | bold |
+| ----------------------------- | -------------- | -------- | ----- | ------ | ------- | --- | ---- | ----------- | ----- | ------- | ---- | ------ | -------- | ---- |
+| `scss/fonts/mono`             | normal, italic |          | ✅    |        | ✅      | ✅  | ✅   | ✅          | ✅    | ✅      | ✅   | ✅     | ✅       | ✅   |
+| `scss/fonts/sans-arabic`      | normal         | ✅       |       |        | ✅      | ✅  | ✅   | ✅          | ✅    | ✅      | ✅   | ✅     | ✅       | ✅   |
+| `scss/fonts/sans-devanagari`  | normal         | ✅       |       |        | ✅      | ✅  | ✅   | ✅          | ✅    | ✅      | ✅   | ✅     | ✅       | ✅   |
+| `scss/fonts/sans-hebrew`      | normal         | ✅       |       |        | ✅      | ✅  | ✅   | ✅          | ✅    | ✅      | ✅   | ✅     | ✅       | ✅   |
+| `scss/fonts/sans-jp`          | normal         |          | ✅    | ✅     | ✅      | ✅  | ✅   | ✅          | ✅    | ✅      | ✅   | ✅     | ✅       | ✅   |
+| `scss/fonts/sans-kr`          | normal         |          | ✅    | ✅     | ✅      | ✅  | ✅   | ✅          | ✅    | ✅      | ✅   | ✅     | ✅       | ✅   |
+| `scss/fonts/sans-thai-looped` | normal         | ✅       |       |        | ✅      | ✅  | ✅   | ✅          | ✅    | ✅      | ✅   | ✅     | ✅       | ✅   |
+| `scss/fonts/sans-thai`        | normal         | ✅       |       |        | ✅      | ✅  | ✅   | ✅          | ✅    | ✅      | ✅   | ✅     | ✅       | ✅   |
+| `scss/fonts/sans-variable`    | normal, italic | ✅       |       |        | ✅      | ✅  |      |             |       |         |      |        |          |      |
+| `scss/fonts/sans`             | normal, italic |          | ✅    |        | ✅      | ✅  | ✅   | ✅          | ✅    | ✅      | ✅   | ✅     | ✅       | ✅   |
+| `scss/fonts/serif`            | normal, italic |          | ✅    |        | ✅      | ✅  | ✅   | ✅          | ✅    | ✅      | ✅   | ✅     | ✅       | ✅   |
+
+## Notes
+
+- The source of the unicode ranges matter, if ordered incorrectly then all are
+  loaded
+- Fonts support all weights (except for VF)
+- Each font has an entrypoint with mixins you can use
+  - weight mixins
+  - all weights
+  - default (our pick, suggested config)
+- How do you configure the font path?
+- Some fonts include "hinting"
+- How to review
+  - Go to a specific font
+  - Open up network panel
+  - Pick fonts
+  - Clear requests
+  - Reload page
+  - Verify regular font has loaded (with split, if applicable)
+  - Go through each font weight, check to see if it loads
+  - Inspect the text, make sure it says "network resource" for the font
+- Seems like TTF is missing for Sans Variable
+- Seems like JP is missing in latest beta
+
+Fonts
+
+- [x] IBM Plex Mono
+- [x] IBM Plex Sans Arabic
+- [x] IBM Plex Sans Devanagari
+- [x] IBM Plex Sans Hebrew
+- [x] IBM Plex Sans JP
+- [x] IBM Plex Sans KR
+- [x] IBM Plex Sans Thai Looped
+- [x] IBM Plex Sans Thai
+- [ ] IBM Plex Sans Variable
+- [x] IBM Plex Sans
+- [x] IBM Plex Serif
