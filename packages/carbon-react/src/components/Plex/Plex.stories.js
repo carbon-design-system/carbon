14010efd86ac56386bfa055073172c5b18950c7e
Josh Black <josh@josh.black>
2021-12-01 13:40:42 -0600

    feat(carbon-react): add new scss exports and update plex stories

--- packages/carbon-react/src/components/Plex/Plex.stories.js
+++ packages/carbon-react/src/components/Plex/Plex.stories.js
@@ -4,62 +4,57 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+import './story.scss';
+
 import React from 'react';
 
 export default {
-  title: 'Elements/Plex',
+  title: 'Elements/IBM Plex',
   argTypes: {
     fontWeight: {
+      control: {
+        type: 'radio',
+      },
       defaultValue: 'Regular',
-      options: [
-        'Thin',
-        'Extra Light',
-        'Light',
-        'Regular',
-        'Text',
-        'Medium',
-        'SemiBold',
-        'Bold',
-      ],
       mapping: {
-        Thin: 100,
-        'Extra Light': 200,
         Light: 300,
         Regular: 400,
-        Text: 450,
-        Medium: 500,
         SemiBold: 600,
-        Bold: 700,
       },
-      control: { type: 'radio' },
-    },
-    fontSize: {
-      defaultValue: 16,
-      control: { type: 'range', min: 12, max: 54, step: 4 },
+      options: ['Light', 'Regular', 'SemiBold'],
     },
   },
 };
 
-export const English = (args) => {
+export const IBMPlexMono = (args) => {
   return (
-    <p dir="auto" style={args}>
-      This paragraph is in English and correctly goes left to right.
-    </p>
-  );
-};
-
-export const Mono = (args) => {
-  return (
-    <code className="text-mono" style={args}>
-      This paragraph is in English and is monospaced.
+    <code dir="auto" style={args} className="text-mono">
+      This paragraph is in English and goes left to right.
     </code>
   );
 };
 
-export const Arabic = (args) => {
+export const IBMPlexSansArabic = (args) => {
   return (
-    <p dir="auto" style={args}>
+    <p dir="auto" style={args} className="text-sans-arabic">
       هذه الفقرة باللغة العربية ، لذا يجب الانتقال من اليمين إلى اليسار.
+    </p>
+  );
+};
+
+export const IBMPlexSans = (args) => {
+  return (
+    <p dir="auto" style={args} className="text-sans">
+      This paragraph is in English and goes left to right.
+    </p>
+  );
+};
+
+export const IBMPlexSerif = (args) => {
+  return (
+    <p dir="auto" style={args} className="text-serif">
+      This paragraph is in English and goes left to right.
     </p>
   );
 };
