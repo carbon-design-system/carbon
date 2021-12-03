48cec747b876cf8353abfe3f2f9ba4790c06e454
Taylor Jones <tay1orjones@users.noreply.github.com>
2021-12-03 15:09:26 -0600

    fix(tooltip): ensure esc closes tooltip, avoid bubbling beyond the body (#10220)
    Co-authored-by: Abbey Hart <abbeyhrt@gmail.com>

--- packages/react/src/components/Tooltip/Tooltip-story.js
+++ packages/react/src/components/Tooltip/Tooltip-story.js
@@ -167,12 +167,6 @@ export const DefaultBottom = () => (
         that should appear inside. If more room is needed please use a modal
         instead.
       </p>
-      <div className={`${prefix}--tooltip__footer`}>
-        <a href="/" className={`${prefix}--link`}>
-          Learn More
-        </a>
-        <Button size="small">Create</Button>
-      </div>
     </Tooltip>
   </div>
 );
