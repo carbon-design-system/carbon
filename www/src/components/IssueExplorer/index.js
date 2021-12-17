df997c0e3688fc46216c0e89bd7709c3086a227b
Josh Black <josh@josh.black>
2021-12-17 14:10:27 -0600

    chore: check-in work

--- www/src/components/IssueExplorer/index.js
+++ www/src/components/IssueExplorer/index.js
@@ -7,67 +7,61 @@
 
 import * as classes from './IssueExplorer.module.scss';
 import {
+  Button,
   Table,
   TableHead,
   TableRow,
   TableHeader,
   TableBody,
   TableCell,
+  Tag,
 } from '@carbon/react';
+import { ChevronRight, ChevronDown } from '@carbon/react/icons';
+import { parseISO, format } from 'date-fns';
+import { Flex } from '../Flex';
+import { Text } from '../Text';
 import { useState } from 'react';
 
-function IssueExplorer({ issues }) {
+function IssueExplorer({ issues, labelGroups }) {
   const [activeLabelIds, setActiveLabelIds] = useState([]);
   const issuesById = new Map();
   const labelsById = new Map();
-  const labelGroups = new Map();
-  const filteredIssues = issues.filter((issue) => {
-    return activeLabelIds.every((labelId) => {
-      return issue.labels.find((label) => {
-        return label.id === labelId;
-      });
-    });
-  });
 
-  for (const issue of filteredIssues) {
-    issuesById.set(issue.number, issue);
-
-    for (const label of issue.labels) {
+  for (const group of labelGroups) {
+    for (const label of group.labels) {
       if (!labelsById.has(label.id)) {
         labelsById.set(label.id, {
           ...label,
           issues: [],
         });
       }
-
-      const details = labelsById.get(label.id);
-      details.issues.push(issue.number);
-
-      if (label.name.includes(': ')) {
-        const [group] = label.name.split(': ');
-
-        if (!labelGroups.has(group)) {
-          labelGroups.set(group, {
-            label: group,
-            labels: new Set(),
-          });
-        }
-
-        const labelGroup = labelGroups.get(group);
-        if (!labelGroup.labels.has(label.id)) {
-          labelGroup.labels.add(label.id);
-        }
-      }
     }
   }
 
-  const groups = Array.from(labelGroups.values()).sort((a, b) => {
-    return b.labels.length - a.labels.length;
+  for (const issue of issues) {
+    const isActive = activeLabelIds.every((labelId) => {
+      return issue.labels.find((label) => {
+        return label.id === labelId;
+      });
+    });
+
+    issuesById.set(issue.number, {
+      ...issue,
+      isActive: isActive,
+    });
+
+    for (const label of issue.labels) {
+      labelsById.get(label.id).issues.push(issue.number);
+    }
+  }
+
+  const filteredIssues = issues.filter((issue) => {
+    return issuesById.get(issue.number).isActive;
   });
 
-  function toggleLabel(label) {
+  function toggleLabel(labelId) {
     const index = activeLabelIds.findIndex((id) => {
-      return id === label.id;
+      return id === labelId;
     });
 
     // Selected
@@ -76,41 +70,61 @@ function IssueExplorer({ issues }) {
       ids.splice(index, 1);
       setActiveLabelIds(ids);
     } else {
-      setActiveLabelIds(activeLabelIds.concat(label.id));
+      setActiveLabelIds(activeLabelIds.concat(labelId));
     }
   }
 
   return (
     <div className={classes.layout}>
       <aside className={classes.filterLayout}>
+        <h1>
+          <Flex align-center justify-between px-5 pt-5 pb-2>
+            <Text productive-heading-01>Issues</Text>
+            <span>{filteredIssues.length}</span>
+          </Flex>
+        </h1>
         <div className={classes.filters}>
-          {groups.map((group) => {
-            const total = Array.from(group.labels).reduce((sum, labelId) => {
-              const label = labelsById.get(labelId);
-              return sum + label.issues.length;
+          {labelGroups.map((group) => {
+            const total = Array.from(group.labels).reduce((sum, label) => {
+              const { issues } = labelsById.get(label.id);
+              const activeIssues = issues.filter((id) => {
+                return issuesById.get(id).isActive;
+              });
+              return sum + activeIssues.length;
             }, 0);
+
             return (
-              <details key={group.label} className={classes.filter}>
+              <details key={group.name} className={classes.filter}>
                 <summary className={classes.filterLabel}>
-                  {group.label} ({total})
+                  <Flex align-center gap-x-2>
+                    <ChevronDown />
+                    <Text body-short-02>{group.name}</Text>
+                  </Flex>
+                  <span>{total}</span>
                 </summary>
 
                 <ul className={classes.options}>
-                  {Array.from(group.labels).map((id) => {
-                    const label = labelsById.get(id);
+                  {group.labels.map((label) => {
+                    const activeIssues = labelsById
+                      .get(label.id)
+                      .issues.filter((id) => {
+                        return issuesById.get(id).isActive;
+                      });
+
                     return (
-                      <li key={id}>
-                        <label>
-                          <input
-                            type="checkbox"
-                            onChange={() => {
-                              toggleLabel(label);
-                            }}
-                            checked={activeLabelIds.includes(label.id)}
-                          />
-                          <span>
-                            {label.name} ({label.issues.length})
-                          </span>
+                      <li key={label.id}>
+                        <label className={classes.label}>
+                          <Flex align-center gap-x-1 as="span">
+                            <input
+                              type="checkbox"
+                              onChange={() => {
+                                toggleLabel(label.id);
+                              }}
+                              checked={activeLabelIds.includes(label.id)}
+                            />
+                            {label.name}
+                          </Flex>
+                          <span>{activeIssues.length}</span>
                         </label>
                       </li>
                     );
@@ -120,49 +134,84 @@ function IssueExplorer({ issues }) {
             );
           })}
         </div>
-        <button
-          type="button"
-          onClick={() => {
-            setActiveLabelIds([]);
-          }}>
-          Reset
-        </button>
+        <Flex col gap-y-3>
+          <Flex px-5 wrap pt-3>
+            {activeLabelIds.length > 0
+              ? activeLabelIds.map((id) => {
+                  const label = labelsById.get(id);
+                  return (
+                    <Tag
+                      type="high-contrast"
+                      filter
+                      onClick={() => {
+                        toggleLabel(id);
+                      }}>
+                      {label.name}
+                    </Tag>
+                  );
+                })
+              : null}
+          </Flex>
+          <Button
+            onClick={() => {
+              setActiveLabelIds([]);
+            }}>
+            Reset
+          </Button>
+        </Flex>
       </aside>
       <div className={classes.issues}>
-        <h1>Issues ({filteredIssues.length})</h1>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableHeader>Title</TableHeader>
-              <TableHeader>Labels</TableHeader>
-              <TableHeader>Created at</TableHeader>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {filteredIssues.map((issue) => {
-              return (
-                <TableRow key={issue.number}>
-                  <TableCell>
-                    <a
-                      rel="noopener noreferrer"
-                      target="_blank"
-                      href={issue.html_url}>
-                      {issue.title}
-                    </a>
-                  </TableCell>
-                  <TableCell>
-                    <ul>
-                      {issue.labels.map((label) => {
-                        return <li key={label.id}>{label.name}</li>;
-                      })}
-                    </ul>
-                  </TableCell>
-                  <TableCell>{issue.created_at}</TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
+        {filteredIssues.length > 0 ? (
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableHeader>Title</TableHeader>
+                <TableHeader>Labels</TableHeader>
+                <TableHeader>Created at</TableHeader>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {filteredIssues.map((issue) => {
+                return (
+                  <TableRow key={issue.number}>
+                    <TableCell>
+                      <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href={issue.html_url}>
+                        {issue.title}
+                      </a>
+                    </TableCell>
+                    <TableCell>
+                      <ul>
+                        {issue.labels.map((label) => {
+                          const isActive = activeLabelIds.includes(label.id);
+                          return (
+                            <li key={label.id}>
+                              <Tag
+                                type={isActive ? 'high-contrast' : 'gray'}
+                                filter={isActive}
+                                onClick={() => {
+                                  toggleLabel(label.id);
+                                }}>
+                                {label.name}
+                              </Tag>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </TableCell>
+                    <TableCell>
+                      {format(parseISO(issue.created_at), 'MMMM do')}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        ) : (
+          <p>No issues available for the current filters</p>
+        )}
       </div>
     </div>
   );
--- www/src/pages/issues/[owner]/[repo].js
+++ www/src/pages/issues/[owner]/[repo].js
@@ -9,12 +9,12 @@ import { Header } from '../../../components/Header';
 import { getRepoIssueDetails, filters } from '../../../github';
 import { IssueExplorer } from '../../../components/IssueExplorer';
 
-export default function IssuePage({ issues }) {
+export default function IssuePage({ issues, labels }) {
   return (
     <>
       <Header />
       <main>
-        <IssueExplorer issues={issues} />
+        <IssueExplorer issues={issues} labelGroups={labels} />
       </main>
     </>
   );
@@ -27,12 +27,84 @@ export async function getStaticProps({ params }) {
     repo,
   });
   const open = filters.apply(issues, filters.open);
+  const labels = new Map();
+
+  for (const issue of open) {
+    for (const label of issue.labels) {
+      if (!labels.has(label.id)) {
+        labels.set(label.id, {
+          id: label.id,
+          name: label.name,
+          color: label.color,
+        });
+      }
+    }
+  }
+
+  const categories = new Map([
+    [
+      'uncategorized',
+      {
+        name: 'uncategorized',
+        labels: [],
+      },
+    ],
+  ]);
+
+  for (const label of labels.values()) {
+    if (label.name.includes(': ')) {
+      const [category] = label.name.split(': ');
+      if (!categories.has(category)) {
+        categories.set(category, {
+          name: category,
+          labels: [],
+        });
+      }
+      categories.get(category).labels.push(label);
+    } else {
+      categories.get('uncategorized').labels.push(label);
+    }
+  }
+
+  const order = [
+    'type',
+    'component',
+    'package',
+    'status',
+    'impact',
+    'severity',
+    'proposal',
+    'planning',
+  ];
 
   return {
     props: {
       owner,
       repo,
       issues: open,
+      labels: Array.from(categories.values())
+        .sort((a, b) => {
+          const weightA = order.indexOf(a.name);
+          const weightB = order.indexOf(b.name);
+          if (weightA === -1 && weightB === -1) {
+            return a.name.localeCompare(b.name);
+          }
+          if (weightA === -1) {
+            return 1;
+          }
+          if (weightB === -1) {
+            return -1;
+          }
+          return weightA - weightB;
+        })
+        .map((group) => {
+          return {
+            ...group,
+            labels: group.labels.sort((a, b) => {
+              return a.name.localeCompare(b.name);
+            }),
+          };
+        }),
     },
   };
 }
