ccfdc2710856d951e7ba2b57df9963b0e637b8e8
Josh Black <josh@josh.black>
2022-01-05 16:41:29 -0600

    chore(project): sync offline mirror and run format

--- packages/carbon-react/.storybook/theme.js
+++ packages/carbon-react/.storybook/theme.js
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-
 import { create } from '@storybook/theming';
 import { g10 } from '@carbon/themes';
 import PackageInfo from './../package.json';
