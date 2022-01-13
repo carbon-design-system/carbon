98efac9abd3ec95a6fe1a0ae5a03e1df7e7d6959
Josh Black <josh@josh.black>
2022-01-12 18:39:49 -0600

    test(react): add browser mock for matchMedia

--- packages/react/src/internal/__tests__/useMatchMedia-test.js
+++ packages/react/src/internal/__tests__/useMatchMedia-test.js
@@ -5,67 +5,116 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { cleanup, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import React from 'react';
+import { act } from 'react-dom/test-utils';
 import { useMatchMedia } from '../useMatchMedia';
 
-function resizeBrowserWidth(width) {}
-
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation((query) => ({
-    matches: query === '(min-width: 768px)' ? true : false,
-    media: query,
-    onchange: jest.fn(),
-    addListener: jest.fn(),
-    removeListener: jest.fn(),
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
-
 describe('useMatchMedia', () => {
-  afterEach(cleanup);
+  let Browser;
+  let listeners;
+
+  beforeEach(() => {
+    listeners = new Set();
+    Browser = {
+      width: 640,
+      setDimensions(width) {
+        Browser.width = width;
+
+        for (const { eventType, listener, match } of listeners) {
+          if (eventType === 'change') {
+            listener({
+              matches: match(),
+            });
+          }
+        }
+      },
+    };
+
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => {
+        const MIN_WIDTH_REGEX = /\(min-width: (\d+)px\)/;
+        const [_match, rawWidth] = query.match(MIN_WIDTH_REGEX);
+        const width = parseInt(rawWidth, 10);
+
+        function match() {
+          return Browser.width >= width;
+        }
+
+        return {
+          matches: match(),
+          media: query,
+          onchange: jest.fn(),
+          addListener: jest.fn(),
+          removeListener: jest.fn(),
+          addEventListener: (eventType, listener) => {
+            listeners.add({
+              eventType,
+              listener,
+              match,
+            });
+          },
+          removeEventListener: jest.fn(),
+          dispatchEvent: jest.fn(),
+        };
+      }),
+    });
+  });
+
+  afterEach(() => {
+    listeners.clear();
+    delete window.matchMedia;
+  });
 
   it('should return true if mediaQueryList matches', () => {
-    const query = '(min-width: 768px)';
+    Browser.setDimensions(768);
+
     let matches;
+
     function Test() {
-      matches = useMatchMedia(query);
-      return <span>test</span>;
+      matches = useMatchMedia('(min-width: 768px)');
+      return null;
     }
+
     render(<Test />);
     expect(matches).toBe(true);
   });
-  it.only('should update the match value if the query no longer applies', () => {
+
+  it('should keep state in sync when mediaQueryString is changed', () => {
+    Browser.setDimensions(768);
+
+    let matches;
+
+    function Test(props) {
+      matches = useMatchMedia(props.query);
+      return null;
+    }
+
+    const { rerender } = render(<Test query="(min-width: 768px)" />);
+    expect(matches).toBe(true);
+
+    rerender(<Test query="(min-width: 900px)" />);
+    expect(matches).toBe(false);
+  });
+
+  it('should update the match value if the query no longer applies', () => {
+    Browser.setDimensions(640);
+
     let matches = null;
 
     function TestComponent() {
-      matches = useMatchMedia('(min-width: 320px)');
+      matches = useMatchMedia('(min-width: 640px)');
       return null;
     }
 
     render(<TestComponent />);
     expect(matches).toBe(true);
 
-    resiveBrowserWidth(960);
+    act(() => {
+      Browser.setDimensions(320);
+    });
 
-    expect(matches).toBe(false);
-  });
-  it('should keep state in sync when mediaQueryString is changed', () => {
-    const query1 = '(min-width: 768px)';
-    const query2 = '(min-width: 900px)';
-    let matches;
-
-    function Test(props) {
-      matches = useMatchMedia(props.query);
-      return <span>test</span>;
-    }
-
-    const { rerender } = render(<Test query={query1} />);
-    expect(matches).toBe(true);
-    rerender(<Test query={query2} />);
     expect(matches).toBe(false);
   });
 });
