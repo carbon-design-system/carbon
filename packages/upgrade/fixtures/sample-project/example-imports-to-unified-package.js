a6c771604b8214b62325b20d0e62e171c75176af
Taylor Jones <taylor.jones826@gmail.com>
2022-01-24 15:38:58 -0600

    feat(upgrade): add imports-to-unified-package migration, associated runner updates

---
+++ packages/upgrade/fixtures/sample-project/example-imports-to-unified-package.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+
+import { Button } from 'carbon-components-react';
+import { Tile, Tooltip } from 'carbon-components-react';
+import CodeSnippet from 'carbon-components-react';
+import Something from 'somewhere-else';
---
+++ packages/upgrade/src/jscodeshift.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright IBM Corp. 2019, 2019
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import which from 'npm-which';
+import spawn from './spawn';
+import path from 'path';
+
+let jscodeshift;
+
+function run(transformFileName, options) {
+  console.log(options);
+  if (!jscodeshift) {
+    jscodeshift = which.sync('jscodeshift', {
+      // Note: we could pass options.cwd but could that potentially resolve
+      // to a version of jscodeshift that we're not expecting?
+      //   cwd: __dirname // doesn't work :(
+      cwd: '/Users/taylorjones/dev/carbon/monorepo',
+    });
+  }
+
+  const {
+    cwd,
+    stdio = 'inherit',
+    TRANSFORM_DIR,
+    ...jscodeshiftOptions
+  } = options;
+  const transformFile = path.join(TRANSFORM_DIR, transformFileName);
+  const args = [
+    jscodeshiftOptions.print && '--print',
+    jscodeshiftOptions.verbose && '-v',
+    '--babel',
+    jscodeshiftOptions.dry && '--dry',
+    '--parser=babylon',
+    `--ignore-pattern=**/build/**`,
+    `--ignore-pattern=**/dist/**`,
+    `--ignore-pattern=**/es/**`,
+    `--ignore-pattern=**/lib/**`,
+    `--ignore-pattern=**/node_modules/**`,
+    `--ignore-pattern=**/storybook-static/**`,
+    `--ignore-pattern=**/umd/**`,
+    `-t=${transformFile}`,
+    jscodeshiftOptions.workspaceDir,
+  ].filter(Boolean);
+
+  return spawn(jscodeshift, args, {
+    cwd,
+    stdio,
+  });
+}
+
+module.exports = {
+  run,
+};
