f80311d10ee59f03b44e87a6c8878c5d64c8ccd7
Taylor Jones <taylor.jones826@gmail.com>
2022-01-27 15:58:54 -0600

    test(codemod): imports-to-unified-package tests

---
+++ packages/upgrade/transforms/__testfixtures__/imports-to-unified-packages.input.js
@@ -0,0 +1,5 @@
+import { Button } from 'carbon-components-react';
+import { Tile, Tooltip } from 'carbon-components-react';
+import CodeSnippet from 'carbon-components-react';
+import Something from 'somewhere-else';
+import { SomethingElse } from 'somewhere-else';
---
+++ packages/upgrade/transforms/__testfixtures__/imports-to-unified-packages.output.js
@@ -0,0 +1,5 @@
+import { Button } from '@carbon/react';
+import { Tile, Tooltip } from '@carbon/react';
+import CodeSnippet from '@carbon/react';
+import Something from 'somewhere-else';
+import { SomethingElse } from 'somewhere-else';
