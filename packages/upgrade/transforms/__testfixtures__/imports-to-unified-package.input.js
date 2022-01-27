b24fcccae57f78428d0d3037861f3bce86adb3f2
Taylor Jones <taylor.jones826@gmail.com>
2022-01-27 16:38:14 -0600

    test(upgrade): handle syntax/formatting changes from output, correct filename

--- packages/upgrade/transforms/__testfixtures__/imports-to-unified-package.input.js
+++ packages/upgrade/transforms/__testfixtures__/imports-to-unified-package.input.js
@@ -1,5 +1,8 @@
+//prettier-ignore
 import { Button } from 'carbon-components-react';
+//prettier-ignore
 import { Tile, Tooltip } from 'carbon-components-react';
+//prettier-ignore
 import CodeSnippet from 'carbon-components-react';
 import Something from 'somewhere-else';
 import { SomethingElse } from 'somewhere-else';
--- packages/upgrade/transforms/__testfixtures__/imports-to-unified-packages.output.js
+++
@@ -1,5 +0,0 @@
-import { Button } from '@carbon/react';
-import { Tile, Tooltip } from '@carbon/react';
-import CodeSnippet from '@carbon/react';
-import Something from 'somewhere-else';
-import { SomethingElse } from 'somewhere-else';
