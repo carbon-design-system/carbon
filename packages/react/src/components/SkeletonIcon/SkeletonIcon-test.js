7a7567998879905d09bcf6d1d461347e88962532
TJ Egan <tw15egan@gmail.com>
2022-02-24 14:18:52 -0500

    feat(SkeletonIcon): add SkeletonIcon component

---
+++ packages/react/src/components/SkeletonIcon/SkeletonIcon-test.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import SkeletonIcon from '../SkeletonIcon';
+import { shallow } from 'enzyme';
+import { settings } from 'carbon-components';
+
+const { prefix } = settings;
+
+describe('SkeletonIcon', () => {
+  const wrapper = shallow(<SkeletonIcon />);
+
+  it('Has the expected classes', () => {
+    expect(wrapper.hasClass(`${prefix}--skeleton--icon`)).toEqual(true);
+  });
+});
---
+++ packages/react/src/components/SkeletonIcon/SkeletonIcon.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import PropTypes from 'prop-types';
+import React from 'react';
+import classNames from 'classnames';
+import { usePrefix } from '../../internal/usePrefix';
+
+const SkeletonIcon = ({ className, ...other }) => {
+  const prefix = usePrefix();
+
+  const skeletonIconClasses = classNames({
+    [`${prefix}--icon--skeleton`]: true,
+    [className]: className,
+  });
+
+  return <div className={skeletonIconClasses} {...other} />;
+};
+
+SkeletonIcon.propTypes = {
+  /**
+   * Specify an optional className to add.
+   */
+  className: PropTypes.string,
+
+  /**
+   * The CSS styles.
+   */
+  style: PropTypes.object,
+};
+
+export default SkeletonIcon;
