b08f1d47e33aba02a36c66b3908883cb84482313
TJ Egan <tw15egan@gmail.com>
2022-03-08 14:36:24 -0500

    fix(DataTable): fix disabled inputs when row is selected

--- packages/react/src/components/DataTable/stories/shared.js
+++ packages/react/src/components/DataTable/stories/shared.js
@@ -6,62 +6,68 @@
  */
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import Link from '../../Link';
+// import Link from '../../Link';
+import RadioButton from '../../RadioButton';
+import Checkbox from '../../Checkbox';
 
 export const rows = [
   {
     id: 'a',
     name: 'Load Balancer 3',
     protocol: 'HTTP',
-    port: 3000,
+    port: (
+      <Checkbox disabled labelText={`Checkbox label`} id="checkbox-label-1" />
+    ),
     rule: 'Round robin',
     attached_groups: 'Kevin’s VM Groups',
-    status: <Link disabled={true}>Disabled</Link>,
+    enabled: (
+      <RadioButton disabled labelText="Option 1" value="radio-1" id="radio-1" />
+    ),
   },
   {
     id: 'b',
     name: 'Load Balancer 1',
     protocol: 'HTTP',
-    port: 443,
+    port: <Checkbox labelText={`Checkbox label`} id="checkbox-label-1" />,
     rule: 'Round robin',
     attached_groups: 'Maureen’s VM Groups',
-    status: <Link>Starting</Link>,
+    enabled: <RadioButton labelText="Option 1" value="radio-1" id="radio-1" />,
   },
   {
     id: 'c',
     name: 'Load Balancer 2',
     protocol: 'HTTP',
-    port: 80,
+    port: <Checkbox labelText={`Checkbox label`} id="checkbox-label-1" />,
     rule: 'DNS delegation',
     attached_groups: 'Andrew’s VM Groups',
-    status: <Link>Active</Link>,
+    enabled: <RadioButton labelText="Option 1" value="radio-1" id="radio-1" />,
   },
   {
     id: 'd',
     name: 'Load Balancer 6',
     protocol: 'HTTP',
-    port: 3000,
+    port: <Checkbox labelText={`Checkbox label`} id="checkbox-label-1" />,
     rule: 'Round robin',
     attached_groups: 'Marc’s VM Groups',
-    status: <Link disabled={true}>Disabled</Link>,
+    enabled: <RadioButton labelText="Option 1" value="radio-1" id="radio-1" />,
   },
   {
     id: 'e',
     name: 'Load Balancer 4',
     protocol: 'HTTP',
-    port: 443,
+    port: <Checkbox labelText={`Checkbox label`} id="checkbox-label-1" />,
     rule: 'Round robin',
     attached_groups: 'Mel’s VM Groups',
-    status: <Link>Starting</Link>,
+    enabled: <RadioButton labelText="Option 1" value="radio-1" id="radio-1" />,
   },
   {
     id: 'f',
     name: 'Load Balancer 5',
     protocol: 'HTTP',
-    port: 80,
+    port: <Checkbox labelText={`Checkbox label`} id="checkbox-label-1" />,
     rule: 'DNS delegation',
     attached_groups: 'Ronja’s VM Groups',
-    status: <Link>Active</Link>,
+    enabled: <RadioButton labelText="Option 1" value="radio-1" id="radio-1" />,
   },
 ];
 
@@ -70,25 +76,29 @@ export const headers = [
     key: 'name',
     header: 'Name',
   },
-  {
-    key: 'protocol',
-    header: 'Protocol',
-  },
+  // {
+  //   key: 'protocol',
+  //   header: 'Protocol',
+  // },
   {
     key: 'port',
     header: 'Port',
   },
-  {
-    key: 'rule',
-    header: 'Rule',
-  },
+  // {
+  //   key: 'rule',
+  //   header: 'Rule',
+  // },
   {
     key: 'attached_groups',
     header: 'Attached Groups',
   },
+  // {
+  //   key: 'status',
+  //   header: 'Status',
+  // },
   {
-    key: 'status',
-    header: 'Status',
+    key: 'enabled',
+    header: 'Enabled',
   },
 ];
 
