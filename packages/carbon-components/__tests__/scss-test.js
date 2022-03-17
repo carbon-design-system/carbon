a31e50fde624c0861d69f33997b0a4b326fdbb04
Josh Black <josh@josh.black>
2022-03-17 11:51:51 -0500

    feat(carbon-components): update package layout
    This commit updates the layout and location of the `carbon-components` package.
    This package will now only re-export the contents of `@carbon/styles`. It will
    no longer export Vanilla JavaScript. It also now has a required `peerDependency`
    on `sass`.
    
    BREAKING CHANGE: this package no longer exports Vanilla JavaScript and only
    re-exports from @carbon/styles

---
+++ packages/carbon-components/__tests__/scss-test.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright IBM Corp. 2018, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @jest-environment node
+ */
+
+'use strict';
+
+const { files } = require('@carbon/styles/files');
+const { SassRenderer } = require('@carbon/test-utils/scss');
+
+const { render } = SassRenderer.create(__dirname);
+
+const relativePaths = files.map((file) => {
+  return file.relativePath;
+});
+
+describe('carbon-components', () => {
+  describe.each(relativePaths)('%s', (relativePath) => {
+    it('should be importable', async () => {
+      await expect(
+        render(`@use '../${relativePath}' as test;`)
+      ).resolves.toBeDefined();
+    });
+  });
+});
