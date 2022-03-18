016ff44ad5f3838f1577f7671c87cec800e40c0e
Josh Black <josh@josh.black>
2022-03-18 16:03:08 -0500

    feat(react): swap carbon-components-react and @carbon/react
    BREAKING CHANGE: carbon-components-react now re-exports from @carbon/react, it
    no longer depends on carbon-components or carbon-icons. It also requires Dart
    Sass and includes Sass styles as exports.

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -4,6 +4,9 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+import './feature-flags';
+
 export Accordion from './components/Accordion';
 export AccordionItem from './components/AccordionItem';
 export { AspectRatio } from './components/AspectRatio';
