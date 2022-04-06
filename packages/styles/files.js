7897a8190fe4fe36c5605cef3570977fd8f996e3
Josh Black <josh@josh.black>
2022-04-06 15:05:01 -0500

    feat(styles): emit layer tokens in theme mixin

--- packages/styles/files.js
+++ packages/styles/files.js
@@ -20,11 +20,11 @@ const files = [
   'scss/_colors.scss',
   'scss/_config.scss',
   'scss/_feature-flags.scss',
-  'scss/_layer.scss',
+  'scss/layer/_index.scss',
   'scss/_motion.scss',
   'scss/_reset.scss',
   'scss/_spacing.scss',
-  'scss/_theme.scss',
+  'scss/theme/_index.scss',
   'scss/_themes.scss',
   'scss/_zone.scss',
 
