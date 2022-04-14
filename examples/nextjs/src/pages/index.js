ac8515058200c6d73b8b99a34f084da7b66297e5
Josh Black <josh@josh.black>
2022-04-14 09:34:06 -0500

    chore(nextjs): remove unstyled section

--- examples/nextjs/src/pages/index.js
+++ examples/nextjs/src/pages/index.js
@@ -18,39 +18,10 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          This is the Button component from Carbon
           <div>
             <Button>Hello world</Button>
           </div>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}>
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
