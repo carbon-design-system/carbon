c2582996b9d3c362fbc618a8f83224b024439ae8
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-04-14 23:36:15 -0400

    feat: switch to vite config

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -9,7 +9,6 @@
 
 const fs = require('fs');
 const glob = require('fast-glob');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 const stories = glob
@@ -85,92 +84,39 @@ module.exports = {
     '@storybook/addon-a11y',
   ],
   core: {
-    builder: 'webpack5',
+    builder: '@storybook/builder-vite',
   },
   features: {
     previewCsfV3: true,
   },
   stories,
-  webpack(config) {
-    const babelLoader = config.module.rules.find((rule) => {
-      return rule.use.some(({ loader }) => {
-        return loader.includes('babel-loader');
-      });
-    });
-
-    // This is a temporary trick to get `babel-loader` to ignore packages that
-    // are brought in that have an es, lib, or umd directory.
-    //
-    // Typically this is covered by /node_modules/ (which is the default), but
-    // in our case it seems like these dependencies are resolving to where their
-    // symlink points to. In other words, `@carbon/icons-react` becomes
-    // `../icons-react/es/index.js`.
-    //
-    // This results in these files being included in `babel-loader` and causing
-    // the build times to increase dramatically
-    babelLoader.exclude = [
-      /node_modules/,
-      /packages\/.*\/(es|lib|umd)/,
-      /packages\/icons-react\/next/,
+  async viteFinal(config) {
+    // Annoying way to add babel plugin,
+    // until https://github.com/storybookjs/builder-vite/issues/286 is fixed
+    config.plugins = [
+      ...config.plugins.filter((plugin) => {
+        return !(
+          Array.isArray(plugin) && plugin[0].name === 'vite:react-babel'
+        );
+      }),
+      require('@vitejs/plugin-react')({
+        exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
+        babel: { presets: ['babel-preset-carbon'] },
+      }),
     ];
-
-    config.module.rules.push({
-      test: /\.s?css$/,
-      sideEffects: true,
-      use: [
-        {
-          loader:
-            process.env.NODE_ENV === 'production'
-              ? MiniCssExtractPlugin.loader
-              : 'style-loader',
+    config.define = {
+      __DEV__: process.env.NODE_ENV !== 'production',
+    };
+    config.css = {
+      preprocessorOptions: {
+        scss: {
+          includePaths: [
+            path.resolve(__dirname, '..', 'node_modules'),
+            path.resolve(__dirname, '..', '..', '..', 'node_modules'),
+          ],
         },
-        {
-          loader: 'css-loader',
-          options: {
-            importLoaders: 2,
-            sourceMap: true,
-          },
-        },
-        {
-          loader: 'postcss-loader',
-          options: {
-            postcssOptions: {
-              plugins: [
-                require('autoprefixer')({
-                  overrideBrowserslist: ['last 1 version'],
-                }),
-              ],
-            },
-            sourceMap: true,
-          },
-        },
-        {
-          loader: 'sass-loader',
-          options: {
-            additionalData(content) {
-              return content;
-            },
-            sassOptions: {
-              implementation: require('sass'),
-              includePaths: [
-                path.resolve(__dirname, '..', 'node_modules'),
-                path.resolve(__dirname, '..', '..', '..', 'node_modules'),
-              ],
-            },
-            sourceMap: true,
-          },
-        },
-      ],
-    });
-
-    if (process.env.NODE_ENV === 'production') {
-      config.plugins.push(
-        new MiniCssExtractPlugin({
-          filename: '[name].[contenthash].css',
-        })
-      );
-    }
-
+      },
+    };
     return config;
   },
 };
