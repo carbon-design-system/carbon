ad2aa50e4e9e47a9dfaa408a35cb12366ce02977
Taylor Jones <taylor.jones826@gmail.com>
2022-05-02 15:35:59 -0500

    docs(progressbar): add storybook playground story

--- packages/react/src/components/ProgressBar/next/ProgressBar.stories.js
+++ packages/react/src/components/ProgressBar/next/ProgressBar.stories.js
@@ -22,6 +22,23 @@ export const _ProgressBar = () => (
   />
 );
 
+const PlaygroundStory = (args) => (
+  <ProgressBar
+    label="Progress bar label"
+    helperText="Optional helper text"
+    {...args}
+  />
+);
+
+export const Playground = PlaygroundStory.bind({});
+
+Playground.argTypes = {
+  status: {
+    options: ['active', 'finished', 'error'],
+    control: { type: 'select' },
+  },
+};
+
 export const Indeterminate = () => (
   <ProgressBar label="Progress bar label" helperText="Optional helper text" />
 );
