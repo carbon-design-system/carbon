7fe38d104993bebd28c80ea72c1d0037385974a2
Alessandra Davila <adavila91@gmail.com>
2022-06-13 18:57:22 -0500

    chore(notification): remove commented out code

--- packages/react/src/components/Notification/index.js
+++ packages/react/src/components/Notification/index.js
@@ -5,38 +5,4 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-// import {
-//   NotificationActionButton as NotificationActionButtonNext,
-//   NotificationButton as NotificationButtonNext,
-//   ToastNotification as ToastNotificationNext,
-//   InlineNotification as InlineNotificationNext,
-//   ActionableNotification as ActionableNotificationNext,
-// } from './Notification';
-// import { createComponentToggle } from '../../internal/ComponentToggle';
-
-// export const NotificationActionButton = createComponentToggle({
-//   name: 'NotificationActionButton',
-//   next: NotificationActionButtonNext,
-// });
-
-// export const NotificationButton = createComponentToggle({
-//   name: 'NotificationButton',
-//   next: NotificationButtonNext,
-// });
-
-// export const ToastNotification = createComponentToggle({
-//   name: 'ToastNotification',
-//   next: ToastNotificationNext,
-// });
-
-// export const InlineNotification = createComponentToggle({
-//   name: 'InlineNotification',
-//   next: InlineNotificationNext,
-// });
-
-// export const ActionableNotification = createComponentToggle({
-//   name: 'ActionableNotification',
-//   next: ActionableNotificationNext,
-// });
-
 export * from './Notification';
