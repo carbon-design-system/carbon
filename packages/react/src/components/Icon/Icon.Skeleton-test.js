c1320e458e1247d4ef8c7abe81ac510a79b001d1
Josefina Mancilla <josefinanoemi4@gmail.com>
2022-06-16 15:16:28 -0500

    chore: add IconSkeleton test

---
+++ packages/react/src/components/Icon/Icon.Skeleton-test.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import IconSkeleton from './Icon.Skeleton';
+
+describe('IconSkeleton', () => {
+  it('should pass in an extra className when one is given', () => {
+    render(<IconSkeleton className="custom-class" data-testid="skeleton" />);
+    expect(screen.getByTestId('skeleton')).toHaveClass('custom-class');
+  });
+});
