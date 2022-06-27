dd3ea1202037c4e0eb5996ed81034c2933e96d00
Abbey Hart <abbeyhrt@gmail.com>
2022-06-27 15:27:43 -0500

    test(react): update tests of form and formgroup to react testing library

--- packages/react/src/components/Form/Form-test.js
+++ packages/react/src/components/Form/Form-test.js
@@ -7,59 +7,16 @@
 
 import React from 'react';
 import Form from '../Form';
-import { shallow, mount } from 'enzyme';
+import { render } from '@testing-library/react';
 
-const prefix = 'cds';
+describe('Form - RTL', () => {
+  it('should support a custom `className` prop on the outermost element', () => {
+    const { container } = render(<Form className="test" />);
+    expect(container.firstChild).toHaveClass('test');
+  });
 
-describe('Form', () => {
-  describe('Renders as expected', () => {
-    const wrapper = shallow(<Form className="extra-class" />);
-
-    it('renders children as expected', () => {
-      expect(wrapper.find('.child').length).toBe(0);
-    });
-    it('renders wrapper as expected', () => {
-      expect(wrapper.length).toBe(1);
-    });
-    it('has the expected classes', () => {
-      expect(wrapper.hasClass(`${prefix}--form`)).toEqual(true);
-    });
-
-    it('renders extra classes passed in via className', () => {
-      expect(wrapper.hasClass('extra-class')).toEqual(true);
-    });
-
-    it('should render wrapper as expected', () => {
-      const form = shallow(
-        <Form>
-          <div className="test-child1" />
-          <div className="test-child2" />
-        </Form>
-      );
-      expect(form.length).toEqual(1);
-    });
-    it('should render children as expected', () => {
-      const form1 = shallow(
-        <Form>
-          <div className="test-child" />
-          <div className="test-child" />
-        </Form>
-      );
-      expect(form1.find('.test-child').length).toBe(2);
-    });
-
-    it('should handle submit events', () => {
-      const onSubmit = jest.fn();
-      const form1 = mount(
-        <Form>
-          <button className="button" type="submit" onSubmit={onSubmit}>
-            Submit
-          </button>
-        </Form>
-      );
-      const btn = form1.find('button');
-      btn.simulate('submit');
-      expect(onSubmit).toHaveBeenCalled();
-    });
+  it('should spread extra props on the outermost element', () => {
+    const { container } = render(<Form data-testid="test" />);
+    expect(container.firstChild).toHaveAttribute('data-testid', 'test');
   });
 });
