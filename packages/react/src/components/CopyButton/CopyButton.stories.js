f4b54f43c63f30c18bdaa63fbbd9f30ea565e086
Alessandra Davila <adavila91@gmail.com>
2022-07-05 10:44:00 -0500

    docs(copy-button): add playground and controls

---
+++ packages/react/src/components/CopyButton/CopyButton.stories.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright IBM Corp. 2016, 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+/* eslint-disable no-console */
+
+import React from 'react';
+import CopyButton from './CopyButton';
+import mdx from './CopyButton';
+
+export default {
+  title: 'Components/CopyButton',
+  component: CopyButton,
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+};
+
+export const CopyButtonStory = () => <CopyButton />;
+
+CopyButtonStory.storyName = 'Default';
+
+export const Playground = (args) => <CopyButton {...args} />;
+
+Playground.argTypes = {
+  className: {
+    table: {
+      disable: true,
+    },
+  },
+  onClick: {
+    action: 'onClick',
+  },
+};
