e3052e381cd477c78da22beaa470ea901c348731
Josefina Mancilla <josefinanoemi4@gmail.com>
2022-07-13 14:49:02 -0500

    chore: clean up v11 code

--- packages/react/src/components/Modal/Modal-test.js
+++ packages/react/src/components/Modal/Modal-test.js
@@ -9,7 +9,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import Modal from '../Modal';
+import Modal from './Modal';
 import TextInput from '../TextInput';
 
 describe('Modal', () => {
