c51c85574322bc5eb8aaaebd3210597aaf23a19a
Josefina Mancilla <josefinanoemi4@gmail.com>
2022-07-20 23:40:37 -0500

    chore: update modal tests

--- packages/react/src/components/ModalWrapper/ModalWrapper-test.js
+++ packages/react/src/components/ModalWrapper/ModalWrapper-test.js
@@ -6,98 +6,204 @@
  */
 
 import React from 'react';
-import { mount } from 'enzyme';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import ModalWrapper from '../ModalWrapper';
 
-const prefix = 'cds';
-
 describe('ModalWrapper', () => {
-  let mockProps = {};
-
-  beforeEach(() => {
-    mockProps = {
-      id: 'modal',
-      buttonTriggerText: 'Test Modal',
-      buttonTriggerClassName: 'btn-trigger',
-      modalHeading: 'Transactional Modal',
-      modalLabel: 'Test Modal Label',
-      primaryButtonText: 'Save',
-      secondaryButtonText: 'Cancel',
-      handleSubmit: jest.fn(() => true),
-      shouldCloseAfterSubmit: true,
-      'aria-label': 'test modal',
-    };
-  });
-
-  it('should render', () => {
-    const wrapper = mount(
-      <ModalWrapper {...mockProps}>
-        <p className={`${prefix}--modal-content__text`}>Text</p>
+  it('should set state to open when trigger button is clicked', () => {
+    render(
+      <ModalWrapper
+        buttonTriggerText="Launch modal"
+        modalHeading="Modal heading"
+        modalLabel="Label"
+        data-testid="modal-3">
+        <p>Modal content here</p>
       </ModalWrapper>
     );
-    expect(wrapper).toMatchSnapshot();
+
+    const triggerBtn = screen.getByText('Launch modal');
+    expect(screen.getByTestId('modal-3')).not.toHaveClass('is-visible');
+    userEvent.click(triggerBtn);
+    expect(screen.getByTestId('modal-3')).toHaveClass('is-visible');
+  });
+
+  it('should set open state to false when escape is pressed', () => {
+    render(
+      <ModalWrapper
+        buttonTriggerText="Launch modal"
+        modalHeading="Modal heading"
+        modalLabel="Label"
+        data-testid="modal-4">
+        <p>Modal content here</p>
+      </ModalWrapper>
+    );
+
+    const triggerBtn = screen.getByText('Launch modal');
+    userEvent.click(triggerBtn);
+    expect(screen.getByTestId('modal-4')).toHaveClass('is-visible');
+    userEvent.keyboard('{esc}');
+    expect(screen.getByTestId('modal-4')).not.toHaveClass('is-visible');
+  });
+
+  it('should set open state to false when close button is clicked', () => {
+    render(
+      <ModalWrapper
+        buttonTriggerText="Launch modal"
+        modalHeading="Modal heading"
+        modalLabel="Label"
+        data-testid="modal-5">
+        <p>Modal content here</p>
+      </ModalWrapper>
+    );
+
+    const triggerBtn = screen.getByText('Launch modal');
+    const closeBtn = document.querySelector('.cds--modal-close');
+    userEvent.click(triggerBtn);
+    expect(screen.getByTestId('modal-5')).toHaveClass('is-visible');
+    userEvent.click(closeBtn);
+    expect(screen.getByTestId('modal-5')).not.toHaveClass('is-visible');
+  });
+
+  it('should set open state to false when secondary button is clicked', () => {
+    render(
+      <ModalWrapper
+        buttonTriggerText="Launch modal"
+        modalHeading="Modal heading"
+        modalLabel="Label"
+        data-testid="modal-6">
+        <p>Modal content here</p>
+      </ModalWrapper>
+    );
+
+    const triggerBtn = screen.getByText('Launch modal');
+    const cancelBtn = screen.getByText('Cancel');
+    userEvent.click(triggerBtn);
+    expect(screen.getByTestId('modal-6')).toHaveClass('is-visible');
+    userEvent.click(cancelBtn);
+    expect(screen.getByTestId('modal-6')).not.toHaveClass('is-visible');
   });
 
   it('should close after a successful submit action', () => {
-    const wrapper = mount(
-      <ModalWrapper {...mockProps}>
-        <p className={`${prefix}--modal-content__text`}>Text</p>
+    const handleSubmit = jest.fn();
+    render(
+      <ModalWrapper
+        buttonTriggerText="Launch modal"
+        modalHeading="Modal heading"
+        modalLabel="Label"
+        handleSubmit={handleSubmit}
+        open
+        data-testid="modal-1">
+        <p>Modal content here</p>
       </ModalWrapper>
     );
-    wrapper.find({ children: mockProps.buttonTriggerText }).simulate('click');
-    expect(wrapper.state('isOpen')).toBe(true);
 
-    wrapper.find({ children: mockProps.primaryButtonText }).simulate('click');
-    expect(wrapper.state('isOpen')).toBe(false);
+    const submitBtn = screen.getByText('Save');
+    userEvent.click(submitBtn);
+
+    expect(handleSubmit).toHaveBeenCalled();
+    expect(screen.getByTestId('modal-1')).not.toHaveClass('is-visible');
   });
 
   it('should return focus to the trigger button after closing', () => {
-    const wrapper = mount(
-      <ModalWrapper {...mockProps}>
-        <p className={`${prefix}--modal-content__text`}>Text</p>
+    const handleSubmit = jest.fn();
+    render(
+      <ModalWrapper
+        buttonTriggerText="Launch modal"
+        modalHeading="Modal heading"
+        modalLabel="Label"
+        handleSubmit={handleSubmit}
+        data-testid="modal-1"
+        open>
+        <p>Modal content here</p>
       </ModalWrapper>
     );
-    const { triggerButton } = wrapper.instance();
-    jest.spyOn(triggerButton.current, 'focus');
-    wrapper.find({ children: mockProps.buttonTriggerText }).simulate('click');
-    wrapper.find({ children: mockProps.primaryButtonText }).simulate('click');
-    expect(triggerButton.current.focus).toHaveBeenCalledTimes(1);
+
+    const triggerBtn = screen.getByText('Launch modal');
+    const submitBtn = screen.getByText('Save');
+    userEvent.click(submitBtn);
+
+    expect(handleSubmit).toHaveBeenCalled();
+    setTimeout(() => {
+      expect(document.activeElement).toEqual(triggerBtn);
+    }, 0);
   });
 
-  it('should not close after an unsuccessful submit action', () => {
-    mockProps.handleSubmit = jest.fn(() => false);
-    const wrapper = mount(
-      <ModalWrapper {...mockProps}>
-        <p className={`${prefix}--modal-content__text`}>Text</p>
+  it('should not close if shouldCloseAfterSubmit is false', () => {
+    const handleSubmit = jest.fn();
+    render(
+      <ModalWrapper
+        buttonTriggerText="Launch modal"
+        modalHeading="Modal heading"
+        modalLabel="Label"
+        handleSubmit={handleSubmit}
+        open
+        shouldCloseAfterSubmit={false}
+        data-testid="modal-2">
+        <p>Modal content here</p>
       </ModalWrapper>
     );
-    wrapper.find({ children: mockProps.buttonTriggerText }).simulate('click');
-    expect(wrapper.state('isOpen')).toBe(true);
 
-    wrapper.find({ children: mockProps.primaryButtonText }).simulate('click');
-    expect(wrapper.state('isOpen')).toBe(true);
+    const submitBtn = screen.getByText('Save');
+    userEvent.click(submitBtn);
+
+    expect(handleSubmit).toHaveBeenCalled();
+    expect(screen.getByTestId('modal-2')).toHaveClass('is-visible');
   });
 
   it('should default to primary button', () => {
-    const wrapper = mount(<ModalWrapper aria-label="test" />);
-    expect(wrapper.find(`.${prefix}--btn--primary`).length).toEqual(2);
-  });
+    render(
+      <ModalWrapper
+        buttonTriggerText="Launch modal"
+        modalHeading="Modal heading"
+        modalLabel="Label">
+        <p>Modal content here</p>
+      </ModalWrapper>
+    );
 
-  it('should render ghost button when ghost is passed', () => {
-    const wrapper = mount(<ModalWrapper aria-label="test" />);
-    wrapper.setProps({ triggerButtonKind: 'ghost' });
-    expect(wrapper.find(`.${prefix}--btn--ghost`).length).toEqual(1);
+    expect(screen.getByText('Save')).toHaveClass('cds--btn--primary');
   });
 
   it('should render danger button when danger is passed', () => {
-    const wrapper = mount(<ModalWrapper aria-label="test" />);
-    wrapper.setProps({ triggerButtonKind: 'danger' });
-    expect(wrapper.find(`.${prefix}--btn--danger`).length).toEqual(1);
+    render(
+      <ModalWrapper
+        buttonTriggerText="Launch modal"
+        modalHeading="Modal heading"
+        modalLabel="Label"
+        danger>
+        <p>Modal content here</p>
+      </ModalWrapper>
+    );
+
+    expect(screen.getByText('Save')).toHaveClass('cds--btn--danger');
   });
 
-  it('should render secondary button when secondary is passed', () => {
-    const wrapper = mount(<ModalWrapper aria-label="test" />);
-    wrapper.setProps({ triggerButtonKind: 'secondary' });
-    expect(wrapper.find(`.${prefix}--btn--secondary`).length).toEqual(2);
+  it('should render a secondary button by default', () => {
+    render(
+      <ModalWrapper
+        buttonTriggerText="Launch modal"
+        modalHeading="Modal heading"
+        modalLabel="Label"
+        danger>
+        <p>Modal content here</p>
+      </ModalWrapper>
+    );
+
+    expect(screen.getByText('Cancel')).toBeInTheDocument();
+  });
+
+  it('should not render a secondary button if text is explicitly null', () => {
+    render(
+      <ModalWrapper
+        buttonTriggerText="Launch modal"
+        modalHeading="Modal heading"
+        modalLabel="Label"
+        secondaryButtonText={null}
+        danger>
+        <p>Modal content here</p>
+      </ModalWrapper>
+    );
+
+    expect(screen.queryByText('Cancel')).not.toBeInTheDocument();
   });
 });
