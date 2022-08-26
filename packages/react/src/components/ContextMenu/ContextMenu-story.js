1f9cab5b6d203ffa29e76bdd2e4fa982ca0be6e2
Josefina Mancilla <josefinanoemi4@gmail.com>
2022-08-26 01:36:24 -0500

    feat: add controls and example for context menu

--- packages/react/src/components/ContextMenu/ContextMenu-story.js
+++ packages/react/src/components/ContextMenu/ContextMenu-story.js
@@ -7,8 +7,18 @@
 
 import React from 'react';
 import { InlineNotification } from '../Notification';
+import CodeSnippet from '../CodeSnippet';
+import UnorderedList from '../UnorderedList';
+import ListItem from '../ListItem';
 
-import Menu from '../Menu';
+import Menu, {
+  MenuDivider,
+  MenuGroup,
+  MenuItem,
+  MenuRadioGroup,
+  MenuSelectableItem,
+} from '../Menu';
+
 import { StoryFrame, buildMenu } from '../Menu/_storybook-utils';
 
 import { useContextMenu } from './index';
@@ -16,6 +26,13 @@ import { useContextMenu } from './index';
 export default {
   title: 'Experimental/unstable_Menu/ContextMenu',
   component: Menu,
+  subcomponents: {
+    MenuItem,
+    MenuGroup,
+    MenuDivider,
+    MenuSelectableItem,
+    MenuRadioGroup,
+  },
 };
 
 const Story = (items) => {
@@ -36,6 +53,42 @@ const Story = (items) => {
     </StoryFrame>
   );
 };
+
+const Text = () => (
+  <div style={{ width: '40rem' }}>
+    <h2>Right click anywere in the story frame</h2>
+    <br />
+    <p>
+      The <CodeSnippet type="inline">{`<Menu>`}</CodeSnippet> props are set
+      using the hook{' '}
+      <CodeSnippet type="inline">{`useContextMenu()`}</CodeSnippet>. This
+      determines the position the menu should have on right click and handles
+      opening. Props that the{' '}
+      <CodeSnippet type="inline">{`useContextMenu()`}</CodeSnippet>
+      hook does not set and can be configured by the user are:
+      <UnorderedList style={{ paddingLeft: '2rem', margin: '2rem 0' }}>
+        <ListItem>size</ListItem>
+        <ListItem>onClose</ListItem>
+        <ListItem>className</ListItem>
+        <ListItem>id</ListItem>
+      </UnorderedList>
+    </p>
+    <p>
+      The
+      <CodeSnippet type="inline">{`<MenuGroup>`}</CodeSnippet> and
+      <CodeSnippet type="inline">{`<MenuItem`}</CodeSnippet>
+      components accept children items for nested menus, although the{' '}
+      <CodeSnippet type="inline">{`<MenuItem>`}</CodeSnippet> component can also
+      be used as a stand alone item. The other types of menu items ({' '}
+      <CodeSnippet type="inline">{`<MenuDivider>`}</CodeSnippet>,
+      <CodeSnippet type="inline">{`<MenuSelectableItem>`}</CodeSnippet>,
+      <CodeSnippet type="inline">{`<MenuRadioGroup>`}</CodeSnippet>) do not
+      accept children. The{' '}
+      <CodeSnippet type="inline">{`<MenuRadioGroup>`}</CodeSnippet> accepts an
+      array of items to display as a group of single choice selection.
+    </p>
+  </div>
+);
 
 export const _ContextMenu = () =>
   Story([
@@ -91,3 +144,73 @@ export const _MultipleGroups = () =>
     },
   ]);
 _MultipleGroups.storyName = 'MultipleGroups';
+
+export const _Playground = (args) => {
+  const props = useContextMenu();
+  return (
+    <div style={{ height: 'calc(100vh - 6.25rem)' }}>
+      <Text />
+      <Menu {...args} {...props}>
+        <MenuItem label="Share with">
+          <MenuRadioGroup
+            label="Share with"
+            items={['None', 'Product team', 'Organization', 'Company']}
+            initialSelectedItem="Product team"
+          />
+        </MenuItem>
+        <MenuDivider />
+        <MenuItem label="Cut" shortcut="⌘X" />
+        <MenuItem label="Copy" shortcut="⌘C" />
+        <MenuItem label="Copy path" shortcut="⌥⌘C" />
+        <MenuItem label="Paste" shortcut="⌘V" disabled />
+        <MenuDivider />
+        <MenuGroup label="Font style">
+          <MenuSelectableItem label="Bold" initialChecked />
+          <MenuSelectableItem label="Italic" />
+        </MenuGroup>
+        <MenuDivider />
+        <MenuRadioGroup
+          label="Text color"
+          items={['None', 'Overline', 'Line-through', 'Underline']}
+          initialSelectedItem="None"
+        />
+        <MenuDivider />
+        <MenuItem label="Danger item" kind="danger" />
+      </Menu>
+    </div>
+  );
+};
+
+_Playground.argTypes = {
+  size: {
+    control: { type: 'select' },
+    options: ['sm', 'md', 'lg'],
+  },
+  children: {
+    control: false,
+  },
+  className: {
+    control: false,
+  },
+  id: {
+    control: false,
+  },
+  level: {
+    control: false,
+  },
+  open: {
+    control: false,
+  },
+  onClose: {
+    control: false,
+  },
+  target: {
+    control: false,
+  },
+  x: {
+    control: false,
+  },
+  y: {
+    control: false,
+  },
+};
