b2290df04ff7926d9c3a67f2c6488e28fc538bb7
Abbey Hart <abbeyhrt@gmail.com>
2022-09-19 17:06:19 -0500

    fix(react): update index

--- packages/react/src/components/Pagination/index.js
+++ packages/react/src/components/Pagination/index.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { Pagination } from './';
+import Pagination from './';
 
 export * from './Pagination.Skeleton';
 
