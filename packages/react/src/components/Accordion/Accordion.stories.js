666135810f9fa5f98bd51e604a14e275549d214a
Josefina Mancilla <32556167+jnm2377@users.noreply.github.com>
2022-09-21 12:45:18 -0500

    feat(Accordion): adds flush alignment (#12027)
    * feat(Accordion): add flush feature
    
    * feat(accordion): add flush alignment
    
    * fix: isflush prop name and styles dupe
    
    * fix: update snapshots
    
    * fix: flush control in storybook
    
    * feat: add flush feature to skeleton and story
    
    * chore: add test for skeleton
    
    * chore: update snapshots
    
    * fix: isflush proptype comment
    
    * fix: hover and focus flush styles
    
    * fix: flush chevron margin
    
    Co-authored-by: Lauren Rice <43969356+laurenmrice@users.noreply.github.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/react/src/components/Accordion/Accordion.stories.js
+++ packages/react/src/components/Accordion/Accordion.stories.js
@@ -69,7 +69,9 @@ export const AccordionStory = () => (
 
 AccordionStory.storyName = 'Accordion';
 
-export const Skeleton = () => <AccordionSkeleton open count={4} />;
+export const Skeleton = (args) => (
+  <AccordionSkeleton open count={4} {...args} />
+);
 
 Skeleton.decorators = [
   (story) => <div style={{ width: '500px' }}>{story()}</div>,
@@ -129,8 +131,39 @@ Playground.argTypes = {
     },
     defaultValue: false,
   },
+  isFlush: {
+    control: {
+      type: 'boolean',
+    },
+    defaultValue: false,
+  },
   size: {
     options: ['sm', 'md', 'lg'],
     control: { type: 'select' },
+  },
+};
+
+Skeleton.argTypes = {
+  align: {
+    options: ['start', 'end'],
+    control: { type: 'select' },
+  },
+  children: {
+    control: false,
+  },
+  className: {
+    control: false,
+  },
+  disabled: {
+    control: false,
+  },
+  isFlush: {
+    control: {
+      type: 'boolean',
+    },
+    defaultValue: false,
+  },
+  size: {
+    control: false,
   },
 };
