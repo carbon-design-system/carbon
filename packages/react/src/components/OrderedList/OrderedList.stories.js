0c5a4e0a6d1662a32bd931533ab530fc6248bcef
Alessandra Davila <adavila91@gmail.com>
2022-09-26 15:41:50 -0500

    chore(ordered-list): add other examples

--- packages/react/src/components/OrderedList/OrderedList.stories.js
+++ packages/react/src/components/OrderedList/OrderedList.stories.js
@@ -34,6 +34,55 @@ export const Default = () => (
   </OrderedList>
 );
 
+export const Nested = () => {
+  return (
+    <OrderedList>
+      <ListItem>
+        Ordered List level 1
+        <OrderedList nested>
+          <ListItem>Ordered List level 2</ListItem>
+          <ListItem>
+            Ordered List level 2
+            <OrderedList nested>
+              <ListItem>Ordered List level 3</ListItem>
+              <ListItem>Ordered List level 3</ListItem>
+            </OrderedList>
+          </ListItem>
+        </OrderedList>
+      </ListItem>
+      <ListItem>Ordered List level 1</ListItem>
+      <ListItem>Ordered List level 1</ListItem>
+    </OrderedList>
+  );
+};
+
+export const NativeListStyles = () => {
+  return (
+    <OrderedList native>
+      <ListItem>Ordered List level 1</ListItem>
+      <ListItem>Ordered List level 1</ListItem>
+      <ListItem>Ordered List level 1</ListItem>
+      <ListItem>
+        Ordered List level 1
+        <OrderedList nested>
+          <ListItem>Ordered List level 2</ListItem>
+          <ListItem>Ordered List level 2</ListItem>
+          <ListItem>Ordered List level 2</ListItem>
+          <ListItem>Ordered List level 2</ListItem>
+        </OrderedList>
+      </ListItem>
+      <ListItem>Ordered List level 1</ListItem>
+      <ListItem>Ordered List level 1</ListItem>
+      <ListItem>Ordered List level 1</ListItem>
+      <ListItem>Ordered List level 1</ListItem>
+      <ListItem>Ordered List level 1</ListItem>
+      <ListItem>Ordered List level 1</ListItem>
+      <ListItem>Ordered List level 1</ListItem>
+      <ListItem>Ordered List level 1</ListItem>
+    </OrderedList>
+  );
+};
+
 export const Playground = (args) => (
   <OrderedList {...args}>
     <ListItem>Ordered List level 1</ListItem>
