cba9e00cc495bd66e425b8929d667f7a80fefe61
Anna Wen <54281166+annawen1@users.noreply.github.com>
2022-09-27 11:34:30 -0400

    chore(react-wrappers): bring back cwc react wrapper code

--- web-components/packages/carbon-web-components/gulp-tasks/build/modules.js
+++ web-components/packages/carbon-web-components/gulp-tasks/build/modules.js
@@ -11,6 +11,9 @@ const gulp = require('gulp');
 require('./modules/css');
 require('./modules/icon-types');
 require('./modules/icons');
+require('./modules/react');
+require('./modules/react-defs');
+require('./modules/react-types');
 require('./modules/scripts');
 require('./modules/scripts-node');
 require('./modules/types');
@@ -21,6 +24,9 @@ gulp.task(
     gulp.task('build:modules:css'),
     gulp.task('build:modules:icon-types'),
     gulp.task('build:modules:icons'),
+    gulp.task('build:modules:react'),
+    gulp.task('build:modules:react-defs'),
+    gulp.task('build:modules:react-types'),
     gulp.task('build:modules:scripts'),
     gulp.task('build:modules:scripts-node'),
     gulp.task('build:modules:types')
