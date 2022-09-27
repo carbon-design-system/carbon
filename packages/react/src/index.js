c7d5d1ec8d793e292fe153bf12891136bbd7bd97
Alessandra Davila <adavila91@gmail.com>
2022-09-27 11:09:49 -0500

    feat(FluidTextArea): adds new fluid text area (#12053)
    * feat(FluidTextArea): adds new fluid text area
    
    * feat: add fluid text area styles
    
    * feat: add text area stories
    
    * chore: add tests
    
    * feat: update fluid form story
    
    * fix: fluid text area in modal styles
    
    * chore: update snapshots
    
    * fix: invalid focus
    
    * fix: disabled resize
    
    * fix: fluid form story source
    
    * fix: fluid form story source
    
    * feat(fluid-text-area): adds skeleton state
    
    * feat(fluid-text-area): adds skeleton styles
    
    * Update packages/react/src/components/FluidTextArea/FluidTextArea.stories.js
    
    Co-authored-by: Josefina Mancilla <josefinanoemi4@gmail.com>
    Co-authored-by: Taylor Jones <tay1orjones@users.noreply.github.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -214,6 +214,7 @@ export {
   useFeatureFlags as unstable_useFeatureFlags,
 } from './components/FeatureFlags';
 export { FluidTextInput as unstable__FluidTextInput } from './components/FluidTextInput';
+export { FluidTextArea as unstable__FluidTextArea } from './components/FluidTextArea';
 export { Heading, Section } from './components/Heading';
 export { IconButton } from './components/IconButton';
 export { Layer, useLayer } from './components/Layer';
