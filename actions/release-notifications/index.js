3116535838c69d3875f953773045dfa4e7d5a253
ShankarV-CodeJunkie <emailmeforshankar@gmail.com>
2022-10-12 13:41:24 +0530

    chore: add copyright header

--- actions/release-notifications/index.js
+++ actions/release-notifications/index.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright IBM Corp. 2022, 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+'use strict';
 const core = require('@actions/core');
 const github = require('@actions/github');
 const { request } = require('@octokit/request');
