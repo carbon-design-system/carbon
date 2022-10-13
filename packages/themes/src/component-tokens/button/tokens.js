653a98f3392628fe8b6517d33e059d8d44a0f1a7
Abbey Hart <abbeyhrt@gmail.com>
2022-10-13 12:53:20 -0500

    chore: check in progress

---
+++ packages/themes/src/component-tokens/button/tokens.js
@@ -0,0 +1,123 @@
+const buttonSeparator = {
+  fallback: '#e0e0e0',
+  white: '#e0e0e0',
+  g10: '#e0e0e0',
+  g90: '#161616',
+  g100: '#161616',
+};
+
+const buttonPrimary = {
+  white: '#0f62fe',
+  g10: '#0f62fe',
+  g90: '#0f62fe',
+  g100: '#0f62fe',
+};
+
+const buttonSecondary = {
+  white: '#393939',
+  g10: '#393939',
+  g90: '#6f6f6f',
+  g100: '#6f6f6f',
+};
+
+const buttonTertiary = {
+  white: '#0f62fe',
+  g10: '#0f62fe',
+  g90: '#ffffff',
+  g100: '#ffffff',
+};
+
+const buttonDangerPrimary = {
+  white: '#da1e28',
+  g10: '#da1e28',
+  g90: '#da1e28',
+  g100: '#da1e28',
+};
+
+const buttonDangerSecondary = {
+  white: '#da1e28',
+  g10: '#da1e28',
+  g90: '#ff8389',
+  g100: '#fa4d56',
+};
+
+const buttonDangerActive = {
+  white: '#750e13',
+  g10: '#750e13',
+  g90: '#750e13',
+  g100: '#750e13',
+};
+
+const buttonPrimaryActive = {
+  white: '#002d9c',
+  g10: '#002d9c',
+  g90: '#002d9c',
+  g100: '#002d9c',
+};
+
+const buttonSecondaryActive = {
+  white: '#6f6f6f',
+  g10: '#6f6f6f',
+  g90: '#393939',
+  g100: '#393939',
+};
+
+const buttonTertiaryActive = {
+  white: '#002d9c',
+  g10: '#002d9c',
+  g90: '#c6c6c6',
+  g100: '#c6c6c6',
+};
+
+const buttonDangerHover = {
+  white: '#b81921',
+  g10: '#b81921',
+  g90: '#b81921',
+  g100: '#b81921',
+};
+
+const buttonPrimaryHover = {
+  white: '#0050e6',
+  g10: '#0050e6',
+  g90: '#0050e6',
+  g100: '#0050e6',
+};
+
+const buttonSecondaryHover = {
+  white: '#474747',
+  g10: '#474747',
+  g90: '#5e5e5e',
+  g100: '#5e5e5e',
+};
+
+const buttonTertiaryHover = {
+  white: '#0050e6',
+  g10: '#0050e6',
+  g90: '#f4f4f4',
+  g100: '#f4f4f4',
+};
+
+const buttonDisabled = {
+  white: '#c6c6c6',
+  g10: '#c6c6c6',
+  g90: 'rgb(141 141 141 / 30%)',
+  g100: 'rgb(141 141 141 / 30%)',
+};
+
+export {
+  buttonSeparator,
+  buttonPrimary,
+  buttonSecondary,
+  buttonTertiary,
+  buttonDangerPrimary,
+  buttonDangerSecondary,
+  buttonDangerActive,
+  buttonPrimaryActive,
+  buttonSecondaryActive,
+  buttonTertiaryActive,
+  buttonDangerHover,
+  buttonPrimaryHover,
+  buttonSecondaryHover,
+  buttonTertiaryHover,
+  buttonDisabled,
+};
