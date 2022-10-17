bbd77f8690295b3df81e4e25779bb69718c1457b
Michael Barrera <mbarrer@us.ibm.com>
2022-10-17 10:32:47 -0500

    chore: add comment

--- packages/react/src/components/Text/TextDirectionContext.d.ts
+++ packages/react/src/components/Text/TextDirectionContext.d.ts
@@ -1,1 +1,7 @@
+// export interface TextDirectionContextData {
+//   direction?: TextDir;
+//   getTextDirection?: MutableRefObject<GetTextDirection | undefined>;
+// }
+
+//TODO: Convert corresponding context file once external components have all been converted to TS
 export const TextDirectionContext: any;
