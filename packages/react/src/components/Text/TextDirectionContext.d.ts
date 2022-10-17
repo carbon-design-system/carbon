29e095d0913f7b932b43e5fff59d2196b848ca74
Michael Barrera <mbarrer@us.ibm.com>
2022-10-17 15:02:47 -0500

    refactor: use @type jsdoc assertion

--- packages/react/src/components/Text/TextDirectionContext.d.ts
+++
@@ -1,7 +0,0 @@
-// export interface TextDirectionContextData {
-//   direction?: TextDir;
-//   getTextDirection?: MutableRefObject<GetTextDirection | undefined>;
-// }
-
-//TODO: Convert corresponding context file once external components have all been converted to TS
-export const TextDirectionContext: any;
