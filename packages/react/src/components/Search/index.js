3527d206cbcc8a0879bf57f3270aa6bb5285ad49
Alison Joseph <alison.joseph@us.ibm.com>
2022-10-24 12:36:07 -0500

    chore(search): remove next folder

--- packages/react/src/components/Search/index.js
+++ packages/react/src/components/Search/index.js
@@ -4,6 +4,7 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import Search from './Search';
 
-export * from './Search.Skeleton';
-export default from './Search';
+export { default as SearchSkeleton } from './Search.Skeleton';
+export default Search;
