b5655df3423ea0d4e50b3fb090a596d40d6af604
Tanner Summers <tannersummers@outlook.com>
2022-10-31 15:42:06 -0500

    Tanner update playgrounds v2 (#12256)
    * chore(grid/flexgrid): updated story books
    
    * chore(formlabel): updated story books
    
    * chore(formlabel): updated story books
    
    * chore(grid/flexgrid): updated story books again
    
    * chore(textarea): updated story books
    
    * chore(react): closes #12251 #12250 #12249 #12248
    
    * chore(react): fixed textinput/area stories, updated snapshots
    
    * chore(react): fixed tabs index file for unit test
    
    * fix(react): update tabs index.js
    
    * Update packages/react/src/components/Grid/FlexGrid.stories.js
    
    Co-authored-by: Abbey Hart <abbeyhrt@gmail.com>
    
    * Update packages/react/src/components/Grid/Grid.mdx
    
    Co-authored-by: Abbey Hart <abbeyhrt@gmail.com>
    
    * Update packages/react/src/components/FormLabel/FormLabel.stories.js
    
    Co-authored-by: Abbey Hart <abbeyhrt@gmail.com>
    
    * chore(react): removed args from tab stories
    
    * chore(react): updated unordered list story
    
    * chore(react): updates to stories
    
    Co-authored-by: Abbey Hart <abbeyhrt@gmail.com>
    Co-authored-by: TJ Egan <tw15egan@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/react/src/components/Tabs/index.js
+++ packages/react/src/components/Tabs/index.js
@@ -5,26 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import * as FeatureFlags from '@carbon/feature-flags';
-import {
-  Tabs as TabsNext,
-  TabPanel,
-  TabPanels,
-  TabList,
-  IconTab,
-  Tab,
-} from './next/Tabs';
-import { default as TabsClassic } from './Tabs';
-import { default as TabsSkeletonClassic } from './Tabs.Skeleton';
-import { default as TabsSkeletonNext } from './next/Tabs.Skeleton';
-
-const Tabs = FeatureFlags.enabled('enable-v11-release')
-  ? TabsNext
-  : TabsClassic;
-
-const TabsSkeleton = FeatureFlags.enabled('enable-v11-release')
-  ? TabsSkeletonNext
-  : TabsSkeletonClassic;
+import { Tabs, TabPanel, TabPanels, TabList, IconTab, Tab } from './Tabs';
+import { default as TabsSkeleton } from './Tabs.Skeleton';
 
 export { TabsSkeleton, TabPanels, TabPanel, TabList, IconTab, Tab };
 
