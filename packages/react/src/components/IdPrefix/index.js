133d29493bb8e72131982bd03762406c4f286df7
Taylor Jones <tay1orjones@users.noreply.github.com>
2022-11-14 14:35:08 -0600

    feat(IdPrefix): add component, docs, tests (#12442)
    * feat(IdPrefix): add component, docs, tests
    
    * feat(IdPrefix): add component, docs, tests
    
    * fix(IdPrefix): update useId to use prefix, tests, and export IdPrefix
    
    * chore: update snaps
    
    * docs(IdPrefix): fix stories
    
    * test(snaps): update snap
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ packages/react/src/components/IdPrefix/index.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import PropTypes from 'prop-types';
+import React from 'react';
+import { IdPrefixContext } from '../../internal/useIdPrefix';
+
+function IdPrefix({ children, prefix }) {
+  return (
+    <IdPrefixContext.Provider value={prefix}>
+      {children}
+    </IdPrefixContext.Provider>
+  );
+}
+
+IdPrefix.propTypes = {
+  children: PropTypes.node,
+
+  /**
+   * The value used to prefix the auto-generated id placed on some DOM elements
+   */
+  prefix: PropTypes.string,
+};
+
+export { IdPrefix };
