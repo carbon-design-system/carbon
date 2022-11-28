026439e7ff679bec6f9927b07eb902a3a3353cbb
Chris Knabe <cknabe@us.ibm.com>
2022-11-28 09:43:55 -0600

    test(SecondaryButton): replace Enzyme with RTL (#12700)
    * test(SecondaryButton): replace Enzyme with RTL
    
    * fix: swap toBeDefined with toBeTruthy
    
    * fix: refactored to the RTL recommended usage

--- packages/react/src/components/SecondaryButton/SecondaryButton-test.js
+++ packages/react/src/components/SecondaryButton/SecondaryButton-test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2016, 2018
+ * Copyright IBM Corp. 2016, 2018, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -7,43 +7,43 @@
 
 import React from 'react';
 import { Search } from '@carbon/icons-react';
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import SecondaryButton from '../SecondaryButton';
-import { shallow, mount } from 'enzyme';
-
-const prefix = 'cds';
 
 describe('SecondaryButton', () => {
   describe('Renders as expected', () => {
-    const wrapper = shallow(
-      <SecondaryButton size="sm" className="extra-class">
-        <div className="child">Test</div>
-        <div className="child">Test</div>
-      </SecondaryButton>
-    );
+    const renderSecondaryButton = (props) => {
+      render(
+        <SecondaryButton {...props} className="extra-class" size="sm">
+          <div className="child">Test</div>
+          <div className="child">Test</div>
+        </SecondaryButton>
+      );
+    };
 
     it('Renders children as expected', () => {
-      expect(wrapper.find('.child').length).toBe(2);
-      expect(wrapper.find('svg').length).toBe(0);
+      renderSecondaryButton();
+      expect(screen.getAllByText('Test').length).toBe(2);
     });
-    it('Renders wrapper as expected', () => {
-      expect(wrapper.length).toBe(1);
-    });
+
     it('Has the expected kind set to "secondary"', () => {
-      expect(wrapper.props().kind).toEqual('secondary');
+      renderSecondaryButton();
+      expect(screen.getByRole('button')).toHaveClass('cds--btn--secondary');
     });
+
     it('Should add extra classes that are passed via className', () => {
-      expect(wrapper.hasClass('extra-class')).toEqual(true);
+      renderSecondaryButton();
+      expect(screen.getByRole('button')).toHaveClass('extra-class');
     });
 
     describe('Renders icon buttons', () => {
-      const iconButton = mount(
-        <SecondaryButton renderIcon={Search} iconDescription="Search">
-          Search
-        </SecondaryButton>
-      );
-      const icon = iconButton.find('svg');
       it('should have the appropriate icon', () => {
-        expect(icon.hasClass(`${prefix}--btn__icon`)).toBe(true);
+        renderSecondaryButton({
+          iconDescription: 'Search',
+          renderIcon: Search,
+        });
+        expect(screen.queryByLabelText('Search')).toHaveClass('cds--btn__icon');
       });
     });
   });
