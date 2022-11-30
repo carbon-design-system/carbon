cf8800afc01729fcd6877102fb0ff535400d46cb
cknabe <cknabe@us.ibm.com>
2022-11-30 14:06:28 -0600

    test(textarea): 12677 - TextArea Enzyme to RTL conversion

--- packages/react/src/components/TextArea/TextArea-test.js
+++ packages/react/src/components/TextArea/TextArea-test.js
@@ -1,221 +1,208 @@
 /**
- * Copyright IBM Corp. 2016, 2018
+ * Copyright IBM Corp. 2016, 2018, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import TextArea from '../TextArea';
 
 const prefix = 'cds';
 
 describe('TextArea', () => {
-  describe('should render as expected', () => {
-    const wrapper = mount(
+  const renderTextArea = (props) => {
+    render(
       <TextArea
+        className="extra-class"
+        data-testid="element"
         id="testing"
         labelText="testlabel"
-        className="extra-class"
-        helperText="testHelper"
+        {...props}
       />
     );
+  };
 
-    const textarea = () => wrapper.find('textarea');
-
-    describe('textarea', () => {
-      it('renders a textarea', () => {
-        expect(textarea().length).toEqual(1);
-      });
-
-      it('has the expected classes', () => {
-        expect(textarea().hasClass(`${prefix}--text-area`)).toEqual(true);
-      });
-
-      it('applies extra classes specified via className', () => {
-        expect(textarea().hasClass('extra-class')).toEqual(true);
-      });
-
-      it('should set rows as expected', () => {
-        expect(textarea().props().rows).toEqual(4);
-        wrapper.setProps({ rows: 10 });
-        expect(textarea().props().rows).toEqual(10);
-      });
-
-      it('should set cols as expected', () => {
-        expect(textarea().props().style).toEqual({ width: '100%' });
-        wrapper.setProps({ cols: 200 });
-        expect(textarea().props().cols).toEqual(200);
-      });
-
-      it('should set disabled as expected', () => {
-        expect(textarea().props().disabled).toEqual(false);
-        wrapper.setProps({ disabled: true });
-        expect(textarea().props().disabled).toEqual(true);
-      });
-
-      it('should set placeholder as expected', () => {
-        wrapper.setProps({ placeholder: 'Type here' });
-        expect(textarea().props().placeholder).toEqual('Type here');
-      });
-
-      it('should set value as expected', () => {
-        wrapper.setProps({ value: 'value set' });
-        expect(textarea().props().value).toEqual('value set');
-      });
-
-      it('should set defaultValue as expected', () => {
-        wrapper.setProps({ defaultValue: 'default value' });
-        expect(textarea().props().defaultValue).toEqual('default value');
-      });
-
-      it('should set enableCounter as expected', () => {
-        wrapper.setProps({ enableCounter: true });
-        expect(wrapper.props().enableCounter).toEqual(true);
-      });
-
-      it('should set maxCount as expected', () => {
-        wrapper.setProps({ maxCount: 5 });
-        expect(wrapper.props().maxCount).toEqual(5);
-      });
+  describe('textarea', () => {
+    it('renders a textarea', () => {
+      renderTextArea();
+      expect(screen.getByLabelText('testlabel')).toBeInTheDocument();
     });
 
-    describe('label', () => {
-      wrapper.setProps({ labelText: 'testLabel' });
-      const renderedLabel = wrapper.find('label');
-
-      it('renders a label', () => {
-        expect(renderedLabel.length).toEqual(1);
-      });
-
-      it('label has expected text', () => {
-        expect(renderedLabel.text()).toEqual('testLabel');
-      });
-
-      it('has the expected classes', () => {
-        expect(renderedLabel.hasClass(`${prefix}--label`)).toEqual(true);
-      });
-    });
-
-    describe('helper', () => {
-      it('renders a helper', () => {
-        const renderedHelper = wrapper.find(`.${prefix}--form__helper-text`);
-        expect(renderedHelper.length).toEqual(1);
-      });
-
-      it('renders children as expected', () => {
-        wrapper.setProps({
-          helperText: <span>This is helper text.</span>,
-        });
-        const renderedHelper = wrapper.find(`.${prefix}--form__helper-text`);
-        expect(renderedHelper.props().children).toEqual(
-          <span>This is helper text.</span>
-        );
-      });
-
-      it('should set helper text as expected', () => {
-        wrapper.setProps({ helperText: 'Helper text' });
-        const renderedHelper = wrapper.find(`.${prefix}--form__helper-text`);
-        expect(renderedHelper.text()).toEqual('Helper text');
-      });
-    });
-
-    describe('counter', () => {
-      const counterTestWrapper1 = mount(
-        <TextArea id="counter1" labelText="someLabel" enableCounter={true} />
+    it('has the expected classes', () => {
+      renderTextArea();
+      expect(screen.getByLabelText('testlabel')).toHaveClass(
+        `${prefix}--text-area`
       );
+    });
 
-      const counterTestWrapper2 = mount(
-        <TextArea id="counter2" labelText="someLabel" maxCount={5} />
+    it('applies extra classes specified via className', () => {
+      renderTextArea();
+      expect(screen.getByLabelText('testlabel')).toHaveClass('extra-class');
+    });
+
+    it('should have the default rows as expected', () => {
+      renderTextArea();
+      expect(screen.getByLabelText('testlabel')).toHaveAttribute('rows', '4');
+    });
+
+    it('should set rows as expected', () => {
+      renderTextArea({ rows: 10 });
+      expect(screen.getByLabelText('testlabel')).toHaveAttribute('rows', '10');
+    });
+
+    it('should have default cols settings as expected', () => {
+      renderTextArea();
+      expect(screen.getByLabelText('testlabel')).toHaveStyle({ width: '100%' });
+      expect(screen.getByLabelText('testlabel')).not.toHaveAttribute('cols');
+    });
+
+    it('should set cols as expected', () => {
+      renderTextArea({ cols: 200 });
+      expect(screen.getByLabelText('testlabel')).toHaveAttribute('cols', '200');
+    });
+
+    it('should not be disabled by default', () => {
+      renderTextArea();
+      expect(screen.getByLabelText('testlabel')).not.toHaveAttribute(
+        'disabled'
       );
+    });
 
-      it('should not render counter with only enableCounter prop passed in', () => {
-        expect(
-          counterTestWrapper1.exists(`${prefix}--text-area__counter`)
-        ).toEqual(false);
-      });
+    it('should be disabled as expected', () => {
+      renderTextArea({ disabled: true });
+      expect(screen.getByLabelText('testlabel')).toHaveAttribute('disabled');
+    });
 
-      it('should not render counter with only maxCount prop passed in', () => {
-        expect(
-          counterTestWrapper2.exists(`${prefix}--text-area__counter`)
-        ).toEqual(false);
-      });
+    it('should set placeholder as expected', () => {
+      renderTextArea({ placeholder: 'Type here' });
+      expect(screen.getByLabelText('testlabel')).toHaveAttribute(
+        'placeholder',
+        'Type here'
+      );
+    });
 
-      it('has the expected classes', () => {
-        const counterTextarea = () =>
-          counterTestWrapper1.find(`.${prefix}--text-area__counter`);
-        expect(counterTextarea).not.toBeNull();
-      });
+    it('should set value as expected', () => {
+      renderTextArea({ value: 'Lorem Ipsum' });
+      expect(screen.getByLabelText('testlabel')).toHaveValue('Lorem Ipsum');
+    });
 
-      it('should have label and counter disabled', () => {
-        counterTestWrapper1.setProps({ disabled: true });
-        const wrapper = counterTestWrapper1.find(
-          `.${prefix}--text-area__label-wrapper`
-        );
-        const labels = wrapper.find(`.${prefix}--label`);
+    it('should set defaultValue as expected', () => {
+      renderTextArea({ defaultValue: 'default value' });
+      expect(screen.getByLabelText('testlabel')).toHaveValue('default value');
+    });
+  });
 
-        labels.forEach((node) => {
-          expect(node.hasClass(`${prefix}--label--disabled`)).toEqual(true);
-        });
-      });
+  describe('label', () => {
+    it('renders a label with the expected text', () => {
+      renderTextArea({ labelText: 'testLabel' });
+      expect(screen.getByText('testLabel')).toBeInTheDocument();
+    });
+
+    it('has the expected classes', () => {
+      renderTextArea({ labelText: 'testLabel' });
+      expect(screen.getByText('testLabel')).toHaveClass(`${prefix}--label`);
+    });
+  });
+
+  describe('helper', () => {
+    it('renders a helper', () => {
+      renderTextArea({ helperText: 'It must be 3 characters' });
+      expect(screen.getByText('It must be 3 characters')).toHaveClass(
+        `${prefix}--form__helper-text`
+      );
+    });
+
+    it('renders children as expected', () => {
+      renderTextArea({ helperText: <span>This is helper text.</span> });
+      expect(screen.getByText('This is helper text.')).not.toHaveClass(
+        `${prefix}--form__helper-text`
+      );
+      expect(screen.getByText('This is helper text.').tagName).toBe('SPAN');
+      expect(
+        screen.getByText('This is helper text.').parentElement
+      ).toHaveClass(`${prefix}--form__helper-text`);
+    });
+  });
+
+  describe('counter', () => {
+    it('should set enableCounter and maxCount as expected', () => {
+      renderTextArea({ enableCounter: true, maxCount: 500 });
+      expect(screen.getByText('0/500')).toBeInTheDocument();
+    });
+
+    it('should not render counter with only enableCounter prop passed in', () => {
+      renderTextArea({ enableCounter: true });
+      // The label and the counter both have the same label class.
+      // The label exists, but not the counter.
+      expect(screen.getByText('testlabel')).toHaveClass(`${prefix}--label`);
+      expect(
+        screen
+          .getByText('testlabel')
+          .closest(`.${prefix}--text-area__label-wrapper`)
+          .getElementsByClassName(`${prefix}--label`).length
+      ).toEqual(1);
+    });
+
+    it('should not render counter with only maxCount prop passed in', () => {
+      renderTextArea({ maxCount: 500 });
+      // The label and the counter both have the same label class.
+      // The label exists, but not the counter.
+      expect(screen.getByText('testlabel')).toHaveClass(`${prefix}--label`);
+      expect(
+        screen
+          .getByText('testlabel')
+          .closest(`.${prefix}--text-area__label-wrapper`)
+          .getElementsByClassName(`${prefix}--label`).length
+      ).toEqual(1);
+    });
+
+    it('should have label and counter disabled', () => {
+      renderTextArea({ disabled: true, enableCounter: true, maxCount: 100 });
+      expect(screen.getByText('testlabel')).toHaveClass(
+        `${prefix}--label--disabled`
+      );
+      expect(screen.getByText('0/100')).toHaveClass(
+        `${prefix}--label--disabled`
+      );
     });
   });
 
   describe('events', () => {
     describe('disabled textarea', () => {
-      const onClick = jest.fn();
-      const onChange = jest.fn();
-
-      const wrapper = shallow(
-        <TextArea
-          id="test"
-          labelText="testlabel"
-          onClick={onClick}
-          onChange={onChange}
-          disabled
-        />
-      );
-
-      const textarea = wrapper.find('textarea');
-
       it('should not invoke onClick when textarea is clicked', () => {
-        textarea.simulate('click');
+        const onClick = jest.fn();
+        renderTextArea({ disabled: true, onClick });
+        userEvent.click(screen.getByLabelText('testlabel'));
         expect(onClick).not.toHaveBeenCalled();
       });
 
       it('should not invoke onChange', () => {
-        textarea.simulate('change');
+        const onChange = jest.fn();
+        renderTextArea({ disabled: true, onChange });
+        userEvent.click(screen.getByLabelText('testlabel'));
+        userEvent.keyboard('big blue');
         expect(onChange).not.toHaveBeenCalled();
       });
     });
 
     describe('enabled textarea', () => {
-      const onClick = jest.fn();
-      const onChange = jest.fn();
-      const eventObject = {
-        target: { value: 'test' },
-      };
-
-      const wrapper = shallow(
-        <TextArea
-          id="test"
-          labelText="testlabel"
-          onClick={onClick}
-          onChange={onChange}
-        />
-      );
-
-      const textarea = wrapper.find('textarea');
-
-      it('should invoke onClick when textarea is clicked', () => {
-        textarea.simulate('click');
+      it('should invoke onClick when textarea is clicked', async () => {
+        const onClick = jest.fn();
+        renderTextArea({ disabled: false, onClick });
+        await userEvent.click(screen.getByLabelText('testlabel'));
         expect(onClick).toHaveBeenCalled();
       });
 
-      it('should invoke onChange when textarea value is changed', () => {
-        textarea.simulate('change', eventObject);
-        expect(onChange).toHaveBeenCalledWith(eventObject);
+      it('should invoke onChange when textarea value is changed', async () => {
+        const onChange = jest.fn();
+        renderTextArea({ disabled: false, onChange });
+        await userEvent.click(screen.getByLabelText('testlabel'));
+        userEvent.keyboard('big blue');
+        expect(onChange).toHaveBeenCalled();
       });
     });
   });
