0386b3ca1bce804c6375368bb2c6e37d4c9a22ba
Tanner Summers <tannersummers@outlook.com>
2023-01-25 01:27:59 -0600

    test(TreeView): updated test to use rtl

--- packages/react/src/components/TreeView/TreeView-test.js
+++ packages/react/src/components/TreeView/TreeView-test.js
@@ -5,91 +5,146 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { CaretDown } from '@carbon/icons-react';
 import React from 'react';
-import { mount } from 'enzyme';
-import { Document, Folder } from '@carbon/icons-react';
+import { fireEvent, render, screen, within } from '@testing-library/react';
+import { Document } from '@carbon/icons-react';
 import TreeView, { TreeNode } from './';
 
-const prefix = 'cds';
-
 describe('TreeView', () => {
-  let wrapper;
-  let onTreeSelect;
-  let onNodeSelect;
+  it('should render tree with two nodes', () => {
+    render(
+      <TreeView label="Tree View">
+        <TreeNode label="Node 1" />
+        <TreeNode label="Node 2" />
+      </TreeView>
+    );
 
-  beforeEach(() => {
-    onTreeSelect = jest.fn();
-    onNodeSelect = jest.fn();
-    wrapper = mount(
-      <TreeView label="Tree view" selected={['1']} onSelect={onTreeSelect}>
-        <TreeNode id="1" value="1" label="1" />
-        <TreeNode id="2" value="2" label="2" onSelect={onNodeSelect} />
-        <TreeNode id="5" value="5" label="5" isExpanded>
-          <TreeNode id="5-1" value="5-1" label="5-1" />
-          <TreeNode id="5-2" value="5-2" label="5-2" />
-          <TreeNode id="5-3" value="5-3" label="5-3" isExpanded>
-            <TreeNode id="5-4" value="5-4" label="5-4" />
-          </TreeNode>
+    expect(screen.getByLabelText('Tree View')).toBeInTheDocument();
+    expect(screen.getByText('Node 1')).toBeInTheDocument();
+    expect(screen.getByText('Node 2')).toBeInTheDocument();
+  });
+
+  it('should render tree with nested node', () => {
+    render(
+      <TreeView label="Tree View">
+        <TreeNode isExpanded={true} data-testid="Node 1" label="Node 1">
+          <TreeNode data-testid="Node 2" label="Node 2" />
         </TreeNode>
       </TreeView>
     );
+
+    const nodeParent = screen.getByTestId('Node 1');
+    const nodeChild = screen.getByTestId('Node 2');
+
+    expect(screen.getByLabelText('Tree View')).toBeInTheDocument();
+    expect(nodeParent).toHaveClass('cds--tree-parent-node');
+    expect(nodeChild).toHaveClass('cds--tree-leaf-node');
+    expect(within(nodeParent).getByText('Node 1')).toBeInTheDocument();
+    expect(within(nodeChild).getByText('Node 2')).toBeInTheDocument();
   });
 
-  it('should render', () => {
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it('should render with icons', () => {
-    wrapper = mount(
-      <TreeView label="Tree view" selected={['1']}>
-        <TreeNode renderIcon={Document} id="1" value="1" label="1" />
-        <TreeNode renderIcon={Document} id="2" value="2" label="2" />
-        <TreeNode renderIcon={Folder} id="5" value="5" label="5" isExpanded>
-          <TreeNode renderIcon={Document} id="5-1" value="5-1" label="5-1" />
-          <TreeNode renderIcon={Document} id="5-2" value="5-2" label="5-2" />
-          <TreeNode
-            renderIcon={Folder}
-            id="5-3"
-            value="5-3"
-            label="5-3"
-            isExpanded>
-            <TreeNode renderIcon={Document} id="5-4" value="5-4" label="5-4" />
-          </TreeNode>
+  it('should render tree with expanded node', () => {
+    render(
+      <TreeView label="Tree View">
+        <TreeNode data-testid="Node 1" label="Node 1" isExpanded={true}>
+          <TreeNode data-testid="Node 2" label="Node 2" />
         </TreeNode>
       </TreeView>
     );
-    expect(wrapper).toMatchSnapshot();
+
+    const nodeParent = screen.getByTestId('Node 1');
+    const nodeChild = nodeParent?.querySelector('div > span');
+
+    expect(screen.getByLabelText('Tree View')).toBeInTheDocument();
+    expect(nodeChild).toHaveClass('cds--tree-parent-node__toggle');
+    expect(within(nodeParent).getByText('Node 1')).toBeInTheDocument();
+    expect(within(nodeParent).getByText('Node 2')).toBeInTheDocument();
   });
 
-  it('should be able to hide the label', () => {
-    expect(wrapper.find('TreeLabel').text()).toBe('Tree view');
-    wrapper.setProps({ hideLabel: true });
-    expect(wrapper.find('TreeLabel').text()).toBeFalsy();
+  it('should render tree with disabled nodes', () => {
+    render(
+      <TreeView label="Tree View">
+        <TreeNode label="Node 1" data-testid="Node 1" />
+        <TreeNode
+          data-testid="Node 2"
+          label="Node 2"
+          disabled={true}
+          isExpanded={true}>
+          <TreeNode isExpanded={true} data-testid="Node 3" label="Node 3" />
+        </TreeNode>
+      </TreeView>
+    );
+
+    const nodeParent = screen.getByTestId('Node 1');
+    const nodeChild = screen.getByTestId('Node 2');
+
+    expect(screen.getByLabelText('Tree View')).toBeInTheDocument();
+    expect(nodeChild).toHaveClass('cds--tree-node--disabled');
+    expect(within(nodeParent).getByText('Node 1')).toBeInTheDocument();
+    expect(within(nodeChild).getByText('Node 2')).toBeInTheDocument();
+    expect(within(nodeChild).getByText('Node 3')).toBeInTheDocument();
   });
 
-  describe('Single node selection', () => {
-    it('should be able to preselect a node', () => {
-      expect(wrapper.find(`.${prefix}--tree-node--selected`).text()).toBe('1');
-    });
+  it('should render tree with icons', () => {
+    render(
+      <TreeView label="Tree View">
+        <TreeNode renderIcon={Document} data-testid="Node 1" label="Node 1" />
+      </TreeView>
+    );
 
-    it('should handle selection at the tree level', () => {
-      const onTreeSelect = jest.fn();
-      wrapper.setProps({ onSelect: onTreeSelect });
-      wrapper.find('TreeNode[value="2"]').simulate('click');
-      expect(onTreeSelect).toHaveBeenCalledTimes(1);
-    });
-
-    it('should handle selection at the node level', () => {
-      wrapper.find('TreeNode[value="2"]').simulate('click');
-      expect(onTreeSelect).toHaveBeenCalledTimes(1);
-      expect(onNodeSelect).toHaveBeenCalledTimes(1);
-    });
+    expect(screen.getByLabelText('Tree View')).toBeInTheDocument();
+    expect(screen.getByTestId('Node 1')).toHaveClass(
+      'cds--tree-node--with-icon'
+    );
   });
 
-  describe('Tree node expansion', () => {
-    it('Caret icon should not render in leaf nodes', () => {
-      expect(wrapper.find(CaretDown).length).toBe(2);
-    });
+  it('should render tree with no label', () => {
+    render(
+      <TreeView id="Node Tree" label="Tree View" hideLabel>
+        <TreeNode
+          id="Node 1"
+          renderIcon={Document}
+          data-testid="Node 1"
+          label="Node 1"
+        />
+      </TreeView>
+    );
+
+    expect(
+      screen.queryByText('Tree View', { selector: 'label' })
+    ).not.toBeInTheDocument();
+  });
+
+  it('should render tree with pre-selected node', () => {
+    render(
+      <TreeView label="Tree View" selected={['Node 1']}>
+        <TreeNode id="Node 1" data-testid="Node 1" label="Node 1" />
+      </TreeView>
+    );
+
+    expect(screen.getByLabelText('Tree View')).toBeInTheDocument();
+    expect(screen.getByRole('treeitem', { name: 'Node 1' })).toHaveClass(
+      'cds--tree-node--selected'
+    );
+  });
+
+  it('should render tree and click on node', () => {
+    const onSelectSpy = jest.fn();
+
+    render(
+      <TreeView label="Tree View">
+        <TreeNode
+          onSelect={onSelectSpy}
+          id="Node 1"
+          data-testid="Node 1"
+          label="Node 1"
+        />
+      </TreeView>
+    );
+
+    fireEvent.click(screen.getByTestId('Node 1'));
+
+    expect(screen.getByLabelText('Tree View')).toBeInTheDocument();
+    expect(onSelectSpy).toHaveBeenCalled();
   });
 });
