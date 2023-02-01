fe9c5f8f7dc1a819a97326d9f211f380ba3e9819
ShankarV-CodeJunkie <56068832+Shankar-CodeJunkie@users.noreply.github.com>
2023-02-02 00:25:25 +0530

    Update packages/cra-template/template/src/index.js
    Co-authored-by: Francine Lucca <40550942+francinelucca@users.noreply.github.com>

--- packages/cra-template/template/src/index.js
+++ packages/cra-template/template/src/index.js
@@ -14,15 +14,6 @@ const client = new ApolloClient({
   },
 });
 
-/*const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
-
-    </React.StrictMode>
-);*/
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
