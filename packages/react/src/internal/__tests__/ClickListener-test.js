6dc7b1333c74edde18173f3ca03e0c65df67c598
Alison Joseph <alison.joseph@us.ibm.com>
2023-02-08 21:15:24 +0000

    test(clicklistener): update tests to rtl (#13111)
    Co-authored-by: Andrea N. Cardona <andreancardona@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/react/src/internal/__tests__/ClickListener-test.js
+++ packages/react/src/internal/__tests__/ClickListener-test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2016, 2018
+ * Copyright IBM Corp. 2016, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -7,7 +7,8 @@
 
 import React from 'react';
 import ClickListener from '../ClickListener';
-import { shallow, mount } from 'enzyme';
+
+import { render } from '@testing-library/react';
 
 describe('ClickListener', () => {
   let onClickOutside;
@@ -22,73 +23,51 @@ describe('ClickListener', () => {
     handleRefSpy.mockRestore();
   });
 
-  it('should render', () => {
-    const wrapper = shallow(
-      <ClickListener onClickOutside={onClickOutside}>
-        <div>
-          <div className="child">Test</div>
-          <div className="child">Test</div>
-        </div>
-      </ClickListener>
-    );
-    expect(wrapper).toMatchSnapshot();
+  describe('renders as expected - Component API', () => {
+    it('should render', () => {
+      const { container } = render(
+        <ClickListener onClickOutside={onClickOutside}>
+          <div>
+            <div className="child">Test</div>
+            <div className="child">Test</div>
+          </div>
+        </ClickListener>
+      );
+      expect(container).toMatchSnapshot();
+    });
   });
 
-  it('should throw a PropType validation error if passed multiple children', () => {
-    // We need to make assertions on `console.error` that come from React, so let's
-    // disable the `no-console` eslint rules on certain lines.
-    // eslint-disable-next-line no-console
-    const originalConsoleError = console.error;
-    const mockConsoleError = jest.fn();
-    // eslint-disable-next-line no-console
-    console.error = mockConsoleError;
+  describe('behaves as expected', () => {
+    it('should invoke onClickOutside if click is outside of the component', () => {
+      render(
+        <ClickListener onClickOutside={onClickOutside}>
+          <div>
+            <div className="child">Test</div>
+            <div className="child">Test</div>
+          </div>
+        </ClickListener>
+      );
 
-    shallow(
-      <ClickListener onClickOutside={onClickOutside}>
-        <div className="child">Test</div>
-        <div className="child">Test</div>
-      </ClickListener>
-    );
-    expect(mockConsoleError).toHaveBeenCalledWith(
-      'Warning: Failed %s type: %s%s',
-      'prop',
-      'Invalid prop `children` of type `array` supplied to `ClickListener`, expected a single ReactElement.',
-      expect.anything() // this argument is the stack trace/report of the source file line and can change
-    );
+      const evt = new MouseEvent('click');
+      document.dispatchEvent(evt);
 
-    // eslint-disable-next-line no-console
-    console.error = originalConsoleError;
-  });
+      expect(onClickOutside).toHaveBeenCalled();
+    });
 
-  it('should invoke onClickOutside if click is outside of the component', () => {
-    mount(
-      <ClickListener onClickOutside={onClickOutside}>
-        <div>
-          <div className="child">Test</div>
-          <div className="child">Test</div>
-        </div>
-      </ClickListener>
-    );
-
-    const evt = new MouseEvent('click');
-    document.dispatchEvent(evt);
-
-    expect(onClickOutside).toHaveBeenCalled();
-  });
-
-  it('should not overwrite any children function refs', () => {
-    const mockRef = jest.fn();
-    class Child extends React.Component {
-      render() {
-        return <div />;
+    it('should not overwrite any children function refs', () => {
+      const mockRef = jest.fn();
+      class Child extends React.Component {
+        render() {
+          return <div />;
+        }
       }
-    }
-    mount(
-      <ClickListener onClickOutside={onClickOutside}>
-        <Child ref={mockRef} />
-      </ClickListener>
-    );
-    expect(handleRefSpy).toHaveBeenCalledTimes(1);
-    expect(mockRef).toHaveBeenCalledTimes(1);
+      render(
+        <ClickListener onClickOutside={onClickOutside}>
+          <Child ref={mockRef} />
+        </ClickListener>
+      );
+      expect(handleRefSpy).toHaveBeenCalledTimes(1);
+      expect(mockRef).toHaveBeenCalledTimes(1);
+    });
   });
 });
