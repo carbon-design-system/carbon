a866008a788e2dadb962d577e7f1d7ef5fae8919
Francine Lucca <francinelucca@hotmail.com>
2023-02-22 14:34:05 -0500

    chore: update 2016 copyright headers

--- packages/carbon-components-react/tasks/build.js
+++ packages/carbon-components-react/tasks/build.js
@@ -51,7 +51,7 @@ async function build() {
 }
 
 const banner = `/**
- * Copyright IBM Corp. 2016, 2021
+ * Copyright IBM Corp. 2016, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
--- packages/icon-build-helpers/src/builders/react/next/templates.js
+++ packages/icon-build-helpers/src/builders/react/next/templates.js
@@ -10,7 +10,7 @@
 const { default: template } = require('@babel/template');
 
 const banner = `/**
- * Copyright IBM Corp. 2016, 2021
+ * Copyright IBM Corp. 2016, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
