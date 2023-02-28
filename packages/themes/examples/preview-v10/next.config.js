587d5a7c65e86a7b48b334229e1de67426334271
TJ Egan <tw15egan@gmail.com>
2023-02-28 14:21:25 -0500

    chore(css-grid): update packages (#13241)
    * chore(css-grid): update packages
    
    * chore(grid:preview): use next instead of parcel
    
    * chore(react): bump examples using next to react v17.0.2
    
    * chore(themes): update v10-preview to next
    
    * chore(yarn): update lockfile
    
    ---------
    
    Co-authored-by: Andrea N. Cardona <cardona.n.andrea@gmail.com>

---
+++ packages/themes/examples/preview-v10/next.config.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright IBM Corp. 2018, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+const { PHASE_PRODUCTION_BUILD } = require('next/constants');
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    return {
+      basePath: '/themes/examples/preview-v10',
+    };
+  }
+
+  return defaultConfig;
+};
