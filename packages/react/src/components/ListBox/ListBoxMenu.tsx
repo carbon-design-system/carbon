2a1e342755ecd72329cff4e5fd264cb50e80f0fb
Michael Barrera <mbarrer@us.ibm.com>
2023-03-03 12:24:08 -0600

    refactor: update usages of ReactAttr

--- packages/react/src/components/ListBox/ListBoxMenu.tsx
+++ packages/react/src/components/ListBox/ListBoxMenu.tsx
@@ -14,15 +14,17 @@ import { ForwardRefReturn, ReactAttr } from '../../types/common';
 type ExcludedAttributes = 'id';
 
 export interface ListBoxMenuProps
-  extends Omit<ReactAttr<HTMLDivElement>, ExcludedAttributes> {
-
+  extends Omit<ReactAttr<'div'>, ExcludedAttributes> {
   /**
    * Specify a custom `id`
    */
   id: string;
 }
 
-export type ListBoxMenuComponent = ForwardRefReturn<HTMLDivElement, ListBoxMenuProps>
+export type ListBoxMenuComponent = ForwardRefReturn<
+  HTMLDivElement,
+  ListBoxMenuProps
+>;
 
 /**
  * `ListBoxMenu` is a simple container node that isolates the `list-box__menu`
@@ -31,7 +33,7 @@ export type ListBoxMenuComponent = ForwardRefReturn<HTMLDivElement, ListBoxMenuP
  */
 const ListBoxMenu: ListBoxMenuComponent = React.forwardRef(function ListBoxMenu(
   { children, id, ...rest }: ListBoxMenuProps,
-  ref: ForwardedRef<HTMLDivElement>,
+  ref: ForwardedRef<HTMLDivElement>
 ) {
   const prefix = usePrefix();
   return (
