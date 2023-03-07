5e52d2053b95c374fd407f7974d461f4bbad3859
Francine Lucca <francinelucca@hotmail.com>
2023-03-07 15:15:43 -0500

    chore: add postinstall deprecation warning to old packages

---
+++ packages/carbon-components-react/scripts/postinstall.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const chalk = require('chalk');
+
+console.warn(
+  chalk.yellow('Warning: ') +
+    chalk.cyan('carbon-components-react') +
+    " is soon to be deprecated and is no longer receiving updates, we recommend to move to it's replacement, " +
+    chalk.cyan('@carbon/react') +
+    '  at your earliest convenience. Please visit ' +
+    chalk.dim.underline.italic(
+      'https://github.com/carbon-design-system/carbon/discussions/12179'
+    ) +
+    ' for more information on the package strategy, deprecation, and plan of action.'
+);
