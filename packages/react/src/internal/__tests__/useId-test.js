e5720b44148bf47a0cff3efecd693059c107c49e
Taylor Jones <taylor.jones826@gmail.com>
2023-03-15 14:24:28 -0500

    test: update tests to use rtl

--- packages/react/src/internal/__tests__/useId-test.js
+++ packages/react/src/internal/__tests__/useId-test.js
@@ -5,28 +5,18 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { render, cleanup } from '@carbon/test-utils/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { useId } from '../useId';
 
 describe('useId', () => {
-  afterEach(cleanup);
-
   it('should generate a unique id that is stable across renders', () => {
     function Test() {
       const id = useId('test');
       return <span id={id}>test</span>;
     }
 
-    const container = document.createElement('div');
-    const getTestId = () => container.firstChild.getAttribute('id');
-
-    render(<Test />, { container });
-
-    const id = getTestId();
-    expect(getTestId()).toBeDefined();
-
-    render(<Test />, { container });
-    expect(id).toBe(getTestId());
+    render(<Test />);
+    expect(screen.getByText('test')).toHaveAttribute('id', 'test-1');
   });
 });
