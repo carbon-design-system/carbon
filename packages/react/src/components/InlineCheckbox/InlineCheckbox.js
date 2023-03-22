6efccc449f81c674adbb8b87a532ace13d0d941b
Taylor Jones <tay1orjones@users.noreply.github.com>
2023-03-22 09:38:57 -0500

    Update packages/react/src/components/InlineCheckbox/InlineCheckbox.js

--- packages/react/src/components/InlineCheckbox/InlineCheckbox.js
+++ packages/react/src/components/InlineCheckbox/InlineCheckbox.js
@@ -86,7 +86,7 @@ InlineCheckbox.propTypes = {
    */
   ariaLabel: deprecate(
     PropTypes.string.isRequired,
-    'This prop syntax has been deprecated. Please use the new `aria-label`.'
+     'The `ariaLabel` prop has been deprecated in favor of `aria-label`. This prop will be removed in the next major release.'
   ),
 
   /**
