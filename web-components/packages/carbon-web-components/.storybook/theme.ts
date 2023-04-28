3ad345fc3ca732296689a4aa83b7b6a3ba58da02
kennylam <909118+kennylam@users.noreply.github.com>
2023-04-28 19:17:24 -0400

    chore(sb): add plex cdn to storybook (#10407)

--- web-components/packages/carbon-web-components/.storybook/theme.ts
+++ web-components/packages/carbon-web-components/.storybook/theme.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2019, 2022
+ * Copyright IBM Corp. 2019, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -11,6 +11,10 @@ import { create, ThemeVars } from '@storybook/theming';
 import { version } from '../package.json';
 
 export default create({
+  // Typography
+  fontBase: "'IBM Plex Sans', 'Helvetica Neue', Arial, sans-serif",
+  fontCode:
+    "'IBM Plex Mono', Menlo, 'DejaVu Sans Mono', 'Bitstream Vera Sans Mono', Courier, monospace",
   brandTitle: `carbon-web-components ${version}`,
   brandUrl: 'https://github.com/carbon-design-system/carbon-web-components',
 } as ThemeVars);
