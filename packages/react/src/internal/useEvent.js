fa01b313c3e3c01d7f292e84f7d33323fd11ca68
Mark Judy <32761859+mrkjdy@users.noreply.github.com>
2023-05-16 22:49:41 -0400

    feature/Toggletip types (#13790)
    * feat: convert Toggletip and useEvent to tsx/ts
    
    * fix: make Popover polymorphic
    
    * fix: use correct syntax for importing in a few JS files
    
    * fix: update tests after modifying types in Toggletip and Popover
    
    * fix: revert useEvent to js
    
    * fix: make useEvent and useWindowEvent generic
    
    ---------
    
    Co-authored-by: Francine Lucca <40550942+francinelucca@users.noreply.github.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/react/src/internal/useEvent.js
+++ packages/react/src/internal/useEvent.js
@@ -1,3 +1,5 @@
+// @ts-check
+/* eslint-disable jsdoc/check-tag-names */
 /**
  * Copyright IBM Corp. 2016, 2023
  *
@@ -7,37 +9,76 @@
 
 import { useEffect, useRef } from 'react';
 
+/**
+ * @template {keyof GlobalEventHandlersEventMap} E
+ * @typedef {(event: GlobalEventHandlersEventMap[E]) => void} GlobalEventCallback
+ */
+
+/**
+ * @template T
+ * @typedef {import('react').MutableRefObject<T>} MutableRefObject<T>
+ */
+
+/**
+ * @template {keyof GlobalEventHandlersEventMap} E
+ * @param {HTMLElement | MutableRefObject<HTMLElement | null>} elementOrRef
+ * @param {E} eventName
+ * @param {GlobalEventCallback<E>} callback
+ */
 export function useEvent(elementOrRef, eventName, callback) {
+  /**
+   * @type {MutableRefObject<GlobalEventCallback<E> | null>}
+   */
   const savedCallback = useRef(null);
 
   useEffect(() => {
     savedCallback.current = callback;
-  });
+  }, [callback]);
 
   useEffect(() => {
-    function handler(event) {
+    /**
+     * @type {GlobalEventCallback<E>}
+     */
+    const handler = (event) => {
       if (savedCallback.current) {
         savedCallback.current(event);
       }
-    }
+    };
 
-    const element = elementOrRef.current ?? elementOrRef;
-    element.addEventListener(eventName, handler);
+    const element =
+      'current' in elementOrRef ? elementOrRef.current : elementOrRef;
+    element?.addEventListener(eventName, handler);
 
     return () => {
-      element.removeEventListener(eventName, handler);
+      element?.removeEventListener(eventName, handler);
     };
   }, [elementOrRef, eventName]);
 }
 
+/**
+ * @template {keyof WindowEventMap} E
+ * @typedef {(event: WindowEventMap[E]) => void} WindowEventCallback
+ */
+
+/**
+ * @template {keyof WindowEventMap} E
+ * @param {E} eventName
+ * @param {WindowEventCallback<E>} callback
+ */
 export function useWindowEvent(eventName, callback) {
+  /**
+   * @type {MutableRefObject<WindowEventCallback<E> | null>}
+   */
   const savedCallback = useRef(null);
 
   useEffect(() => {
     savedCallback.current = callback;
-  });
+  }, [callback]);
 
   useEffect(() => {
+    /**
+     * @type {WindowEventCallback<E>}
+     */
     function handler(event) {
       if (savedCallback.current) {
         savedCallback.current(event);
