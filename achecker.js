1be9009cee9af5709b2ada6673e71c8b96dba838
Taylor Jones <taylor.jones826@gmail.com>
2023-05-24 10:10:44 -0500

    fix(aChecker): track latest ruleset

--- achecker.js
+++ achecker.js
@@ -10,7 +10,7 @@
 const path = require('path');
 
 module.exports = {
-  ruleArchive: '15May2023',
+  ruleArchive: 'latest',
   policies: ['Custom_Ruleset'],
   failLevels: ['violation'],
   reportLevels: [
