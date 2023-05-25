e7e0b8ebee472934235431c57582b01664af69de
Max You <JianzhongMax.You@ibm.com>
2023-05-25 16:58:25 -0400

    accordian

--- packages/react/src/components/Accordion/Accordion.stories.js
+++ packages/react/src/components/Accordion/Accordion.stories.js
@@ -18,6 +18,17 @@ import mdx from './Accordion.mdx';
 
 import { WithLayer } from '../../../.storybook/templates/WithLayer';
 
+const CarbonBuilderLink = () => {
+  return (
+    <>
+      <a href="https://builder.carbondesignsystem.com/from-json/%7B%22title%22%3A%22AccordionFragment%22%2C%22data%22%3A%7B%22items%22%3A%5B%7B%22type%22%3A%22accordion%22%2C%22align%22%3A%22end%22%2C%22size%22%3A%22md%22%2C%22items%22%3A%5B%7B%22type%22%3A%22accordion-item%22%2C%22title%22%3A%22Accordion%20item%22%2C%22items%22%3A%5B%5D%2C%22id%22%3A%223%22%2C%22codeContext%22%3A%7B%22name%22%3A%22accordion-item-3%22%7D%7D%5D%2C%22id%22%3A%222%22%2C%22codeContext%22%3A%7B%22name%22%3A%22accordion-2%22%7D%7D%5D%2C%22id%22%3A1%7D%2C%22allCssClasses%22%3A%5B%5D%7D" target="_blank" rel="noreferrer"> 
+        Edit on Carbon UI Builder </a>
+      <br></br>
+      <br></br>
+    </>
+  );
+};
+
 export default {
   title: 'Components/Accordion',
   component: Accordion,
@@ -33,79 +44,85 @@ export default {
 };
 
 export const Default = () => (
-  <Accordion>
-    <AccordionItem title="Section 1 title">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </p>
-    </AccordionItem>
-    <AccordionItem title="Section 2 title">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </p>
-    </AccordionItem>
-    <AccordionItem title="Section 3 title">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </p>
-    </AccordionItem>
-    <AccordionItem title="Section 4 title">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </p>
-    </AccordionItem>
-  </Accordion>
-);
-
-export const _WithLayer = () => (
-  <WithLayer>
+  <div>
+    <CarbonBuilderLink></CarbonBuilderLink>
     <Accordion>
       <AccordionItem title="Section 1 title">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
         </p>
       </AccordionItem>
       <AccordionItem title="Section 2 title">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
         </p>
       </AccordionItem>
       <AccordionItem title="Section 3 title">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
         </p>
       </AccordionItem>
       <AccordionItem title="Section 4 title">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
         </p>
       </AccordionItem>
     </Accordion>
-  </WithLayer>
+  </div>
+);
+
+export const _WithLayer = () => (
+  <div>
+    <CarbonBuilderLink></CarbonBuilderLink>
+    <WithLayer>
+      <Accordion>
+        <AccordionItem title="Section 1 title">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </AccordionItem>
+        <AccordionItem title="Section 2 title">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </AccordionItem>
+        <AccordionItem title="Section 3 title">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </AccordionItem>
+        <AccordionItem title="Section 4 title">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </AccordionItem>
+      </Accordion>
+    </WithLayer>
+  </div>
 );
 
 export const Skeleton = (args) => (
--- packages/react/src/components/Button/Button.stories.js
+++ packages/react/src/components/Button/Button.stories.js
@@ -61,15 +61,12 @@ export default {
 const CarbonBuilderLink = () => {
   return (
     <>
-      <a
-        href="https://builder.carbondesignsystem.com/from-json/%7B%22
-      title%22%3A%22ButtonFragment%22%2C%22data%22%3A%7B%22items%22%3A
-      %5B%7B%22type%22%3A%22button%22%2C%22kind%22%3A%22primary%22%2C
-      %22text%22%3A%22Button%22%2C%22size%22%3A%22%22%2C%22id%22%3A%222%22
-      %2C%22codeContext%22%3A%7B%22name%22%3A%22button-2%22%7D%7D%5D%2C%22
-      id%22%3A1%7D%2C%22allCssClasses%22%3A%5B%5D%7D"
-        target="_blank"
-        rel="noreferrer">
+      <a href="https://builder.carbondesignsystem.com/from-json/%7B%22&#13;
+      title%22%3A%22ButtonFragment%22%2C%22data%22%3A%7B%22items%22%3A&#13;
+      %5B%7B%22type%22%3A%22button%22%2C%22kind%22%3A%22primary%22%2C&#13;
+      %22text%22%3A%22Button%22%2C%22size%22%3A%22%22%2C%22id%22%3A%222%22&#13;
+      %2C%22codeContext%22%3A%7B%22name%22%3A%22button-2%22%7D%7D%5D%2C%22&#13;
+      id%22%3A1%7D%2C%22allCssClasses%22%3A%5B%5D%7D" target="_blank" rel="noreferrer">
         Edit on Carbon UI Builder
       </a>
       <br></br>
