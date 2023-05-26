ca93f973814ec8eb334f6bace038638b1d5c1da4
ShankarV-CodeJunkie <emailmeforshankar@gmail.com>
2023-05-26 13:44:06 +0530

    chore: modify playground story for sidenavigation with proper content

--- packages/react/src/components/UIShell/UIShell.stories.js
+++ packages/react/src/components/UIShell/UIShell.stories.js
@@ -1025,59 +1025,110 @@ export const SideNavWLargeSideNavItems = () => (
 const UIShellPlayground = (props) => {
   const { isRail, enterDelayMs } = props;
   return (
-    <SideNav
-      aria-label="Side navigation"
-      href="#main-content"
-      isRail={isRail}
-      enterDelayMs={enterDelayMs}>
-      <SideNavItems>
-        <SideNavMenu renderIcon={Fade} title="Category title">
-          <SideNavMenuItem href="https://www.carbondesignsystem.com/">
-            Link
-          </SideNavMenuItem>
-          <SideNavMenuItem
-            aria-current="page"
-            href="https://www.carbondesignsystem.com/">
-            Link
-          </SideNavMenuItem>
-          <SideNavMenuItem href="https://www.carbondesignsystem.com/">
-            Link
-          </SideNavMenuItem>
-        </SideNavMenu>
-        <SideNavMenu renderIcon={Fade} title="Category title">
-          <SideNavMenuItem href="https://www.carbondesignsystem.com/">
-            Link
-          </SideNavMenuItem>
-          <SideNavMenuItem href="https://www.carbondesignsystem.com/">
-            Link
-          </SideNavMenuItem>
-          <SideNavMenuItem href="https://www.carbondesignsystem.com/">
-            Link
-          </SideNavMenuItem>
-        </SideNavMenu>
-        <SideNavMenu renderIcon={Fade} title="Category title">
-          <SideNavMenuItem href="https://www.carbondesignsystem.com/">
-            Link
-          </SideNavMenuItem>
-          <SideNavMenuItem href="https://www.carbondesignsystem.com/">
-            Link
-          </SideNavMenuItem>
-          <SideNavMenuItem href="https://www.carbondesignsystem.com/">
-            Link
-          </SideNavMenuItem>
-        </SideNavMenu>
-        <SideNavLink
-          renderIcon={Fade}
-          href="https://www.carbondesignsystem.com/">
-          Link
-        </SideNavLink>
-        <SideNavLink
-          renderIcon={Fade}
-          href="https://www.carbondesignsystem.com/">
-          Link
-        </SideNavLink>
-      </SideNavItems>
-    </SideNav>
+    <HeaderContainer
+      render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+        <>
+          <Header aria-label="IBM Platform Name">
+            <SkipToContent />
+            <HeaderMenuButton
+              aria-label={isSideNavExpanded ? 'Close menu' : 'Open menu'}
+              isCollapsible
+              onClick={onClickSideNavExpand}
+              isActive={isSideNavExpanded}
+              aria-expanded={isSideNavExpanded}
+            />
+            <HeaderName href="#" prefix="IBM">
+              [Platform]
+            </HeaderName>
+            <HeaderNavigation aria-label="IBM [Platform]">
+              <HeaderMenuItem href="#">Link 1</HeaderMenuItem>
+              <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
+              <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
+              <HeaderMenu aria-label="Link 4" menuLinkName="Link 4">
+                <HeaderMenuItem href="#">Sub-link 1</HeaderMenuItem>
+                <HeaderMenuItem href="#">Sub-link 2</HeaderMenuItem>
+                <HeaderMenuItem href="#">Sub-link 3</HeaderMenuItem>
+              </HeaderMenu>
+            </HeaderNavigation>
+            <HeaderGlobalBar>
+              <HeaderGlobalAction
+                aria-label="Search"
+                onClick={action('search click')}>
+                <Search size={20} />
+              </HeaderGlobalAction>
+              <HeaderGlobalAction
+                aria-label="Notifications"
+                onClick={action('notification click')}>
+                <Notification size={20} />
+              </HeaderGlobalAction>
+              <HeaderGlobalAction
+                aria-label="App Switcher"
+                onClick={action('app-switcher click')}
+                tooltipAlignment="end">
+                <SwitcherIcon size={20} />
+              </HeaderGlobalAction>
+            </HeaderGlobalBar>
+            <SideNav
+              aria-label="Side navigation"
+              isRail={isRail}
+              expanded={isSideNavExpanded}
+              onOverlayClick={onClickSideNavExpand}
+              href="#main-content"
+              enterDelayMs={enterDelayMs}
+              onSideNavBlur={onClickSideNavExpand}>
+              <SideNavItems>
+                <SideNavMenu renderIcon={Fade} title="Category title">
+                  <SideNavMenuItem href="https://www.carbondesignsystem.com/">
+                    Link
+                  </SideNavMenuItem>
+                  <SideNavMenuItem href="https://www.carbondesignsystem.com/">
+                    Link
+                  </SideNavMenuItem>
+                  <SideNavMenuItem href="https://www.carbondesignsystem.com/">
+                    Link
+                  </SideNavMenuItem>
+                </SideNavMenu>
+                <SideNavMenu renderIcon={Fade} title="Category title">
+                  <SideNavMenuItem href="https://www.carbondesignsystem.com/">
+                    Link
+                  </SideNavMenuItem>
+                  <SideNavMenuItem
+                    aria-current="page"
+                    href="https://www.carbondesignsystem.com/">
+                    Link
+                  </SideNavMenuItem>
+                  <SideNavMenuItem href="https://www.carbondesignsystem.com/">
+                    Link
+                  </SideNavMenuItem>
+                </SideNavMenu>
+                <SideNavMenu renderIcon={Fade} title="Category title">
+                  <SideNavMenuItem href="https://www.carbondesignsystem.com/">
+                    Link
+                  </SideNavMenuItem>
+                  <SideNavMenuItem href="https://www.carbondesignsystem.com/">
+                    Link
+                  </SideNavMenuItem>
+                  <SideNavMenuItem href="https://www.carbondesignsystem.com/">
+                    Link
+                  </SideNavMenuItem>
+                </SideNavMenu>
+                <SideNavLink
+                  renderIcon={Fade}
+                  href="https://www.carbondesignsystem.com/">
+                  Link
+                </SideNavLink>
+                <SideNavLink
+                  renderIcon={Fade}
+                  href="https://www.carbondesignsystem.com/">
+                  Link
+                </SideNavLink>
+              </SideNavItems>
+            </SideNav>
+          </Header>
+          <StoryContent />
+        </>
+      )}
+    />
   );
 };
 export const Playground = UIShellPlayground.bind({});
