7a5a989cf40067a6228f9742e6d9648e961b9d68
Jan Hassel <jan.hassel@ibm.com>
2023-06-06 18:27:47 +0200

    chore(menu): move to stable (#13678)
    * chore(menu): update copyright banners
    
    * chore(menu): remove unstable__ prefix from exports
    
    * chore(menu): update copyright banners
    
    * chore(menu): move to stable stories
    
    * test(overflow-menu-v2): add vrt and avt
    
    * fix(menu-item): move default prop function out
    
    * docs(overflow-menu-v2): add mdx
    
    * fix(combo-button): sync tooltipAlignment prop naming
    
    * feat(overflow-menu-v2): add props.label and props.tooltipAlignment
    
    * fix(overflow-menu-v2): fix a11y violation with aria-owns
    
    * refactor(overflowmenu): expose next overflowmenu under feature flag
    
    * fix(overflowmenu): add back explicit displayName
    
    * fix: add aria-controls, move aria-owns to parent container
    
    * fix(menu): vertically center-align nesting caret
    
    * ci: remove aria_child_tabbable from testing denylists
    
    * test: re-add aria_child_tabbable to AC denylist
    
    ---------
    
    Co-authored-by: Taylor Jones <tay1orjones@users.noreply.github.com>
    Co-authored-by: Taylor Jones <taylor.jones826@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -18,30 +18,37 @@ export * from './components/CheckboxGroup';
 export * from './components/ClassPrefix';
 export * from './components/CodeSnippet';
 export * from './components/ComboBox';
+export * from './components/ComboButton';
 export * from './components/ComposedModal';
 export * from './components/ContainedList';
 export * from './components/ContentSwitcher';
+export * from './components/ContextMenu';
 export * from './components/Copy';
 export * from './components/CopyButton';
 export * from './components/DangerButton';
 export * from './components/DataTable';
+export * from './components/DataTableSkeleton';
 export * from './components/DatePicker';
 export * from './components/DatePickerInput';
 export * from './components/Dropdown';
 export * from './components/ErrorBoundary';
+export * from './components/ExpandableSearch';
 export * from './components/FileUploader';
 export * from './components/FilterableMultiSelect';
-export * from './components/Form';
 export * from './components/FluidForm';
+export * from './components/Form';
 export * from './components/FormGroup';
 export * from './components/FormItem';
 export * from './components/FormLabel';
 export * from './components/Grid';
+export * from './components/Icon/Icon.Skeleton';
 export * from './components/IdPrefix';
 export * from './components/InlineLoading';
 export * from './components/Link';
 export * from './components/ListItem';
 export * from './components/Loading';
+export * from './components/Menu';
+export * from './components/MenuButton';
 export * from './components/Modal';
 export * from './components/ModalWrapper';
 export * from './components/MultiSelect';
@@ -51,50 +58,46 @@ export * from './components/OrderedList';
 export * from './components/OverflowMenu';
 export * from './components/OverflowMenuItem';
 export * from './components/Pagination';
+export * from './components/Pagination/Pagination.Skeleton';
 export * from './components/PaginationNav';
 export * from './components/PasswordInput';
 export * from './components/PrimaryButton';
 export * from './components/ProgressIndicator';
 export * from './components/RadioButton';
+export * from './components/RadioButton/RadioButton.Skeleton';
 export * from './components/RadioButtonGroup';
+export * from './components/RadioTile';
 export * from './components/Search';
-export * from './components/ExpandableSearch';
 export * from './components/SecondaryButton';
 export * from './components/Select';
 export * from './components/SelectItem';
 export * from './components/SelectItemGroup';
-export * from './components/Switch';
+export * from './components/SkeletonIcon';
+export * from './components/SkeletonPlaceholder';
+export * from './components/SkeletonText';
 export * from './components/Slider';
 export * from './components/StructuredList';
+export * from './components/Switch';
 export * from './components/Tab';
 export * from './components/TabContent';
 export * from './components/Tabs';
 export * from './components/Tag';
+export * from './components/Tag/Tag.Skeleton';
 export * from './components/TextArea';
 export * from './components/TextInput';
 export * from './components/Tile';
-export * from './components/RadioTile';
 export * from './components/TileGroup';
 export * from './components/TimePicker';
 export * from './components/TimePickerSelect';
 export * from './components/Toggle';
-export * from './components/Toggletip';
-export * from './components/TreeView';
-export * from './components/UnorderedList';
-export * from './components/SkeletonText';
-export * from './components/SkeletonPlaceholder';
-export * from './components/SkeletonIcon';
-export * from './components/DataTableSkeleton';
-export * from './components/Pagination/Pagination.Skeleton';
-export * from './components/RadioButton/RadioButton.Skeleton';
-export * from './components/Tag/Tag.Skeleton';
 export * from './components/Toggle/Toggle.Skeleton';
 export * from './components/ToggleSmall/ToggleSmall.Skeleton';
-export * from './components/Icon/Icon.Skeleton';
+export * from './components/Toggletip';
+export * from './components/TreeView';
 export * from './components/UIShell';
+export * from './components/UnorderedList';
 
 // Experimental
-export { useContextMenu as unstable_useContextMenu } from './components/ContextMenu';
 export {
   FeatureFlags as unstable_FeatureFlags,
   useFeatureFlag as unstable_useFeatureFlag,
@@ -142,17 +145,7 @@ export {
   LayoutDirection as unstable_LayoutDirection,
   useLayoutDirection as unstable_useLayoutDirection,
 } from './components/LayoutDirection';
-export {
-  Menu as unstable_Menu,
-  MenuItem as unstable_MenuItem,
-  MenuItemDivider as unstable_MenuItemDivider,
-  MenuItemGroup as unstable_MenuItemGroup,
-  MenuItemRadioGroup as unstable_MenuItemRadioGroup,
-  MenuItemSelectable as unstable_MenuItemSelectable,
-} from './components/Menu';
 export { OverflowMenuV2 as unstable_OverflowMenuV2 } from './components/OverflowMenuV2';
-export { ComboButton as unstable_ComboButton } from './components/ComboButton';
-export { MenuButton as unstable_MenuButton } from './components/MenuButton';
 export {
   PageSelector as unstable_PageSelector,
   Pagination as unstable_Pagination,
