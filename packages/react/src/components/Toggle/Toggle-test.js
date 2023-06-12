d9cdd9c6569d84e7cf854d9d55de83b64ec8fada
TJ Egan <tw15egan@gmail.com>
2023-06-12 11:37:14 -0400

    chore(Toggle): add screen to import

--- packages/react/src/components/Toggle/Toggle-test.js
+++ packages/react/src/components/Toggle/Toggle-test.js
@@ -7,7 +7,7 @@
 
 import React from 'react';
 import Toggle from './Toggle';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 const prefix = 'cds';
