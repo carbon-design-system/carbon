cb43ff5aee155b776e4e6eca9959db69eb68b8c0
TJ Egan <tw15egan@gmail.com>
2023-06-21 16:39:42 -0400

    test(snapshot): update snapshots

--- packages/type/src/__tests__/fluid-test.js
+++ packages/type/src/__tests__/fluid-test.js
@@ -61,6 +61,7 @@ describe('fluid', () => {
           "letterSpacing": "-0.96px",
           "lineHeight": 1.05,
         },
+        "fontFamily": "'IBM Plex Sans', 'Helvetica Neue', Arial, sans-serif",
         "fontSize": "calc(2.625rem + 1.625 * ((100vw - 20rem) / 22))",
         "fontWeight": 300,
         "letterSpacing": 0,
