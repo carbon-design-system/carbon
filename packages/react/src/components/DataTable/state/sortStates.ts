ab058a36fa55129d8dd4ec350880859e28f06919
David Ragsdale <djragsdale@users.noreply.github.com>
2023-07-10 15:41:31 -0400

    Add types for DataTable (#14149)
    * feat(react): add DataTable types
    
    Add TypeScript types for the DataTable component for better developer experience
    
    Closes #12515
    
    * fix: format & lint
    
    ---------
    
    Co-authored-by: Francine Lucca <40550942+francinelucca@users.noreply.github.com>
    Co-authored-by: Francine Lucca <francinelucca@hotmail.com>
    Co-authored-by: Andrea N. Cardona <cardona.n.andrea@gmail.com>

--- packages/react/src/components/DataTable/state/sortStates.ts
+++ packages/react/src/components/DataTable/state/sortStates.ts
@@ -5,12 +5,14 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+export type DataTableSortState = 'NONE' | 'DESC' | 'ASC';
+
 /**
  * We currently support the following sorting states for DataTable headers,
  * namely: `NONE` for no sorting being applied, and then `DESC` and `ASC` for
  * the corresponding direction of the sorting order.
  */
-export const sortStates = {
+export const sortStates: Record<DataTableSortState, DataTableSortState> = {
   NONE: 'NONE',
   DESC: 'DESC',
   ASC: 'ASC',
