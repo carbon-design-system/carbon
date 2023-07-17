336137291fb7b45225bb8f3f2d5ba7820d53634c
Alison Joseph <alison.joseph@us.ibm.com>
2023-07-17 11:14:09 -0500

    chore(test): add AVT for codesnippet

---
+++ e2e/components/CodeSnippet/CodeSnippet-test.avt.e2e.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright IBM Corp. 2016, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+const { expect, test } = require('@playwright/test');
+const { visitStory } = require('../../test-utils/storybook');
+
+test.describe('CodeSnippet', () => {
+  test('accessibility-checker @avt', async ({ page }) => {
+    await visitStory(page, {
+      component: 'CodeSnippet',
+      id: 'components-codesnippet--inline',
+      globals: {
+        theme: 'white',
+      },
+    });
+    await expect(page).toHaveNoACViolations('CodeSnippet inline');
+  });
+
+  test('accessibility-checker @avt', async ({ page }) => {
+    await visitStory(page, {
+      component: 'CodeSnippet',
+      id: 'components-codesnippet--multiline',
+      globals: {
+        theme: 'white',
+      },
+    });
+    await expect(page).toHaveNoACViolations('CodeSnippet multiline');
+  });
+
+  test('accessibility-checker @avt', async ({ page }) => {
+    await visitStory(page, {
+      component: 'CodeSnippet',
+      id: 'components-codesnippet--singleline',
+      globals: {
+        theme: 'white',
+      },
+    });
+    await expect(page).toHaveNoACViolations('CodeSnippet singleline');
+  });
+
+  test('accessibility-checker @avt', async ({ page }) => {
+    await visitStory(page, {
+      component: 'CodeSnippet',
+      id: 'components-codesnippet--skeleton',
+      globals: {
+        theme: 'white',
+      },
+    });
+    await expect(page).toHaveNoACViolations('CodeSnippet skeleton');
+  });
+});
