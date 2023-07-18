b75fe896ae976f4b592383f0d77ac5c0b17613a8
Alison Joseph <alison.joseph@us.ibm.com>
2023-07-18 10:39:53 -0500

    chore(test): add hover and focus for codesnippet avt

--- e2e/components/CodeSnippet/CodeSnippet-test.avt.e2e.js
+++ e2e/components/CodeSnippet/CodeSnippet-test.avt.e2e.js
@@ -22,16 +22,51 @@ test.describe('CodeSnippet', () => {
     await expect(page).toHaveNoACViolations('CodeSnippet inline');
   });
 
-  //   test('accessibility-checker CodeSnippet multiline @avt', async ({ page }) => {
-  //     await visitStory(page, {
-  //       component: 'CodeSnippet',
-  //       id: 'components-codesnippet--multiline',
-  //       globals: {
-  //         theme: 'white',
-  //       },
-  //     });
-  //     await expect(page).toHaveNoACViolations('CodeSnippet multiline');
-  //   });
+  test('accessibility-checker CodeSnippet inline hover @avt', async ({
+    page,
+  }) => {
+    await visitStory(page, {
+      component: 'CodeSnippet',
+      id: 'components-codesnippet--inline',
+      globals: {
+        theme: 'white',
+      },
+    });
+
+    await expect(page.getByRole('button')).toBeVisible();
+    page.getByRole('button').hover();
+    await expect(page).toHaveNoACViolations('CodeSnippet inline hover');
+  });
+
+  test('accessibility-checker CodeSnippet inline focused @avt', async ({
+    page,
+  }) => {
+    await visitStory(page, {
+      component: 'CodeSnippet',
+      id: 'components-codesnippet--inline',
+      globals: {
+        theme: 'white',
+      },
+    });
+
+    await expect(page.getByRole('button')).toBeVisible();
+    page.getByRole('button').focus();
+    await expect(page).toHaveNoACViolations('CodeSnippet inline focused');
+  });
+
+  // Skipping due to a11y violation issue #14237
+  test.skip('accessibility-checker CodeSnippet multiline @avt', async ({
+    page,
+  }) => {
+    await visitStory(page, {
+      component: 'CodeSnippet',
+      id: 'components-codesnippet--multiline',
+      globals: {
+        theme: 'white',
+      },
+    });
+    await expect(page).toHaveNoACViolations('CodeSnippet multiline');
+  });
 
   test('accessibility-checker CodeSnippet singleline @avt', async ({
     page,
