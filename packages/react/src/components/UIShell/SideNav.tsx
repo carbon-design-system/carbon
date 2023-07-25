236ef66c2fa16568fe24716870cfd617703d1eeb
guidari <guilhermedatilio@gmail.com>
2023-07-25 12:12:00 -0300

    fix: remove isRail from children

--- packages/react/src/components/UIShell/SideNav.tsx
+++ packages/react/src/components/UIShell/SideNav.tsx
@@ -143,7 +143,6 @@ function SideNavRenderFunction(
         )
           ? {
               isSideNavExpanded: currentExpansionState,
-              isRail,
             }
           : {}),
       });
