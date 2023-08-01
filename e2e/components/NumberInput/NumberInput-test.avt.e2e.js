e798b159445fb334c64c2589b0102dc6ca522650
TJ Egan <tw15egan@gmail.com>
2023-08-01 14:48:22 -0400

    test(NumberInput): add @avt tests

---
+++ e2e/components/NumberInput/NumberInput-test.avt.e2e.js
@@ -0,0 +1,78 @@
+/**
+ * Copyright IBM Corp. 2016, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+const { expect, test } = require('@playwright/test');
+const { visitStory } = require('../../test-utils/storybook');
+
+test.describe('NumberInput @avt', () => {
+  test.skip('accessibility-checker default', async ({ page }) => {
+    await visitStory(page, {
+      component: 'NumberInput',
+      id: 'components-numberinput--default',
+      globals: {
+        theme: 'white',
+      },
+    });
+    await expect(page).toHaveNoACViolations('components-numberinput--default');
+  });
+
+  test.skip('accessibility-checker skeleton', async ({ page }) => {
+    await visitStory(page, {
+      component: 'NumberInput',
+      id: 'components-numberinput--skeleton',
+      globals: {
+        theme: 'white',
+      },
+    });
+    await expect(page).toHaveNoACViolations('components-numberinput--skeleton');
+  });
+
+  test('number input - keyboard nav', async ({ page }) => {
+    await visitStory(page, {
+      component: 'NumberInput',
+      id: 'components-numberinput--default',
+      globals: {
+        theme: 'white',
+      },
+    });
+
+    const input = page.getByRole('spinbutton');
+    const increment = page.getByRole('button', {
+      name: 'Increment number',
+    });
+
+    const decrement = page.getByRole('button', {
+      name: 'Decrement number',
+    });
+
+    await expect(input).toBeVisible();
+    await expect(input).toHaveValue('50');
+
+    // Tab to the NumberInput and receive focus
+    await page.keyboard.press('Tab');
+    await expect(input).toBeFocused();
+
+    // Increase value with ArrowUp
+    await page.keyboard.press('ArrowUp');
+    await expect(input).toHaveValue('51');
+
+    // Decrease value with ArrowDown
+    await page.keyboard.press('ArrowDown');
+    await expect(input).toHaveValue('50');
+
+    // Increase value with increment button
+    await increment.click();
+    await expect(input).toHaveValue('51');
+
+    // Decrease value with decrement button
+    await decrement.click();
+    await expect(input).toHaveValue('50');
+    await expect(input).not.toBeFocused();
+  });
+});
