b4a65d67097f340c70bbe07c68455258d46b27c5
TJ Egan <tw15egan@gmail.com>
2023-08-02 13:26:17 -0400

    test(List): add @avt tests (#14363)
    Co-authored-by: Guilherme Datilio Ribeiro <guilhermedatilio@gmail.com>

--- e2e/components/UnorderedList/UnorderedList-test.e2e.js
+++ e2e/components/UnorderedList/UnorderedList-test.e2e.js
@@ -7,9 +7,9 @@
 
 'use strict';
 
-const { expect, test } = require('@playwright/test');
+const { test } = require('@playwright/test');
 const { themes } = require('../../test-utils/env');
-const { snapshotStory, visitStory } = require('../../test-utils/storybook');
+const { snapshotStory } = require('../../test-utils/storybook');
 
 test.describe('UnorderedList', () => {
   themes.forEach((theme) => {
@@ -30,16 +30,5 @@ test.describe('UnorderedList', () => {
         });
       });
     });
-  });
-
-  test('accessibility-checker @avt', async ({ page }) => {
-    await visitStory(page, {
-      component: 'UnorderedList',
-      id: 'components-unorderedlist--default',
-      globals: {
-        theme: 'white',
-      },
-    });
-    await expect(page).toHaveNoACViolations('UnorderedList');
   });
 });
