dfa081c8af3583f72e8bbcb9ceaf652d8a0b6d58
kennylam <909118+kennylam@users.noreply.github.com>
2023-08-04 13:15:30 -0400

    feat(package): update packages to Carbon v11 (#10779)
    * chore(v11): update tokens, mixins and imports to v11
    
    * chore(styles): upgrade web components to v11
    
    * chore(styles): update import paths to local
    
    * chore(docs): remove unneeded styles readme
    
    * chore(styles): update web-components style imports
    
    * chore(styles): update dotcom wc to v11
    
    * chore(gulp): add watch task to wc
    
    * chore(styles): remove plex css for now
    
    * chore(styles): update tokens
    
    * chore(styles): update web components to v11
    
    * chore(project): bump package versions; remove react stuff
    
    * chore(deps): bump beta package versions
    
    * chore(styles): add missing import
    
    * chore(deps): add @carbon/react
    
    * chore(test): ignore certain tests for now
    
    * chore(styles): tokens for everybody
    
    * chore(storybook): update container tokens
    
    * chore(deps): update package settings
    
    * chore(storybook): fix css imports
    
    * chore(storybook): fix react-wrapper sb
    
    * chore(prefix): update prefix to cds
    
    * chore(deps): update styles and snapshots
    
    * chore(deps): update yarn.lock
    
    * chore(license): update file license

--- web-components/packages/carbon-web-components/src/globals/decorators/carbon-element.ts
+++ web-components/packages/carbon-web-components/src/globals/decorators/carbon-element.ts
@@ -1,7 +1,10 @@
 /**
  * @license
- * Copyright 2017 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright IBM Corp. 2019, 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 /*
