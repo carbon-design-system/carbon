7f9119b751d09f36441672c745e962424700ec0b
guidari <guilhermedatilio@gmail.com>
2023-08-14 13:01:10 -0300

    test: added avt test

--- e2e/components/accordion/accordion-test.e2e.js
+++ e2e/components/accordion/accordion-test.e2e.js
@@ -7,9 +7,8 @@
 
 'use strict';
 
-const { expect, test } = require('@playwright/test');
 const { themes } = require('../../test-utils/env');
-const { snapshotStory, visitStory } = require('../../test-utils/storybook');
+const { snapshotStory } = require('../../test-utils/storybook');
 
 test.describe('accordion', () => {
   themes.forEach((theme) => {
@@ -22,16 +21,5 @@ test.describe('accordion', () => {
         });
       });
     });
-  });
-
-  test('accessibility-checker @avt', async ({ page }) => {
-    await visitStory(page, {
-      component: 'accordion',
-      story: 'default',
-      globals: {
-        theme: 'white',
-      },
-    });
-    await expect(page).toHaveNoACViolations('accordion');
   });
 });
