00960a162be2c2fb1da09451c91d517fc23500cf
guidari <guilhermedatilio@gmail.com>
2023-08-17 12:25:04 -0300

    fix: changed aria-busy position

--- packages/react/src/components/StructuredList/StructuredList.stories.js
+++ packages/react/src/components/StructuredList/StructuredList.stories.js
@@ -105,7 +105,7 @@ Default.argTypes = {
 export const Selection = (args) => {
   const structuredListBodyRowGenerator = (numRows) => {
     return Array.apply(null, Array(numRows)).map((n, i) => (
-      <StructuredListRow key={`row-${i}`} aria-busy="true">
+      <StructuredListRow key={`row-${i}`}>
         <StructuredListCell>Row {i}</StructuredListCell>
         <StructuredListCell>Row {i}</StructuredListCell>
         <StructuredListCell>
