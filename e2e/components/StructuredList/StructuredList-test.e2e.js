4e4df516382664e43e7507b80016f92e5a937b10
Guilherme Datilio Ribeiro <guilhermedatilio@gmail.com>
2023-08-28 09:59:30 -0300

    [AVT] - StructuredList test (#14375)
    * test: added avt test
    
    * test: added AVT tests
    
    * fix: added aria-label
    
    * fix: changed aria-busy position
    
    * test: fixed version in esbuild
    
    * test: sanpshot e2e updated
    
    * test: updated snapshot

--- e2e/components/StructuredList/StructuredList-test.e2e.js
+++ e2e/components/StructuredList/StructuredList-test.e2e.js
@@ -7,9 +7,9 @@
 
 'use strict';
 
-const { expect, test } = require('@playwright/test');
+const { test } = require('@playwright/test');
 const { themes } = require('../../test-utils/env');
-const { snapshotStory, visitStory } = require('../../test-utils/storybook');
+const { snapshotStory } = require('../../test-utils/storybook');
 
 test.describe('StructuredList', () => {
   themes.forEach((theme) => {
@@ -30,16 +30,5 @@ test.describe('StructuredList', () => {
         });
       });
     });
-  });
-
-  test('accessibility-checker @avt', async ({ page }) => {
-    await visitStory(page, {
-      component: 'StructuredList',
-      id: 'components-structuredlist--default',
-      globals: {
-        theme: 'white',
-      },
-    });
-    await expect(page).toHaveNoACViolations('StructuredList');
   });
 });
