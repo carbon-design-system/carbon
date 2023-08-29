b23820c980a634779d0e400eac7182635a870a66
TJ Egan <tw15egan@gmail.com>
2023-08-29 09:09:02 -0400

    test(TreeView): add @avt tests (#14512)
    Co-authored-by: Guilherme Datilio Ribeiro <guilhermedatilio@gmail.com>

---
+++ e2e/components/TreeView/TreeView-test.avt.e2e.js
@@ -0,0 +1,101 @@
+/**
+ * Copyright IBM Corp. 2016, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+const { expect, test } = require('@playwright/test');
+const { visitStory } = require('../../test-utils/storybook');
+
+test.describe('TreeView @avt', () => {
+  test.skip('accessibility-checker default', async ({ page }) => {
+    await visitStory(page, {
+      component: 'TreeView',
+      id: 'components-treeview--default',
+      globals: {
+        theme: 'white',
+      },
+    });
+    await expect(page).toHaveNoACViolations('components-treeview--default');
+  });
+
+  test.skip('accessibility-checker with-icons', async ({ page }) => {
+    await visitStory(page, {
+      component: 'TreeView',
+      id: 'components-treeview--with-icons',
+      globals: {
+        theme: 'white',
+      },
+    });
+    await expect(page).toHaveNoACViolations('components-treeview--with-icons');
+  });
+
+  test('treeview - keyboard nav', async ({ page }) => {
+    await visitStory(page, {
+      component: 'TreeView',
+      id: 'components-treeview--with-icons',
+      globals: {
+        theme: 'white',
+      },
+    });
+    const tree = page.getByRole('tree');
+    const treeLeaf = page.getByRole('treeitem', {
+      name: 'Artificial intelligence',
+    });
+    const treeBranchOne = page.getByRole('treeitem', {
+      name: 'Cloud computing',
+    });
+    const treeBranchTwo = page.getByRole('treeitem', {
+      name: 'Data & Analytics',
+    });
+    const deepNode = page.getByRole('treeitem', {
+      name: 'Resources',
+    });
+
+    await expect(tree).toBeVisible();
+    await expect(treeLeaf).toBeVisible();
+    await expect(treeBranchOne).toBeVisible();
+    await expect(treeBranchTwo).toBeVisible();
+    await expect(deepNode).toBeVisible();
+
+    // Tab to the first item
+    await page.keyboard.press('Tab');
+    await expect(treeLeaf).toBeFocused();
+    // Second tab should tab off of the tree entirely
+    await page.keyboard.press('Tab');
+    await expect(treeLeaf).not.toBeFocused();
+    await expect(tree).not.toBeFocused();
+    // Tab back into tree, navigate with arrow keys
+    await page.keyboard.press('Shift+Tab');
+    await expect(treeLeaf).toBeFocused();
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+    await expect(treeBranchOne).toBeFocused();
+    // Close menu with ArrowLeft
+    await expect(treeBranchOne).toHaveAttribute('aria-expanded', 'true');
+    await page.keyboard.press('ArrowLeft');
+    await expect(treeBranchOne).toHaveAttribute('aria-expanded', 'false');
+    // Down arrow should go to next branch when closed, and should not go to disabled item
+    await page.keyboard.press('ArrowDown');
+    await expect(treeBranchTwo).toBeFocused();
+    await page.keyboard.press('ArrowDown');
+    await expect(treeBranchTwo).toBeFocused();
+    // Go back up to collapsed section, expand it, and traverse to bottom of branch
+    await page.keyboard.press('ArrowUp');
+    await expect(treeBranchOne).toBeFocused();
+    await page.keyboard.press('ArrowRight');
+    await expect(treeBranchOne).toHaveAttribute('aria-expanded', 'true');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+    await expect(deepNode).toBeFocused();
+  });
+});
