71c446322406859fd3151d666edc435c1ae8aeed
guidari <guilhermedatilio@gmail.com>
2023-09-12 11:38:01 -0300

    test: added keyboard navigation test

---
+++ e2e/components/breadcrumb/breadcrumb-test.avt.e2e.js
@@ -0,0 +1,116 @@
+/**
+ * Copyright IBM Corp. 2016, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+import { expect, test } from '@playwright/test';
+import { visitStory } from '../../test-utils/storybook';
+
+test.describe('breadcrumb @avt', () => {
+  test('accessibility-checker', async ({ page }) => {
+    await visitStory(page, {
+      component: 'Breadcrumb',
+      id: 'components-breadcrumb--default',
+      globals: {
+        theme: 'white',
+      },
+    });
+    await expect(page).toHaveNoACViolations('Breadcrumb');
+  });
+
+  test('accessibility-checker with overflow menu', async ({ page }) => {
+    await visitStory(page, {
+      component: 'Breadcrumb',
+      id: 'components-breadcrumb--breadcrumb-with-overflow-menu',
+      globals: {
+        theme: 'white',
+      },
+    });
+    await expect(page).toHaveNoACViolations('Breadcrumb-with-overflow-menu');
+  });
+
+  test('accessibility-checker skeleton', async ({ page }) => {
+    await visitStory(page, {
+      component: 'Breadcrumb',
+      id: 'components-breadcrumb--skeleton',
+      globals: {
+        theme: 'white',
+      },
+    });
+    await expect(page).toHaveNoACViolations('Breadcrumb-skeleton');
+  });
+
+  test('accessibility-checker keyboard navigation', async ({ page }) => {
+    await visitStory(page, {
+      component: 'Breadcrumb',
+      id: 'components-breadcrumb--default',
+      globals: {
+        theme: 'white',
+      },
+    });
+    await expect(page.getByText('Breadcrumb 1')).toBeVisible();
+    await page.keyboard.press('Tab');
+    await expect(page.getByText('Breadcrumb 1')).toBeFocused();
+
+    // Testing link. For this test pass the href must be like that: href="#"
+    await page.keyboard.press('Enter');
+    await expect(page).toHaveURL(
+      '?path=/docs/getting-started-welcome--welcome'
+    );
+  });
+
+  test('accessibility-checker keyboard navigation - item without href prop', async ({
+    page,
+  }) => {
+    await visitStory(page, {
+      component: 'Breadcrumb',
+      id: 'components-breadcrumb--default',
+      globals: {
+        theme: 'white',
+      },
+    });
+    await expect(page.getByText('Breadcrumb 3')).toBeVisible();
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await expect(page.getByText('Breadcrumb 3')).toBeFocused();
+
+    // The Breadcrumb 4 should not be focusable
+    await page.keyboard.press('Tab');
+    await expect(page.getByText('Breadcrumb 4')).not.toBeFocused();
+  });
+
+  test('accessibility-checker with overflow menu keyboard navigation', async ({
+    page,
+  }) => {
+    await visitStory(page, {
+      component: 'Breadcrumb',
+      id: 'components-breadcrumb--breadcrumb-with-overflow-menu',
+      globals: {
+        theme: 'white',
+      },
+    });
+
+    await expect(page.getByText('Breadcrumb 1')).toBeVisible();
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+
+    // Verify icon-description
+    await expect(page.getByText('Options')).toBeVisible();
+
+    // Entering and navigating the options
+    await page.keyboard.press('Enter');
+    await expect(
+      page.locator('button', { hasText: 'Breadcrumb 3' })
+    ).toBeFocused();
+    await page.keyboard.press('ArrowDown');
+    await expect(
+      page.locator('button', { hasText: 'Breadcrumb 4' })
+    ).toBeFocused();
+  });
+});
