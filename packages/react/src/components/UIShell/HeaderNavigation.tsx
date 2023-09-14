0b4160dfd5e4e19f225c07bd9f024edd6722428c
Ian <52504170+ibacher@users.noreply.github.com>
2023-09-14 15:07:21 -0400

    fix(UIShell): Export prop types for components

--- packages/react/src/components/UIShell/HeaderNavigation.tsx
+++ packages/react/src/components/UIShell/HeaderNavigation.tsx
@@ -11,7 +11,7 @@ import PropTypes from 'prop-types';
 import { AriaLabelPropType } from '../../prop-types/AriaPropTypes';
 import { usePrefix } from '../../internal/usePrefix';
 
-type HeaderNavigationProps = ComponentProps<'nav'>;
+export interface HeaderNavigationProps extends ComponentProps<'nav'> {};
 
 export default function HeaderNavigation({
   'aria-label': ariaLabel,
