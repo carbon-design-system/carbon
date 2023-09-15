4f2c38e430aa24b314fcc39049a88f05917da4d9
Taylor Jones <taylor.jones826@gmail.com>
2023-09-15 08:52:09 -0500

    chore: remove use strict

--- actions/promote/index.js
+++ actions/promote/index.js
@@ -4,9 +4,6 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
-'use strict';
-
 import core from '@actions/core';
 import execa from 'execa';
 import glob from 'fast-glob';
