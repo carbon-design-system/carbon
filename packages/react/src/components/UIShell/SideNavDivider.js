4334e096a3d0eb451dad7ad5ad3b60d3fb4a1ab7
TJ Egan <tw15egan@gmail.com>
2023-09-27 12:19:23 -0400

    fix(UIShell): wrap hr in li

--- packages/react/src/components/UIShell/SideNavDivider.js
+++ packages/react/src/components/UIShell/SideNavDivider.js
@@ -13,7 +13,11 @@ import { usePrefix } from '../../internal/usePrefix';
 function SideNavDivider({ className }) {
   const prefix = usePrefix();
   const classNames = cx(`${prefix}--side-nav__divider`, className);
-  return <hr className={classNames} />;
+  return (
+    <li className={classNames}>
+      <hr />
+    </li>
+  );
 }
 
 SideNavDivider.propTypes = {
