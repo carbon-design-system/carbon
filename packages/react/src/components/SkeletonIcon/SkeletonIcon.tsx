7867ef3d588d840b782850cc37680678ffa7196b
Aman Lajpal <alnaviation@gmail.com>
2023-10-05 22:45:27 +0530

    refactor: added TypeScript types to SkeletonIcon

--- packages/react/src/components/SkeletonIcon/SkeletonIcon.tsx
+++ packages/react/src/components/SkeletonIcon/SkeletonIcon.tsx
@@ -5,32 +5,31 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
 import { usePrefix } from '../../internal/usePrefix';
 
-const SkeletonIcon = ({ className, ...other }) => {
-  const prefix = usePrefix();
-
-  const skeletonIconClasses = classNames({
-    [`${prefix}--icon--skeleton`]: true,
-    [className]: className,
-  });
-
-  return <div className={skeletonIconClasses} {...other} />;
-};
-
-SkeletonIcon.propTypes = {
+interface SkeletonIconProps {
   /**
    * Specify an optional className to add.
    */
-  className: PropTypes.string,
+  className?: string;
 
   /**
    * The CSS styles.
    */
-  style: PropTypes.object,
+  style?: React.CSSProperties;
+}
+
+const SkeletonIcon: React.FC<SkeletonIconProps> = ({ className, ...other }) => {
+  const prefix = usePrefix();
+
+  const skeletonIconClasses = classNames({
+    [`${prefix}--icon--skeleton`]: true,
+    [className!]: className,
+  });
+
+  return <div className={skeletonIconClasses} {...other} />;
 };
 
 export default SkeletonIcon;
