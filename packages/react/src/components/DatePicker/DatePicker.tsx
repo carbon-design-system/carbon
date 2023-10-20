c6fbade12b7ccddd5f6fe9f36b3144ee6c77440c
Omkar-Ajagunde <omkar.ajagunde@ibm.com>
2023-10-20 11:12:50 +0530

    updated allowInput description

--- packages/react/src/components/DatePicker/DatePicker.tsx
+++ packages/react/src/components/DatePicker/DatePicker.tsx
@@ -825,11 +825,8 @@ const DatePicker = React.forwardRef(function DatePicker(
 
 DatePicker.propTypes = {
   /**
-   * flatpickr prop passthrough. Allows the user to enter a date directly
-   * into the input field, Note: If we set this flag to false and a user picks a date,
-   * they won't be able to remove it by just clicking on it.
-   * Instead, you'll need to set the value to an empty string yourself if you want to clear the date.
-   * This will make the input behave like a controlled form element.
+   * Flatpickr prop passthrough enables direct date input, and when set to false,
+   * we must clear dates manually by resetting the value prop to empty string making it a controlled input.
    */
   allowInput: PropTypes.bool,
 
