f781b95223db453a6596cd09175ca084b1a31936
TJ Egan <tw15egan@gmail.com>
2023-10-24 14:28:36 -0400

    chore(snapshot): update snapshots

--- packages/themes/src/tokens/__tests__/metadata-test.js
+++ packages/themes/src/tokens/__tests__/metadata-test.js
@@ -926,6 +926,26 @@ test('metadata', () => {
           "type": "color",
         },
         Object {
+          "name": "slug-background",
+          "type": "color",
+        },
+        Object {
+          "name": "slug-gradient",
+          "type": "color",
+        },
+        Object {
+          "name": "slug-background-hover",
+          "type": "color",
+        },
+        Object {
+          "name": "slug-gradient-hover",
+          "type": "color",
+        },
+        Object {
+          "name": "slug-hollow-hover",
+          "type": "color",
+        },
+        Object {
           "name": "highlight",
           "type": "color",
         },
