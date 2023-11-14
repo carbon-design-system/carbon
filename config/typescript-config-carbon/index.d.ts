4683609cd25c18e525a364429ae84f9fe819e282
guidari <guilhermedatilio@gmail.com>
2023-11-14 16:28:27 -0300

    fix: removed wrong files

--- config/typescript-config-carbon/index.d.ts
+++
@@ -1,4 +0,0 @@
-import ts from 'typescript';
-export declare const diagnosticToMessage: (diagnostic: ts.Diagnostic) => string;
-export declare const loadTsCompilerOpts: (path: string) => ts.CompilerOptions;
-export declare const loadBaseTsCompilerOpts: () => ts.CompilerOptions;
