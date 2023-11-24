140d7fadedfe90f4738abdfe30c63c59a0816e59
Alisa Romankova <alissa9090@mail.ru>
2023-11-24 20:36:08 +0100

    refactor(InlineLoading): add TypeScript types

--- packages/react/src/components/InlineLoading/InlineLoading.tsx
+++ packages/react/src/components/InlineLoading/InlineLoading.tsx
@@ -6,21 +6,57 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { CheckmarkFilled, ErrorFilled } from '@carbon/icons-react';
 import Loading from '../Loading';
 import { usePrefix } from '../../internal/usePrefix';
 
-export default function InlineLoading({
+export interface InlineLoadingProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
+  /**
+   * Specify a custom className to be applied to the container node
+   */
+  className?: string;
+
+  /**
+   * Specify the description for the inline loading text
+   */
+  description?: React.ReactNode;
+
+  /**
+   * Specify the description for the inline loading text
+   */
+  iconDescription?: string;
+
+  /**
+   * Provide an optional handler to be invoked when <InlineLoading> is
+   * successful
+   */
+  onSuccess?: (...args: unknown[]) => unknown;
+
+  /**
+   * Specify the loading status
+   */
+  status?: 'inactive' | 'active' | 'finished' | 'error';
+
+  /**
+   * Provide a delay for the `setTimeout` for success
+   */
+  successDelay?: number;
+}
+
+const InlineLoading = ({
   className,
   status = 'active',
   iconDescription,
   description,
   onSuccess,
   successDelay = 1500,
-  ...other
-}) {
+  ...divProps
+}: InlineLoadingProps) => {
   const prefix = usePrefix();
   const loadingClasses = classNames(`${prefix}--inline-loading`, className);
   const getLoading = () => {
@@ -70,43 +106,12 @@ export default function InlineLoading({
   return (
     <div
       className={loadingClasses}
-      {...other}
-      aria-live={'assertive' || other['aria-live']}>
+      {...divProps}
+      aria-live={'assertive' || divProps['aria-live']}>
       {loadingAnimation}
       {description && loadingText}
     </div>
   );
-}
-
-InlineLoading.propTypes = {
-  /**
-   * Specify a custom className to be applied to the container node
-   */
-  className: PropTypes.string,
-
-  /**
-   * Specify the description for the inline loading text
-   */
-  description: PropTypes.node,
-
-  /**
-   * Specify the description for the inline loading text
-   */
-  iconDescription: PropTypes.string,
-
-  /**
-   * Provide an optional handler to be invoked when <InlineLoading> is
-   * successful
-   */
-  onSuccess: PropTypes.func,
-
-  /**
-   * Specify the loading status
-   */
-  status: PropTypes.oneOf(['inactive', 'active', 'finished', 'error']),
-
-  /**
-   * Provide a delay for the `setTimeout` for success
-   */
-  successDelay: PropTypes.number,
 };
+
+export default InlineLoading;
