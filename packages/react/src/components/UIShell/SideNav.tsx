4e292274265d703a362f5636f2d24bcdea092365
guidari <guilhermedatilio@gmail.com>
2023-12-06 17:18:37 -0300

    fix: fixed menu open state

--- packages/react/src/components/UIShell/SideNav.tsx
+++ packages/react/src/components/UIShell/SideNav.tsx
@@ -145,6 +145,7 @@ function SideNavRenderFunction(
         )
           ? {
               isSideNavExpanded: currentExpansionState,
+              isFixedNav,
             }
           : {}),
       });
