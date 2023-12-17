14d837fe932fe4ff44d3c7dff08f426232db3733
Matias Borghi <borghi.matias@gmail.com>
2023-12-16 22:03:39 -0300

    refactor: add types for SecondaryButton

--- packages/react/src/components/SecondaryButton/SecondaryButton.tsx
+++ packages/react/src/components/SecondaryButton/SecondaryButton.tsx
@@ -6,8 +6,10 @@
  */
 
 import React from 'react';
-import Button from '../Button';
+import { Button, ButtonProps } from '../Button';
 
-const SecondaryButton = (props) => <Button kind="secondary" {...props} />;
+const SecondaryButton = <T extends React.ElementType>(
+  props: ButtonProps<T>
+) => <Button kind="secondary" {...props} />;
 
 export default SecondaryButton;
