cbab49072a8faf09564d3ddc26eb16bc114642cc
IgnacioBecerra <i1becerr@ucsd.edu>
2023-12-20 09:49:06 -0800

    feat(icon): updated to v7

--- web-components/packages/carbon-web-components/src/components/icon/icon.stories.ts
+++ web-components/packages/carbon-web-components/src/components/icon/icon.stories.ts
@@ -20,14 +20,17 @@ import Add24 from '@carbon/web-components/es/icons/add/24';
 // @ts-ignore
 import Add32 from '@carbon/web-components/es/icons/add/32';
 
-import storyDocs from './icon-story.mdx';
+import storyDocs from './icon.mdx';
 
-export const Default = () =>
-  html` ${Add16()} ${Add20()} ${Add24()} ${Add32()} `;
 
-Default.storyName = 'Default';
+export const Default = {
+  render: () => html` ${Add16()} ${Add20()} ${Add24()} ${Add32()} `
+}
 
-export const withCustomClass = () => html`
+
+export const withCustomClass = {
+  name: 'With custom class',
+  render: () => html`
   <style>
     .test-class {
       fill: #0062ff;
@@ -35,18 +38,20 @@ export const withCustomClass = () => html`
   </style>
   ${Add16({ class: 'test-class' })} ${Add20({ class: 'test-class' })}
   ${Add24({ class: 'test-class' })} ${Add32({ class: 'test-class' })}
-`;
+`
+}
 
-withCustomClass.storyName = 'With custom class';
-
-export const withAriaLabel = () => html`
+export const withAriaLabel = {
+  name: 'With aria label',
+  render: () => html`
   ${Add16({ 'aria-label': 'add' })} ${Add20({ 'aria-label': 'add' })}
   ${Add24({ 'aria-label': 'add' })} ${Add32({ 'aria-label': 'add' })}
-`;
+`
+}
 
-withAriaLabel.storyName = 'With aria-label';
-
-export const withTitle = () => html`
+export const withTitle =  {
+  name: 'With title',
+  render: () => html`
   ${Add16({
     'aria-describedby': 'id-title-1',
     // @ts-ignore
@@ -67,13 +72,16 @@ export const withTitle = () => html`
     // @ts-ignore
     children: svg`<title id="id-title-4">add</title>`,
   })}
-`;
+`
+}
 
-withTitle.storyName = 'With title';
-
-export default {
+const meta = {
   title: 'Components/Icon',
   parameters: {
-    ...storyDocs.parameters,
+    docs: {
+      page: storyDocs,
+    },
   },
 };
+
+export default meta;
