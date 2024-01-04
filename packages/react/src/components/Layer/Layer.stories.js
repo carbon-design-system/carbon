faf3686d06585f84838f5411b320d5bb5ddf08fe
TJ Egan <tw15egan@gmail.com>
2024-01-04 11:22:37 -0500

    chore(storybook): remove test story

--- packages/react/src/components/Layer/Layer.stories.js
+++ packages/react/src/components/Layer/Layer.stories.js
@@ -9,7 +9,6 @@ import './Layer-story.scss';
 import React from 'react';
 import { Layer, useLayer } from '../Layer';
 import mdx from './Layer.mdx';
-import { Theme } from '../Theme';
 
 export default {
   title: 'Components/Layer',
@@ -111,65 +110,3 @@ const PlaygroundStory = (args) => {
 };
 
 export const Playground = PlaygroundStory.bind({});
-
-function Demo({ children }) {
-  return (
-    <div
-      style={{
-        background: 'var(--cds-layer)',
-        border: '0.0625rem solid var(--cds-border-subtle)',
-        blockSize: '4rem',
-        padding: '1rem',
-      }}>
-      Element on {children}
-    </div>
-  );
-}
-
-const themes = ['white', 'g10', 'g90', 'g100'];
-
-const Test = () => {
-  return (
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: '1fr '.repeat(themes.length),
-      }}>
-      {themes.map((theme) => (
-        <Theme theme={theme} key={theme}>
-          <div
-            style={{
-              backgroundColor: 'var(--cds-background)',
-              padding: '1rem',
-            }}>
-            <Demo>{`${theme} layer 1`}</Demo>
-          </div>
-
-          <div
-            style={{
-              backgroundColor: 'var(--cds-layer)',
-              padding: '1rem',
-            }}>
-            <Layer>
-              <Demo>{`${theme} layer 2`}</Demo>
-            </Layer>
-          </div>
-
-          <Layer>
-            <div
-              style={{
-                backgroundColor: 'var(--cds-layer)',
-                padding: '1rem',
-              }}>
-              <Layer>
-                <Demo>{`${theme} layer 3`}</Demo>
-              </Layer>
-            </div>
-          </Layer>
-        </Theme>
-      ))}
-    </div>
-  );
-};
-
-export const TestStory = Test.bind({});
