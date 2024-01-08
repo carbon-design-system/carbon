88857d5e9292115e7f80d19b50f6511f432f1eb1
guidari <guilhermedatilio@gmail.com>
2024-01-08 15:07:50 -0300

    fix: added different code from the lib

--- packages/react/src/internal/shortAndSweet.js
+++ packages/react/src/internal/shortAndSweet.js
@@ -1,130 +1,181 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
+/* eslint-disable curly */
 /**
  * short-and-sweet v1.0.4 - Accessible character counter for input elements
  * Copyright (c) 2021 Rik Schennink <rik@pqina.nl> (https://pqina.nl/)
  * https://github.com/rikschennink/short-and-sweet/tree/master
  */
 
-!(function (t, e) {
-  'object' == typeof exports && 'undefined' != typeof module
-    ? (module.exports = e())
-    : 'function' == typeof define && define.amd
-    ? define(e)
-    : ((t =
-        'undefined' != typeof globalThis
-          ? globalThis
-          : t || self).shortAndSweet = e());
-})(this, function () {
-  'use strict';
-  return (function (t) {
-    if (typeof window === 'undefined' ? null : window) {
-      var e = function (t) {
-          var e =
-              arguments.length > 1 && void 0 !== arguments[1]
-                ? arguments[1]
-                : {},
-            n = document.createElement(t);
-          return (
-            Object.keys(e).forEach(function (t) {
-              n.setAttribute(t, e[t]);
-            }),
-            n
-          );
-        },
-        n = 0,
-        i = function (t, i) {
-          if (t.getAttribute('maxlength')) {
-            var a = t.dataset.counterLabel || i.counterLabel,
-              r = (function (t) {
-                var i = t.assistDelay,
-                  a = t.counterClassName,
-                  r = e('span', { class: a, 'aria-hidden': 'true' }),
-                  o = e('span', {
-                    style:
-                      'position:absolute;overflow:hidden;height:1px;width:1px;padding:0;border:0;clip:rect(1px, 1px, 1px, 1px);clip-path:inset(50%);white-space:nowrap;',
-                    id: 'short-and-sweet-counter-'.concat(n++),
-                    role: 'status',
-                    'aria-live': 'polite',
-                  }),
-                  s = document.createDocumentFragment();
-                s.appendChild(r), s.appendChild(o);
-                var u = null;
-                return {
-                  id: o.id,
-                  fragment: s,
-                  updateText: function (t) {
-                    r.textContent = t;
-                  },
-                  updateAssist: function (t, e) {
-                    clearTimeout(u),
-                      e
-                        ? (o.textContent = t)
-                        : (u = setTimeout(function () {
-                            o.textContent = t;
-                          }, i));
-                  },
-                  syncAssist: function () {
-                    clearTimeout(u), (o.textContent = r.textContent);
-                  },
-                  resetAssist: function () {
-                    clearTimeout(u), (o.textContent = '');
-                  },
-                };
-              })(i);
-            t.setAttribute('aria-controls', r.id), i.append(t, r.fragment);
-            var o = !0,
-              s = function () {
-                var e,
-                  n =
-                    null != (e = t.getAttribute('maxlength'))
-                      ? parseInt(e, 10)
-                      : null,
-                  i = t.value.length > n;
-                i &&
-                  (function (e) {
-                    t.value = t.value.substr(0, e);
-                  })(n);
-                var s,
-                  u = t.value.length,
-                  l =
-                    ((s = { maxlength: n, length: u, remaining: n - u }),
-                    a.replace(/(?:{([a-zA-Z]+)})/g, function (t, e) {
-                      return s[e];
-                    }));
-                r.updateText(l), o ? (o = !1) : r.updateAssist(l, i);
-              };
-            t.addEventListener('focus', r.syncAssist),
-              t.addEventListener('blur', r.resetAssist),
-              t.addEventListener('input', s),
-              s();
-          }
-        },
-        a = {
-          counterClassName: 'short-and-sweet-counter',
-          counterLabel: '{remaining} characters left',
-          assistDelay: 2e3,
-          append: function (t, e) {
-            t.parentNode.appendChild(e);
-          },
-        },
-        r = function (t) {
-          var e =
-            arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : {};
-          return t.map(function (t) {
-            return i(t, Object.assign({}, a, e));
-          });
-        };
-      return function (t, e) {
-        return 'string' == typeof t
-          ? r(
-              ((n = document.querySelectorAll(t)),
-              Array.prototype.slice.call(n)),
-              e
-            )
-          : r([t], e)[0];
-        var n;
-      };
-    }
-  })();
-});
+export default ((w) => {
+  // no window, early exit
+  if (!w) return;
+
+  const toArray = (nodeList) => Array.prototype.slice.call(nodeList);
+
+  const h = (name, attr = {}) => {
+    const element = document.createElement(name);
+    Object.keys(attr).forEach((key) => {
+      element.setAttribute(key, attr[key]);
+    });
+    return element;
+  };
+
+  const replaceInString = (string, replacements) =>
+    string.replace(/(?:{([a-zA-Z]+)})/g, (match, group) => replacements[group]);
+
+  const toInt = (v) => (v != null ? parseInt(v, 10) : null);
+
+  let uid = 0;
+
+  const createCounter = ({ assistDelay, counterClassName }) => {
+    // create visual counter node
+    const counterVisual = h('span', {
+      class: counterClassName,
+      'aria-hidden': 'true',
+    });
+
+    // create assist node
+    const counterAssistant = h('span', {
+      style:
+        'position:absolute;overflow:hidden;height:1px;width:1px;padding:0;border:0;clip:rect(1px, 1px, 1px, 1px);clip-path:inset(50%);white-space:nowrap;',
+      id: `short-and-sweet-counter-${uid++}`,
+      role: 'status',
+      'aria-live': 'polite',
+    });
+
+    // we'll group the counter and assistant in a fragment so we can add both nodes in one go
+    const fragment = document.createDocumentFragment();
+    fragment.appendChild(counterVisual);
+    fragment.appendChild(counterAssistant);
+
+    // updates the counter
+    const updateText = (text) => {
+      counterVisual.textContent = text;
+    };
+
+    let assistTimerId = null;
+    const updateAssist = (text, immidiate) => {
+      clearTimeout(assistTimerId);
+      if (immidiate) {
+        counterAssistant.textContent = text;
+        return;
+      }
+      assistTimerId = setTimeout(() => {
+        counterAssistant.textContent = text;
+      }, assistDelay);
+    };
+
+    const syncAssist = () => {
+      clearTimeout(assistTimerId);
+      counterAssistant.textContent = counterVisual.textContent;
+    };
+
+    const resetAssist = () => {
+      clearTimeout(assistTimerId);
+      counterAssistant.textContent = '';
+    };
+
+    return {
+      id: counterAssistant.id,
+      fragment,
+      updateText,
+      updateAssist,
+      syncAssist,
+      resetAssist,
+    };
+  };
+
+  // create our short and sweet instance
+  const create = (element, options) => {
+    // if no max length defined, exit here
+    if (!element.getAttribute('maxlength')) return;
+
+    // get label from options or element
+    const label = element.dataset.counterLabel || options.counterLabel;
+
+    // create the counter element
+    const counter = createCounter(options);
+
+    // link the counter to the element
+    element.setAttribute('aria-controls', counter.id);
+
+    // by default is inserted as a sibling of the element
+    options.append(element, counter.fragment);
+
+    // assist timer
+    let isFirstUpdate = true;
+
+    const limit = (maxlength) =>
+      (element.value = element.value.substr(0, maxlength));
+
+    // update counter value
+    const update = () => {
+      const maxlength = toInt(element.getAttribute('maxlength'));
+
+      // have we reached the maximum amount of characters
+      const overflowing = element.value.length > maxlength;
+
+      // limit textarea value to the maxlength
+      if (overflowing) limit(maxlength);
+
+      // current length (after limiting)
+      const length = element.value.length;
+
+      // determine current label
+      const dynamicLabel = replaceInString(label, {
+        maxlength,
+        length,
+        remaining: maxlength - length,
+      });
+
+      // update counter value
+      counter.updateText(dynamicLabel);
+
+      // don't update the assist the first time, this makes sure it's empty when we focus the field
+      if (isFirstUpdate) {
+        isFirstUpdate = false;
+        return;
+      }
+
+      // update assistive counter
+      counter.updateAssist(dynamicLabel, overflowing);
+    };
+
+    // tell us the amount of characters left when focusing the field
+    element.addEventListener('focus', counter.syncAssist);
+
+    // reset assist when leaving field so when we refocus the field it correctly tells us again the amount of characters left
+    element.addEventListener('blur', counter.resetAssist);
+
+    // listen for user input so we can update the counter on changes
+    element.addEventListener('input', update);
+
+    // update counter so it matches current input value
+    update();
+  };
+
+  // default short and sweet options
+  const defaultOptions = {
+    counterClassName: 'short-and-sweet-counter',
+    counterLabel: '{remaining} characters left', // {maxlength}, {length}, {remaining}
+    assistDelay: 2000,
+    append: (element, counter) => {
+      element.parentNode.appendChild(counter);
+    },
+  };
+
+  // array of elements in, short and sweet instances out
+  const createAtElements = (elements, options = {}) =>
+    elements.map((element) =>
+      create(element, Object.assign({}, defaultOptions, options))
+    );
+
+  // export our short and sweet function
+  return function shortAndSweet(target, options) {
+    // if target is a string
+    return typeof target === 'string'
+      ? // treat it as a querySelector
+        createAtElements(toArray(document.querySelectorAll(target)), options)
+      : // create single short and sweet counter
+        createAtElements([target], options)[0];
+  };
+})(typeof window === 'undefined' ? null : window);
