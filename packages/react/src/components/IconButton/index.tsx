8b4ba0980f3ec44d262626380eb64eb1e660895a
Rajat <rd.rajat23@gmail.com>
2024-01-12 00:59:52 +0530

    fix: Modal Close button a11y issues (#15057)
    * fix: modal close button
    
    * fix: spacing
    
    * fix: changes
    
    * chore: refactor closeButtonLabel default
    
    * feat(IconButton): add TypeScript types
    
    * chore(modal): improve types
    
    * fix(Button): adjust typescript types of Button and IconButton
    
    * test(react): update index-test.js with new export
    
    * chore(test): update snapshots
    
    * style(ComposedModal): add new classes to ComposedModal to fix alignment
    
    ---------
    
    Co-authored-by: Taylor Jones <taylor.jones826@gmail.com>
    Co-authored-by: Taylor Jones <tay1orjones@users.noreply.github.com>
    Co-authored-by: Joseph D. Harvey <jdharvey@us.ibm.com>
    Co-authored-by: TJ Egan <tw15egan@gmail.com>
    Co-authored-by: Joe Harvey <51208233+jdharvey-ibm@users.noreply.github.com>

--- packages/react/src/components/IconButton/index.tsx
+++ packages/react/src/components/IconButton/index.tsx
@@ -5,16 +5,104 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import PropTypes from 'prop-types';
-import React from 'react';
-import Button from '../Button';
+import PropTypes, { ReactNodeLike } from 'prop-types';
+import React, { ForwardedRef } from 'react';
+import Button, { ButtonSize } from '../Button';
 import classNames from 'classnames';
 import { Tooltip } from '../Tooltip';
 import { usePrefix } from '../../internal/usePrefix';
-import cx from 'classnames';
 
-const IconButton = React.forwardRef(function IconButton(props, ref) {
-  const {
+export const IconButtonKinds = [
+  'primary',
+  'secondary',
+  'ghost',
+  'tertiary',
+] as const;
+
+export type IconButtonKind = (typeof IconButtonKinds)[number];
+
+interface IconButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  /**
+   * Specify how the trigger should align with the tooltip
+   */
+  align?:
+    | 'top'
+    | 'top-left'
+    | 'top-right'
+    | 'bottom'
+    | 'bottom-left'
+    | 'bottom-right'
+    | 'left'
+    | 'right';
+
+  /**
+   * Provide an icon or asset to be rendered inside of the IconButton
+   */
+  children?: React.ReactNode;
+
+  /**
+   * Specify an optional className to be added to your Button
+   */
+  className?: string;
+
+  /**
+   * Determines whether the tooltip should close when inner content is activated (click, Enter or Space)
+   */
+  closeOnActivation?: boolean;
+
+  /**
+   * Specify whether the tooltip should be open when it first renders
+   */
+  defaultOpen?: boolean;
+
+  /**
+   * Specify whether the Button should be disabled, or not
+   */
+  disabled?: boolean;
+
+  /**
+   * Specify the duration in milliseconds to delay before displaying the tooltip
+   */
+  enterDelayMs?: number;
+
+  /**
+   * Specify whether the IconButton is currently selected
+   */
+
+  isSelected?: boolean;
+
+  /**
+   * Specify the type of button to be used as the base for the IconButton
+   */
+  kind?: IconButtonKind;
+
+  /**
+   * Provide the label to be rendered inside of the Tooltip. The label will use
+   * `aria-labelledby` and will fully describe the child node that is provided.
+   * This means that if you have text in the child node it will not be
+   * announced to the screen reader.
+   */
+  label: ReactNodeLike;
+
+  /**
+   * Specify the duration in milliseconds to delay before hiding the tooltip
+   */
+  leaveDelayMs?: number;
+
+  /**
+   * Specify the size of the Button. Defaults to `md`.
+   */
+  size?: ButtonSize;
+
+  /**
+   * Specify an optional className to be added to your Tooltip wrapper
+   */
+  wrapperClasses?: string;
+}
+
+const IconButton = React.forwardRef(function IconButton(
+  {
     align,
     children,
     className,
@@ -29,7 +117,9 @@ const IconButton = React.forwardRef(function IconButton(props, ref) {
     size,
     isSelected,
     ...rest
-  } = props;
+  }: IconButtonProps,
+  ref: ForwardedRef<unknown> // TODO: this is unknown on Button, so should it be here as well?
+) {
   const prefix = usePrefix();
 
   const tooltipClasses = classNames(wrapperClasses, `${prefix}--icon-tooltip`, {
@@ -51,7 +141,7 @@ const IconButton = React.forwardRef(function IconButton(props, ref) {
         kind={kind}
         ref={ref}
         size={size}
-        className={cx(
+        className={classNames(
           `${prefix}--btn--icon-only`,
           {
             [`${prefix}--btn--selected`]: isSelected,
@@ -118,7 +208,7 @@ IconButton.propTypes = {
   /**
    * Specify the type of button to be used as the base for the IconButton
    */
-  kind: PropTypes.oneOf(['primary', 'secondary', 'ghost', 'tertiary']),
+  kind: PropTypes.oneOf(IconButtonKinds),
 
   /**
    * Provide the label to be rendered inside of the Tooltip. The label will use
