364e095d09708efac26a03cd11762a8c1e5424d0
TJ Egan <tw15egan@gmail.com>
2024-02-01 10:06:44 -0500

    feat(ChatButton): scaffold initial structure

---
+++ packages/react/src/components/ChatButton/ChatButton.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright IBM Corp. 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import PropTypes from 'prop-types';
+import React from 'react';
+import classnames from 'classnames';
+import Button from '../Button';
+import { usePrefix } from '../../internal/usePrefix';
+
+const ChatButton = React.forwardRef(function ChatButton(
+  { className, children, ...other },
+  ref
+) {
+  const prefix = usePrefix();
+  const classNames = classnames(`${prefix}--select--fluid`, className);
+
+  return (
+    <Button className={classNames} ref={ref} {...other}>
+      {children}
+    </Button>
+  );
+});
+
+ChatButton.propTypes = {
+  /**
+   * Provide the contents of your Select
+   */
+  children: PropTypes.node,
+
+  /**
+   * Specify an optional className to be applied to the node containing the label and the select box
+   */
+  className: PropTypes.string,
+};
+
+export default ChatButton;
---
+++ packages/react/src/components/ChatButton/ChatButton.stories.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright IBM Corp. 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import { ChatButton } from '.';
+
+export default {
+  title: 'Experimental/unstable__ChatButton',
+  component: ChatButton,
+  parameters: {},
+  argTypes: {
+    onChange: {
+      action: 'onChange',
+      table: {
+        disable: true,
+      },
+    },
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+    className: {
+      table: {
+        disable: true,
+      },
+    },
+    defaultValue: {
+      table: {
+        disable: true,
+      },
+    },
+    id: {
+      table: {
+        disable: true,
+      },
+    },
+    light: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+};
+
+export const Default = () => <ChatButton>Test</ChatButton>;
