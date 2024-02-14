7f8ab338e735cbe5d5034cbd19d3fc0a2b8b91e9
TJ Egan <tw15egan@gmail.com>
2024-02-14 11:48:02 -0500

    fix(Slug): update Slug Modal examples to show open/close button

--- packages/react/src/components/Slug/Slug-examples.stories.js
+++ packages/react/src/components/Slug/Slug-examples.stories.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../Button';
 import Checkbox from '../Checkbox';
 import CheckboxGroup from '../CheckboxGroup';
@@ -282,38 +282,42 @@ export const _ComposedModal = {
         '**Experimental**: Provide a `Slug` component to be rendered inside the component',
     },
   },
-  render: () => (
-    <div className="slug-modal">
-      <ComposedModal slug={slug} open>
-        <ModalHeader label="Account resources" title="Add a custom domain" />
-        <ModalBody>
-          <p style={{ marginBottom: '1rem' }}>
-            Custom domains direct requests for your apps in this Cloud Foundry
-            organization to a URL that you own. A custom domain can be a shared
-            domain, a shared subdomain, or a shared domain and host.
-          </p>
-          <TextInput
-            data-modal-primary-focus
-            id="text-input-1"
-            labelText="Domain name"
-            placeholder="e.g. github.com"
-            style={{ marginBottom: '1rem' }}
+  render: () => {
+    const [open, setOpen] = useState(true); // eslint-disable-line
+    return (
+      <div className="slug-modal">
+        <Button onClick={() => setOpen(true)}>Launch composed modal</Button>
+        <ComposedModal open={open} onClose={() => setOpen(false)} slug={slug}>
+          <ModalHeader label="Account resources" title="Add a custom domain" />
+          <ModalBody>
+            <p style={{ marginBottom: '1rem' }}>
+              Custom domains direct requests for your apps in this Cloud Foundry
+              organization to a URL that you own. A custom domain can be a
+              shared domain, a shared subdomain, or a shared domain and host.
+            </p>
+            <TextInput
+              data-modal-primary-focus
+              id="text-input-1"
+              labelText="Domain name"
+              placeholder="e.g. github.com"
+              style={{ marginBottom: '1rem' }}
+            />
+            <Select id="select-1" defaultValue="us-south" labelText="Region">
+              <SelectItem value="us-south" text="US South" />
+              <SelectItem value="us-east" text="US East" />
+            </Select>
+          </ModalBody>
+          <ModalFooter
+            primaryButtonText="Add"
+            secondaryButtons={[
+              { buttonText: 'Keep both' },
+              { buttonText: 'Rename' },
+            ]}
           />
-          <Select id="select-1" defaultValue="us-south" labelText="Region">
-            <SelectItem value="us-south" text="US South" />
-            <SelectItem value="us-east" text="US East" />
-          </Select>
-        </ModalBody>
-        <ModalFooter
-          primaryButtonText="Add"
-          secondaryButtons={[
-            { buttonText: 'Keep both' },
-            { buttonText: 'Rename' },
-          ]}
-        />
-      </ComposedModal>
-    </div>
-  ),
+        </ComposedModal>
+      </div>
+    );
+  },
 };
 
 export const _DatePicker = {
@@ -382,34 +386,39 @@ export const _Modal = {
         '**Experimental**: Provide a `Slug` component to be rendered inside the component',
     },
   },
-  render: () => (
-    <div className="slug-modal">
-      <Modal
-        open
-        modalHeading="Add a custom domain"
-        modalLabel="Account resources"
-        primaryButtonText="Add"
-        secondaryButtonText="Cancel"
-        slug={slug}>
-        <p>
-          Custom domains direct requests for your apps in this Cloud Foundry
-          organization to a URL that you own. A custom domain can be a shared
-          domain, a shared subdomain, or a shared domain and host.
-        </p>
-        <TextInput
-          data-modal-primary-focus
-          id="text-input-1"
-          labelText="Domain name"
-          placeholder="e.g. github.com"
-        />
-        <Select id="select-1" defaultValue="us-south" labelText="Region">
-          <SelectItem value="us-south" text="US South" />
-          <SelectItem value="us-east" text="US East" />
-        </Select>
-        <TextArea labelText="Comments" />
-      </Modal>
-    </div>
-  ),
+  render: () => {
+    const [open, setOpen] = useState(true); // eslint-disable-line
+    return (
+      <div className="slug-modal">
+        <Button onClick={() => setOpen(true)}>Launch modal</Button>
+        <Modal
+          open={open}
+          onRequestClose={() => setOpen(false)}
+          modalHeading="Add a custom domain"
+          modalLabel="Account resources"
+          primaryButtonText="Add"
+          secondaryButtonText="Cancel"
+          slug={slug}>
+          <p>
+            Custom domains direct requests for your apps in this Cloud Foundry
+            organization to a URL that you own. A custom domain can be a shared
+            domain, a shared subdomain, or a shared domain and host.
+          </p>
+          <TextInput
+            data-modal-primary-focus
+            id="text-input-1"
+            labelText="Domain name"
+            placeholder="e.g. github.com"
+          />
+          <Select id="select-1" defaultValue="us-south" labelText="Region">
+            <SelectItem value="us-south" text="US South" />
+            <SelectItem value="us-east" text="US East" />
+          </Select>
+          <TextArea labelText="Comments" />
+        </Modal>
+      </div>
+    );
+  },
 };
 
 export const _Multiselect = {
