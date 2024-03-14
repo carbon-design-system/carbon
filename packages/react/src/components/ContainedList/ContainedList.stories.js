d31f740e81f4bd6baf12dabea315e76d9b8cc36e
Andrea N. Cardona <cardona.n.andrea@gmail.com>
2024-03-14 12:42:27 -0700

    fix: 13938-iphone- containedlist-bug

--- packages/react/src/components/ContainedList/ContainedList.stories.js
+++ packages/react/src/components/ContainedList/ContainedList.stories.js
@@ -62,13 +62,13 @@ export default {
 
 export const Default = () => (
   <>
-    <ContainedList label="List title" kind="on-page">
+    <ContainedList role="list" label="List title" kind="on-page">
       <ContainedListItem>List item</ContainedListItem>
       <ContainedListItem>List item</ContainedListItem>
       <ContainedListItem>List item</ContainedListItem>
       <ContainedListItem>List item</ContainedListItem>
     </ContainedList>
-    <ContainedList label="List title" kind="on-page">
+    <ContainedList role="list" label="List title" kind="on-page">
       <ContainedListItem>List item</ContainedListItem>
       <ContainedListItem>List item</ContainedListItem>
       <ContainedListItem>List item</ContainedListItem>
