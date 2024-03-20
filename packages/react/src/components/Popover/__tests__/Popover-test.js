afce28df13ae3475eb32c7b65e0599eb86775458
Taylor Jones <tay1orjones@users.noreply.github.com>
2024-03-20 13:05:01 -0500

    feat(popover): use floating-ui for autoAlign (#14654)
    * WIP: add floating-ui to popover
    
    * WIP: use scss for align positioning, update CSSOM styling, add flip, add autoUpdate, add arrow, add offset
    
    * wip(popover): explore logical properties support, popover offset from logical property
    
    * WIP
    
    * WIP: add fixed strategy, remove default popover styling when autoAlign
    
    * WIP: properly set refs on reference element and floating element
    
    * WIP
    
    * wip(popover): correct caret placement
    
    * wip(popover): update styles to support new alignment names
    
    * feat(popover): cleanup autoalign implementation/styles
    
    * fix(popover): improve autoAlign
    
    * fix(popover): add forwardRef to ToggletipButton
    
    * fix(popover): improve autoAlign compatibility with slug
    
    * fix(popover): position absolute is not necessary on the container
    
    * fix(slug): scope popover selectors for autoalign functionality
    
    * chore: yarn dedupe
    
    * chore(popover): remove console logs
    
    * fix(popover): improve tests, cloneElement logic
    
    * fix(popover): update align props across the entire system
    
    * chore(popover): fix typescript issues
    
    * fix(popover): ensure component api table in docs works
    
    * chore(popover): typescript fixes
    
    * fix(popover): ensure autoAlign properly pads out the slug caret
    
    * fix(popover): typescript error
    
    * fix(popover): remove offset when isTabTip
    
    * fix(popover): correct caret style typo
    
    * docs(popover): clean up stories
    
    * test(popover): update story name
    
    * chore: fix toggletip story width, vrt popover playground
    
    ---------
    
    Co-authored-by: Andrea N. Cardona <cardona.n.andrea@gmail.com>

--- packages/react/src/components/Popover/__tests__/Popover-test.js
+++ packages/react/src/components/Popover/__tests__/Popover-test.js
@@ -90,18 +90,130 @@ describe('Popover', () => {
       expect(container.firstChild).toHaveClass(`${prefix}--popover--tab-tip`);
     });
 
-    it('should not allow other alignments than bottom-left or bottom-right when isTabTip is present', () => {
+    it('should not allow other alignments than bottom-start or bottom-end when isTabTip is present', () => {
       const { container } = render(
-        <Popover open isTabTip align="top-left">
+        <Popover open isTabTip align="top-start">
           <button type="button">Settings</button>
           <PopoverContent data-testid="test">test</PopoverContent>
         </Popover>
       );
       expect(container.firstChild).not.toHaveClass(
-        `${prefix}--popover--top-left`
+        `${prefix}--popover--top-start`
       );
       expect(container.firstChild).toHaveClass(
+        `${prefix}--popover--bottom-start`
+      );
+    });
+
+    it('should shim legacy align prop top-left to top-start', () => {
+      const { container } = render(
+        <Popover open align="top-left">
+          <button type="button">Settings</button>
+          <PopoverContent>Test</PopoverContent>
+        </Popover>
+      );
+      expect(container.firstChild).not.toHaveClass(
+        `${prefix}--popover--top-left`
+      );
+      expect(container.firstChild).toHaveClass(`${prefix}--popover--top-start`);
+    });
+
+    it('should shim legacy align prop top-right to top-end', () => {
+      const { container } = render(
+        <Popover open align="top-right">
+          <button type="button">Settings</button>
+          <PopoverContent>Test</PopoverContent>
+        </Popover>
+      );
+      expect(container.firstChild).not.toHaveClass(
+        `${prefix}--popover--top-right`
+      );
+      expect(container.firstChild).toHaveClass(`${prefix}--popover--top-end`);
+    });
+
+    it('should shim legacy align prop bottom-left to bottom-start', () => {
+      const { container } = render(
+        <Popover open align="bottom-left">
+          <button type="button">Settings</button>
+          <PopoverContent>Test</PopoverContent>
+        </Popover>
+      );
+      expect(container.firstChild).not.toHaveClass(
         `${prefix}--popover--bottom-left`
+      );
+      expect(container.firstChild).toHaveClass(
+        `${prefix}--popover--bottom-start`
+      );
+    });
+
+    it('should shim legacy align prop bottom-right to bottom-end', () => {
+      const { container } = render(
+        <Popover open align="bottom-right">
+          <button type="button">Settings</button>
+          <PopoverContent>Test</PopoverContent>
+        </Popover>
+      );
+      expect(container.firstChild).not.toHaveClass(
+        `${prefix}--popover--bottom-right`
+      );
+      expect(container.firstChild).toHaveClass(
+        `${prefix}--popover--bottom-end`
+      );
+    });
+
+    it('should shim legacy align prop left-bottom to left-end', () => {
+      const { container } = render(
+        <Popover open align="left-bottom">
+          <button type="button">Settings</button>
+          <PopoverContent>Test</PopoverContent>
+        </Popover>
+      );
+      expect(container.firstChild).not.toHaveClass(
+        `${prefix}--popover--left-bottom`
+      );
+      expect(container.firstChild).toHaveClass(`${prefix}--popover--left-end`);
+    });
+
+    it('should shim legacy align prop left-top to left-start', () => {
+      const { container } = render(
+        <Popover open align="left-top">
+          <button type="button">Settings</button>
+          <PopoverContent>Test</PopoverContent>
+        </Popover>
+      );
+      expect(container.firstChild).not.toHaveClass(
+        `${prefix}--popover--left-top`
+      );
+      expect(container.firstChild).toHaveClass(
+        `${prefix}--popover--left-start`
+      );
+    });
+
+    it('should shim legacy align prop right-bottom to right-end', () => {
+      const { container } = render(
+        <Popover open align="right-bottom">
+          <button type="button">Settings</button>
+          <PopoverContent>Test</PopoverContent>
+        </Popover>
+      );
+      expect(container.firstChild).not.toHaveClass(
+        `${prefix}--popover--right-bottom`
+      );
+      expect(container.firstChild).toHaveClass(`${prefix}--popover--right-end`);
+    });
+
+    it('should shim legacy align prop right-top to right-start', () => {
+      const { container } = render(
+        <Popover open align="right-top">
+          <button type="button">Settings</button>
+          <PopoverContent>Test</PopoverContent>
+        </Popover>
+      );
+      expect(container.firstChild).not.toHaveClass(
+        `${prefix}--popover--right-top`
+      );
+      expect(container.firstChild).toHaveClass(
+        `${prefix}--popover--right-start`
       );
     });
 
