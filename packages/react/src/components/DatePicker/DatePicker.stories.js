c8d0f9797ca5b54c46a3fb20497de4d62c761663
riddhybansal <riddhybansal@gmail.com>
2024-04-15 17:37:30 +0530

    fix: datepicker onchange trigger issue

--- packages/react/src/components/DatePicker/DatePicker.stories.js
+++ packages/react/src/components/DatePicker/DatePicker.stories.js
@@ -58,7 +58,7 @@ export const SingleWithCalendar = () => (
 
 export const RangeWithCalendar = () => {
   return (
-    <DatePicker datePickerType="range">
+    <DatePicker allowInput={false} datePickerType="range">
       <DatePickerInput
         id="date-picker-input-id-start"
         placeholder="mm/dd/yyyy"
@@ -108,7 +108,7 @@ export const SingleWithCalendarWithLayer = () => (
 export const RangeWithCalendarWithLayer = () => (
   <WithLayer>
     {(layer) => (
-      <DatePicker datePickerType="range">
+      <DatePicker allowInput={false} datePickerType="range">
         <DatePickerInput
           id={`date-picker-input-id-start-${layer}`}
           placeholder="mm/dd/yyyy"
