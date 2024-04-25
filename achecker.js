c71e3df2f1f09ff5af739c5cd1754ac5629d829c
Francine Lucca <francinelucca@hotmail.com>
2024-04-25 17:09:48 -0400

    fix: use latest-1 a11y ruleset

--- achecker.js
+++ achecker.js
@@ -10,7 +10,7 @@
 const path = require('path');
 
 module.exports = {
-  ruleArchive: 'latest',
+  ruleArchive: '18March2024',
   policies: ['Custom_Ruleset'],
   failLevels: ['violation'],
   reportLevels: [
