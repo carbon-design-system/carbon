ce353330f32a681d7be48fc873691bef94b0944b
Gururajj77 <gururajtakeshi@gmail.com>
2024-04-29 20:21:26 +0530

    refactor: changes for successful build

--- packages/react/src/components/OverflowMenu/next/index.tsx
+++ packages/react/src/components/OverflowMenu/next/index.tsx
@@ -68,91 +68,91 @@ interface OverflowMenuProps {
     | 'right';
 }
 
-const OverflowMenu: React.ForwardRefRenderFunction<
-  HTMLDivElement,
-  OverflowMenuProps
-> = (
-  {
-    children,
-    className,
-    label = 'Options',
-    renderIcon: IconElement = OverflowMenuVertical,
-    size = defaultSize,
-    menuAlignment = 'bottom-start',
-    tooltipAlignment,
-    ...rest
-  },
-  forwardRef
-) => {
-  const id = useId('overflowmenu');
-  const prefix = usePrefix();
-
-  const triggerRef = useRef<HTMLDivElement>(null);
-  const {
-    open,
-    x,
-    y,
-    handleClick: hookOnClick,
-    handleMousedown,
-    handleClose,
-  } = useAttachedMenu(triggerRef);
-
-  function handleTriggerClick() {
-    if (triggerRef.current) {
-      hookOnClick();
-    }
-  }
-
-  const containerClasses = classNames(
-    className,
-    `${prefix}--overflow-menu__container`
-  );
-
-  const menuClasses = classNames(`${prefix}--overflow-menu__${menuAlignment}`);
-
-  const triggerClasses = classNames(
-    `${prefix}--overflow-menu`,
+const OverflowMenu = React.forwardRef<HTMLDivElement, OverflowMenuProps>(
+  function OverflowMenu(
     {
-      [`${prefix}--overflow-menu--open`]: open,
+      children,
+      className,
+      label = 'Options',
+      renderIcon: IconElement = OverflowMenuVertical,
+      size = defaultSize,
+      menuAlignment = 'bottom-start',
+      tooltipAlignment,
+      ...rest
     },
-    size !== defaultSize && `${prefix}--overflow-menu--${size}`
-  );
+    forwardRef
+  ) {
+    const id = useId('overflowmenu');
+    const prefix = usePrefix();
 
-  return (
-    <div
-      {...rest}
-      className={containerClasses}
-      aria-owns={open ? id : undefined}
-      ref={forwardRef}>
-      <IconButton
-        aria-controls={open ? id : undefined}
-        aria-haspopup
-        aria-expanded={open}
-        className={triggerClasses}
-        onClick={handleTriggerClick}
-        onMouseDown={handleMousedown}
-        ref={triggerRef}
-        label={label}
-        align={tooltipAlignment}>
-        <IconElement className={`${prefix}--overflow-menu__icon`} />
-      </IconButton>
-      <Menu
-        containerRef={triggerRef}
-        menuAlignment={menuAlignment}
-        className={menuClasses}
-        id={id}
-        size={size}
-        open={open}
-        onClose={handleClose}
-        x={x}
-        y={y}
-        label={label}>
-        {children}
-      </Menu>
-    </div>
-  );
-};
+    const triggerRef = useRef<HTMLDivElement>(null);
+    const {
+      open,
+      x,
+      y,
+      handleClick: hookOnClick,
+      handleMousedown,
+      handleClose,
+    } = useAttachedMenu(triggerRef);
 
+    function handleTriggerClick() {
+      if (triggerRef.current) {
+        hookOnClick();
+      }
+    }
+
+    const containerClasses = classNames(
+      className,
+      `${prefix}--overflow-menu__container`
+    );
+
+    const menuClasses = classNames(
+      `${prefix}--overflow-menu__${menuAlignment}`
+    );
+
+    const triggerClasses = classNames(
+      `${prefix}--overflow-menu`,
+      {
+        [`${prefix}--overflow-menu--open`]: open,
+      },
+      size !== defaultSize && `${prefix}--overflow-menu--${size}`
+    );
+
+    return (
+      <div
+        {...rest}
+        className={containerClasses}
+        aria-owns={open ? id : undefined}
+        ref={forwardRef}>
+        <IconButton
+          aria-controls={open ? id : undefined}
+          aria-haspopup
+          aria-expanded={open}
+          className={triggerClasses}
+          onClick={handleTriggerClick}
+          onMouseDown={handleMousedown}
+          ref={triggerRef}
+          label={label}
+          align={tooltipAlignment}>
+          <IconElement className={`${prefix}--overflow-menu__icon`} />
+        </IconButton>
+        <Menu
+          containerRef={triggerRef}
+          menuAlignment={menuAlignment}
+          className={menuClasses}
+          id={id}
+          size={size}
+          open={open}
+          onClose={handleClose}
+          x={x}
+          y={y}
+          label={label}>
+          {children}
+        </Menu>
+      </div>
+    );
+  }
+);
 OverflowMenu.propTypes = {
   /**
    * A collection of MenuItems to be rendered within this OverflowMenu.
@@ -182,6 +182,7 @@ OverflowMenu.propTypes = {
   /**
    * Optionally provide a custom icon to be rendered on the trigger button.
    */
+  // @ts-expect-error: PropTypes are not expressive enough to cover this case
   renderIcon: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
 
   /**
