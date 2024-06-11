c2f2a1cbfb0bfecf8a438248215c310ae4dc33ca
Sangeetha Babu <58620134+sangeethababu9223@users.noreply.github.com>
2024-06-11 10:45:34 +0530

    feat(menu): add license text to menu.ts

--- packages/carbon-web-components/src/components/menu/menu.ts
+++ packages/carbon-web-components/src/components/menu/menu.ts
@@ -1,3 +1,12 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2024
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { LitElement, html } from 'lit';
 import { prefix } from '../../globals/settings';
 import { property, state } from 'lit/decorators.js';
