9c407faec9ff00abefeee2b3c03fba4a7d898201
Yael Arturo Chavoya Andalón <ychavoya@ibm.com>
2024-06-18 12:21:54 -0600

    docs(examples): change react-router example to use UI Shell

--- packages/react/examples/react-router/src/index.js
+++ packages/react/examples/react-router/src/index.js
@@ -3,9 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
-createRoot(document.getElementById('root'))
-  .render(
-    <Router>
-      <App />
-    </Router>
-  );
+createRoot(document.getElementById('root')).render(
+  <Router>
+    <App />
+  </Router>
+);
