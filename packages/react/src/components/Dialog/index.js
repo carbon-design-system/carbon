7345a37a2c24e6f0143af45cd430c7154349457d
Guilherme Datilio Ribeiro <guilhermedatilio@gmail.com>
2024-07-09 10:07:32 -0300

    v10: Remove unneeded wicg-inert polyfill (#16915)
    * fix: remove unneeded wicg-inert polyfill
    
    * fix: ran npx browserslist@latest --update-db

--- packages/react/src/components/Dialog/index.js
+++ packages/react/src/components/Dialog/index.js
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import 'wicg-inert';
 import PropTypes from 'prop-types';
 import React, { useEffect, useRef } from 'react';
 import { FocusScope } from '../FocusScope';
