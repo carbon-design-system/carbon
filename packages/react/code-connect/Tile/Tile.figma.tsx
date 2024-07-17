06541e853ee93ac33cd095368c3e2afca7d7d5e3
ariellalgilmore <ariellalgilmore@gmail.com>
2024-07-17 10:51:34 -0700

    fix(tile): update variants

--- packages/react/code-connect/Tile/Tile.figma.tsx
+++ packages/react/code-connect/Tile/Tile.figma.tsx
@@ -7,33 +7,169 @@
 
 // @ts-nocheck
 import React from 'react';
-import { Tile } from '@carbon/react';
+import {
+  ClickableTile,
+  ExpandableTile,
+  SelectableTile,
+  Tile,
+  TileAboveTheFoldContent,
+  TileBelowTheFoldContent,
+} from '@carbon/react';
 import figma from '@figma/code-connect';
 
 figma.connect(
   Tile,
   'https://www.figma.com/design/YAnB1jKx0yCUL29j6uSLpg/(v11)-All-themes---Carbon-Design-System?node-id=20125-279432&m=dev',
   {
+    variant: { Type: 'Base' },
     props: {
       swapslot: figma.instance('Swap slot'),
       descriptiontext: figma.string('Description text'),
-      description: figma.boolean('Description'),
-      slot: figma.boolean('Slot'),
-      title: figma.boolean('Title'),
       titletext: figma.string('Title text'),
-      type: figma.enum('Type', {
-        Base: 'base',
-        Clickable: 'clickable',
-        'Single-select': 'single-select',
-        'Multi-select': 'multi-select',
-        Expandable: 'expandable',
-        'Expandable (Interactive)': 'expandable--interactive-',
+      disabled: figma.enum('State', {
+        Disabled: true,
       }),
+    },
+    example: ({ disabled, titletext, swapslot, descriptiontext }) => (
+      <Tile disabled={disabled}>
+        {titletext}
+        <br />
+        {descriptiontext}
+        {swapslot}
+      </Tile>
+    ),
+  }
+);
+
+figma.connect(
+  ClickableTile,
+  'https://www.figma.com/design/YAnB1jKx0yCUL29j6uSLpg/(v11)-All-themes---Carbon-Design-System?node-id=20125-279432&m=dev',
+  {
+    variant: { Type: 'Clickable' },
+    props: {
+      swapslot: figma.instance('Swap slot'),
+      descriptiontext: figma.string('Description text'),
+      titletext: figma.string('Title text'),
+      disabled: figma.enum('State', {
+        Disabled: true,
+      }),
+    },
+    example: ({ disabled, titletext, swapslot, descriptiontext }) => (
+      <ClickableTile disabled={disabled}>
+        {titletext}
+        <br />
+        {descriptiontext}
+        {swapslot}
+      </ClickableTile>
+    ),
+  }
+);
+
+figma.connect(
+  SelectableTile,
+  'https://www.figma.com/design/YAnB1jKx0yCUL29j6uSLpg/(v11)-All-themes---Carbon-Design-System?node-id=20125-279432&m=dev',
+  {
+    variant: { Type: 'Single-select' },
+    props: {
+      swapslot: figma.instance('Swap slot'),
+      descriptiontext: figma.string('Description text'),
+      titletext: figma.string('Title text'),
       disabled: figma.enum('State', {
         Disabled: true,
       }),
       selected: figma.boolean('Selected'),
     },
-    example: (props) => <Tile {...props} />,
+    example: ({ disabled, titletext, swapslot, descriptiontext, selected }) => (
+      <SelectableTile disabled={disabled} selected={selected}>
+        {titletext}
+        <br />
+        {descriptiontext}
+        {swapslot}
+      </SelectableTile>
+    ),
+  }
+);
+
+figma.connect(
+  SelectableTile,
+  'https://www.figma.com/design/YAnB1jKx0yCUL29j6uSLpg/(v11)-All-themes---Carbon-Design-System?node-id=20125-279432&m=dev',
+  {
+    variant: { Type: 'Multi-select' },
+    props: {
+      swapslot: figma.instance('Swap slot'),
+      descriptiontext: figma.string('Description text'),
+      titletext: figma.string('Title text'),
+      disabled: figma.enum('State', {
+        Disabled: true,
+      }),
+      selected: figma.boolean('Selected'),
+    },
+    example: ({ disabled, titletext, swapslot, descriptiontext, selected }) => (
+      <SelectableTile disabled={disabled} selected={selected}>
+        {titletext}
+        <br />
+        {descriptiontext}
+        {swapslot}
+      </SelectableTile>
+    ),
+  }
+);
+
+figma.connect(
+  ExpandableTile,
+  'https://www.figma.com/design/YAnB1jKx0yCUL29j6uSLpg/(v11)-All-themes---Carbon-Design-System?node-id=20125-279432&m=dev',
+  {
+    variant: { Type: 'Expandable' },
+    props: {
+      descriptiontext: figma.string('Description text'),
+      titletext: figma.string('Title text'),
+    },
+    example: ({ titletext, descriptiontext }) => (
+      <ExpandableTile
+        tileCollapsedIconText="Interact to Expand tile"
+        tileExpandedIconText="Interact to Collapse tile">
+        <TileAboveTheFoldContent>
+          <div style={{ height: '200px' }}>
+            {titletext}
+            <br />
+            {descriptiontext}
+          </div>
+        </TileAboveTheFoldContent>
+        <TileBelowTheFoldContent>
+          <div style={{ height: '400px' }}>Below the fold content here</div>
+        </TileBelowTheFoldContent>
+      </ExpandableTile>
+    ),
+  }
+);
+
+figma.connect(
+  ExpandableTile,
+  'https://www.figma.com/design/YAnB1jKx0yCUL29j6uSLpg/(v11)-All-themes---Carbon-Design-System?node-id=20125-279432&m=dev',
+  {
+    variant: { Type: 'Expandable (Interactive)' },
+    props: {
+      swapslot: figma.instance('Swap slot'),
+      descriptiontext: figma.string('Description text'),
+      titletext: figma.string('Title text'),
+    },
+    example: ({ titletext, descriptiontext }) => (
+      <ExpandableTile
+        onClick={() => console.log('click')}
+        tileCollapsedIconText="Interact to Expand tile"
+        tileExpandedIconText="Interact to Collapse tile">
+        <TileAboveTheFoldContent>
+          <div style={{ height: '200px' }}>
+            {titletext}
+            <br />
+            {descriptiontext}
+            {swapslot}
+          </div>
+        </TileAboveTheFoldContent>
+        <TileBelowTheFoldContent>
+          <div style={{ height: '400px' }}>Below the fold content here</div>
+        </TileBelowTheFoldContent>
+      </ExpandableTile>
+    ),
   }
 );
