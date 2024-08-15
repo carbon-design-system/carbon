3e0d15b1bc66f4c3897e0441fd7c2c9c38cbfce3
Alison Joseph <alisonejoseph@Gmail.com>
2024-08-15 12:31:37 -0500

    Update Slug-test.avt.e2e.js

--- e2e/components/AILabel/Slug-test.avt.e2e.js
+++ e2e/components/AILabel/Slug-test.avt.e2e.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-'use strict'; 
+'use strict';
 
 const { expect, test } = require('@playwright/test');
 const { visitStory } = require('../../test-utils/storybook');
