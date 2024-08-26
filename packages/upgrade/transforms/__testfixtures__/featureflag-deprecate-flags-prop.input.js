4d596f9dc87492386ba5370640b4209d0f0a33a2
Nikhil Tomar <nikhiltomar753@gmail.com>
2024-08-26 17:34:09 +0530

    refactor: test fixtures

--- packages/upgrade/transforms/__testfixtures__/featureflag-deprecate-flags-prop.input.js
+++ packages/upgrade/transforms/__testfixtures__/featureflag-deprecate-flags-prop.input.js
@@ -38,211 +38,23 @@ export const EnableExperimentalFocusWrapWithoutSentinels = () => {
   );
 };
 export const EnableTreeviewControllable = (args) => {
-  const [selected, setSelected] = useState([]);
-  const [active, setActive] = useState(null);
-  const nodes = [
-    {
-      id: '1',
-      value: 'Artificial intelligence',
-      label: <span>Artificial intelligence</span>,
-      renderIcon: Document,
-    },
-    {
-      id: '2',
-      value: 'Blockchain',
-      label: 'Blockchain',
-      renderIcon: Document,
-    },
-    {
-      id: '3',
-      value: 'Business automation',
-      label: 'Business automation',
-      renderIcon: Folder,
-      children: [
-        {
-          id: '3-1',
-          value: 'Business process automation',
-          label: 'Business process automation',
-          renderIcon: Document,
-        },
-        {
-          id: '3-2',
-          value: 'Business process mapping',
-          label: 'Business process mapping',
-          renderIcon: Document,
-        },
-      ],
-    },
-    {
-      id: '4',
-      value: 'Business operations',
-      label: 'Business operations',
-      renderIcon: Document,
-    },
-    {
-      id: '5',
-      value: 'Cloud computing',
-      label: 'Cloud computing',
-      isExpanded: true,
-      renderIcon: Folder,
-      children: [
-        {
-          id: '5-1',
-          value: 'Containers',
-          label: 'Containers',
-          renderIcon: Document,
-        },
-        {
-          id: '5-2',
-          value: 'Databases',
-          label: 'Databases',
-          renderIcon: Document,
-        },
-        {
-          id: '5-3',
-          value: 'DevOps',
-          label: 'DevOps',
-          isExpanded: true,
-          renderIcon: Folder,
-          children: [
-            {
-              id: '5-4',
-              value: 'Solutions',
-              label: 'Solutions',
-              renderIcon: Document,
-            },
-            {
-              id: '5-5',
-              value: 'Case studies',
-              label: 'Case studies',
-              isExpanded: true,
-              renderIcon: Folder,
-              children: [
-                {
-                  id: '5-6',
-                  value: 'Resources',
-                  label: 'Resources',
-                  renderIcon: Document,
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: '6',
-      value: 'Data & Analytics',
-      label: 'Data & Analytics',
-      renderIcon: Folder,
-      children: [
-        {
-          id: '6-1',
-          value: 'Big data',
-          label: 'Big data',
-          renderIcon: Document,
-        },
-        {
-          id: '6-2',
-          value: 'Business intelligence',
-          label: 'Business intelligence',
-          renderIcon: Document,
-        },
-      ],
-    },
-    {
-      id: '7',
-      value: 'Models',
-      label: 'Models',
-      isExpanded: true,
-      disabled: true,
-      renderIcon: Folder,
-      children: [
-        {
-          id: '7-1',
-          value: 'Audit',
-          label: 'Audit',
-          renderIcon: Document,
-        },
-        {
-          id: '7-2',
-          value: 'Monthly data',
-          label: 'Monthly data',
-          renderIcon: Document,
-        },
-        {
-          id: '8',
-          value: 'Data warehouse',
-          label: 'Data warehouse',
-          isExpanded: true,
-          renderIcon: Folder,
-          children: [
-            {
-              id: '8-1',
-              value: 'Report samples',
-              label: 'Report samples',
-              renderIcon: Document,
-            },
-            {
-              id: '8-2',
-              value: 'Sales performance',
-              label: 'Sales performance',
-              renderIcon: Document,
-            },
-          ],
-        },
-      ],
-    },
-  ];
-  function renderTree(nodes) {
-    if (!nodes) {
-      return;
-    }
-
-    return nodes.map(({ children, isExpanded, ...nodeProps }) => (
-      <TreeNode
-        key={nodeProps.id}
-        defaultIsExpanded={isExpanded}
-        {...nodeProps}>
-        {renderTree(children)}
-      </TreeNode>
-    ));
-  }
-
   return (
-    <VStack gap={6}>
-      <VStack gap={2}>
-        <Button
-          kind="tertiary"
-          size="sm"
-          onClick={() => {
-            setSelected(['5-2']);
+    <div>
+      <FeatureFlags flags={{ 'enable-treeview-controllable': true }}>
+        <TreeView
+          label="Tree View"
+          active={true}
+          onActivate={() => {
+            console.log('test');
+          }}
+          selected={[]}
+          onSelect={() => {
+            console.log('test');
           }}>
-          Select &quot;Databases&quot;
-        </Button>
-        <Button
-          kind="tertiary"
-          size="sm"
-          onClick={() => {
-            setActive('5-2');
-          }}>
-          Activate &quot;Databases&quot;
-        </Button>
-      </VStack>
-      <div>
-        <FeatureFlags flags={{ 'enable-treeview-controllable': true }}>
-          <TreeView
-            label="Tree View"
-            {...args}
-            active={active}
-            onActivate={setActive}
-            selected={selected}
-            onSelect={setSelected}>
-            {renderTree(nodes)}
-          </TreeView>
-        </FeatureFlags>
-      </div>
-    </VStack>
+          {renderTree(nodes)}
+        </TreeView>
+      </FeatureFlags>
+    </div>
   );
 };
 export const EnableV12Overflowmenu = () => {
@@ -273,6 +85,28 @@ export const EnableV12TileRadioIcons = () => {
     </FeatureFlags>
   );
 };
-export const Test = () => {
+export const TestRegularJsx = () => {
   return <div className={wrapperClasses}></div>;
+};
+export const CombinedFlags = () => {
+  return (
+    // prettier-ignore
+    <FeatureFlags
+      flags={{
+        'enable-v12-tile-default-icons': true,
+        'enable-v12-tile-radio-icons': true,
+        'enable-v12-overflowmenu': true,
+        'enable-treeview-controllable': true,       
+        'enable-experimental-focus-wrap-without-sentinels': true,
+      }}>
+      <TileGroup legend="TestGroup" name="test">
+        <RadioTile id="test-1" value="test-1">
+          Option 1
+        </RadioTile>
+        <RadioTile id="test-2" value="test-2">
+          Option 2
+        </RadioTile>
+      </TileGroup>
+    </FeatureFlags>
+  );
 };
