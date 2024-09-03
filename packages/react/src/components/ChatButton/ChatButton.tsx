156a191391b79438dc65905c11111685184db657
Nikhil Tomar <nikhiltomar753@gmail.com>
2024-09-03 14:45:35 +0530

    fix(typescript): adds TypeScript types to ChatButto & Skeleton

--- packages/react/src/components/ChatButton/ChatButton.tsx
+++ packages/react/src/components/ChatButton/ChatButton.tsx
@@ -1,61 +1,108 @@
 /**
- * Copyright IBM Corp. 2022
+ * Copyright IBM Corp. 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
+import React, { type ComponentType, type FunctionComponent } from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
 import classnames from 'classnames';
 import Button from '../Button';
 import { usePrefix } from '../../internal/usePrefix';
 
-const ChatButton = React.forwardRef(function ChatButton(
-  {
-    className,
-    children,
-    disabled,
-    isQuickAction,
-    isSelected,
-    kind,
-    renderIcon,
-    size,
-    ...other
-  },
-  ref
-) {
-  const prefix = usePrefix();
-  const classNames = classnames(className, {
-    [`${prefix}--chat-btn`]: true,
-    [`${prefix}--chat-btn--with-icon`]: renderIcon,
-    [`${prefix}--chat-btn--quick-action`]: isQuickAction,
-    [`${prefix}--chat-btn--quick-action--selected`]: isSelected,
-  });
+export type ChatButtonKind =
+  | 'primary'
+  | 'secondary'
+  | 'danger'
+  | 'ghost'
+  | 'tertiary';
+export type ChatButtonSize = 'sm' | 'md' | 'lg';
 
-  const allowedSizes = ['sm', 'md', 'lg'];
+export interface ChatButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  /**
+   * Provide the contents of your Select
+   */
+  children?: React.ReactNode;
+  /**
+   * Specify an optional className to be applied to the node containing the label and the select box
+   */
+  className?: string;
+  /**
+   * Specify whether the `ChatButton` should be disabled
+   */
+  disabled?: boolean;
+  /**
+   * Specify whether the `ChatButton` should be rendered as a quick action button
+   */
+  isQuickAction?: boolean;
+  /**
+   * Specify whether the quick action `ChatButton` should be rendered as selected. This disables the input
+   */
+  isSelected?: boolean;
+  /**
+   * Specify the kind of `ChatButton` you want to create
+   */
+  kind?: ChatButtonKind;
+  /**
+   * Optional prop to specify an icon to be rendered.
+   * Can be a React component class
+   */
+  renderIcon?: ComponentType | FunctionComponent;
+  /**
+   * Specify the size of the `ChatButton`, from the following list of sizes:
+   */
+  size?: ChatButtonSize;
+}
 
-  if (isQuickAction) {
-    kind = 'ghost';
-    size = 'sm';
-  } else {
-    // Do not allow size larger than `lg`
-    size = allowedSizes.includes(size) ? size : 'lg';
+const ChatButton = React.forwardRef<HTMLButtonElement, ChatButtonProps>(
+  function ChatButton(
+    {
+      className,
+      children,
+      disabled,
+      isQuickAction,
+      isSelected,
+      kind,
+      renderIcon,
+      size,
+      ...other
+    }: ChatButtonProps,
+    ref
+  ) {
+    const prefix = usePrefix();
+    const classNames = classnames(className, {
+      [`${prefix}--chat-btn`]: true,
+      [`${prefix}--chat-btn--with-icon`]: renderIcon,
+      [`${prefix}--chat-btn--quick-action`]: isQuickAction,
+      [`${prefix}--chat-btn--quick-action--selected`]: isSelected,
+    });
+
+    const allowedSizes: ChatButtonSize[] = ['sm', 'md', 'lg'];
+
+    if (isQuickAction) {
+      kind = 'ghost';
+      size = 'sm';
+    } else {
+      // Do not allow size larger than `lg`
+      size = allowedSizes.includes(size as ChatButtonSize) ? size : 'lg';
+    }
+
+    return (
+      <Button
+        disabled={disabled}
+        className={classNames}
+        kind={kind}
+        ref={ref}
+        size={size as ChatButtonSize}
+        renderIcon={renderIcon}
+        {...other}>
+        {children}
+      </Button>
+    );
   }
-
-  return (
-    <Button
-      disabled={disabled}
-      className={classNames}
-      kind={kind}
-      ref={ref}
-      size={size}
-      renderIcon={renderIcon}
-      {...other}>
-      {children}
-    </Button>
-  );
-});
+);
 
 ChatButton.propTypes = {
   /**
@@ -98,6 +145,7 @@ ChatButton.propTypes = {
    * Optional prop to specify an icon to be rendered.
    * Can be a React component class
    */
+  // @ts-expect-error: PropTypes are not expressive enough to cover this case
   renderIcon: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
 
   /**
