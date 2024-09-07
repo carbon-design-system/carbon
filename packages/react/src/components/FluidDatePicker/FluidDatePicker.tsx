9ba5595427d7a9cdbc7b0275cea9bef3641e3d8d
riddhybansal <riddhybansal@gmail.com>
2024-09-07 21:47:49 +0530

    fix: rename fluidDatePicker js files to tsx

--- packages/react/src/components/FluidDatePicker/FluidDatePicker.tsx
+++ packages/react/src/components/FluidDatePicker/FluidDatePicker.tsx
@@ -12,7 +12,41 @@ import DatePicker from '../DatePicker';
 import { usePrefix } from '../../internal/usePrefix';
 import { FormContext } from '../FluidForm/FormContext';
 
-const FluidDatePicker = React.forwardRef(function FluidDatePicker(
+export interface FluidDatePickerProps {
+  /**
+   * The child node(s)
+   */
+  children?: React.ReactNode;
+  /**
+   * Specify an optional className to be applied to the outer FluidForm wrapper
+   */
+  className?: string;
+  /**
+   * Specify whether or not the control is invalid
+   */
+  invalid?: boolean;
+  /**
+   * Provide the text that is displayed when the control is in error state
+   */
+  invalidText?: React.ReactNode;
+  /**
+   * Whether the input should be read-only
+   */
+  readOnly?: boolean;
+  /**
+   * Specify whether the control is currently in warning state
+   */
+  warn?: boolean;
+  /**
+   * Provide the text that is displayed when the control is in warning state
+   */
+  warnText?: React.ReactNode;
+}
+
+const FluidDatePicker: React.FC<FluidDatePickerProps> = React.forwardRef<
+  HTMLInputElement,
+  FluidDatePickerProps
+>(function FluidDatePicker(
   {
     className,
     children,
