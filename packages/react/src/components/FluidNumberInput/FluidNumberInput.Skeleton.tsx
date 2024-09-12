c49b590297411af79f17be770258a531674a86e4
Riddhi Bansal <41935566+riddhybansal@users.noreply.github.com>
2024-09-12 20:25:49 +0530

    Add typescript types to fluid number input (#17373)
    * fix: rename js files to tsx files
    
    * fix: added typescript types to fluid numberinput and skeleton
    
    ---------
    
    Co-authored-by: Gururaj J <89023023+Gururajj77@users.noreply.github.com>
    Co-authored-by: Taylor Jones <tay1orjones@users.noreply.github.com>

--- packages/react/src/components/FluidNumberInput/FluidNumberInput.Skeleton.tsx
+++ packages/react/src/components/FluidNumberInput/FluidNumberInput.Skeleton.tsx
@@ -11,9 +11,18 @@ import classnames from 'classnames';
 import { usePrefix } from '../../internal/usePrefix';
 import { FormContext } from '../FluidForm/FormContext';
 
-function FluidNumberInputSkeleton({ className, ...other }) {
-  const prefix = usePrefix();
+export interface FluidNumberInputSkeletonProps {
+  /**
+   * Specify an optional className to be applied to the outer FluidForm wrapper
+   */
+  className?: string;
+}
 
+const FluidNumberInputSkeleton: React.FC<FluidNumberInputSkeletonProps> = ({
+  className,
+  ...other
+}) => {
+  const prefix = usePrefix();
   return (
     <FormContext.Provider value={{ isFluid: true }}>
       <div
@@ -27,7 +36,7 @@ function FluidNumberInputSkeleton({ className, ...other }) {
       </div>
     </FormContext.Provider>
   );
-}
+};
 
 FluidNumberInputSkeleton.propTypes = {
   /**
