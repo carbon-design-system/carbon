f814d1ce22f0791585457a685597757d3471e4a7
guidari <guilhermedatilio@gmail.com>
2024-09-20 16:58:16 -0300

    test: added tests for FluiTimePicker

--- packages/react/src/components/FluidTimePicker/__tests__/FluidTimePicker-test.js
+++ packages/react/src/components/FluidTimePicker/__tests__/FluidTimePicker-test.js
@@ -9,7 +9,7 @@ import React from 'react';
 import FluidTimePicker from '../FluidTimePicker';
 import FluidTimePickerSelect from '../../FluidTimePickerSelect';
 import SelectItem from '../../SelectItem';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 const prefix = 'cds';
 
@@ -59,6 +59,82 @@ describe('FluidTimePicker', () => {
       );
 
       expect(container.firstChild).toHaveClass(`${prefix}--time-picker--fluid`);
+    });
+
+    it('returns invalidText when invalid is true', async () => {
+      render(
+        <FluidTimePicker
+          className="custom-class"
+          id="time=picker-1"
+          data-testid="timePicker-1"
+          labelText="Time"
+          invalid
+          invalidText="Invalid text">
+          <FluidTimePickerSelect id="select-1" labelText="Clock">
+            <SelectItem value="am" text="AM" />
+            <SelectItem value="pm" text="PM" />
+          </FluidTimePickerSelect>
+          <FluidTimePickerSelect id="select-2" labelText="Timezone">
+            <SelectItem value="et" text="Eastern Time (ET)" />
+            <SelectItem value="ct" text="Central Time (CT)" />
+            <SelectItem value="mt" text="Mountain Time (MT)" />
+            <SelectItem value="pt" text="Pacific Time (PT)" />
+          </FluidTimePickerSelect>
+        </FluidTimePicker>
+      );
+      await expect(screen.getByText('Invalid text')).toBeInTheDocument();
+    });
+
+    it('returns warnText when warn is true', async () => {
+      render(
+        <FluidTimePicker
+          className="custom-class"
+          id="time=picker-1"
+          data-testid="timePicker-1"
+          labelText="Time"
+          warn
+          warnText="Warn text">
+          <FluidTimePickerSelect id="select-1" labelText="Clock">
+            <SelectItem value="am" text="AM" />
+            <SelectItem value="pm" text="PM" />
+          </FluidTimePickerSelect>
+          <FluidTimePickerSelect id="select-2" labelText="Timezone">
+            <SelectItem value="et" text="Eastern Time (ET)" />
+            <SelectItem value="ct" text="Central Time (CT)" />
+            <SelectItem value="mt" text="Mountain Time (MT)" />
+            <SelectItem value="pt" text="Pacific Time (PT)" />
+          </FluidTimePickerSelect>
+        </FluidTimePicker>
+      );
+      await expect(screen.getByText('Warn text')).toBeInTheDocument();
+    });
+
+    it('should set disabled as expected', () => {
+      const onClick = jest.fn();
+
+      render(
+        <FluidTimePicker
+          className="custom-class"
+          id="time=picker-1"
+          data-testid="timePicker-1"
+          labelText="Time"
+          onClick={onClick}
+          disabled>
+          <FluidTimePickerSelect id="select-1" labelText="Clock">
+            <SelectItem value="am" text="AM" />
+            <SelectItem value="pm" text="PM" />
+          </FluidTimePickerSelect>
+          <FluidTimePickerSelect id="select-2" labelText="Timezone">
+            <SelectItem value="et" text="Eastern Time (ET)" />
+            <SelectItem value="ct" text="Central Time (CT)" />
+            <SelectItem value="mt" text="Mountain Time (MT)" />
+            <SelectItem value="pt" text="Pacific Time (PT)" />
+          </FluidTimePickerSelect>
+        </FluidTimePicker>
+      );
+
+      fireEvent.click(screen.getByRole('textbox'));
+      expect(onClick).not.toHaveBeenCalled();
     });
   });
 });
