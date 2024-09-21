23b8ff392ba2dd3314a17879920ebde7126dbfa8
Nikhil Tomar <63502271+2nikhiltom@users.noreply.github.com>
2024-09-22 02:08:33 +0530

    feat(upgrade): adds featureflag-deprecate-flags-prop codemod and deprecates flags prop (#17266)
    * fix: add new props to feature flag props
    
    * feat(upgrade): featureflag-deprecate-flags-prop codemod
    
    * feat(upgrade): codemod, test cases and fixtures updated
    
    * refactor: test fixtures
    
    * refactor: sets default flag values to false at destructuring
    
    * fix: skips transformation of files without FeatureFlags component
    
    * refactor: makes conditions readable
    
    * feat(upgrade):  removes old flags trasnformation, added tests
    
    * feat(upgrade): adds codemod to @carbon/upgrade migrate
    
    * chore: message updated
    
    * feat(upgrade): tests updates, old flag removed
    
    * fix: removes oldflag
    
    * fix: added pattern matching for .ts & .tsx
    
    * refactor: yarn dedupe changes
    
    * refactor: yarn dedupe change, snapshotss
    
    * fix: removes old flags, updates test outputs
    
    ---------
    
    Co-authored-by: riddhybansal <riddhybansal@gmail.com>
    Co-authored-by: kennylam <909118+kennylam@users.noreply.github.com>

--- packages/react/src/components/FeatureFlags/index.js
+++ packages/react/src/components/FeatureFlags/index.js
@@ -17,7 +17,7 @@ import React, {
   useRef,
   useState,
 } from 'react';
-
+import deprecate from '../../prop-types/deprecate';
 /**
  * Our FeatureFlagContext is used alongside the FeatureFlags component to enable
  * or disable feature flags in a given React tree
@@ -29,17 +29,35 @@ const FeatureFlagContext = createContext(GlobalFeatureFlags);
  * along with the current `FeatureFlagContext` to provide consumers to check if
  * a feature flag is enabled or disabled in a given React tree
  */
-function FeatureFlags({ children, flags = {} }) {
+function FeatureFlags({
+  children,
+  flags = {},
+  enableV12TileDefaultIcons = false,
+  enableV12TileRadioIcons = false,
+  enableV12Overflowmenu = false,
+  enableTreeviewControllable = false,
+  enableExperimentalFocusWrapWithoutSentinels = false,
+}) {
   const parentScope = useContext(FeatureFlagContext);
   const [prevParentScope, setPrevParentScope] = useState(parentScope);
+
+  const combinedFlags = {
+    'enable-v12-tile-default-icons': enableV12TileDefaultIcons,
+    'enable-v12-tile-radio-icons': enableV12TileRadioIcons,
+    'enable-v12-overflowmenu': enableV12Overflowmenu,
+    'enable-treeview-controllable': enableTreeviewControllable,
+    'enable-experimental-focus-wrap-without-sentinels':
+      enableExperimentalFocusWrapWithoutSentinels,
+    ...flags,
+  };
   const [scope, updateScope] = useState(() => {
-    const scope = createScope(flags);
+    const scope = createScope(combinedFlags);
     scope.mergeWithScope(parentScope);
     return scope;
   });
 
   if (parentScope !== prevParentScope) {
-    const scope = createScope(flags);
+    const scope = createScope(combinedFlags);
     scope.mergeWithScope(parentScope);
     updateScope(scope);
     setPrevParentScope(parentScope);
@@ -48,7 +66,7 @@ function FeatureFlags({ children, flags = {} }) {
   // We use a custom hook to detect if any of the keys or their values change
   // for flags that are passed in. If they have changed, then we re-create the
   // FeatureFlagScope using the new flags
-  useChangedValue(flags, isEqual, (changedFlags) => {
+  useChangedValue(combinedFlags, isEqual, (changedFlags) => {
     const scope = createScope(changedFlags);
     scope.mergeWithScope(parentScope);
     updateScope(scope);
@@ -65,9 +83,19 @@ FeatureFlags.propTypes = {
   children: PropTypes.node,
 
   /**
-   * Provide the feature flags to enabled or disabled in the current React tree
+   * Provide the feature flags to enabled or disabled in the current Rea,ct tree
    */
-  flags: PropTypes.objectOf(PropTypes.bool),
+  flags: deprecate(
+    PropTypes.objectOf(PropTypes.bool),
+    'The `flags` prop for `FeatureFlag` has ' +
+      'been deprecated. Please run the `featureflag-deprecate-flags-prop` codemod to migrate to individual boolean props.' +
+      `npx @carbon/upgrade migrate featureflag-deprecate-flags-prop --write`
+  ),
+  enableV12TileDefaultIcons: PropTypes.bool,
+  enableV12TileRadioIcons: PropTypes.bool,
+  enableV12Overflowmenu: PropTypes.bool,
+  enableTreeviewControllable: PropTypes.bool,
+  enableExperimentalFocusWrapWithoutSentinels: PropTypes.bool,
 };
 
 /**
@@ -113,7 +141,8 @@ function useChangedValue(value, compare, callback) {
  */
 function useFeatureFlag(flag) {
   const scope = useContext(FeatureFlagContext);
-  return scope.enabled(flag);
+  //updated to return false for undefined flags
+  return scope.enabled(flag) ?? false;
 }
 
 /**
