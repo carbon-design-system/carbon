56a80d9c9667da23f3c23eeb996747230b3b5c9a
Nikhil Tomar <nikhiltomar753@gmail.com>
2024-10-02 23:33:23 +0530

    refactor: removed unused import

--- packages/react/src/components/Tag/Tag-test.js
+++ packages/react/src/components/Tag/Tag-test.js
@@ -6,7 +6,7 @@
  */
 
 import { Add } from '@carbon/icons-react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import Tag, { TagSkeleton } from './';
 import DismissibleTag from './DismissibleTag';
