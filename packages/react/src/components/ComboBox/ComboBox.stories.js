f232cb606e949fa94101f464a8905a82e1e6859a
Noah Sgorbati <noahsgorbati@gmail.com>
2024-10-07 18:22:27 +0100

    fix: refactor and fix controlled combobox (#17527)
    * Refactor code related to controlled combobox for simplicity.
    * Update ComboBox when selectedItem prop changes externally.
    * Call onChange when combobox is cleared or updated.
    * Unit tests for these cases.
    * Storybook example for fully controlled component.

--- packages/react/src/components/ComboBox/ComboBox.stories.js
+++ packages/react/src/components/ComboBox/ComboBox.stories.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 
 import { WithLayer } from '../../../.storybook/templates/WithLayer';
 
@@ -213,6 +213,52 @@ export const withAILabel = () => (
     />
   </div>
 );
+
+export const _fullyControlled = () => {
+  const options = [
+    {
+      id: 'option-1',
+      text: 'Option 1',
+    },
+    {
+      id: 'option-2',
+      text: 'Option 2',
+    },
+    {
+      id: 'option-3',
+      text: 'Option 3',
+    },
+  ];
+  const [value, setValue] = useState(options[0]);
+  const onChange = ({ selectedItem }) => {
+    setValue(selectedItem);
+  };
+
+  return (
+    <div>
+      <ComboBox
+        onChange={onChange}
+        id="carbon-combobox"
+        items={options}
+        selectedItem={value}
+        itemToString={(item) => (item ? item.text : '')}
+        titleText="Fully Controlled ComboBox title"
+        helperText="Combobox helper text"
+      />
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}>
+        <Button onClick={() => setValue(null)}>Reset</Button>
+        <Button onClick={() => setValue(options[0])}>Option 1</Button>
+        <Button onClick={() => setValue(options[1])}>Option 2</Button>
+        <Button onClick={() => setValue(options[2])}>Option 3</Button>
+      </div>
+    </div>
+  );
+};
 
 export const Playground = (args) => (
   <div style={{ width: 300 }}>
