6040ad595884c0c0ff5507084857617a91fd38f1
guidari <guilhermedatilio@gmail.com>
2024-10-10 09:09:46 -0300

    fix: removed wrong import

--- packages/react/src/components/Tag/InteractiveTag.stories.js
+++ packages/react/src/components/Tag/InteractiveTag.stories.js
@@ -16,7 +16,6 @@ import mdx from './InteractiveTag.mdx';
 import './storyInteractiveTag.scss';
 import { Text } from '../Text';
 import Button from '../Button';
-import { AILabel } from '../AILabel';
 
 export default {
   title: 'Experimental/unstable__InteractiveTag',
