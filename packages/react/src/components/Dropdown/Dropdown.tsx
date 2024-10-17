fbdb4864c9722c858ec78479a7da806023c42a58
Shankar-CodeJunkie <emailmeforshankar@gmail.com>
2024-10-17 23:25:30 +0530

    test(Dropdown): increase test coverage to 100% for Dropdown component

--- packages/react/src/components/Dropdown/Dropdown.tsx
+++ packages/react/src/components/Dropdown/Dropdown.tsx
@@ -460,8 +460,8 @@ const Dropdown = React.forwardRef(
             // NOTE: does not prevent click
             evt.preventDefault();
             // focus on the element as per readonly input behavior
-            if (mergedRef.current !== undefined) {
-              mergedRef.current.focus();
+            if (mergedRef !== undefined) {
+              mergedRef?.current?.focus();
             }
           },
           onKeyDown: (evt: React.KeyboardEvent<HTMLButtonElement>) => {
@@ -480,7 +480,6 @@ const Dropdown = React.forwardRef(
             ) {
               setIsTyping(true);
             }
-
             if (
               (isTyping && evt.code === 'Space') ||
               !['ArrowDown', 'ArrowUp', ' ', 'Enter'].includes(evt.key)
