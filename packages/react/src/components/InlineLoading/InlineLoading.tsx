7b6c7ad82cd3336d7b7b7385106d360a97ef21a7
Daniel Adebonojo <daniel@bridgestudio.co>
2024-10-23 23:34:16 +0100

    Revert "fix: remove loading animation in inlineloading inactive state-"
    This reverts commit 5e7e769129fe28e5bc176d5dcd1172dc8aa2206b.

--- packages/react/src/components/InlineLoading/InlineLoading.tsx
+++ packages/react/src/components/InlineLoading/InlineLoading.tsx
@@ -94,9 +94,9 @@ const InlineLoading = ({
         </CheckmarkFilled>
       );
     }
-    if (status === 'inactive' || status === 'active') {
+    if (status === 'active') {
       if (!iconDescription) {
-        iconLabel = status === 'active' ? 'loading' : 'not loading';
+        iconLabel = 'loading';
       }
       return (
         <Loading
@@ -105,6 +105,16 @@ const InlineLoading = ({
           withOverlay={false}
           active={status === 'active'}
         />
+      );
+    }
+    if (status === 'inactive') {
+      if (!iconDescription) {
+        iconLabel = 'not loading';
+      }
+      return (
+        <title className={`${prefix}--inline-loading__inactive-status`}>
+          {iconLabel}
+        </title>
       );
     }
     return undefined;
