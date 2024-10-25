371a0b1ad8f0a6814b8912ef73961d3de11ff613
Soumya Raju <soumya.raju1@ibm.com>
2024-10-25 21:57:15 +0530

    test: overflow menu v2- adding tests

---
+++ packages/react/src/components/OverflowMenuV2/OverFlowMenuV2-test.js
@@ -0,0 +1,37 @@
+import { OverflowMenu } from '../OverflowMenu';
+import { OverflowMenuV2 } from './';
+import React from 'react';
+import { render } from '@testing-library/react';
+
+jest.mock('../OverflowMenu', () => ({
+  OverflowMenu: jest.fn(() => <div>Mocked OverflowMenu</div>),
+}));
+
+jest.mock('../FeatureFlags', () => ({
+  FeatureFlags: ({ children }) => <div>{children}</div>,
+}));
+
+describe('<OverflowMenuV2 />', () => {
+  let consoleWarnSpy;
+
+  beforeEach(() => {
+    consoleWarnSpy = jest.spyOn(console, 'warn').mockImplementation(() => {});
+    jest.clearAllMocks();
+  });
+
+  afterEach(() => {
+    consoleWarnSpy.mockRestore();
+  });
+
+  it('renders OverflowMenu wrapped in FeatureFlags', () => {
+    const { getByText } = render(<OverflowMenuV2 />);
+    expect(getByText('Mocked OverflowMenu')).toBeInTheDocument();
+  });
+
+  it('passes props to OverflowMenu', () => {
+    const props = { item1: 'value1', item2: 'value2' };
+    render(<OverflowMenuV2 {...props} />);
+
+    expect(OverflowMenu).toHaveBeenCalledWith(props, expect.anything());
+  });
+});
