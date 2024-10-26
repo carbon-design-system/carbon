dc8a21ff35989d71c36d680a8d2052d438638c42
Soumya Raju <soumya.raju1@ibm.com>
2024-10-26 13:42:54 +0530

    test: overflow menu v2- adding tests

--- packages/react/src/components/OverflowMenuV2/OverFlowMenuV2-test.js
+++ packages/react/src/components/OverflowMenuV2/OverFlowMenuV2-test.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright IBM Corp. 2016, 2024
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { OverflowMenu } from '../OverflowMenu';
 import { OverflowMenuV2 } from './';
 import React from 'react';
