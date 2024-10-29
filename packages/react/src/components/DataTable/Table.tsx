5c3ca7177032c558c1c72f8d742b70beba903e49
guidari <guilhermedatilio@gmail.com>
2024-10-29 14:08:40 -0300

    feat: exposed tabIndex to Table component

--- packages/react/src/components/DataTable/Table.tsx
+++ packages/react/src/components/DataTable/Table.tsx
@@ -54,6 +54,11 @@ interface TableProps {
    * `true` to add useZebraStyles striping.
    */
   useZebraStyles?: boolean;
+
+  /**
+   * Specify the table tabIndex
+   */
+  tabIndex?: number;
 }
 
 const isElementWrappingContent = (
@@ -108,6 +113,7 @@ export const Table = ({
   stickyHeader,
   overflowMenuOnHover = true,
   experimentalAutoAlign = false,
+  tabIndex,
   ...other
 }: PropsWithChildren<TableProps>) => {
   const { titleId, descriptionId } = useContext(TableContext);
@@ -226,7 +232,9 @@ export const Table = ({
     <div
       className={`${prefix}--data-table-content`}
       // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
-      tabIndex={isScrollable ? 0 : undefined}>
+      tabIndex={
+        tabIndex === undefined ? (isScrollable ? 0 : undefined) : tabIndex
+      }>
       <table
         aria-labelledby={titleId}
         aria-describedby={descriptionId}
@@ -287,6 +295,11 @@ Table.propTypes = {
    * `true` to add useZebraStyles striping.
    */
   useZebraStyles: PropTypes.bool,
+
+  /**
+   * Specify the table tabIndex
+   */
+  tabIndex: PropTypes.number,
 };
 
 export default Table;
