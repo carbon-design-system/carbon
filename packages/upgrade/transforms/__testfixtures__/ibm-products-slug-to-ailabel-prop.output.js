c297af51d5fef62ac27594bdb1d24e38009bae85
Sangeetha Babu <58620134+sangeethababu9223@users.noreply.github.com>
2024-11-06 12:17:42 +0530

    fix(upgrade): test fail fix

--- packages/upgrade/transforms/__testfixtures__/ibm-products-slug-to-ailabel-prop.output.js
+++ packages/upgrade/transforms/__testfixtures__/ibm-products-slug-to-ailabel-prop.output.js
@@ -6,9 +6,7 @@ const sampleAILabel = (
         <p className="secondary">AI Explained</p>
         <h1>84%</h1>
         <p className="secondary bold">Confidence score</p>
-        <p className="secondary">
-          test
-        </p>
+        <p className="secondary">test</p>
         <hr />
         <p className="secondary">Model type</p>
         <p className="bold">Foundation model</p>
