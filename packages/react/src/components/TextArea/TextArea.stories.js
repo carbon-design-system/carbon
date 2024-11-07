0cfda56b25704d47b28e97baadbd39915d72e92c
Ariella Gilmore <ariellalgilmore@gmail.com>
2024-11-06 20:48:50 -0800

    fix(ai-label): update classes from slug to ai-label (#17972)
    * fix(ai-label): update classes from slug to ai-label
    
    * fix(fluid-date-picker): add space
    
    * fix(fluid-list-box): merge conflict update
    
    * fix(format): update files
    
    * fix(classes): remove additions
    
    * fix(format): updates
    
    * fix(tag): comma
    
    * fix(format): updates
    
    * fix(date-picker): update styles
    
    * fix(date-picker): format
    
    * fix(date-picker): fluid gradient
    
    * fix(date-picker): format
    
    ---------
    
    Co-authored-by: Guilherme Datilio Ribeiro <guilhermedatilio@gmail.com>

--- packages/react/src/components/TextArea/TextArea.stories.js
+++ packages/react/src/components/TextArea/TextArea.stories.js
@@ -52,7 +52,7 @@ export const _WithLayer = () => (
 );
 
 const aiLabel = (
-  <AILabel className="slug-container">
+  <AILabel className="ai-label-container">
     <AILabelContent>
       <div>
         <p className="secondary">AI Explained</p>
