ce0950c9f1c9596066a15ea6ce2b2ab1dc8ef956
Lee Chase <lee.chase@uk.ibm.com>
2024-11-11 19:05:10 +0000

    fix: remove unused html import from column

--- packages/web-components/src/components/grid/column.ts
+++ packages/web-components/src/components/grid/column.ts
@@ -7,7 +7,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { LitElement, html } from 'lit';
+import { LitElement } from 'lit';
 import { property } from 'lit/decorators.js';
 import { carbonElement as customElement } from '../../globals/decorators/carbon-element';
 import { prefix } from '../../globals/settings';
