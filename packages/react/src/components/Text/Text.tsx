c79662924efc8f1dce662c1f5ce4a1d94e31aaec
guidari <guilhermedatilio@gmail.com>
2024-11-25 10:21:01 -0300

    feat: adding reusable polymorphicProps

--- packages/react/src/components/Text/Text.tsx
+++ packages/react/src/components/Text/Text.tsx
@@ -6,64 +6,91 @@
  */
 
 import PropTypes from 'prop-types';
-import React, { ReactNode, useContext, useRef } from 'react';
-import { PolymorphicProps } from '../../types/common';
+import React, { ReactNode, useContext } from 'react';
 import { TextDir } from './TextDirection';
 import { TextDirectionContext } from './TextDirectionContext';
-import { useMergeRefs } from '@floating-ui/react';
 
 export interface TextBaseProps {
   dir?: TextDir | undefined;
+  children?: ReactNode;
 }
 
-export type TextProps<T extends React.ElementType> = PolymorphicProps<
-  T,
-  TextBaseProps
->;
-const Text = React.forwardRef(function Text<T extends React.ElementType>(
-  { as, children, dir = 'auto', ...rest }: TextProps<T>,
-  ref: React.Ref<HTMLElement>
-) {
-  // TODO: Update with context typing once its been converted to TS
-  const context = useContext<any>(TextDirectionContext);
-  const textProps: { dir?: TextDir } = {};
-  const BaseComponent = as ?? 'span';
-  const value = {
-    ...context,
-  };
+type AsProp<C extends React.ElementType> = {
+  as?: C;
+};
 
-  if (!context) {
-    textProps.dir = dir;
-    value.direction = dir;
-  } else {
-    const { direction: parentDirection, getTextDirection } = context;
+type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P);
 
-    if (getTextDirection && getTextDirection.current) {
-      const text = getTextFromChildren(children);
-      const override = getTextDirection.current(text);
+// This is the first reusable type utility we built
+type PolymorphicComponentProp<
+  C extends React.ElementType,
+  Props = {},
+> = React.PropsWithChildren<Props & AsProp<C>> &
+  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
 
-      if (parentDirection !== override) {
-        textProps.dir = override;
-        value.direction = override;
-      } else if (parentDirection === 'auto') {
-        textProps.dir = override;
-      }
-    } else if (parentDirection !== dir) {
+// This is a new type utitlity with ref!
+type PolymorphicComponentPropWithRef<
+  C extends React.ElementType,
+  Props = {},
+> = PolymorphicComponentProp<C, Props> & { ref?: PolymorphicRef<C> };
+
+// This is the type for the "ref" only
+type PolymorphicRef<C extends React.ElementType> =
+  React.ComponentPropsWithRef<C>['ref'];
+
+export type TextProps<T extends React.ElementType> =
+  PolymorphicComponentPropWithRef<T, TextBaseProps>;
+
+type TextComponent = <T extends React.ElementType = 'span'>(
+  props: TextProps<T>
+) => React.ReactElement | any;
+
+const Text: TextComponent = React.forwardRef(
+  <T extends React.ElementType = 'span'>(
+    { as, children, dir = 'auto', ...rest }: TextProps<T>,
+    ref?: PolymorphicRef<T>
+  ) => {
+    // TODO: Update with context typing once its been converted to TS
+    const context = useContext<any>(TextDirectionContext);
+    const textProps: { dir?: TextDir } = {};
+    const BaseComponent = as ?? 'span';
+    const value = {
+      ...context,
+    };
+
+    if (!context) {
       textProps.dir = dir;
       value.direction = dir;
-    } else if (parentDirection === 'auto') {
-      textProps.dir = dir;
-    }
-  }
+    } else {
+      const { direction: parentDirection, getTextDirection } = context;
 
-  return (
-    <TextDirectionContext.Provider value={value}>
-      <BaseComponent ref={ref} {...rest} {...textProps}>
-        {children}
-      </BaseComponent>
-    </TextDirectionContext.Provider>
-  );
-});
+      if (getTextDirection && getTextDirection.current) {
+        const text = getTextFromChildren(children);
+        const override = getTextDirection.current(text);
+
+        if (parentDirection !== override) {
+          textProps.dir = override;
+          value.direction = override;
+        } else if (parentDirection === 'auto') {
+          textProps.dir = override;
+        }
+      } else if (parentDirection !== dir) {
+        textProps.dir = dir;
+        value.direction = dir;
+      } else if (parentDirection === 'auto') {
+        textProps.dir = dir;
+      }
+    }
+
+    return (
+      <TextDirectionContext.Provider value={value}>
+        <BaseComponent ref={ref} {...rest} {...textProps}>
+          {children}
+        </BaseComponent>
+      </TextDirectionContext.Provider>
+    );
+  }
+);
 
 Text.propTypes = {
   /**
