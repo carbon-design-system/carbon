e950edd1e149e0a0d743f4211e91f67293858583
guidari <guilhermedatilio@gmail.com>
2024-12-26 11:48:37 -0300

    feat: added PolymorphicProps to a new file

--- packages/react/src/components/Text/Text.tsx
+++ packages/react/src/components/Text/Text.tsx
@@ -9,34 +9,15 @@ import PropTypes from 'prop-types';
 import React, { ReactNode, useContext } from 'react';
 import { TextDir } from './TextDirection';
 import { TextDirectionContext } from './TextDirectionContext';
+import {
+  PolymorphicComponentPropWithRef,
+  PolymorphicRef,
+} from '../../internal/PolymorphicProps';
 
 export interface TextBaseProps {
   dir?: TextDir | undefined;
   children?: ReactNode;
 }
-
-type AsProp<C extends React.ElementType> = {
-  as?: C;
-};
-
-type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P);
-
-// This is the first reusable type utility we built
-type PolymorphicComponentProp<
-  C extends React.ElementType,
-  Props = {},
-> = React.PropsWithChildren<Props & AsProp<C>> &
-  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
-
-// This is a new type utitlity with ref!
-type PolymorphicComponentPropWithRef<
-  C extends React.ElementType,
-  Props = {},
-> = PolymorphicComponentProp<C, Props> & { ref?: PolymorphicRef<C> };
-
-// This is the type for the "ref" only
-type PolymorphicRef<C extends React.ElementType> =
-  React.ComponentPropsWithRef<C>['ref'];
 
 export type TextProps<T extends React.ElementType> =
   PolymorphicComponentPropWithRef<T, TextBaseProps>;
@@ -92,7 +73,7 @@ const Text: TextComponent = React.forwardRef(
   }
 );
 
-Text.propTypes = {
+(Text as React.FC).propTypes = {
   /**
    * Provide a custom element type used to render the outermost node
    */
