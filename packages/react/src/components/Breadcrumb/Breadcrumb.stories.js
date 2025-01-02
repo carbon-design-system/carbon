eab657f4f6ea42d9934bdd0936677a5220a26dbc
Guilherme Datilio Ribeiro <guilhermedatilio@gmail.com>
2025-01-02 14:10:30 -0300

    Storybook Playground and Stackblitz (#18250)
    * feat: converted all stories from Playground to Default
    
    * feat: added @stackblitz/sdk
    
    * fix: removed unsed stuff
    
    * feat: added new replace and new prop to codePreview
    
    * feat: added final stack/sdk code
    
    * fix: fixed Tooltip storybook
    
    * fix: fixed stackblitz/sdk package
    
    * test: fixed avt tests
    
    * fix: changed last playground stories

--- packages/react/src/components/Breadcrumb/Breadcrumb.stories.js
+++ packages/react/src/components/Breadcrumb/Breadcrumb.stories.js
@@ -27,8 +27,16 @@ export default {
   },
 };
 
-export const Default = () => (
-  <Breadcrumb>
+const sharedArgTypes = {
+  children: {
+    table: {
+      disable: true,
+    },
+  },
+};
+
+export const Default = (args) => (
+  <Breadcrumb {...args}>
     <BreadcrumbItem>
       <a href="/#">Breadcrumb 1</a>
     </BreadcrumbItem>
@@ -38,8 +46,12 @@ export const Default = () => (
   </Breadcrumb>
 );
 
-export const BreadcrumbWithOverflowMenu = () => (
-  <Breadcrumb noTrailingSlash>
+Default.argTypes = {
+  ...sharedArgTypes,
+};
+
+export const BreadcrumbWithOverflowMenu = (args) => (
+  <Breadcrumb noTrailingSlash {...args}>
     <BreadcrumbItem>
       <a href="/#">Breadcrumb 1</a>
     </BreadcrumbItem>
@@ -55,23 +67,8 @@ export const BreadcrumbWithOverflowMenu = () => (
   </Breadcrumb>
 );
 
-export const Skeleton = () => <BreadcrumbSkeleton />;
-
-export const Playground = (args) => (
-  <Breadcrumb {...args}>
-    <BreadcrumbItem>
-      <a href="/#">Breadcrumb 1</a>
-    </BreadcrumbItem>
-    <BreadcrumbItem href="#">Breadcrumb 2</BreadcrumbItem>
-    <BreadcrumbItem href="#">Breadcrumb 3</BreadcrumbItem>
-    <BreadcrumbItem href="#">Breadcrumb 4</BreadcrumbItem>
-  </Breadcrumb>
-);
-
-Playground.argTypes = {
-  children: {
-    table: {
-      disable: true,
-    },
-  },
+BreadcrumbWithOverflowMenu.argTypes = {
+  ...sharedArgTypes,
 };
+
+export const Skeleton = () => <BreadcrumbSkeleton />;
