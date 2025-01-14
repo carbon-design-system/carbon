f831710af2bb84124edb7ff75ed7bc4f6cb6661a
ariellalgilmore <ariellalgilmore@gmail.com>
2025-01-13 17:09:21 -0800

    fix(button): revert

--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -196,9 +196,7 @@ const Button = React.forwardRef(function Button<T extends React.ElementType>(
         onClick={composeEventHandlers([onClick, handleClick])}
         renderIcon={iconOnlyImage ? null : ButtonImageElement} // avoid doubling the icon.
       >
-        {/* // TODO: SHOULD THIS BE CHANGED? */}
-        {iconOnlyImage}
-        {children}
+        {iconOnlyImage ?? children}
       </IconButton>
     );
   }
