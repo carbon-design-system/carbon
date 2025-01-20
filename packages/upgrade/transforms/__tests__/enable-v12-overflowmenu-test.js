d050605d0c1266003d0cf2e8a0ea447c882dd3cc
Riddhi Bansal <41935566+riddhybansal@users.noreply.github.com>
2025-01-20 14:32:32 +0530

    Update packages/upgrade/transforms/__tests__/enable-v12-overflowmenu-test.js
    Co-authored-by: Taylor Jones <tay1orjones@users.noreply.github.com>

--- packages/upgrade/transforms/__tests__/enable-v12-overflowmenu-test.js
+++ packages/upgrade/transforms/__tests__/enable-v12-overflowmenu-test.js
@@ -1,4 +1,12 @@
-// __tests__/enable-v12-overflowmenu-test.js
+/**
+ * Copyright IBM Corp. 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
 const { defineTest } = require('jscodeshift/dist/testUtils');
 
 // Test with wrapping (default)
