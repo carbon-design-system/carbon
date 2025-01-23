635e11ab7fe5630b77a040ed5edef603cd860bdc
Afsal K <makafsal@gmail.com>
2025-01-23 17:20:53 +0530

    fix(select): remove playground and resolve invalid state style

--- packages/web-components/src/components/select/select.stories.ts
+++ packages/web-components/src/components/select/select.stories.ts
@@ -140,107 +140,11 @@ const argTypes = {
   },
 };
 
+const defaultArgs = {
+  helperText: 'Optional helper text',
+};
+
 export const Default = {
-  render: () => {
-    return html`
-      <cds-form-item>
-        <cds-select
-          helper-text="Optional helper text"
-          label-text="Select an option"
-          placeholder="Choose an option">
-          <cds-select-item-group label="Category 1">
-            <cds-select-item value="all">Option 1</cds-select-item>
-            <cds-select-item value="cloudFoundry">Option 2</cds-select-item>
-          </cds-select-item-group>
-          <cds-select-item-group label="Category 2">
-            <cds-select-item value="staging">Option 3</cds-select-item>
-            <cds-select-item value="dea">Option 4</cds-select-item>
-            <cds-select-item value="router">Option 5</cds-select-item>
-          </cds-select-item-group>
-        </cds-select>
-      </cds-form-item>
-    `;
-  },
-};
-
-export const Inline = {
-  render: () => {
-    return html`
-      <cds-form-item>
-        <cds-select
-          inline
-          helper-text="Optional helper text"
-          label-text="Select an option"
-          placeholder="Choose an option">
-          <cds-select-item-group label="Category 1">
-            <cds-select-item value="all">Option 1</cds-select-item>
-            <cds-select-item value="cloudFoundry">Option 2</cds-select-item>
-          </cds-select-item-group>
-          <cds-select-item-group label="Category 2">
-            <cds-select-item value="staging">Option 3</cds-select-item>
-            <cds-select-item value="dea">Option 4</cds-select-item>
-            <cds-select-item value="router">Option 5</cds-select-item>
-          </cds-select-item-group>
-        </cds-select>
-      </cds-form-item>
-    `;
-  },
-};
-
-export const Skeleton = {
-  parameters: {
-    percy: {
-      skip: true,
-    },
-  },
-  render: () => html` <cds-select-skeleton></cds-select-skeleton> `,
-};
-
-export const WithAILabel = {
-  render: () =>
-    html` <div style="width: 400px">
-      <cds-select
-        helper-text="Optional helper text"
-        label-text="Select an option">
-        <cds-ai-label alignment="bottom-left">
-          ${content}${actions}</cds-ai-label
-        >
-        <cds-select-item-group label="Category 1">
-          <cds-select-item value="all">Option 1</cds-select-item>
-          <cds-select-item value="cloudFoundry">Option 2</cds-select-item>
-        </cds-select-item-group>
-        <cds-select-item-group label="Category 2">
-          <cds-select-item value="staging">Option 3</cds-select-item>
-          <cds-select-item value="dea">Option 4</cds-select-item>
-          <cds-select-item value="router">Option 5</cds-select-item>
-        </cds-select-item-group>
-      </cds-select>
-    </div>`,
-};
-
-export const WithLayer = {
-  render: () => {
-    return html`
-      <sb-template-layers>
-        <cds-select
-          helper-text="Optional helper text"
-          placeholder="Choose an option">
-          <cds-select-item-group label="Category 1">
-            <cds-select-item value="all">Option 1</cds-select-item>
-            <cds-select-item value="cloudFoundry">Option 2</cds-select-item>
-          </cds-select-item-group>
-          <cds-select-item-group label="Category 2">
-            <cds-select-item value="staging">Option 3</cds-select-item>
-            <cds-select-item value="dea">Option 4</cds-select-item>
-            <cds-select-item value="router">Option 5</cds-select-item>
-          </cds-select-item-group>
-        </cds-select>
-      </sb-template-layers>
-    `;
-  },
-};
-
-export const Playground = {
   args,
   argTypes,
   render: (args) => {
@@ -293,6 +197,200 @@ export const Playground = {
           ${children}
         </cds-select>
       </cds-form-item>
+    `;
+  },
+};
+
+export const Inline = {
+  args: {
+    ...args,
+    inline: true,
+  },
+  argTypes,
+  render: (args) => {
+    const {
+      disabled,
+      helperText,
+      hideLabel,
+      inline,
+      invalid,
+      invalidText,
+      labelText,
+      name,
+      placeholder,
+      size,
+      readOnly,
+      warn,
+      warnText,
+      value,
+      children = html`
+        <cds-select-item-group label="Category 1">
+          <cds-select-item value="all">Option 1</cds-select-item>
+          <cds-select-item value="cloudFoundry">Option 2</cds-select-item>
+        </cds-select-item-group>
+        <cds-select-item-group label="Category 2">
+          <cds-select-item value="staging">Option 3</cds-select-item>
+          <cds-select-item value="dea">Option 4</cds-select-item>
+          <cds-select-item value="router">Option 5</cds-select-item>
+        </cds-select-item-group>
+      `,
+      onInput,
+    } = args ?? {};
+    return html`
+      <cds-form-item>
+        <cds-select
+          ?inline="${inline}"
+          ?disabled="${disabled}"
+          helper-text="${ifDefined(helperText)}"
+          ?hide-label="${hideLabel}"
+          ?invalid="${invalid}"
+          invalid-text="${ifDefined(invalidText)}"
+          label-text="${ifDefined(labelText)}"
+          name="${ifDefined(name)}"
+          placeholder="${ifDefined(placeholder)}"
+          size="${ifDefined(size)}"
+          ?readonly="${readOnly}"
+          ?warn="${warn}"
+          warn-text="${ifDefined(warnText)}"
+          value="${ifDefined(value)}"
+          @cds-select-selected="${ifDefined(onInput)}">
+          ${children}
+        </cds-select>
+      </cds-form-item>
+    `;
+  },
+};
+
+export const Skeleton = {
+  parameters: {
+    percy: {
+      skip: true,
+    },
+  },
+  render: () => html` <cds-select-skeleton></cds-select-skeleton> `,
+};
+
+export const WithAILabel = {
+  args,
+  argTypes: {
+    ...argTypes,
+    inline: {
+      control: false,
+    },
+  },
+  render: (args) => {
+    const {
+      disabled,
+      helperText,
+      hideLabel,
+      invalid,
+      invalidText,
+      labelText,
+      name,
+      placeholder,
+      size,
+      readOnly,
+      warn,
+      warnText,
+      value,
+      children = html`
+        <cds-select-item-group label="Category 1">
+          <cds-select-item value="all">Option 1</cds-select-item>
+          <cds-select-item value="cloudFoundry">Option 2</cds-select-item>
+        </cds-select-item-group>
+        <cds-select-item-group label="Category 2">
+          <cds-select-item value="staging">Option 3</cds-select-item>
+          <cds-select-item value="dea">Option 4</cds-select-item>
+          <cds-select-item value="router">Option 5</cds-select-item>
+        </cds-select-item-group>
+      `,
+      onInput,
+    } = args ?? {};
+
+    return html` <div style="width: 400px">
+      <cds-select
+        ?inline="${false}"
+        ?disabled="${disabled}"
+        helper-text="${ifDefined(helperText)}"
+        ?hide-label="${hideLabel}"
+        ?invalid="${invalid}"
+        invalid-text="${ifDefined(invalidText)}"
+        label-text="${ifDefined(labelText)}"
+        name="${ifDefined(name)}"
+        placeholder="${ifDefined(placeholder)}"
+        size="${ifDefined(size)}"
+        ?readonly="${readOnly}"
+        ?warn="${warn}"
+        warn-text="${ifDefined(warnText)}"
+        value="${ifDefined(value)}"
+        @cds-select-selected="${ifDefined(onInput)}">
+        <cds-ai-label alignment="bottom-left">
+          ${content}${actions}</cds-ai-label
+        >
+        ${children}
+      </cds-select>
+    </div>`;
+  },
+};
+
+export const WithLayer = {
+  args,
+  argTypes: {
+    ...argTypes,
+    inline: {
+      control: false,
+    },
+  },
+  render: (args) => {
+    const {
+      disabled,
+      helperText,
+      hideLabel,
+      invalid,
+      invalidText,
+      labelText,
+      name,
+      placeholder,
+      size,
+      readOnly,
+      warn,
+      warnText,
+      value,
+      children = html`
+        <cds-select-item-group label="Category 1">
+          <cds-select-item value="all">Option 1</cds-select-item>
+          <cds-select-item value="cloudFoundry">Option 2</cds-select-item>
+        </cds-select-item-group>
+        <cds-select-item-group label="Category 2">
+          <cds-select-item value="staging">Option 3</cds-select-item>
+          <cds-select-item value="dea">Option 4</cds-select-item>
+          <cds-select-item value="router">Option 5</cds-select-item>
+        </cds-select-item-group>
+      `,
+      onInput,
+    } = args ?? {};
+
+    return html`
+      <sb-template-layers>
+        <cds-select
+          ?inline="${false}"
+          ?disabled="${disabled}"
+          helper-text="${ifDefined(helperText)}"
+          ?hide-label="${hideLabel}"
+          ?invalid="${invalid}"
+          invalid-text="${ifDefined(invalidText)}"
+          label-text="${ifDefined(labelText)}"
+          name="${ifDefined(name)}"
+          placeholder="${ifDefined(placeholder)}"
+          size="${ifDefined(size)}"
+          ?readonly="${readOnly}"
+          ?warn="${warn}"
+          warn-text="${ifDefined(warnText)}"
+          value="${ifDefined(value)}"
+          @cds-select-selected="${ifDefined(onInput)}">
+          ${children}
+        </cds-select>
+      </sb-template-layers>
     `;
   },
 };
