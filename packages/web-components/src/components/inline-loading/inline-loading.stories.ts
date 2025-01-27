c10058d25f661f1b20094ea0b29393fedfc69a17
Amal K Joy <amal.joy.k@gmail.com>
2025-01-27 15:48:50 +0530

    fix: remove playground story

--- packages/web-components/src/components/inline-loading/inline-loading.stories.ts
+++ packages/web-components/src/components/inline-loading/inline-loading.stories.ts
@@ -29,97 +29,43 @@ const defaultArgs = {
   successDelay: 2000,
 };
 
-const controls = {
-  description: {
-    control: 'text',
-    description: 'Specify the description for the inline loading text.',
-  },
-  assistiveText: {
-    control: 'text',
-    description:
-      'The `assistiveText` property will be deprecated in the next major release. Please use `iconDescription` instead.',
-  },
-  iconDescription: {
-    control: 'text',
-    description:
-      'Specify a description that would be used to best describe the loading state.',
-  },
-  status: {
-    control: 'select',
-    description: 'Specify the loading status.',
-    options: states,
-  },
-  onSuccess: {
-    action: `${prefix}-inline-loading-onsuccess`,
-    description: 'Provide an optional handler to be invoked when is successful',
-  },
-  successDelay: {
-    description: 'Provide a delay for the setTimeout for success',
-  },
+const getControls = ({ disableControl }) => {
+  return {
+    description: {
+      control: disableControl || 'text',
+      description: 'Specify the description for the inline loading text.',
+    },
+    assistiveText: {
+      control: disableControl || 'text',
+      description:
+        'The `assistiveText` property will be deprecated in the next major release. Please use `iconDescription` instead.',
+    },
+    iconDescription: {
+      control: disableControl || 'text',
+      description:
+        'Specify a description that would be used to best describe the loading state.',
+    },
+    status: {
+      control: disableControl || 'select',
+      description: 'Specify the loading status.',
+      options: states,
+    },
+    onSuccess: {
+      control: disableControl,
+      action: `${prefix}-inline-loading-onsuccess`,
+      description:
+        'Provide an optional handler to be invoked when is successful',
+    },
+    successDelay: {
+      control: disableControl,
+      description: 'Provide a delay for the setTimeout for success',
+    },
+  };
 };
+
 export const Default = {
-  render: () => html`<cds-inline-loading>Loading data...</cds-inline-loading>`,
-};
-
-export const UxExample = () => {
-  const onSubmit = () => {
-    const submit = document.querySelector('#submit');
-    const cancel = document
-      .querySelector('#cancel')
-      ?.shadowRoot?.querySelector('button');
-    const loadingElem = document.querySelector('cds-inline-loading');
-
-    if (loadingElem) {
-      (loadingElem as HTMLElement).style.display = 'inherit';
-
-      loadingElem.setAttribute('status', 'active');
-      loadingElem.setAttribute('aria-live', 'assertive');
-    }
-
-    submit && ((submit as HTMLElement).style.display = 'none');
-
-    cancel && (cancel.disabled = true);
-
-    // Instead of making a real request, we mock it with a timer
-    setTimeout(() => {
-      loadingElem && loadingElem.setAttribute('status', 'finished');
-
-      // To make submittable again, we reset the state after a bit so the user gets completion feedback
-      setTimeout(() => {
-        loadingElem && ((loadingElem as HTMLElement).style.display = 'none');
-
-        submit && ((submit as HTMLElement).style.display = 'block');
-
-        cancel && (cancel.disabled = false);
-        loadingElem && loadingElem.setAttribute('aria-live', 'off');
-      }, 1500);
-    }, 2000);
-  };
-
-  const onSuccess = () => {
-    console.log('on success');
-  };
-
-  return html`<div style="display:flex;width:300px">
-    <cds-button kind="secondary" id="cancel"> Cancel </cds-button>
-    <cds-button @click=${onSubmit} id="submit">Submit</cds-button>
-    <cds-inline-loading
-        style='display:none;margin-left:1rem'
-        success-delay=${2000}
-        icon-description='Submitting'
-        aria-live='off'
-        @cds-inline-loading-onsuccess=${onSuccess}
-        >
-       Submitting
-      </cds-inline-loading>
-   
-   </div></div>
-    `;
-};
-
-export const Playground = {
   args: defaultArgs,
-  argTypes: controls,
+  argTypes: getControls({ disableControl: false }),
   parameters: {
     percy: { skip: true },
   },
@@ -141,6 +87,64 @@ export const Playground = {
         ${description}
       </cds-inline-loading>
     `;
+  },
+};
+export const UxExample = {
+  args: defaultArgs,
+  argTypes: getControls({ disableControl: true }),
+  parameters: {
+    percy: { skip: true },
+  },
+  render: ({ onSuccess = noop }) => {
+    const onSubmit = () => {
+      const submit = document.querySelector('#submit');
+      const cancel = document
+        .querySelector('#cancel')
+        ?.shadowRoot?.querySelector('button');
+      const loadingElem = document.querySelector('cds-inline-loading');
+
+      if (loadingElem) {
+        (loadingElem as HTMLElement).style.display = 'inherit';
+
+        loadingElem.setAttribute('status', 'active');
+        loadingElem.setAttribute('aria-live', 'assertive');
+      }
+
+      submit && ((submit as HTMLElement).style.display = 'none');
+
+      cancel && (cancel.disabled = true);
+
+      // Instead of making a real request, we mock it with a timer
+      setTimeout(() => {
+        loadingElem && loadingElem.setAttribute('status', 'finished');
+
+        // To make submittable again, we reset the state after a bit so the user gets completion feedback
+        setTimeout(() => {
+          loadingElem && ((loadingElem as HTMLElement).style.display = 'none');
+
+          submit && ((submit as HTMLElement).style.display = 'block');
+
+          cancel && (cancel.disabled = false);
+          loadingElem && loadingElem.setAttribute('aria-live', 'off');
+        }, 1500);
+      }, 2000);
+    };
+
+    return html`<div style="display:flex;width:300px">
+        <cds-button kind="secondary" id="cancel"> Cancel </cds-button>
+        <cds-button @click=${onSubmit} id="submit">Submit</cds-button>
+        <cds-inline-loading
+            style='display:none;margin-left:1rem'
+            success-delay=${2000}
+            icon-description='Submitting'
+            aria-live='off'
+            @cds-inline-loading-onsuccess=${onSuccess}
+            >
+           Submitting
+          </cds-inline-loading>
+       
+       </div></div>
+        `;
   },
 };
 
