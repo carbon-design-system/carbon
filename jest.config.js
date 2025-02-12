70118858dc56498e203ce2f3faf28c65a4e7079f
ariellalgilmore <ariellalgilmore@gmail.com>
2025-02-11 17:16:33 -0800

    fix(coverage): ignore wc

--- jest.config.js
+++ jest.config.js
@@ -18,6 +18,7 @@ module.exports = {
     '!**/*.stories.js',
     '!**/*-test.e2e.js',
   ],
+  coveragePathIgnorePatterns: ['packages/web-components/*'],
   testPathIgnorePatterns: ['packages/web-components/*'],
   transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es)'],
   moduleNameMapper: {
