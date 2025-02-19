e43c18a907bdad69b65745b251c24819ff49b731
Taylor Jones <taylor.jones826@gmail.com>
2025-02-19 12:39:18 -0600

    fix(es-toolkit): update import path to es-toolkit/dist/compat

--- packages/react/src/components/Modal/Modal.tsx
+++ packages/react/src/components/Modal/Modal.tsx
@@ -19,7 +19,7 @@ import wrapFocus, {
   wrapFocusWithoutSentinels,
   elementOrParentIsFloatingMenu,
 } from '../../internal/wrapFocus';
-import { debounce } from 'es-toolkit/compat';
+import { debounce } from 'es-toolkit/dist/compat';
 import useIsomorphicEffect from '../../internal/useIsomorphicEffect';
 import { useId } from '../../internal/useId';
 import { usePrefix } from '../../internal/usePrefix';
