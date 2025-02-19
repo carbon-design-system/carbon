8e67d6a79b251efb9a922045c6273f943836cf2c
Adam Alston <aalston9@gmail.com>
2025-02-19 13:40:09 -0500

    docs: correct spelling throughout codebase

--- packages/react/src/components/ComboBox/ComboBox.tsx
+++ packages/react/src/components/ComboBox/ComboBox.tsx
@@ -366,7 +366,7 @@ export interface ComboBoxProps<ItemType>
   titleText?: ReactNode;
 
   /**
-   * **Experimental**: will enable autcomplete and typeahead for the input field
+   * **Experimental**: will enable autocomplete and typeahead for the input field
    */
   typeahead?: boolean;
 
@@ -1360,7 +1360,7 @@ ComboBox.propTypes = {
   translateWithId: PropTypes.func,
 
   /**
-   * **Experimental**: will enable autcomplete and typeahead for the input field
+   * **Experimental**: will enable autocomplete and typeahead for the input field
    */
   typeahead: PropTypes.bool,
 
