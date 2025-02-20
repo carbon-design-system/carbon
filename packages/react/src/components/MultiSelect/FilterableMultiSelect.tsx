e2d106a2e76b2f453eafb3847f1da415a8602bda
Adam Alston <aalston9@gmail.com>
2025-02-20 15:47:18 -0500

    refactor: rewrite selection in typescript (#18642)
    * refactor: rewrite selection in typescript
    
    * refactor: delete todo
    
    * refactor: improve selection types
    
    * perf: optimize selection handling

--- packages/react/src/components/MultiSelect/FilterableMultiSelect.tsx
+++ packages/react/src/components/MultiSelect/FilterableMultiSelect.tsx
@@ -373,9 +373,6 @@ const FilterableMultiSelect = React.forwardRef(function FilterableMultiSelect<
     clearSelection,
   } = useSelection({
     disabled,
-    // TODO: remove @ts-ignore when type is fixed,
-    // see https://github.com/carbon-design-system/carbon/issues/18575
-    // @ts-ignore
     initialSelectedItems,
     onChange,
     selectedItems: selected,
