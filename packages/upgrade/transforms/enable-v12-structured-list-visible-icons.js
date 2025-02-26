f223b5d3339045cf26735b425e981591a8a70058
Nikhil Tomar <nikhiltomar753@gmail.com>
2025-02-26 18:34:26 +0530

    chore: refactor and clean ups

--- packages/upgrade/transforms/enable-v12-structured-list-visible-icons.js
+++ packages/upgrade/transforms/enable-v12-structured-list-visible-icons.js
@@ -23,274 +23,166 @@ function transform(fileInfo, api, options) {
   const j = api.jscodeshift;
   const root = j(fileInfo.source);
   const printOptions = options.printOptions || defaultOptions;
-  const rowGeneratorFunctions = new Map(); // Using Map for better performance
 
-  // Collect functions that generate StructuredListRows
-  collectRowGenerators(j, root, rowGeneratorFunctions);
+  // Find all row generator functions regardless of name
+  transformRowGeneratorFunctions(j, root);
 
-  // Transform components inside selection wrappers
-  transformSelectionWrappers(j, root, rowGeneratorFunctions);
+  // Process all wrappers with selection prop
+  transformSelectionWrappers(j, root);
 
   return root.toSource(printOptions);
 }
 
-function collectRowGenerators(j, root, rowGeneratorFunctions) {
-  // Function types to search for
-  const functionTypes = [
-    j.FunctionDeclaration,
-    j.ArrowFunctionExpression,
-    j.FunctionExpression,
-  ];
+function transformRowGeneratorFunctions(j, root) {
+  // Process regular function declarations
+  processNodesThatGenerateRows(j, root, root.find(j.FunctionDeclaration));
 
-  functionTypes.forEach((type) => {
-    root.find(type).forEach((func) => {
-      // Skip if function doesn't contain StructuredListRow
-      if (!containsJsxElement(j, func, STRUCTURED_LIST_ROW)) {
-        return;
-      }
-
-      const name = extractFunctionName(func);
-      if (name) {
-        rowGeneratorFunctions.set(name, func);
-      }
+  // Process arrow functions in variable declarations
+  root
+    .find(j.VariableDeclarator)
+    .filter((decl) => {
+      // Check if it's an arrow function
+      return (
+        decl.node.init && decl.node.init.type === 'ArrowFunctionExpression'
+      );
+    })
+    .forEach((func) => {
+      processNodeIfGeneratesRows(j, func);
     });
+}
+
+function processNodesThatGenerateRows(j, root, collection) {
+  collection.forEach((func) => {
+    processNodeIfGeneratesRows(j, func);
   });
 }
 
-function containsJsxElement(j, node, elementName) {
-  return (
-    j(node)
-      .find(j.JSXElement, {
-        openingElement: { name: { name: elementName } },
+function processNodeIfGeneratesRows(j, func) {
+  // Check if function contains a map that generates StructuredListRow
+  const containsRowMap =
+    j(func)
+      .find(j.CallExpression, {
+        callee: { property: { name: 'map' } },
       })
-      .size() > 0
-  );
+      .filter((mapCall) => {
+        return (
+          j(mapCall)
+            .find(j.JSXElement, {
+              openingElement: { name: { name: STRUCTURED_LIST_ROW } },
+            })
+            .size() > 0
+        );
+      })
+      .size() > 0;
+
+  if (containsRowMap) {
+    // Find and transform all rows in map callbacks inside this function
+    j(func)
+      .find(j.JSXElement, {
+        openingElement: { name: { name: STRUCTURED_LIST_ROW } },
+      })
+      .forEach((row) => transformRow(j, row));
+  }
 }
 
-function extractFunctionName(func) {
-  // Direct function name
-  if (func.node.id) {
-    return func.node.id.name;
-  }
-
-  const { type } = func.parent.node;
-
-  // Variable declaration (const fn = () => {})
-  if (type === 'VariableDeclarator' && func.parent.node.id) {
-    return func.parent.node.id.name;
-  }
-
-  // Object property (obj = { fn: () => {} })
-  if (
-    (type === 'Property' || type === 'ObjectProperty') &&
-    func.parent.node.key
-  ) {
-    return func.parent.node.key.name;
-  }
-
-  // Assignment (exports.fn = () => {})
-  if (type === 'AssignmentExpression') {
-    const { left } = func.parent.node;
-
-    // Simple assignment (fn = () => {})
-    if (left.name) {
-      return left.name;
-    }
-
-    // Member expression (obj.fn = () => {})
-    if (left.type === 'MemberExpression' && left.property) {
-      return left.property.name;
-    }
-  }
-
-  return null;
-}
-
-function transformSelectionWrappers(j, root, rowGeneratorFunctions) {
+function transformSelectionWrappers(j, root) {
   // Find all wrappers with selection prop
-  const selectionWrappers = root.find(j.JSXElement, {
-    openingElement: {
-      name: { name: STRUCTURED_LIST_WRAPPER },
-      attributes: (attrs) => hasAttribute(attrs, ATTR_SELECTION),
-    },
-  });
-
-  // Process each wrapper
-  selectionWrappers.forEach((wrapper) => {
-    // Process direct rows in wrapper
-    processRows(j, wrapper);
-
-    // Process function calls in wrapper
-    processFunctionCalls(j, root, wrapper, rowGeneratorFunctions);
-
-    // Process map callbacks in wrapper
-    processMapCallbacks(j, wrapper);
-  });
-}
-
-function hasAttribute(attrs, attrName) {
-  return attrs.some(
-    (attr) => attr.type === 'JSXAttribute' && attr.name.name === attrName
-  );
-}
-
-function processRows(j, wrapper) {
-  j(wrapper)
+  root
     .find(j.JSXElement, {
-      openingElement: { name: { name: STRUCTURED_LIST_ROW } },
+      openingElement: {
+        name: { name: STRUCTURED_LIST_WRAPPER },
+        attributes: (attrs) =>
+          attrs.some(
+            (attr) =>
+              attr.type === 'JSXAttribute' && attr.name.name === ATTR_SELECTION
+          ),
+      },
     })
-    .forEach((row) => transformRow(j, row));
-}
-
-function processFunctionCalls(j, root, wrapper, rowGeneratorFunctions) {
-  const processedFunctions = new Set();
-
-  j(wrapper)
-    .find(j.JSXExpressionContainer)
-    .find(j.CallExpression)
-    .forEach((call) => {
-      const funcName = extractCalledFunctionName(call);
-
-      // Skip if not a row generator or already processed
-      if (
-        !funcName ||
-        !rowGeneratorFunctions.has(funcName) ||
-        processedFunctions.has(funcName)
-      ) {
-        return;
-      }
-
-      processedFunctions.add(funcName);
-      const func = rowGeneratorFunctions.get(funcName);
-
-      // For function declarations
-      if (func.type === 'FunctionDeclaration') {
-        transformFunctionBody(j, func);
-        return;
-      }
-
-      // For arrow functions and function expressions
-      transformFunctionBody(j, func);
-    });
-}
-
-function extractCalledFunctionName(call) {
-  const { callee } = call.node;
-
-  // Direct function call: func()
-  if (callee.type === 'Identifier') {
-    return callee.name;
-  }
-
-  if (
-    callee.type === 'MemberExpression' &&
-    callee.property.type === 'Identifier' &&
-    callee.property.name !== 'map'
-  ) {
-    return callee.property.name;
-  }
-
-  return null;
-}
-
-function processMapCallbacks(j, wrapper) {
-  j(wrapper)
-    .find(j.CallExpression, { callee: { property: { name: 'map' } } })
-    .forEach((mapCall) => {
-      const callback = mapCall.node.arguments[0];
-
-      if (!callback) {
-        return;
-      }
-
-      // Only process function expressions or arrow functions
-      if (
-        callback.type !== 'ArrowFunctionExpression' &&
-        callback.type !== 'FunctionExpression'
-      ) {
-        return;
-      }
-
-      // Find and transform all rows in the callback
-      j(callback)
+    .forEach((wrapper) => {
+      // Find and transform direct rows in wrapper
+      j(wrapper)
         .find(j.JSXElement, {
           openingElement: { name: { name: STRUCTURED_LIST_ROW } },
         })
         .forEach((row) => transformRow(j, row));
-    });
-}
 
-function transformFunctionBody(j, func) {
-  // Transform direct StructuredListRow elements
-  j(func)
-    .find(j.JSXElement, {
-      openingElement: { name: { name: STRUCTURED_LIST_ROW } },
-    })
-    .forEach((row) => transformRow(j, row));
+      // Find all function calls that might be generators
+      j(wrapper)
+        .find(j.JSXExpressionContainer)
+        .find(j.CallExpression)
+        .forEach((call) => {
+          // If it's a direct call to a function (not a method like map)
+          if (call.node.callee.type === 'Identifier') {
+            const funcName = call.node.callee.name;
 
-  // Transform rows in map callbacks
-  j(func)
-    .find(j.CallExpression, { callee: { property: { name: 'map' } } })
-    .forEach((mapCall) => {
-      const callback = mapCall.node.arguments[0];
+            // Find any function declaration with this name
+            root
+              .find(j.FunctionDeclaration)
+              .filter((func) => func.node.id && func.node.id.name === funcName)
+              .forEach((func) => {
+                // Transform all rows inside that function
+                j(func)
+                  .find(j.JSXElement, {
+                    openingElement: { name: { name: STRUCTURED_LIST_ROW } },
+                  })
+                  .forEach((row) => transformRow(j, row));
+              });
 
-      if (
-        !callback ||
-        (callback.type !== 'ArrowFunctionExpression' &&
-          callback.type !== 'FunctionExpression')
-      ) {
-        return;
-      }
-
-      j(callback)
-        .find(j.JSXElement, {
-          openingElement: { name: { name: STRUCTURED_LIST_ROW } },
-        })
-        .forEach((row) => transformRow(j, row));
+            // Also check for variable declarations (for arrow functions)
+            root
+              .find(j.VariableDeclarator, { id: { name: funcName } })
+              .forEach((func) => {
+                // Transform all rows inside that function
+                j(func)
+                  .find(j.JSXElement, {
+                    openingElement: { name: { name: STRUCTURED_LIST_ROW } },
+                  })
+                  .forEach((row) => transformRow(j, row));
+              });
+          }
+        });
     });
 }
 
 function transformRow(j, row) {
-  // Add selection prop to all rows (including those with head prop)
-  addSelectionPropIfMissing(j, row);
-
-  // Only remove CheckmarkFilled cells from non-head rows
-  if (!hasElementAttribute(row, ATTR_HEAD)) {
-    removeCheckmarkCells(j, row);
-  }
-}
-
-function hasElementAttribute(element, attrName) {
-  return element.node.openingElement.attributes.some(
-    (attr) => attr.type === 'JSXAttribute' && attr.name.name === attrName
+  // Add selection prop to all rows (except those with head prop)
+  const hasHeadProp = row.node.openingElement.attributes.some(
+    (attr) => attr.type === 'JSXAttribute' && attr.name.name === ATTR_HEAD
   );
-}
 
-function addSelectionPropIfMissing(j, row) {
-  if (!hasElementAttribute(row, ATTR_SELECTION)) {
+  const hasSelectionProp = row.node.openingElement.attributes.some(
+    (attr) => attr.type === 'JSXAttribute' && attr.name.name === ATTR_SELECTION
+  );
+
+  // Add selection prop if it's missing
+  if (!hasSelectionProp) {
     row.node.openingElement.attributes.push(
       j.jsxAttribute(j.jsxIdentifier(ATTR_SELECTION))
     );
   }
-}
 
-function removeCheckmarkCells(j, row) {
-  row.node.children = row.node.children.filter((child) => {
-    // Only check JSX elements that are StructuredListCell
-    if (
-      child.type !== 'JSXElement' ||
-      child.openingElement.name.name !== STRUCTURED_LIST_CELL
-    ) {
-      return true;
-    }
+  // Only remove CheckmarkFilled cells from non-head rows
+  if (!hasHeadProp) {
+    // Filter out cells with CheckmarkFilled
+    row.node.children = row.node.children.filter((child) => {
+      // Only check JSX elements that are StructuredListCell
+      if (
+        child.type !== 'JSXElement' ||
+        child.openingElement.name.name !== STRUCTURED_LIST_CELL
+      ) {
+        return true;
+      }
 
-    // Keep cells that don't contain CheckmarkFilled
-    return !j(child)
-      .find(j.JSXElement, {
-        openingElement: { name: { name: CHECKMARK_FILLED } },
-      })
-      .size();
-  });
+      // Check if cell contains CheckmarkFilled
+      return (
+        j(child)
+          .find(j.JSXElement, {
+            openingElement: { name: { name: CHECKMARK_FILLED } },
+          })
+          .size() === 0
+      );
+    });
+  }
 }
 
 module.exports = transform;
