97a0cf58b1b4df5333506b2664044b493b231cf5
Ariella Gilmore <ariellalgilmore@gmail.com>
2025-03-04 07:17:29 -0800

    Update packages/react/src/index.ts

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -189,7 +189,6 @@ export * from './components/Tooltip/DefinitionTooltip';
 export * from './components/Theme';
 export * from './internal/usePrefix';
 export { useIdPrefix } from './internal/useIdPrefix';
-export { IconIndicator as unstable__IconIndicator } from './components/IconIndicator';
 
 /* prop's interface exports */
 
