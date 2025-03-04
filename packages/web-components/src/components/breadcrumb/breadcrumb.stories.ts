bafa817529b54a5bc333dcd829dd6dc1afe371ed
Anamika T S <anamika.t.s@ibm.com>
2025-03-04 20:18:20 +0530

    fix(breadcrumb): resolve conflict

--- packages/web-components/src/components/breadcrumb/breadcrumb.stories.ts
+++ packages/web-components/src/components/breadcrumb/breadcrumb.stories.ts
@@ -78,15 +78,7 @@ export const Default = {
           <cds-breadcrumb-link href="#">Breadcrumb 2</cds-breadcrumb-link>
         </cds-breadcrumb-item>
         <cds-breadcrumb-item>
-          <cds-breadcrumb-overflow-menu>
-            <cds-overflow-menu-body>
-              <cds-overflow-menu-item>Breadcrumb 3</cds-overflow-menu-item>
-              <cds-overflow-menu-item>Breadcrumb 4</cds-overflow-menu-item>
-            </cds-overflow-menu-body>
-          </cds-breadcrumb-overflow-menu>
-        </cds-breadcrumb-item>
-        <cds-breadcrumb-item>
-          <cds-breadcrumb-link href="/#">Breadcrumb 5</cds-breadcrumb-link>
+          <cds-breadcrumb-link href="#">Breadcrumb 3</cds-breadcrumb-link>
         </cds-breadcrumb-item>
         <cds-breadcrumb-item>
           <cds-breadcrumb-link>Breadcrumb 6</cds-breadcrumb-link>
