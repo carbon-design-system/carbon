734afb0a868d30f621952bfaa54fd97b91b75796
Taylor Jones <taylor.jones826@gmail.com>
2025-03-06 15:03:21 -0600

    chore: update copyright, remove old component files

--- packages/react/src/components/PageHeader/PageHeader-test.js
+++ packages/react/src/components/PageHeader/PageHeader-test.js
@@ -1,10 +1,9 @@
 /**
- * Copyright IBM Corp. 2016, 2023
+ * Copyright IBM Corp. 2025
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
--- packages/react/src/components/PageHeader/PageHeader.stories.js
+++ packages/react/src/components/PageHeader/PageHeader.stories.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright IBM Corp. 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import React from 'react';
 import { unstable__PageHeader as PageHeader } from '../../';
 import {
