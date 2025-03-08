6baddc9bf38499b5340641ef0dcd880982cf795d
Kenny Lam <909118+kennylam@users.noreply.github.com>
2025-03-08 00:28:53 -0600

    chore(link): add tests and snapshots

---
+++ packages/web-components/src/components/link/__tests__/__snapshots__/link-test.snap.js
@@ -0,0 +1,11 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots['cds-link should render an <a> element'] = `<cds-link
+  href="https://carbondesignsystem.com"
+  size="md"
+>
+  test
+</cds-link>
+`;
+/* end snapshot cds-link should render an <a> element */
