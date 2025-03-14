701f287a67476152562489dfb1c7e482a349e9d8
Taylor Jones <tay1orjones@users.noreply.github.com>
2025-03-14 14:59:48 -0500

    feat(modal): refactor to use native dialog element (#18553)
    * refactor(modal): add dialog feature flag
    
    * refactor(modal): begin dialog exploration
    
    * feat(modal): unify conditional exports under wrapper component
    
    * revert(modal): changes from initial dialog refactor
    
    * refactor(modal): remove unecessary close button dom location condition
    
    * fix(modal): add dialog refactor scss feature flag
    
    * fix: named export
    
    * feat(dialog): break children elements out into composable components
    
    * feat(dialog): improve stories, documentation, and Modal refactor
    
    * feat(modal): document dialog feature flag
    
    * fix(composedmodal): refactor to use dialog
    
    * refactor(modal): remove conditional export, move enableDialogElement into existing Modal source file
    
    * Update packages/react/src/components/Dialog/Dialog.stories.js
    
    Co-authored-by: Heloise Lui <71858203+heloiselui@users.noreply.github.com>
    
    * Update packages/react/scss/components/dialog/_index.scss
    
    Co-authored-by: Heloise Lui <71858203+heloiselui@users.noreply.github.com>
    
    * Update packages/react/scss/components/dialog/_dialog.scss
    
    Co-authored-by: Heloise Lui <71858203+heloiselui@users.noreply.github.com>
    
    * fix(dialog): incorporate feedback
    
    ---------
    
    Co-authored-by: “heloiselui” <helolui27@gmail.com>
    Co-authored-by: Heloise Lui <71858203+heloiselui@users.noreply.github.com>

--- packages/react/src/components/Modal/Modal.featureflag.stories.js
+++ packages/react/src/components/Modal/Modal.featureflag.stories.js
@@ -6,14 +6,16 @@
  */
 
 import React, { useState } from 'react';
-import Modal from './Modal';
+import Modal from './';
 import Button from '../Button';
 import Select from '../Select';
 import MultiSelect from '../MultiSelect';
 import Dropdown from '../Dropdown';
 import SelectItem from '../SelectItem';
 import TextInput from '../TextInput';
+import { ClassPrefix } from '../ClassPrefix';
 import { WithFeatureFlags } from '../../../.storybook/templates/WithFeatureFlags';
+import './Modal.stories.scss';
 
 export default {
   title: 'Components/Modal/Feature Flag',
@@ -28,60 +30,64 @@ export default {
   ],
 };
 
-export const FocusWrapWithoutSentinel = () => {
-  const [open, setOpen] = useState(true);
+export const DialogRefactor = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <Button onClick={() => setOpen(true)}>Launch modal</Button>
-      <Modal
-        open={open}
-        onRequestClose={() => setOpen(false)}
-        modalHeading="Add a custom domain"
-        modalLabel="Account resources"
-        primaryButtonText="Add"
-        secondaryButtonText="Cancel">
-        <p style={{ marginBottom: '1rem' }}>
-          Custom domains direct requests for your apps in this Cloud Foundry
-          organization to a URL that you own. A custom domain can be a shared
-          domain, a shared subdomain, or a shared domain and host.
-        </p>
-        <TextInput
-          data-modal-primary-focus
-          id="text-input-1"
-          labelText="Domain name"
-          placeholder="e.g. github.com"
-          style={{ marginBottom: '1rem' }}
-        />
-        <Select id="select-1" defaultValue="us-south" labelText="Region">
-          <SelectItem value="us-south" text="US South" />
-          <SelectItem value="us-east" text="US East" />
-        </Select>
-        <Dropdown
-          id="drop"
-          label="Dropdown"
-          titleText="Dropdown"
-          items={[
-            { id: 'one', label: 'one', name: 'one' },
-            { id: 'two', label: 'two', name: 'two' },
-          ]}
-        />
-        <MultiSelect
-          id="test"
-          label="Multiselect"
-          titleText="Multiselect"
-          items={[
-            {
-              id: 'downshift-1-item-0',
-              text: 'Option 1',
-            },
-            {
-              id: 'downshift-1-item-1',
-              text: 'Option 2',
-            },
-          ]}
-          itemToString={(item) => (item ? item.text : '')}
-        />
-      </Modal>
+      <ClassPrefix prefix="dialog-refactor">
+        <div className="experimental-modal">
+          <Modal
+            open={open}
+            onRequestClose={() => setOpen(false)}
+            modalHeading="Add a custom domain"
+            modalLabel="Account resources"
+            primaryButtonText="Add"
+            secondaryButtonText="Cancel">
+            <p style={{ marginBottom: '1rem' }}>
+              Custom domains direct requests for your apps in this Cloud Foundry
+              organization to a URL that you own. A custom domain can be a
+              shared domain, a shared subdomain, or a shared domain and host.
+            </p>
+            <TextInput
+              autofocus="true"
+              id="text-input-1"
+              labelText="Domain name"
+              placeholder="e.g. github.com"
+              style={{ marginBottom: '1rem' }}
+            />
+            <Select id="select-1" defaultValue="us-south" labelText="Region">
+              <SelectItem value="us-south" text="US South" />
+              <SelectItem value="us-east" text="US East" />
+            </Select>
+            <Dropdown
+              id="drop"
+              label="Dropdown"
+              titleText="Dropdown"
+              items={[
+                { id: 'one', label: 'one', name: 'one' },
+                { id: 'two', label: 'two', name: 'two' },
+              ]}
+            />
+            <MultiSelect
+              id="test"
+              label="Multiselect"
+              titleText="Multiselect"
+              items={[
+                {
+                  id: 'downshift-1-item-0',
+                  text: 'Option 1',
+                },
+                {
+                  id: 'downshift-1-item-1',
+                  text: 'Option 2',
+                },
+              ]}
+              itemToString={(item) => (item ? item.text : '')}
+            />
+          </Modal>
+        </div>
+      </ClassPrefix>
     </>
   );
 };
