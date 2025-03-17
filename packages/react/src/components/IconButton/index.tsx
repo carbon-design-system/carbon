300b93d3f9cbe47e39825ef2b06ec6ffa11eccbd
Adam Alston <aalston9@gmail.com>
2025-03-17 09:56:39 -0400

    fix: update icon button types and docs (#18658)

--- packages/react/src/components/IconButton/index.tsx
+++ packages/react/src/components/IconButton/index.tsx
@@ -161,9 +161,9 @@ export interface IconButtonProps
   rel?: React.AnchorHTMLAttributes<HTMLAnchorElement>['rel'];
 
   /**
-   * Specify the size of the Button. Defaults to `md`.
+   * Specify the size of the Button.
    */
-  size?: ButtonSize;
+  size?: Extract<ButtonSize, 'sm' | 'md' | 'lg'>;
 
   /**
    * Optionally specify a `target` when using an `<a>` element.
@@ -381,7 +381,7 @@ IconButton.propTypes = {
   rel: PropTypes.string,
 
   /**
-   * Specify the size of the Button. Defaults to `md`.
+   * Specify the size of the Button.
    */
   size: PropTypes.oneOf(['sm', 'md', 'lg']),
 
