85dc6805db62528ee7adde89c977921b020f1faf
Gururajj77 <gururajtakeshi@gmail.com>
2025-03-26 17:25:11 +0530

    feat: low contrast content switcher implementation

---
+++ packages/themes/src/component-tokens/content-switcher/index.js
@@ -0,0 +1,1 @@
+export * as contentSwitcherTokens from './tokens';
---
+++ packages/themes/src/component-tokens/content-switcher/tokens.js
@@ -0,0 +1,26 @@
+const contentSwitcherSelected = {
+  whiteTheme: '#ffffff',
+  g10: '#ffffff',
+  g90: '#8d8d8d',
+  g100: '#8d8d8d',
+};
+
+const contentSwitcherBackground = {
+  whiteTheme: '#e0e0e0',
+  g10: '#e0e0e0',
+  g90: 'transparent',
+  g100: 'transparent',
+};
+
+const contentSwitcherBackgroundHover = {
+  whiteTheme: '#d1d1d1',
+  g10: '#d1d1d1',
+  g90: '#8d8d8d',
+  g100: '#8d8d8d',
+};
+
+export {
+  contentSwitcherSelected,
+  contentSwitcherBackground,
+  contentSwitcherBackgroundHover,
+};
