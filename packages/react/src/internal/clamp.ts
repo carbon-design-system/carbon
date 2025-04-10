b2458ea6d4c419b5e03891a63de5d1a588edddca
Adam Alston <aalston9@gmail.com>
2025-04-10 19:23:54 -0400

    revert: revert unrelated changes

--- packages/react/src/internal/clamp.ts
+++ packages/react/src/internal/clamp.ts
@@ -1,17 +1,18 @@
 /**
- * Copyright IBM Corp. 2025
+ * Copyright IBM Corp. 2016, 2025
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
 /**
- * Clamps a value between a minimum and maximum value (inclusive).
+ * Synonymous to ECMA2017+ `Math.clamp`.
  *
- * @param value - The number to clamp.
- * @param [min] - The lower bound.
- * @param [max] - The upper bound.
- * @returns The clamped value.
+ * @param {number} val
+ * @param {number} min
+ * @param {number} max
+ *
+ * @returns `val` if `max>=val>=min`; `min` if `val<min`; `max` if `val>max`.
  */
 export const clamp = (value: number, min?: number, max?: number) =>
   Math.min(max ?? Infinity, Math.max(min ?? -Infinity, value));
