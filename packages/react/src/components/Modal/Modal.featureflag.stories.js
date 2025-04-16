6d3a9a08e30c8980220f341677252e80515097c9
Taylor Jones <tay1orjones@users.noreply.github.com>
2025-04-16 10:34:16 -0500

    test(dialog): initial unit tests, mock `HTMLDialogElement` in jest config, revert feature flag unification (#19134)
    * test(dialog): initial unit tests, mock HTMLDialogElement in jest config
    
    * fix(composed-modal): separate dialog element and focuswrap w/out sentinel flags
    
    * fix(modal): separate dialog element and focuswrap w/out sentinel flags
    
    * Update packages/react/src/components/Modal/Modal.tsx
    
    Co-authored-by: Adam Alston <aalston9@gmail.com>
    
    ---------
    
    Co-authored-by: Nikhil Tomar <63502271+2nikhiltom@users.noreply.github.com>
    Co-authored-by: Adam Alston <aalston9@gmail.com>

--- packages/react/src/components/Modal/Modal.featureflag.stories.js
+++ packages/react/src/components/Modal/Modal.featureflag.stories.js
@@ -14,80 +14,166 @@ import Dropdown from '../Dropdown';
 import SelectItem from '../SelectItem';
 import TextInput from '../TextInput';
 import { ClassPrefix } from '../ClassPrefix';
-import { WithFeatureFlags } from '../../../.storybook/templates/WithFeatureFlags';
 import './Modal.stories.scss';
+import { FeatureFlags } from '../FeatureFlags';
+import { Annotation } from '../../../.storybook/templates/Annotation';
+import LinkTo from '@storybook/addon-links/react';
 
 export default {
-  title: 'Components/Modal/Feature Flag',
+  title: 'Components/Modal/Feature Flags',
   component: Modal,
   tags: ['!autodocs'],
-  decorators: [
-    (Story) => (
-      <WithFeatureFlags>
-        <Story />
-      </WithFeatureFlags>
-    ),
-  ],
 };
 
-export const DialogRefactor = () => {
+export const EnableDialogElement = () => {
   const [open, setOpen] = useState(false);
   return (
-    <>
-      <Button onClick={() => setOpen(true)}>Launch modal</Button>
-      <ClassPrefix prefix="dialog-refactor">
-        <div className="experimental-modal">
-          <Modal
-            open={open}
-            onRequestClose={() => setOpen(false)}
-            modalHeading="Add a custom domain"
-            modalLabel="Account resources"
-            primaryButtonText="Add"
-            secondaryButtonText="Cancel">
-            <p style={{ marginBottom: '1rem' }}>
-              Custom domains direct requests for your apps in this Cloud Foundry
-              organization to a URL that you own. A custom domain can be a
-              shared domain, a shared subdomain, or a shared domain and host.
-            </p>
-            <TextInput
-              autofocus="true"
-              id="text-input-1"
-              labelText="Domain name"
-              placeholder="e.g. github.com"
-              style={{ marginBottom: '1rem' }}
-            />
-            <Select id="select-1" defaultValue="us-south" labelText="Region">
-              <SelectItem value="us-south" text="US South" />
-              <SelectItem value="us-east" text="US East" />
-            </Select>
-            <Dropdown
-              id="drop"
-              label="Dropdown"
-              titleText="Dropdown"
-              items={[
-                { id: 'one', label: 'one', name: 'one' },
-                { id: 'two', label: 'two', name: 'two' },
-              ]}
-            />
-            <MultiSelect
-              id="test"
-              label="Multiselect"
-              titleText="Multiselect"
-              items={[
-                {
-                  id: 'downshift-1-item-0',
-                  text: 'Option 1',
-                },
-                {
-                  id: 'downshift-1-item-1',
-                  text: 'Option 2',
-                },
-              ]}
-              itemToString={(item) => (item ? item.text : '')}
-            />
-          </Modal>
-        </div>
-      </ClassPrefix>
-    </>
+    <FeatureFlags enableDialogElement>
+      <Annotation
+        type="feature-flags"
+        text={
+          <span>
+            This story is rendered with{' '}
+            <LinkTo title="Getting Started/Feature Flags" name="Overview">
+              enable-dialog-element
+            </LinkTo>{' '}
+            enabled
+          </span>
+        }>
+        <Button onClick={() => setOpen(true)}>Launch modal</Button>
+        <ClassPrefix prefix="dialog-refactor">
+          <div className="experimental-modal">
+            <Modal
+              open={open}
+              onRequestClose={() => setOpen(false)}
+              modalHeading="Add a custom domain"
+              modalLabel="Account resources"
+              primaryButtonText="Add"
+              secondaryButtonText="Cancel">
+              <p style={{ marginBottom: '1rem' }}>
+                Custom domains direct requests for your apps in this Cloud
+                Foundry organization to a URL that you own. A custom domain can
+                be a shared domain, a shared subdomain, or a shared domain and
+                host.
+              </p>
+              <TextInput
+                autofocus="true"
+                id="text-input-1"
+                labelText="Domain name"
+                placeholder="e.g. github.com"
+                style={{ marginBottom: '1rem' }}
+              />
+              <Select id="select-1" defaultValue="us-south" labelText="Region">
+                <SelectItem value="us-south" text="US South" />
+                <SelectItem value="us-east" text="US East" />
+              </Select>
+              <Dropdown
+                id="drop"
+                label="Dropdown"
+                titleText="Dropdown"
+                items={[
+                  { id: 'one', label: 'one', name: 'one' },
+                  { id: 'two', label: 'two', name: 'two' },
+                ]}
+              />
+              <MultiSelect
+                id="test"
+                label="Multiselect"
+                titleText="Multiselect"
+                items={[
+                  {
+                    id: 'downshift-1-item-0',
+                    text: 'Option 1',
+                  },
+                  {
+                    id: 'downshift-1-item-1',
+                    text: 'Option 2',
+                  },
+                ]}
+                itemToString={(item) => (item ? item.text : '')}
+              />
+            </Modal>
+          </div>
+        </ClassPrefix>
+      </Annotation>
+    </FeatureFlags>
   );
 };
+EnableDialogElement.storyName = 'enable-dialog-element';
+
+export const EnableExperimentalFocusWrapWithoutSentinels = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <FeatureFlags enableExperimentalFocusWrapWithoutSentinels>
+      <Annotation
+        type="feature-flags"
+        text={
+          <span>
+            This story is rendered with{' '}
+            <LinkTo title="Getting Started/Feature Flags" name="Overview">
+              enable-experimental-focus-wrap-without-sentinels
+            </LinkTo>{' '}
+            enabled
+          </span>
+        }>
+        <Button onClick={() => setOpen(true)}>Launch modal</Button>
+        <ClassPrefix prefix="dialog-refactor">
+          <div className="experimental-modal">
+            <Modal
+              open={open}
+              onRequestClose={() => setOpen(false)}
+              modalHeading="Add a custom domain"
+              modalLabel="Account resources"
+              primaryButtonText="Add"
+              secondaryButtonText="Cancel">
+              <p style={{ marginBottom: '1rem' }}>
+                Custom domains direct requests for your apps in this Cloud
+                Foundry organization to a URL that you own. A custom domain can
+                be a shared domain, a shared subdomain, or a shared domain and
+                host.
+              </p>
+              <TextInput
+                autofocus="true"
+                id="text-input-1"
+                labelText="Domain name"
+                placeholder="e.g. github.com"
+                style={{ marginBottom: '1rem' }}
+              />
+              <Select id="select-1" defaultValue="us-south" labelText="Region">
+                <SelectItem value="us-south" text="US South" />
+                <SelectItem value="us-east" text="US East" />
+              </Select>
+              <Dropdown
+                id="drop"
+                label="Dropdown"
+                titleText="Dropdown"
+                items={[
+                  { id: 'one', label: 'one', name: 'one' },
+                  { id: 'two', label: 'two', name: 'two' },
+                ]}
+              />
+              <MultiSelect
+                id="test"
+                label="Multiselect"
+                titleText="Multiselect"
+                items={[
+                  {
+                    id: 'downshift-1-item-0',
+                    text: 'Option 1',
+                  },
+                  {
+                    id: 'downshift-1-item-1',
+                    text: 'Option 2',
+                  },
+                ]}
+                itemToString={(item) => (item ? item.text : '')}
+              />
+            </Modal>
+          </div>
+        </ClassPrefix>
+      </Annotation>
+    </FeatureFlags>
+  );
+};
+EnableExperimentalFocusWrapWithoutSentinels.storyName =
+  'enable-experimental-focus-wrap-without-sentinels';
