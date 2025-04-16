c535423775ea51267db0ebb7df9321682e230b82
“heloiselui” <helolui27@gmail.com>
2025-04-16 10:41:58 +0100

    docs(icons): update storybook with rem sizing instructions

--- packages/react/src/components/Icons/Icons.stories.js
+++ packages/react/src/components/Icons/Icons.stories.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2016, 2023
+ * Copyright IBM Corp. 2016, 2025
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -7,11 +7,18 @@
 
 import './Icons.stories.scss';
 import React from 'react';
-import { Bee, Bicycle, ChevronUp } from '../../../icons';
+import { Bee, Edit } from '../../../icons';
+import { IconButton } from '../IconButton';
+import mdx from './Icons.mdx';
 
 // eslint-disable-next-line storybook/csf-component
 export default {
   title: 'Elements/Icons',
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
 };
 
 export const Default = (args) => {
@@ -35,3 +42,31 @@ Default.argTypes = {
     control: { type: 'select' },
   },
 };
+
+export const IconWithRemSize = () => {
+  return (
+    <section className="demo-icon-example" style={{ fontSize: '1rem' }}>
+      <h2>1rem (responsive)</h2>
+      <IconButton label="Edit icon" kind="primary">
+        <Edit size="1rem" />
+      </IconButton>
+    </section>
+  );
+};
+
+IconWithRemSize.storyName = 'Icon with relative size';
+
+export const IconWithRenderFunction = () => {
+  return (
+    <section className="demo-icon-example">
+      <h2>Using renderIcon with rem</h2>
+      <IconButton
+        label="Edit icon"
+        kind="primary"
+        renderIcon={() => <Edit size="1rem" />}
+      />
+    </section>
+  );
+};
+
+IconWithRenderFunction.storyName = 'Icon with renderIcon function';
