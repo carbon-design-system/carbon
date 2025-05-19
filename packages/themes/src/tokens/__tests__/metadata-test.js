3f699f9cf70161b7ae4a20ecdddb33a59070e8fa
Ariella Gilmore <ariellalgilmore@gmail.com>
2025-05-19 08:37:07 -0700

    feat(layer): new layer-background token and withBackground prop (#19381)
    * feat(layer): new layer-background token and withBackground prop
    
    * chore: add test story
    
    * fix(layer): overview page
    
    * Update packages/react/src/components/Layer/index.tsx
    
    Co-authored-by: Alison Joseph <alisonejoseph@Gmail.com>
    
    * fix(layer): remove test story
    
    ---------
    
    Co-authored-by: Alison Joseph <alison.joseph@us.ibm.com>
    Co-authored-by: Alison Joseph <alisonejoseph@Gmail.com>

--- packages/themes/src/tokens/__tests__/metadata-test.js
+++ packages/themes/src/tokens/__tests__/metadata-test.js
@@ -602,6 +602,10 @@ test('metadata', () => {
           "type": "color",
         },
         Object {
+          "name": "layer-background-01",
+          "type": "color",
+        },
+        Object {
           "name": "layer-hover-01",
           "type": "color",
         },
@@ -622,6 +626,10 @@ test('metadata', () => {
           "type": "color",
         },
         Object {
+          "name": "layer-background-02",
+          "type": "color",
+        },
+        Object {
           "name": "layer-hover-02",
           "type": "color",
         },
@@ -639,6 +647,10 @@ test('metadata', () => {
         },
         Object {
           "name": "layer-active-03",
+          "type": "color",
+        },
+        Object {
+          "name": "layer-background-03",
           "type": "color",
         },
         Object {
