ef939c94268c04e6b4ca2dbd52b1879d2ddbca93
Will Scott <78674552+sct-will@users.noreply.github.com>
2025-05-25 14:45:25 -0500

    Update ContainedListItem.figma.tsx

--- packages/react/code-connect/ContainedListItem/ContainedListItem.figma.tsx
+++ packages/react/code-connect/ContainedListItem/ContainedListItem.figma.tsx
@@ -28,7 +28,7 @@ figma.connect(
       // item3: figma.boolean('Item 3'),
     },
     example: ({ rowCellItem, ...props }) => (
-      <ContainedListItem renderIcon={} action={}>
+      <ContainedListItem renderIcon={"IconName"} action={}>
         {"text"}
       </ContainedListItem>     
     ),
