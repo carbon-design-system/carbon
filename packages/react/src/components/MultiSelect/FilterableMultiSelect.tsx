96f19b77b66ed32287755a0e46d4cc81d0aa590f
sanraj2000 <70739919+sanraj2000@users.noreply.github.com>
2025-06-03 17:52:20 +0530

    Update packages/react/src/components/MultiSelect/FilterableMultiSelect.tsx
    Co-authored-by: Adam Alston <aalston9@gmail.com>

--- packages/react/src/components/MultiSelect/FilterableMultiSelect.tsx
+++ packages/react/src/components/MultiSelect/FilterableMultiSelect.tsx
@@ -320,7 +320,7 @@ export interface FilterableMultiSelectProps<ItemType>
   /**
    * Props to be passed directly to the underlying <input> element.
    * Useful for customizing input behavior, such as setting maxLength, placeholder and other native input attributes.
-   * *
+   *
    * Note: These props are merged with internal input props. Custom values will override defaults when applicable.
    */
   inputProp?: React.InputHTMLAttributes<HTMLInputElement>;
