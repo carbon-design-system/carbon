d3c924eb47adb4a33073dc55e4fa565efebdba8d
Anna Wen <54281166+annawen1@users.noreply.github.com>
2025-06-06 15:03:13 -0400

    chore: add documentation

--- packages/web-components/src/components/page-header/defs.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- *
- * Copyright IBM Corp. 2020, 2023
- *
- * This source code is licensed under the Apache-2.0 license found in the
- * LICENSE file in the root directory of this source tree.
- */
