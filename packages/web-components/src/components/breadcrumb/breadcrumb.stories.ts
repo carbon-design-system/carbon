06f52810de1d2c593e9fe1eb584cc944d2816546
Preeti Bansal <design.preeti@gmail.com>
2025-06-10 19:45:20 +0530

    fix: update example for more clarity

--- packages/web-components/src/components/breadcrumb/breadcrumb.stories.ts
+++ packages/web-components/src/components/breadcrumb/breadcrumb.stories.ts
@@ -79,29 +79,50 @@ export const Default = {
             <cds-breadcrumb-link href="#">Breadcrumb 4</cds-breadcrumb-link>
           </cds-breadcrumb-item>
         </cds-breadcrumb>
+        <br />
+        <feature-flags enable-v12-tile-default-icons="false">
+          <cds-breadcrumb
+            ?no-trailing-slash="${noTrailingSlash}"
+            .class="${className}"
+            .aria-label="${ariaLabel}"
+            .size="${size}">
+            <cds-breadcrumb-item>
+              <cds-breadcrumb-link href="#">Breadcrumb 1</cds-breadcrumb-link>
+            </cds-breadcrumb-item>
+            <cds-breadcrumb-item>
+              <cds-breadcrumb-link href="#">Breadcrumb 2</cds-breadcrumb-link>
+            </cds-breadcrumb-item>
+            <cds-breadcrumb-item>
+              <cds-breadcrumb-link href="#">Breadcrumb 3</cds-breadcrumb-link>
+            </cds-breadcrumb-item>
+            <cds-breadcrumb-item>
+              <cds-breadcrumb-link href="#">Breadcrumb 4</cds-breadcrumb-link>
+            </cds-breadcrumb-item>
+          </cds-breadcrumb>
+          <br />
+          <feature-flags enable-v12-tile-default-icons="true">
+            <cds-breadcrumb
+              ?no-trailing-slash="${noTrailingSlash}"
+              .class="${className}"
+              .aria-label="${ariaLabel}"
+              .size="${size}">
+              <cds-breadcrumb-item>
+                <cds-breadcrumb-link href="#">Breadcrumb 1</cds-breadcrumb-link>
+              </cds-breadcrumb-item>
+              <cds-breadcrumb-item>
+                <cds-breadcrumb-link href="#">Breadcrumb 2</cds-breadcrumb-link>
+              </cds-breadcrumb-item>
+              <cds-breadcrumb-item>
+                <cds-breadcrumb-link href="#">Breadcrumb 3</cds-breadcrumb-link>
+              </cds-breadcrumb-item>
+              <cds-breadcrumb-item>
+                <cds-breadcrumb-link href="#">Breadcrumb 4</cds-breadcrumb-link>
+              </cds-breadcrumb-item>
+            </cds-breadcrumb>
+          </feature-flags>
+        </feature-flags>
       </feature-flags>
-      <br />
-      <feature-flags enable-v12-tile-default-icons="false">
-        <cds-breadcrumb
-          ?no-trailing-slash="${noTrailingSlash}"
-          .class="${className}"
-          .aria-label="${ariaLabel}"
-          .size="${size}">
-          <cds-breadcrumb-item>
-            <cds-breadcrumb-link href="#">Breadcrumb 1</cds-breadcrumb-link>
-          </cds-breadcrumb-item>
-          <cds-breadcrumb-item>
-            <cds-breadcrumb-link href="#">Breadcrumb 2</cds-breadcrumb-link>
-          </cds-breadcrumb-item>
-          <cds-breadcrumb-item>
-            <cds-breadcrumb-link href="#">Breadcrumb 3</cds-breadcrumb-link>
-          </cds-breadcrumb-item>
-          <cds-breadcrumb-item>
-            <cds-breadcrumb-link href="#">Breadcrumb 4</cds-breadcrumb-link>
-          </cds-breadcrumb-item>
-        </cds-breadcrumb>
-      </feature-flags>
-      <br />
+      <br /><br />
       <cds-breadcrumb
         ?no-trailing-slash="${noTrailingSlash}"
         .class="${className}"
