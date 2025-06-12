1f3a296e5186f89bcb97f03cd7d1bc2b296be5ec
riddhybansal <riddhybansal@gmail.com>
2025-06-12 18:10:23 +0530

    fix: removed license header

--- packages/web-components/src/components/page-header/page-header-breadcrumb.ts
+++ packages/web-components/src/components/page-header/page-header-breadcrumb.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright IBM Corp. 2025, 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import { LitElement, html } from 'lit';
 import { classMap } from 'lit/directives/class-map.js';
 import { prefix } from '../../globals/settings';
