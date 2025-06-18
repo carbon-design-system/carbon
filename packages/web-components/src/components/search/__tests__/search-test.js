128abdcdcadb1dcc43de3a12e75d247eb045994b
Riddhi Bansal <41935566+riddhybansal@users.noreply.github.com>
2025-06-18 19:22:18 +0530

    Update packages/web-components/src/components/search/__tests__/search-test.js
    Co-authored-by: kennylam <909118+kennylam@users.noreply.github.com>

--- packages/web-components/src/components/search/__tests__/search-test.js
+++ packages/web-components/src/components/search/__tests__/search-test.js
@@ -29,7 +29,7 @@ describe('cds-search', () => {
       await expect(el).shadowDom.to.equalSnapshot();
     });
 
-    it('should respect closeButtonLabelText prop', async () => {
+    it('should respect the close-button-label-text attribute', async () => {
       const el = await fixture(html`
         <cds-search
           label-text="test-search"
