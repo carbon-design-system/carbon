b4d3e800a2e65ac0b7aa33d00a740490e0f4effd
Abhishek Paul <abpaul24@in.ibm.com>
2025-06-26 13:55:31 +0530

    chore: fixed NumberInputv2 component import issue

--- packages/react/src/components/NumberInputV2/index.js
+++ packages/react/src/components/NumberInputV2/index.js
@@ -4,4 +4,4 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-export { default as NumberInputV2 } from './NumberInputV2';
+export default from './NumberInputV2';
