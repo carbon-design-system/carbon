35ad262afb6069a130ef167a8f901b8ae53ea397
“heloise-lui” <heloise.lui@ibm.com>
2025-06-30 13:44:26 +0100

    chore(text-area): update copyright

--- packages/react/src/components/TextArea/TextArea-test.js
+++ packages/react/src/components/TextArea/TextArea-test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2016, 2023
+ * Copyright IBM Corp. 2016, 2025
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
