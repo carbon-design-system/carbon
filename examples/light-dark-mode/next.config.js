9ce7faa9493e0c44724e1bc8451161a39100b1e8
Kenny Lam <909118+kennylam@users.noreply.github.com>
2025-07-09 09:15:54 -0400

    chore(nextjs): update examples to use app router

--- examples/light-dark-mode/next.config.js
+++ examples/light-dark-mode/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@carbon/feature-flags', '@carbon/react'],
-  experimental: {
-    esmExternals: 'loose',
-  },
 };
 
 module.exports = nextConfig;
