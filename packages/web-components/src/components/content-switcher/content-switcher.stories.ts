348794dd79c840cb2d58f4ba37a587706244f623
“heloise-lui” <heloise.lui@ibm.com>
2025-07-11 16:12:56 +0100

    fix(content-switcher): align web component with React for parity

--- packages/web-components/src/components/content-switcher/content-switcher.stories.ts
+++ packages/web-components/src/components/content-switcher/content-switcher.stories.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2019, 2024
+ * Copyright IBM Corp. 2019, 2025
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -16,18 +16,18 @@ import ViewMode2_16 from '@carbon/icons/lib/view--mode-2/16.js';
 import '../layer/index';
 import '../../../.storybook/templates/with-layer';
 
-const noop = () => {};
-
 const sizes = {
-  'Medium (md - default)': null,
   [`Small (${CONTENT_SWITCHER_SIZE.SMALL})`]: CONTENT_SWITCHER_SIZE.SMALL,
+  'Medium (md - default)': null,
   [`Large (${CONTENT_SWITCHER_SIZE.LARGE})`]: CONTENT_SWITCHER_SIZE.LARGE,
 };
 
 const args = {
-  value: '',
+  value: 'all',
   size: null,
-  disableSelection: false,
+  selectionMode: 'automatic',
+  selectedIndex: 0,
+  lowContrast: false,
 };
 
 const argTypes = {
@@ -35,51 +35,110 @@ const argTypes = {
     control: 'text',
     description: 'The value of the selected item (value)',
   },
-  size: {
-    control: 'select',
-    options: sizes,
-    description: 'Button size (size)',
+  selectedIndex: {
+    control: 'number',
+    description: 'Specify a selected index for the initially selected content',
   },
-  disableSelection: {
-    control: 'boolean',
-    description: `Disable user-initiated selection change (Call event.preventDefault() in ${prefix}-content-switcher-beingselected event)`,
+  selectionMode: {
+    control: 'radio',
+    options: ['automatic', 'manual'],
+    description:
+      'Choose whether or not to automatically change selection on focus when left/right arrow pressed. Defaults to `automatic`',
+  },
+  size: {
+    control: 'radio',
+    options: sizes,
+    description:
+      'Specify the size of the Content Switcher. Currently supports either sm, md (default) or lg as an option.',
   },
   onBeforeSelect: {
     action: `${prefix}-content-switcher-beingselected`,
   },
-  onSelect: {
+  onChange: {
     action: `${prefix}-content-switcher-selected`,
+  },
+  lowContrast: {
+    control: 'boolean',
+    description: 'Use the low contrast version of the content switcher',
   },
 };
 
 export const Default = {
-  render: () => html`
-    <cds-content-switcher value="all">
-      <cds-content-switcher-item value="all">
-        First section
-      </cds-content-switcher-item>
-      <cds-content-switcher-item value="cloudFoundry">
-        Second section
-      </cds-content-switcher-item>
-      <cds-content-switcher-item value="staging">
-        Third section
-      </cds-content-switcher-item>
-    </cds-content-switcher>
-  `,
+  args,
+  argTypes,
+  render: ({
+    value,
+    onBeforeSelect,
+    onChange,
+    size,
+    selectionMode,
+    selectedIndex,
+    lowContrast,
+  }) => {
+    const handleBeforeSelect = (event: CustomEvent) => {
+      onBeforeSelect?.(event);
+    };
+
+    return html`
+      <cds-content-switcher
+        value="${ifDefined(value)}"
+        selectionMode="${selectionMode}"
+        selectedIndex="${selectedIndex}"
+        size="${size}"
+        ?lowcontrast="${lowContrast}"
+        @cds-content-switcher-beingselected="${handleBeforeSelect}"
+        @cds-content-switcher-selected="${onChange}">
+        <cds-content-switcher-item
+          value="all"
+          name="one"
+          text="First section"
+          ?lowcontrast="${lowContrast}">
+          First section
+        </cds-content-switcher-item>
+        <cds-content-switcher-item
+          value="cloudFoundry"
+          name="two"
+          text="Second section"
+          ?lowcontrast="${lowContrast}">
+          Second section
+        </cds-content-switcher-item>
+        <cds-content-switcher-item
+          value="staging"
+          name="three"
+          text="Third section"
+          ?lowcontrast="${lowContrast}">
+          Third section
+        </cds-content-switcher-item>
+      </cds-content-switcher>
+    `;
+  },
 };
 
 export const IconOnly = {
-  render: () => html`
-    <cds-content-switcher value="all">
-      <cds-content-switcher-item icon value="all">
+  args,
+  argTypes,
+  render: ({ value, size, selectionMode, selectedIndex, lowContrast }) => html`
+    <cds-content-switcher
+      value="${ifDefined(value)}"
+      size="${size}"
+      selectionMode="${selectionMode}"
+      selectedIndex="${selectedIndex}"
+      ?lowcontrast="${lowContrast}">
+      <cds-content-switcher-item icon value="all" ?lowcontrast="${lowContrast}">
         ${TableOfContents16()}
         <span slot="tooltip-content">Table of Contents</span>
       </cds-content-switcher-item>
-      <cds-content-switcher-item icon value="cloudFoundry">
+      <cds-content-switcher-item
+        icon
+        value="cloudFoundry"
+        ?lowcontrast="${lowContrast}">
         ${Workspace16()}
         <span slot="tooltip-content">Workspace Test</span>
       </cds-content-switcher-item>
-      <cds-content-switcher-item icon value="staging">
+      <cds-content-switcher-item
+        icon
+        value="staging"
+        ?lowcontrast="${lowContrast}">
         ${ViewMode2_16()}
         <span slot="tooltip-content">View Mode</span>
       </cds-content-switcher-item>
@@ -88,18 +147,34 @@ export const IconOnly = {
 };
 
 export const IconOnlyWithLayer = {
-  render: () => html`
+  args,
+  argTypes,
+  render: ({ value, size, selectionMode, selectedIndex, lowContrast }) => html`
     <sb-template-layers>
-      <cds-content-switcher value="all">
-        <cds-content-switcher-item icon value="all">
+      <cds-content-switcher
+        value="${ifDefined(value)}"
+        size="${size}"
+        selectionMode="${selectionMode}"
+        selectedIndex="${selectedIndex}"
+        ?lowcontrast="${lowContrast}">
+        <cds-content-switcher-item
+          icon
+          value="all"
+          ?lowcontrast="${lowContrast}">
           ${TableOfContents16()}
           <span slot="tooltip-content">Table of Contents</span>
         </cds-content-switcher-item>
-        <cds-content-switcher-item icon value="cloudFoundry">
+        <cds-content-switcher-item
+          icon
+          value="cloudFoundry"
+          ?lowcontrast="${lowContrast}">
           ${Workspace16()}
           <span slot="tooltip-content">Workspace Test</span>
         </cds-content-switcher-item>
-        <cds-content-switcher-item icon value="staging">
+        <cds-content-switcher-item
+          icon
+          value="staging"
+          ?lowcontrast="${lowContrast}">
           ${ViewMode2_16()}
           <span slot="tooltip-content">View Mode</span>
         </cds-content-switcher-item>
@@ -108,10 +183,98 @@ export const IconOnlyWithLayer = {
   `,
 };
 
+export const LowContrast = {
+  args: {
+    ...args,
+    lowContrast: true,
+  },
+  argTypes,
+  render: ({
+    value,
+    onBeforeSelect,
+    onChange,
+    size,
+    selectionMode,
+    selectedIndex,
+    lowContrast,
+  }) => {
+    const handleBeforeSelect = (event: CustomEvent) => {
+      onBeforeSelect?.(event);
+    };
+
+    return html`
+      <cds-content-switcher
+        value="${ifDefined(value)}"
+        selectionMode="${selectionMode}"
+        selectedIndex="${selectedIndex}"
+        size="${size}"
+        ?lowcontrast="${lowContrast}"
+        @cds-content-switcher-beingselected="${handleBeforeSelect}"
+        @cds-content-switcher-selected="${onChange}">
+        <cds-content-switcher-item ?lowcontrast="${lowContrast}" value="all">
+          First section
+        </cds-content-switcher-item>
+        <cds-content-switcher-item
+          ?lowcontrast="${lowContrast}"
+          value="cloudFoundry">
+          Second section
+        </cds-content-switcher-item>
+        <cds-content-switcher-item
+          ?lowcontrast="${lowContrast}"
+          value="staging">
+          Third section
+        </cds-content-switcher-item>
+      </cds-content-switcher>
+    `;
+  },
+};
+
+export const lowContrastIconOnly = {
+  args: {
+    ...args,
+    lowContrast: true,
+  },
+  argTypes,
+  render: ({ value, size, selectionMode, selectedIndex, lowContrast }) => html`
+    <cds-content-switcher
+      value="${ifDefined(value)}"
+      size="${size}"
+      selectionMode="${selectionMode}"
+      selectedIndex="${selectedIndex}"
+      ?lowcontrast="${lowContrast}">
+      <cds-content-switcher-item icon value="all" ?lowcontrast="${lowContrast}">
+        ${TableOfContents16()}
+        <span slot="tooltip-content">Table of Contents</span>
+      </cds-content-switcher-item>
+      <cds-content-switcher-item
+        icon
+        value="cloudFoundry"
+        ?lowcontrast="${lowContrast}">
+        ${Workspace16()}
+        <span slot="tooltip-content">Workspace Test</span>
+      </cds-content-switcher-item>
+      <cds-content-switcher-item
+        icon
+        value="staging"
+        ?lowcontrast="${lowContrast}">
+        ${ViewMode2_16()}
+        <span slot="tooltip-content">View Mode</span>
+      </cds-content-switcher-item>
+    </cds-content-switcher>
+  `,
+};
+
 export const WithLayer = {
-  render: () => html`
+  args,
+  argTypes,
+  render: ({ value, size, selectionMode, selectedIndex, lowContrast }) => html`
     <sb-template-layers>
-      <cds-content-switcher value="all">
+      <cds-content-switcher
+        value="${ifDefined(value)}"
+        size="${size}"
+        selectionMode="${selectionMode}"
+        selectedIndex="${selectedIndex}"
+        ?lowcontrast="${lowContrast}">
         <cds-content-switcher-item value="all">
           First section
         </cds-content-switcher-item>
@@ -124,44 +287,6 @@ export const WithLayer = {
       </cds-content-switcher>
     </sb-template-layers>
   `,
-};
-
-export const Playground = {
-  args,
-  argTypes,
-  render: (args) => {
-    const {
-      value,
-      disableSelection,
-      onBeforeSelect = noop,
-      onSelect = noop,
-      size,
-    } = args ?? {};
-    const handleBeforeSelected = (event: CustomEvent) => {
-      onBeforeSelect(event);
-      if (disableSelection) {
-        event.preventDefault();
-      }
-    };
-
-    return html`
-      <cds-content-switcher
-        value="${ifDefined(value)}"
-        @cds-content-switcher-beingselected="${handleBeforeSelected}"
-        @cds-content-switcher-selected="${onSelect}"
-        size="${size}">
-        <cds-content-switcher-item value="all">
-          First section
-        </cds-content-switcher-item>
-        <cds-content-switcher-item value="cloudFoundry">
-          Second section
-        </cds-content-switcher-item>
-        <cds-content-switcher-item value="staging">
-          Third section
-        </cds-content-switcher-item>
-      </cds-content-switcher>
-    `;
-  },
 };
 
 const meta = {
