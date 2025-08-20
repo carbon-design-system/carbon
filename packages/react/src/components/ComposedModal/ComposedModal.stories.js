f6d665c0f317892f7c8fff1523a16fef2ecd0095
Taylor Jones <taylor.jones826@gmail.com>
2025-08-19 19:18:22 -0500

    chore: remove uneeded test story

--- packages/react/src/components/ComposedModal/ComposedModal.stories.js
+++ packages/react/src/components/ComposedModal/ComposedModal.stories.js
@@ -264,56 +264,6 @@ export const WithStateManager = () => {
   );
 };
 
-export const WithStateManagerPreventClose = () => {
-  const button = React.useRef();
-
-  /**
-   * Simple state manager for modals.
-   */
-  const ModalStateManager = ({
-    renderLauncher: LauncherContent,
-    children: ModalContent,
-  }) => {
-    const [open, setOpen] = React.useState(false);
-    return (
-      <>
-        {!ModalContent || typeof document === 'undefined'
-          ? null
-          : ReactDOM.createPortal(
-              <ModalContent open={open} setOpen={setOpen} />,
-              document.body
-            )}
-        {LauncherContent && <LauncherContent open={open} setOpen={setOpen} />}
-      </>
-    );
-  };
-  return (
-    <ModalStateManager
-      renderLauncher={({ setOpen }) => (
-        <Button ref={button} onClick={() => setOpen(true)}>
-          Launch composed modal
-        </Button>
-      )}>
-      {({ open, setOpen }) => (
-        <ComposedModal
-          open={open}
-          preventCloseOnClickOutside
-          onClose={() => {
-            console.log(`CALLED ON CLOSE`);
-            setOpen(false);
-          }}
-          launcherButtonRef={button}>
-          <ModalHeader label="A test" title="of preventCloseOnClickOutside" />
-          <ModalBody>
-            <p style={{ marginBottom: '1rem' }}>test</p>
-          </ModalBody>
-          <ModalFooter primaryButtonText="Add" secondaryButtonText="Cancel" />
-        </ComposedModal>
-      )}
-    </ModalStateManager>
-  );
-};
-
 export const WithScrollingContent = () => {
   const [open, setOpen] = useState(true);
   return (
