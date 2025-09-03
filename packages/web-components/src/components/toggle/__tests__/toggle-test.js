b9f94ef5940c8f2034395cc7e9fcb695f11a0b44
Mahmoud <maradwan26@gmail.com>
2025-09-03 13:13:42 -0400

    test: add and refactor tests

--- packages/web-components/src/components/toggle/__tests__/toggle-test.js
+++ packages/web-components/src/components/toggle/__tests__/toggle-test.js
@@ -6,15 +6,15 @@
  */
 
 import '@carbon/web-components/es/components/toggle/index.js';
-import { expect, fixture, html } from '@open-wc/testing';
+import { expect, fixture, html, oneEvent } from '@open-wc/testing';
 
 describe('cds-toggle', () => {
   it('should match button and label ids', async () => {
     const el = await fixture(html`
       <cds-toggle
         label-text="test label"
-        label-a="Off"
-        label-b="On"></cds-toggle>
+        label-a="On"
+        label-b="Off"></cds-toggle>
     `);
 
     const switchEl = el.shadowRoot.querySelector('[role="switch"]');
@@ -24,12 +24,13 @@ describe('cds-toggle', () => {
     expect(labelEl).to.exist;
     expect(switchEl.id).to.equal(labelEl.getAttribute('for'));
   });
+
   it('renders labelA when unchecked and labelB when checked', async () => {
     const el = await fixture(html`
       <cds-toggle
         label-text="Airplane mode"
-        label-a="Off"
-        label-b="On"></cds-toggle>
+        label-a="On"
+        label-b="Off"></cds-toggle>
     `);
     await el.updateComplete;
 
@@ -37,6 +38,7 @@ describe('cds-toggle', () => {
     expect(el.shadowRoot.textContent).to.not.contain('On');
 
     el.checked = true;
+    await el.updateComplete;
 
     expect(el.shadowRoot.textContent).to.contain('On');
     expect(el.shadowRoot.textContent).to.not.contain('Off');
@@ -46,14 +48,14 @@ describe('cds-toggle', () => {
     let el = await fixture(html`
       <cds-toggle
         label-text="Test label"
-        label-a="Off"
-        label-b="On"
+        label-a="On"
+        label-b="Off"
         size="sm"></cds-toggle>
     `);
     await el.updateComplete;
 
-    input = el.shadowRoot.querySelector('input');
-    classList = input?.classList || [];
+    const inputDiv = el.shadowRoot.querySelector('label > div');
+    const classList = inputDiv?.classList || [];
     expect(
       Array.from(classList).some((cls) =>
         cls.includes('--toggle__appearance--sm')
@@ -65,13 +67,13 @@ describe('cds-toggle', () => {
     const el = await fixture(html`
       <cds-toggle
         label-text="Test label"
-        label-a="Off"
-        label-b="On"
+        label-a="On"
+        label-b="Off"
         hideLabel></cds-toggle>
     `);
     await el.updateComplete;
 
-    const label = el.shadowRoot.querySelector('label');
+    const label = el.shadowRoot.querySelector('label > span');
     const classList = label?.classList || [];
     expect(
       Array.from(classList).some((cls) => cls.includes('--visually-hidden'))
@@ -92,37 +94,33 @@ describe('cds-toggle', () => {
     const elRegular = await fixture(html`<cds-toggle></cds-toggle>`);
     await elRegular.updateComplete;
 
-    const sideLabelElRegular = [
-      ...elRegular.shadowRoot.querySelectorAll('*'),
-    ].find(
-      (node) =>
-        node.classList &&
-        [...node.classList].some((cls) => cls.endsWith('--toggle__text'))
-    );
+    expect(elRegular.shadowRoot.textContent).to.contain('Off');
 
-    expect(sideLabelElRegular).to.exist;
+    const buttonRegular = elRegular.shadowRoot.querySelector('button');
+    buttonRegular.click();
+
+    await elRegular.updateComplete;
+    expect(elRegular.shadowRoot.textContent).to.contain('On');
 
     const elHideLabel = await fixture(
       html`<cds-toggle hideLabel></cds-toggle>`
     );
     await elHideLabel.updateComplete;
 
-    const sideLabelElHideLabel = [
-      ...elHideLabel.shadowRoot.querySelectorAll('*'),
-    ].find(
-      (node) =>
-        node.classList &&
-        [...node.classList].some((cls) => cls.endsWith('--toggle__text'))
-    );
+    expect(elHideLabel.shadowRoot.textContent.trim()).to.equal('');
 
-    expect(sideLabelElHideLabel).to.not.exist;
+    const buttonHideLabel = elHideLabel.shadowRoot.querySelector('button');
+    buttonHideLabel.click();
+
+    await elHideLabel.updateComplete;
+    expect(elHideLabel.shadowRoot.textContent.trim()).to.equal('');
   });
 
   it('supports disabled', async () => {
     const el = await fixture(html` <cds-toggle disabled> </cds-toggle> `);
     await el.updateComplete;
-    const input = el.shadowRoot.querySelector('input');
-    expect(input.disabled).to.be.true;
+    const button = el.shadowRoot.querySelector('button');
+    expect(button.disabled).to.be.true;
   });
 
   it('respects disabled to prevent changes', async () => {
@@ -130,18 +128,10 @@ describe('cds-toggle', () => {
     await el.updateComplete;
 
     expect(el.checked).to.be.false;
-    el.click();
+    const button = el.shadowRoot.querySelector('button');
+    button.click();
     await el.updateComplete;
     expect(el.checked).to.be.false;
-  });
-
-  it('can be controlled with using checked', async () => {
-    const el = await fixture(html` <cds-toggle> </cds-toggle> `);
-    await el.updateComplete;
-    expect(el.checked).to.be.false;
-    el.checked = true;
-    await el.updateComplete;
-    expect(el.checked).to.be.true;
   });
 
   it('respects readonly to prevent changes', async () => {
@@ -149,8 +139,79 @@ describe('cds-toggle', () => {
     await el.updateComplete;
 
     expect(el.checked).to.be.false;
-    el.click();
+    const button = el.shadowRoot.querySelector('button');
+    button.click();
     await el.updateComplete;
     expect(el.checked).to.be.false;
+  });
+
+  it('can be controlled with using checked', async () => {
+    const el = await fixture(html` <cds-toggle> </cds-toggle> `);
+    await el.updateComplete;
+
+    expect(el.checked).to.be.false;
+    el.checked = true;
+    await el.updateComplete;
+    expect(el.checked).to.be.true;
+  });
+
+  it('should fire cds-toggle-changed event when toggle is changed', async () => {
+    const el = await fixture(html`<cds-toggle></cds-toggle>`);
+
+    await el.updateComplete;
+
+    const button = el.shadowRoot.querySelector('button');
+    const listener = oneEvent(el, 'cds-toggle-changed');
+    button.click();
+    const event = await listener;
+
+    expect(event).to.exist;
+    expect(event.detail).to.deep.equal({
+      checked: true,
+    });
+  });
+
+  it('should not fire changed event when readonly', async () => {
+    const toggle = html`<cds-toggle read-only></cds-toggle>`;
+    const el = await fixture(toggle);
+
+    await el.updateComplete;
+
+    const button = el.shadowRoot.querySelector('button');
+
+    const eventPromise = oneEvent(el, 'cds-toggle-changed');
+    button.click();
+
+    const result = await Promise.race([
+      eventPromise.then(() => 'cds-toggle-changed event fired'),
+      new Promise((resolve) => setTimeout(() => resolve('timeout'), 100)),
+    ]);
+
+    expect(result).to.equal(
+      'timeout',
+      'cds-toggle-changed event should not fire when readonly'
+    );
+  });
+
+  it('should not fire changed event when disabled', async () => {
+    const toggle = html`<cds-toggle disabled></cds-toggle>`;
+    const el = await fixture(toggle);
+
+    await el.updateComplete;
+
+    const button = el.shadowRoot.querySelector('button');
+
+    const eventPromise = oneEvent(el, 'cds-toggle-changed');
+    button.click();
+
+    const result = await Promise.race([
+      eventPromise.then(() => 'cds-toggle-changed event fired'),
+      new Promise((resolve) => setTimeout(() => resolve('timeout'), 100)),
+    ]);
+
+    expect(result).to.equal(
+      'timeout',
+      'cds-toggle-changed event should not fire when disabled'
+    );
   });
 });
