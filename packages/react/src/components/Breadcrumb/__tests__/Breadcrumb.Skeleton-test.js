3db2a9f717c3176685ea657e96f1950c850127cb
Mahmoud <maradwan26@gmail.com>
2025-09-04 13:18:58 -0400

    test(react): add test cases

--- packages/react/src/components/Breadcrumb/__tests__/Breadcrumb.Skeleton-test.js
+++ packages/react/src/components/Breadcrumb/__tests__/Breadcrumb.Skeleton-test.js
@@ -7,7 +7,9 @@
 
 import React from 'react';
 import { BreadcrumbSkeleton } from '../';
-import { render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
+
+const prefix = 'cds';
 
 describe('BreadcrumbSkeleton', () => {
   it('should support a custom `className` prop on the outermost element', () => {
@@ -18,5 +20,31 @@ describe('BreadcrumbSkeleton', () => {
   it('should spread additional props on the outermost element', () => {
     const { container } = render(<BreadcrumbSkeleton data-testid="test" />);
     expect(container.firstChild).toHaveAttribute('data-testid', 'test');
+  });
+
+  it('should render the specified number of skeleton items', () => {
+    const { container } = render(<BreadcrumbSkeleton items={5} />);
+    const items = container.querySelectorAll(`.${prefix}--breadcrumb-item`);
+    expect(items).toHaveLength(5);
+  });
+
+  it('should respect size prop', () => {
+    render(<BreadcrumbSkeleton size="sm" />);
+    expect(screen.getByLabelText('Breadcrumb')).toHaveClass(
+      `${prefix}--breadcrumb--sm`
+    );
+  });
+
+  it('should accept a `noTrailingSlash` and omit the trailing slash', () => {
+    render(<BreadcrumbSkeleton noTrailingSlash />);
+
+    // The slashes are implemented with pseudo elements that can't be detected in jsdom.
+    // So we have to settle here for just validating against the class. Pseudo elements
+    // should be tested in the browser/e2e tests.
+    // https://testing-library.com/docs/dom-testing-library/api-configuration/#computedstylesupportspseudoelements
+    // https://github.com/jsdom/jsdom/issues/1928
+    expect(screen.getByLabelText('Breadcrumb')).toHaveClass(
+      `${prefix}--breadcrumb--no-trailing-slash`
+    );
   });
 });
