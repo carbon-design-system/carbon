4364afcabb82ef31354224f526160426445d2f4c
Tim Fuhrmann <tim.fuhrmann@ibm.com>
2025-09-11 20:20:48 +0200

    fix(Modal): fix stories

--- packages/react/src/components/Modal/ModalPresence.featureflag.stories.js
+++ packages/react/src/components/Modal/ModalPresence.featureflag.stories.js
@@ -14,7 +14,7 @@ import Dropdown from '../Dropdown';
 import SelectItem from '../SelectItem';
 import TextInput from '../TextInput';
 import { ClassPrefix } from '../ClassPrefix';
-import './Modal.stories.scss';
+import './ModalPresence.stories.scss';
 import { FeatureFlags } from '../FeatureFlags';
 import { Annotation } from '../../../.storybook/templates/Annotation';
 import LinkTo from '@storybook/addon-links/react';
@@ -32,24 +32,24 @@ export default {
   },
 };
 
-export const EnableDialogElement = () => {
+export const EnablePresence = () => {
   const [open, setOpen] = useState(false);
   return (
-    <FeatureFlags enableDialogElement>
+    <FeatureFlags enablePresence>
       <Annotation
         type="feature-flags"
         text={
           <span>
             This story is rendered with{' '}
             <LinkTo title="Getting Started/Feature Flags" name="Overview">
-              enable-dialog-element
+              enable-presence
             </LinkTo>{' '}
             enabled
           </span>
         }>
         <Button onClick={() => setOpen(true)}>Launch modal</Button>
         <ClassPrefix prefix="dialog-refactor">
-          <div className="preview-modal">
+          <div className="experimental-modal-with-presence">
             <Modal
               open={open}
               onRequestClose={() => setOpen(false)}
@@ -106,81 +106,4 @@ export const EnableDialogElement = () => {
     </FeatureFlags>
   );
 };
-EnableDialogElement.storyName = 'enable-dialog-element';
-
-export const EnableExperimentalFocusWrapWithoutSentinels = () => {
-  const [open, setOpen] = useState(false);
-  return (
-    <FeatureFlags enableExperimentalFocusWrapWithoutSentinels>
-      <Annotation
-        type="feature-flags"
-        text={
-          <span>
-            This story is rendered with{' '}
-            <LinkTo title="Getting Started/Feature Flags" name="Overview">
-              enable-preview-focus-wrap-without-sentinels
-            </LinkTo>{' '}
-            enabled
-          </span>
-        }>
-        <Button onClick={() => setOpen(true)}>Launch modal</Button>
-        <ClassPrefix prefix="dialog-refactor">
-          <div className="preview-modal">
-            <Modal
-              open={open}
-              onRequestClose={() => setOpen(false)}
-              modalHeading="Add a custom domain"
-              modalLabel="Account resources"
-              primaryButtonText="Add"
-              secondaryButtonText="Cancel">
-              <p style={{ marginBottom: '1rem' }}>
-                Custom domains direct requests for your apps in this Cloud
-                Foundry organization to a URL that you own. A custom domain can
-                be a shared domain, a shared subdomain, or a shared domain and
-                host.
-              </p>
-              <TextInput
-                autofocus="true"
-                id="text-input-1"
-                labelText="Domain name"
-                placeholder="e.g. github.com"
-                style={{ marginBottom: '1rem' }}
-              />
-              <Select id="select-1" defaultValue="us-south" labelText="Region">
-                <SelectItem value="us-south" text="US South" />
-                <SelectItem value="us-east" text="US East" />
-              </Select>
-              <Dropdown
-                id="drop"
-                label="Dropdown"
-                titleText="Dropdown"
-                items={[
-                  { id: 'one', label: 'one', name: 'one' },
-                  { id: 'two', label: 'two', name: 'two' },
-                ]}
-              />
-              <MultiSelect
-                id="test"
-                label="Multiselect"
-                titleText="Multiselect"
-                items={[
-                  {
-                    id: 'downshift-1-item-0',
-                    text: 'Option 1',
-                  },
-                  {
-                    id: 'downshift-1-item-1',
-                    text: 'Option 2',
-                  },
-                ]}
-                itemToString={(item) => (item ? item.text : '')}
-              />
-            </Modal>
-          </div>
-        </ClassPrefix>
-      </Annotation>
-    </FeatureFlags>
-  );
-};
-EnableExperimentalFocusWrapWithoutSentinels.storyName =
-  'enable-preview-focus-wrap-without-sentinels';
+EnablePresence.storyName = 'enable-presence';
