149f0f23fe82a3062713b1b8bde5d627047763bd
Adam Alston <aalston9@gmail.com>
2025-09-15 17:16:29 -0400

    refactor: rewrite mergeRefs in typescript (#20404)

--- packages/react/src/tools/__tests__/mergeRefs-test.js
+++ packages/react/src/tools/__tests__/mergeRefs-test.js
@@ -1,15 +1,15 @@
 /**
- * Copyright IBM Corp. 2016, 2023
+ * Copyright IBM Corp. 2016, 2025
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
 import React from 'react';
-import mergeRefs from '../mergeRefs';
+import { mergeRefs } from '../mergeRefs';
 
 describe('mergeRefs', () => {
-  it('supports forwardRef as well as ref callback', () => {
+  it('should support object refs and ref callbacks', () => {
     const elem = document.createElement('div');
     const ref = React.createRef();
     let functionRefResult;
@@ -20,9 +20,67 @@ describe('mergeRefs', () => {
     expect(functionRefResult).toBe(elem);
   });
 
-  it('skips empty ref', () => {
+  it('should skip empty refs', () => {
     expect(() => {
       mergeRefs(undefined)(document.createElement('div'));
-    }).not.toThrow(Error);
+    }).not.toThrow();
+    expect(() => {
+      mergeRefs(null)(document.createElement('div'));
+    }).not.toThrow();
+  });
+
+  it('should merge multiple refs and update them all', () => {
+    const element = document.createElement('div');
+    const ref1 = React.createRef();
+    const ref2 = React.createRef();
+    let callback1, callback2;
+    const merged = mergeRefs(
+      ref1,
+      ref2,
+      (el) => (callback1 = el),
+      (el) => (callback2 = el)
+    );
+
+    merged(element);
+
+    expect(ref1.current).toBe(element);
+    expect(ref2.current).toBe(element);
+    expect(callback1).toBe(element);
+    expect(callback2).toBe(element);
+  });
+
+  it('should allow clearing', () => {
+    const ref = React.createRef();
+    let callbackValue;
+    const merged = mergeRefs(ref, (el) => {
+      callbackValue = el;
+    });
+    const element = document.createElement('div');
+
+    merged(element);
+
+    expect(ref.current).toBe(element);
+    expect(callbackValue).toBe(element);
+
+    // Setting the value to `null` should update the object and callback refs.
+    merged(null);
+
+    expect(ref.current).toBeNull();
+    expect(callbackValue).toBeNull();
+  });
+
+  it('should not mutate non-ref plain objects', () => {
+    const element = document.createElement('div');
+    const randomObject = {};
+
+    mergeRefs(randomObject)(element);
+
+    expect('current' in randomObject).toBe(false);
+  });
+
+  it('should ignore primitive values passed accidentally as refs', () => {
+    const element = document.createElement('div');
+
+    expect(() => mergeRefs(42, 'hello')(element)).not.toThrow();
   });
 });
