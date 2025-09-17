a87b662901ddcd11655557de7a3abea934c3fe74
Taylor Jones <tay1orjones@users.noreply.github.com>
2025-09-17 13:52:13 -0500

    Revert "fix(19982): fix toggletip-clickhandler (#20094)"
    This reverts commit 88637fd6a8005c2aa94f7ca64f475ad7b66ad403.

--- packages/web-components/src/components/toggle-tip/toggletip.ts
+++ packages/web-components/src/components/toggle-tip/toggletip.ts
@@ -110,11 +110,9 @@ class CDSToggletip extends HostListenerMixin(FocusMixin(LitElement)) {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment -- https://github.com/carbon-design-system/carbon/issues/20071
   // @ts-ignore: The decorator refers to this method but TS thinks this method is not referred to
   protected _handleFocusOut(event: FocusEvent) {
-    const path = event.composedPath();
-    if (path.includes(this as unknown as EventTarget)) {
-      return;
+    if (!this.contains(event.relatedTarget as Node)) {
+      this.open = false;
     }
-    this.open = false;
   }
 
   protected _renderToggleTipLabel = () => {
