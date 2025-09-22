1bfe853b072350fbdd957d320fe1779e73477f18
Neethu9074 <neethu9074@gmail.com>
2025-09-22 15:02:38 +0530

    chore(tests): add menu unit tests

---
+++ packages/web-components/src/components/menu/__tests__/__snapshots__/menu-test.snap.js
@@ -0,0 +1,14 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots['cds-menu should render with default properties'] = `<ul
+  aria-label=""
+  class="cds--menu cds--menu--open cds--menu--shown cds--menu--sm"
+  role="menu"
+  tabindex="-1"
+>
+  <slot>
+  </slot>
+</ul>
+`;
+/* end snapshot cds-menu should render with default properties */
