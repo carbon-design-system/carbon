9bb5e00630c27e935a2256e98e70021f83509059
naaa760 <neh6a683@gmail.com>
2025-10-17 08:44:20 +0530

    fix(text-input): disallow invalid/warn states when disabled or readonly

--- packages/web-components/src/components/text-input/text-input.ts
+++ packages/web-components/src/components/text-input/text-input.ts
@@ -355,8 +355,8 @@ class CDSTextInput extends ValidityMixin(FormMixin(LitElement)) {
       icon: ReturnType<typeof iconLoader>;
     } = {
       disabled: !readonly && disabled,
-      invalid: !readonly && invalid,
-      warn: !readonly && !invalid && warn,
+      invalid: !readonly && !disabled && invalid,
+      warn: !readonly && !disabled && !invalid && warn,
       'slot-name': '',
       'slot-text': '',
       icon: null,
