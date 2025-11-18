a4172c3a0f19abdce5d672d751a49241ff6304e7
Riddhi Bansal <41935566+riddhybansal@users.noreply.github.com>
2025-11-18 11:05:13 +0530

    Revert "fix(dropdown): align styles and behavior with React parity (#20863)" (#21037)
    This reverts commit 18b87e5c848d468dec5256c414176e046115e1c9.

--- packages/web-components/src/components/dropdown/dropdown.stories.ts
+++ packages/web-components/src/components/dropdown/dropdown.stories.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2019, 2025
+ * Copyright IBM Corp. 2019, 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -15,9 +15,6 @@ import Folders16 from '@carbon/icons/es/folders/16.js';
 import './dropdown-item';
 import './dropdown-skeleton';
 import '../ai-label';
-import '../toggle-tip';
-import '../link';
-import '../button';
 
 const directionOptions = {
   [`Top`]: DROPDOWN_DIRECTION.TOP,
@@ -61,28 +58,15 @@ const items = [
     value: 'option-5',
     text: 'Option 5',
   },
-  {
-    value: 'option-6',
-    text: 'Option 6',
-  },
-  {
-    value: 'option-7',
-    text: 'Option 7',
-  },
-  {
-    value: 'option-8',
-    text: 'Option 8',
-  },
 ];
 
 const defaultArgs = {
-  ariaLabel: '',
   direction: DROPDOWN_DIRECTION.BOTTOM,
   disabled: false,
   hideLabel: false,
-  helperText: '',
+  helperText: 'This is some helper text',
   invalid: false,
-  invalidText: '',
+  invalidText: 'invalid selection',
   label: 'This is an example label',
   open: false,
   readOnly: false,
@@ -91,35 +75,34 @@ const defaultArgs = {
   type: null,
   value: '',
   warn: false,
-  warnText: '',
+  warnText: 'please notice the warning',
 };
 
 const controls = {
   ariaLabel: {
     control: 'text',
-    description:
-      'Specify a label to be read by screen readers on the container node.',
+    description: 'aria-label',
+  },
+  disabled: {
+    control: 'boolean',
+    description: `Specify if the dropdown should be disabled, or not.`,
   },
   direction: {
     control: 'select',
     options: directionOptions,
-    description: `Specify the direction of the dropdown. Can be either 'top' or 'bottom'.`,
-  },
-  disabled: {
-    control: 'boolean',
-    description: `Disable the control.`,
-  },
-  helperText: {
-    control: 'text',
-    description: `Provide helper text that is used alongside the control label for additional help.`,
+    description: `Dropdown direction.`,
   },
   hideLabel: {
     control: 'boolean',
-    description: `Specify whether the title text should be hidden or not.`,
+    description: `Specify if the title text should be hidden, or not.`,
+  },
+  helperText: {
+    control: 'text',
+    description: `The helper text for the dropdown.`,
   },
   invalid: {
     control: 'boolean',
-    description: `Specify if the currently selected value is invalid.`,
+    description: `Specify if the dropdown should display an invalid icon, or not.`,
   },
   invalidText: {
     control: 'text',
@@ -135,12 +118,12 @@ const controls = {
   },
   readOnly: {
     control: 'boolean',
-    description: `Whether or not the Dropdown is readonly.`,
+    description: `Specify if the dropdown should be read only, or not.`,
   },
   size: {
     control: 'select',
     options: sizes,
-    description: `Specify the size of the ListBox. Currently supports either 'sm', 'md' or 'lg' as an option.`,
+    description: `Dropdown size.`,
   },
   titleText: {
     control: 'text',
@@ -149,7 +132,7 @@ const controls = {
   type: {
     control: 'select',
     options: types,
-    description: `The dropdown type, 'default' or 'inline'`,
+    description: `Dropdown size.`,
   },
   value: {
     control: 'text',
@@ -157,57 +140,20 @@ const controls = {
   },
   warn: {
     control: 'boolean',
-    description: `Specify whether the control is currently in warning state`,
+    description: `Specify whether the control is currently in warning state.`,
   },
   warnText: {
     control: 'text',
-    description: `Provide the text that is displayed when the control is in warning state`,
+    description: `Text that is displayed when the control is in warning state.`,
   },
 };
 
 export const Default = {
-  argTypes: controls,
-  args: {
-    ...defaultArgs,
-    helperText: 'This is some helper text',
-    invalidText: 'invalid selection',
-    warnText: 'please notice the warning',
-  },
-  render: ({
-    ariaLabel,
-    open,
-    direction,
-    disabled,
-    helperText,
-    hideLabel,
-    invalid,
-    invalidText,
-    titleText,
-    readOnly,
-    size,
-    type,
-    value,
-    label,
-    warn,
-    warnText,
-  }) => html`
+  render: () => html`
     <cds-dropdown
-      aria-label=${ariaLabel}
-      ?open=${open}
-      ?disabled="${disabled}"
-      ?hide-label=${hideLabel}
-      helper-text=${helperText}
-      ?invalid=${invalid}
-      ?read-only=${readOnly}
-      invalid-text=${invalidText}
-      direction="${direction}"
-      title-text=${ifDefined(titleText)}
-      size="${ifDefined(size)}"
-      type="${ifDefined(type)}"
-      value=${ifDefined(value)}
-      label=${ifDefined(label)}
-      ?warn=${warn}
-      warn-text=${warnText}>
+      helper-text="This is some helper text"
+      title-text="This is an example title"
+      label="This is an example label">
       ${items.map(
         (elem) => html`
           <cds-dropdown-item ?disabled=${elem.disabled} value="${elem.value}"
@@ -219,110 +165,13 @@ export const Default = {
   `,
 };
 
-export const ExperimentalAutoAlign = {
-  argTypes: controls,
-  args: {
-    ...defaultArgs,
-    direction: DROPDOWN_DIRECTION.TOP,
-    helperText: 'This is some helper text',
-    label: 'Option 1',
-    titleText: 'Dropdown label',
-    value: 'option-1',
-  },
-  render: ({
-    ariaLabel,
-    open,
-    direction,
-    disabled,
-    helperText,
-    hideLabel,
-    invalid,
-    invalidText,
-    titleText,
-    readOnly,
-    size,
-    type,
-    value,
-    label,
-    warn,
-    warnText,
-  }) => html`
-    <div style="width:400px">
-      <div style="height: 300px"></div>
-      <cds-dropdown
-        aria-label=${ariaLabel}
-        ?open=${open}
-        ?disabled="${disabled}"
-        ?hide-label=${hideLabel}
-        helper-text=${helperText}
-        ?invalid=${invalid}
-        ?read-only=${readOnly}
-        invalid-text=${invalidText}
-        direction="${direction}"
-        title-text=${ifDefined(titleText)}
-        size="${ifDefined(size)}"
-        type="${ifDefined(type)}"
-        value=${ifDefined(value)}
-        label=${ifDefined(label)}
-        ?warn=${warn}
-        warn-text=${warnText}>
-        ${items.map(
-          (elem) => html`
-            <cds-dropdown-item ?disabled=${elem.disabled} value="${elem.value}"
-              >${elem.text}</cds-dropdown-item
-            >
-          `
-        )}
-      </cds-dropdown>
-      <div style="height: 800px"></div>
-    </div>
-  `,
-};
-
 export const Inline = {
-  argTypes: controls,
-  args: {
-    ...defaultArgs,
-    label: 'Option 1',
-    titleText: 'Inline dropdown label',
-    type: DROPDOWN_TYPE.INLINE,
-    value: 'option-1',
-  },
-  render: ({
-    ariaLabel,
-    open,
-    direction,
-    disabled,
-    helperText,
-    hideLabel,
-    invalid,
-    invalidText,
-    titleText,
-    readOnly,
-    size,
-    type,
-    value,
-    label,
-    warn,
-    warnText,
-  }) => html`
+  render: () => html`
     <cds-dropdown
-      aria-label=${ariaLabel}
-      ?open=${open}
-      ?disabled="${disabled}"
-      ?hide-label=${hideLabel}
-      helper-text=${helperText}
-      ?invalid=${invalid}
-      ?read-only=${readOnly}
-      invalid-text=${invalidText}
-      direction="${direction}"
-      title-text=${ifDefined(titleText)}
-      size="${ifDefined(size)}"
-      type="${ifDefined(type)}"
-      value=${ifDefined(value)}
-      label=${ifDefined(label)}
-      ?warn=${warn}
-      warn-text=${warnText}>
+      type="inline"
+      title-text="Inline dropdown label"
+      label="Dropdown menu options"
+      value="option-1">
       ${items.map(
         (elem) => html`
           <cds-dropdown-item ?disabled=${elem.disabled} value="${elem.value}"
@@ -335,51 +184,36 @@ export const Inline = {
 };
 
 export const InlineWithLayer = {
-  argTypes: controls,
-  args: {
-    ...defaultArgs,
-    label: 'Option 1',
-    titleText: 'Inline dropdown label',
-    type: DROPDOWN_TYPE.INLINE,
-    value: 'option-1',
-  },
-  render: ({
-    ariaLabel,
-    open,
-    direction,
-    disabled,
-    helperText,
-    hideLabel,
-    invalid,
-    invalidText,
-    titleText,
-    readOnly,
-    size,
-    type,
-    value,
-    label,
-    warn,
-    warnText,
-  }) => html`
+  render: () => html`
     <sb-template-layers>
       <div style="width:400px">
         <cds-dropdown
-          aria-label=${ariaLabel}
-          ?open=${open}
-          ?disabled="${disabled}"
-          ?hide-label=${hideLabel}
-          helper-text=${helperText}
-          ?invalid=${invalid}
-          ?read-only=${readOnly}
-          invalid-text=${invalidText}
-          direction="${direction}"
-          title-text=${ifDefined(titleText)}
-          size="${ifDefined(size)}"
-          type="${ifDefined(type)}"
-          value=${ifDefined(value)}
-          label=${ifDefined(label)}
-          ?warn=${warn}
-          warn-text=${warnText}>
+          type="inline"
+          title-text="Inline dropdown label"
+          label="Dropdown menu options">
+          ${items.map(
+            (elem) => html`
+              <cds-dropdown-item
+                ?disabled=${elem.disabled}
+                value="${elem.value}"
+                >${elem.text}</cds-dropdown-item
+              >
+            `
+          )}
+        </cds-dropdown>
+      </div>
+    </sb-template-layers>
+  `,
+};
+
+export const WithLayer = {
+  render: () => html`
+    <sb-template-layers>
+      <div style="width:400px">
+        <cds-dropdown
+          title-text="Dropdown label"
+          helper-text="This is some helper text"
+          label="Dropdown menu options">
           ${items.map(
             (elem) => html`
               <cds-dropdown-item
@@ -396,8 +230,6 @@ export const InlineWithLayer = {
 };
 
 export const Skeleton = {
-  argTypes: controls,
-  args: defaultArgs,
   parameters: {
     percy: {
       skip: true,
@@ -438,49 +270,12 @@ const actions = html`
 `;
 
 export const WithAILabel = {
-  argTypes: controls,
-  args: {
-    ...defaultArgs,
-    helperText: 'This is some helper text',
-    label: 'Option 1',
-    titleText: 'Dropdown title',
-    value: 'option-1',
-  },
-  render: ({
-    ariaLabel,
-    open,
-    direction,
-    disabled,
-    helperText,
-    hideLabel,
-    invalid,
-    invalidText,
-    titleText,
-    readOnly,
-    size,
-    type,
-    value,
-    label,
-    warn,
-    warnText,
-  }) => html`
+  render: () => html`
     <cds-dropdown
-      aria-label=${ariaLabel}
-      ?open=${open}
-      ?disabled="${disabled}"
-      ?hide-label=${hideLabel}
-      helper-text=${helperText}
-      ?invalid=${invalid}
-      ?read-only=${readOnly}
-      invalid-text=${invalidText}
-      direction="${direction}"
-      title-text=${ifDefined(titleText)}
-      size="${ifDefined(size)}"
-      type="${ifDefined(type)}"
-      value=${ifDefined(value)}
-      label=${ifDefined(label)}
-      ?warn=${warn}
-      warn-text=${warnText}>
+      helper-text="This is some helper text"
+      title-text="Dropdown title"
+      label="Dropdown menu options"
+      value="option-1">
       <cds-ai-label alignment="bottom-left"> ${content}${actions}</cds-ai-label>
       ${items.map(
         (elem) => html`
@@ -493,15 +288,9 @@ export const WithAILabel = {
   `,
 };
 
-export const WithLayer = {
+export const Playground = {
   argTypes: controls,
-  args: {
-    ...defaultArgs,
-    helperText: 'This is some helper text',
-    label: 'Option 1',
-    titleText: 'Dropdown label',
-    value: 'option-1',
-  },
+  args: defaultArgs,
   render: ({
     ariaLabel,
     open,
@@ -520,70 +309,12 @@ export const WithLayer = {
     warn,
     warnText,
   }) => html`
-    <sb-template-layers>
-      <div style="width:400px">
-        <cds-dropdown
-          aria-label=${ariaLabel}
-          ?open=${open}
-          ?disabled="${disabled}"
-          ?hide-label=${hideLabel}
-          helper-text=${helperText}
-          ?invalid=${invalid}
-          ?read-only=${readOnly}
-          invalid-text=${invalidText}
-          direction="${direction}"
-          title-text=${ifDefined(titleText)}
-          size="${ifDefined(size)}"
-          type="${ifDefined(type)}"
-          value=${ifDefined(value)}
-          label=${ifDefined(label)}
-          ?warn=${warn}
-          warn-text=${warnText}>
-          ${items.map(
-            (elem) => html`
-              <cds-dropdown-item
-                ?disabled=${elem.disabled}
-                value="${elem.value}"
-                >${elem.text}</cds-dropdown-item
-              >
-            `
-          )}
-        </cds-dropdown>
-      </div>
-    </sb-template-layers>
-  `,
-};
-
-export const WithToggletipLabel = {
-  argTypes: controls,
-  args: {
-    ...defaultArgs,
-    label: 'placeholder',
-    titleText: 'Dropdown title',
-    value: 'placeholder',
-  },
-  render: ({
-    ariaLabel,
-    open,
-    direction,
-    disabled,
-    hideLabel,
-    invalid,
-    invalidText,
-    titleText,
-    readOnly,
-    size,
-    type,
-    value,
-    label,
-    warn,
-    warnText,
-  }) => html`
     <cds-dropdown
-      aria-label=${ariaLabel}
+      aria-label=${ifDefined(ariaLabel)}
       ?open=${open}
       ?disabled="${disabled}"
       ?hide-label=${hideLabel}
+      helper-text=${ifDefined(helperText)}
       ?invalid=${invalid}
       ?read-only=${readOnly}
       invalid-text=${invalidText}
@@ -595,18 +326,13 @@ export const WithToggletipLabel = {
       label=${ifDefined(label)}
       ?warn=${warn}
       warn-text=${warnText}>
-      <span slot="title-text" style="display: flex; align-items: center;">
-        ${titleText}
-        <cds-toggletip alignment="bottom">
-          <p slot="body-text">
-            Lorem ipsum dolor sit amet, di os consectetur adipiscing elit, sed
-            do eiusmod tempor incididunt ut fsil labore et dolore magna aliqua.
-          </p>
-          <cds-link href="#" slot="actions">Link action</cds-link>
-          <cds-button size="sm" slot="actions">Button</cds-button>
-        </cds-toggletip>
-      </span>
-      ${[]}
+      ${items.map(
+        (elem) => html`
+          <cds-dropdown-item ?disabled=${elem.disabled} value="${elem.value}"
+            >${elem.text}</cds-dropdown-item
+          >
+        `
+      )}
     </cds-dropdown>
   `,
 };
@@ -615,15 +341,7 @@ const meta = {
   title: 'Components/Dropdown',
   decorators: [
     (story, { name }) => {
-      const isLayerStory = name.toLowerCase().includes('layer');
-      const isSkeletonStory = name.toLowerCase().includes('skeleton');
-
-      const width = isLayerStory
-        ? ''
-        : isSkeletonStory
-          ? 'width:300px'
-          : 'width:400px';
-
+      const width = !name.toLowerCase().includes('layer') ? `width:400px` : ``;
       return html` <div style="${width}">${story()}</div> `;
     },
   ],
