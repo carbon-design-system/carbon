84d82a93ea115b4f3c188d7442d0162cfc7c54ab
Adam Alston <aalston9@gmail.com>
2026-01-28 04:55:32 -0500

    fix(Dropdown): add support for string items in itemToElement (#21285)
    * fix(Dropdown): add support for string items in itemToElement
    
    * fix: align fluid component itemToElement types with parents
    
    * test: revert unintentional test change

--- packages/react/src/components/ComboBox/ComboBox.tsx
+++ packages/react/src/components/ComboBox/ComboBox.tsx
@@ -17,7 +17,6 @@ import React, {
   useMemo,
   useRef,
   useState,
-  type ComponentType,
   type FocusEvent,
   type ForwardedRef,
   type InputHTMLAttributes,
@@ -253,10 +252,9 @@ export interface ComboBoxProps<ItemType>
   invalidText?: ReactNode;
 
   /**
-   * Optional function to render items as custom components instead of strings.
-   * Defaults to null and is overridden by a getter
+   * Renders an item as a custom React node instead of a string.
    */
-  itemToElement?: ComponentType<ItemType> | null;
+  itemToElement?: ((item: ItemType) => ReactNode) | null;
 
   /**
    * Helper function passed to downshift that allows the library to render a
@@ -800,9 +798,6 @@ const ComboBox = forwardRef(
       [`${prefix}--combo-box--input--focus`]: isFocused,
     });
 
-    // needs to be Capitalized for react to render it correctly
-    const ItemToElement = itemToElement;
-
     // AILabel always size `mini`
     const candidate = slug ?? decorator;
     const candidateIsAILabel = isComponentElement(candidate, AILabel);
@@ -1240,11 +1235,9 @@ const ComboBox = forwardRef(
                         title={title}
                         disabled={disabled}
                         {...modifiedItemProps}>
-                        {ItemToElement ? (
-                          <ItemToElement key={itemProps.id} {...item} />
-                        ) : (
-                          itemToString(item)
-                        )}
+                        {itemToElement
+                          ? itemToElement(item)
+                          : itemToString(item)}
                         {isEqual(currentSelectedItem, item) && (
                           <Checkmark
                             className={`${prefix}--list-box__menu-item__selected-icon`}
@@ -1375,8 +1368,7 @@ ComboBox.propTypes = {
   invalidText: PropTypes.node,
 
   /**
-   * Optional function to render items as custom components instead of strings.
-   * Defaults to null and is overridden by a getter
+   * Renders an item as a custom React node instead of a string.
    */
   itemToElement: PropTypes.func,
 
