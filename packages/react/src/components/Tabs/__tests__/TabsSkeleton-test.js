06e025b2270abec289e2de2b0c1d0db48bc39cc5
Adam Alston <aalston9@gmail.com>
2026-02-12 16:29:01 -0500

    test: standardize test file naming conventions and update tests

--- packages/react/src/components/Tabs/__tests__/TabsSkeleton-test.js
+++ packages/react/src/components/Tabs/__tests__/TabsSkeleton-test.js
@@ -1,22 +1,22 @@
 /**
- * Copyright IBM Corp. 2016, 2023
+ * Copyright IBM Corp. 2016, 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
 import React from 'react';
-import StructuredListSkeleton from '../StructuredList.Skeleton';
+import { TabsSkeleton } from '../Tabs.Skeleton';
 import { render } from '@testing-library/react';
 
-describe('StructuredListSkeleton', () => {
+describe('TabsSkeleton', () => {
   it('should support a custom `className` prop on the outermost element', () => {
-    const { container } = render(<StructuredListSkeleton className="test" />);
+    const { container } = render(<TabsSkeleton className="test" />);
     expect(container.firstChild).toHaveClass('test');
   });
 
   it('should spread additional props on the outermost element', () => {
-    const { container } = render(<StructuredListSkeleton data-testid="test" />);
+    const { container } = render(<TabsSkeleton data-testid="test" />);
     expect(container.firstChild).toHaveAttribute('data-testid', 'test');
   });
 });
