078acdd521ac7c8d85a84348bc9d4ec5921b6b35
Adam Alston <aalston9@gmail.com>
2026-02-13 17:56:44 -0500

    fix(eslint): resolve rules-of-hooks violations

--- packages/react/src/components/Tag/Tag.tsx
+++ packages/react/src/components/Tag/Tag.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2016, 2025
+ * Copyright IBM Corp. 2016, 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -164,8 +164,8 @@ const TagBase = React.forwardRef<
       );
     }
     const ref = useMergedRefs([forwardRef, tagRef]);
-    // eslint-disable-next-line  react-hooks/rules-of-hooks -- https://github.com/carbon-design-system/carbon/issues/20452
-    const tagId = id || `tag-${useId()}`;
+    const generatedTagId = useId();
+    const tagId = id ?? `tag-${generatedTagId}`;
     const [isEllipsisApplied, setIsEllipsisApplied] = useState(false);
 
     useIsomorphicEffect(() => {
