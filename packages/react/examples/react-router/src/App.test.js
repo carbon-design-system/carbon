33930c653723e47b87574a228675fd7f3ed3028b
Kenny Lam <909118+kennylam@users.noreply.github.com>
2026-02-25 08:25:34 -0500

    chore(examples): remove react-router example

--- packages/react/examples/react-router/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  createRoot(div).render(<App />);
-});
