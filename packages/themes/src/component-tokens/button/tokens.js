15313e1c94798d4661651986fed104398f19bd7e
Benjamin Santos <Benjamin.Santos@ibm.com>
2026-02-27 15:21:40 -0500

    button tokens for vscode

--- packages/themes/src/component-tokens/button/tokens.js
+++ packages/themes/src/component-tokens/button/tokens.js
@@ -11,6 +11,7 @@ const buttonSeparator = {
   g10: '#e0e0e0',
   g90: '#161616',
   g100: '#161616',
+  vscode: 'var(--vscode-input-border, #3c3c3c)',
 };
 
 const buttonPrimary = {
@@ -18,6 +19,7 @@ const buttonPrimary = {
   g10: '#0f62fe',
   g90: '#0f62fe',
   g100: '#0f62fe',
+  vscode: 'var(--vscode-button-background, #0e639c)',
 };
 
 const buttonSecondary = {
@@ -25,6 +27,7 @@ const buttonSecondary = {
   g10: '#393939',
   g90: '#6f6f6f',
   g100: '#6f6f6f',
+  vscode: 'var(--vscode-button-secondaryBackground, #3a3d41)',
 };
 
 const buttonTertiary = {
@@ -32,6 +35,7 @@ const buttonTertiary = {
   g10: '#0f62fe',
   g90: '#ffffff',
   g100: '#ffffff',
+  vscode: 'var(--vscode-textLink-foreground, #3794ff)',
 };
 
 const buttonDangerPrimary = {
@@ -39,6 +43,7 @@ const buttonDangerPrimary = {
   g10: '#da1e28',
   g90: '#da1e28',
   g100: '#da1e28',
+  vscode: 'var(--vscode-errorForeground, #f48771)',
 };
 
 const buttonDangerSecondary = {
@@ -46,6 +51,7 @@ const buttonDangerSecondary = {
   g10: '#da1e28',
   g90: '#ff8389',
   g100: '#fa4d56',
+  vscode: 'var(--vscode-errorForeground, #f48771)',
 };
 
 const buttonDangerActive = {
@@ -53,6 +59,7 @@ const buttonDangerActive = {
   g10: '#750e13',
   g90: '#750e13',
   g100: '#750e13',
+  vscode: 'var(--vscode-errorForeground, #f48771)',
 };
 
 const buttonPrimaryActive = {
@@ -60,6 +67,7 @@ const buttonPrimaryActive = {
   g10: '#002d9c',
   g90: '#002d9c',
   g100: '#002d9c',
+  vscode: 'var(--vscode-button-hoverBackground, #1177bb)',
 };
 
 const buttonSecondaryActive = {
@@ -67,6 +75,7 @@ const buttonSecondaryActive = {
   g10: '#6f6f6f',
   g90: '#393939',
   g100: '#393939',
+  vscode: 'var(--vscode-button-secondaryHoverBackground, #45494e)',
 };
 
 const buttonTertiaryActive = {
@@ -74,6 +83,7 @@ const buttonTertiaryActive = {
   g10: '#002d9c',
   g90: '#c6c6c6',
   g100: '#c6c6c6',
+  vscode: 'var(--vscode-textLink-activeForeground, #4daafc)',
 };
 
 const buttonDangerHover = {
@@ -81,6 +91,7 @@ const buttonDangerHover = {
   g10: '#b81921',
   g90: '#b81921',
   g100: '#b81921',
+  vscode: 'var(--vscode-errorForeground, #f48771)',
 };
 
 const buttonPrimaryHover = {
@@ -88,6 +99,7 @@ const buttonPrimaryHover = {
   g10: '#0050e6',
   g90: '#0050e6',
   g100: '#0050e6',
+  vscode: 'var(--vscode-button-hoverBackground, #1177bb)',
 };
 
 const buttonSecondaryHover = {
@@ -95,6 +107,7 @@ const buttonSecondaryHover = {
   g10: '#474747',
   g90: '#5e5e5e',
   g100: '#5e5e5e',
+  vscode: 'var(--vscode-button-secondaryHoverBackground, #45494e)',
 };
 
 const buttonTertiaryHover = {
@@ -102,6 +115,7 @@ const buttonTertiaryHover = {
   g10: '#0050e6',
   g90: '#f4f4f4',
   g100: '#f4f4f4',
+  vscode: 'var(--vscode-textLink-activeForeground, #4daafc)',
 };
 
 const buttonDisabled = {
@@ -109,6 +123,7 @@ const buttonDisabled = {
   g10: '#c6c6c6',
   g90: 'rgb(141 141 141 / 30%)',
   g100: 'rgb(141 141 141 / 30%)',
+  vscode: 'var(--vscode-disabledForeground, #656565)',
 };
 
 export {
