f4ec18f3a2d0e290f2b79052db36ef2cf26ab738
“heloiselui” <helolui27@gmail.com>
2026-03-30 12:44:08 +0100

    fix(Menu): avoid rerender focus reset when label changes

--- packages/react/src/components/Menu/Menu.tsx
+++ packages/react/src/components/Menu/Menu.tsx
@@ -166,8 +166,12 @@ const Menu = forwardRef<HTMLUListElement, MenuProps>(function Menu(
   const ref = useMergedRefs([forwardRef, menu]);
 
   const [position, setPosition] = useState([-1, -1]);
-  const focusableItems = childContext.state.items.filter(
-    (item) => !item.disabled && item.ref.current
+  const focusableItems = useMemo(
+    () =>
+      childContext.state.items.filter(
+        (item) => !item.disabled && item.ref.current
+      ),
+    [childContext.state.items]
   );
 
   // Getting the width from the parent container element - controlled
@@ -424,7 +428,7 @@ const Menu = forwardRef<HTMLUListElement, MenuProps>(function Menu(
       return () => cancelAnimationFrame(raf);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [open, focusableItems]);
+  }, [open, focusableItems, isRoot, position]);
 
   useEffect(() => {
     if (open) {
