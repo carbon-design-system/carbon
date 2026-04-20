9e9e5225f3a52a7c1c9f4d62ed40826a2bf74017
Adam Alston <aalston9@gmail.com>
2026-04-20 12:49:00 -0400

    refactor: use different method to hide controls

--- packages/react/src/components/Pagination/Pagination.stories.js
+++ packages/react/src/components/Pagination/Pagination.stories.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2016, 2023
+ * Copyright IBM Corp. 2016, 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -199,20 +199,8 @@ export const PaginationUnknownPages = (args) => {
 };
 
 PaginationUnknownPages.storyName = 'Unknown pages and items';
-PaginationUnknownPages.argTypes = {
-  pageInputDisabled: {
-    table: {
-      disable: true,
-    },
-  },
-  pagesUnknown: {
-    table: {
-      disable: true,
-    },
-  },
-  totalItems: {
-    table: {
-      disable: true,
-    },
+PaginationUnknownPages.parameters = {
+  controls: {
+    exclude: ['pageInputDisabled', 'pagesUnknown', 'totalItems'],
   },
 };
--- packages/web-components/src/components/pagination/pagination.stories.ts
+++ packages/web-components/src/components/pagination/pagination.stories.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2019, 2025
+ * Copyright IBM Corp. 2019, 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -213,21 +213,9 @@ export const PaginationUnknownPages = {
   args: {
     totalItems: undefined,
   },
-  argTypes: {
-    pageInputDisabled: {
-      table: {
-        disable: true,
-      },
-    },
-    pagesUnknown: {
-      table: {
-        disable: true,
-      },
-    },
-    totalItems: {
-      table: {
-        disable: true,
-      },
+  parameters: {
+    controls: {
+      exclude: ['pageInputDisabled', 'pagesUnknown', 'totalItems'],
     },
   },
   render: (args) => {
