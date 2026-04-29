4d32f5933d29c38e680e3656f0667dd244984a33
Adam Alston <aalston9@gmail.com>
2026-04-29 19:59:45 -0400

    feat: add support for TypeScript stories in @carbon/react

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2016, 2025
+ * Copyright IBM Corp. 2016, 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -20,17 +20,25 @@ function getAbsolutePath(value) {
 
 const require = createRequire(import.meta.url);
 
-// We can't use .mdx files in conjuction with `storyStoreV7`, which we are using to preload stories for CI purposes only.
-// MDX files are fine to ignore in CI mode since they don't make a difference for VRT testing
+// Keep top level MDX docs pages in Storybook, but exclude component level docs
+// directories from story discovery because those pages are referenced through
+// colocated component docs instead of the main stories index.
+//
+// TODO: Remove `*.js` story support once React stories have been migrated to
+// TypeScript.
 const storyGlobs = [
   './Welcome/Welcome.mdx',
   '../src/**/*.stories.js',
+  '../src/**/*.stories.tsx',
   '../src/**/*.mdx',
   '../src/components/Tile/Tile.mdx',
   '../src/**/next/*.stories.js',
+  '../src/**/next/*.stories.tsx',
   '../src/**/next/**/*.stories.js',
+  '../src/**/next/**/*.stories.tsx',
   '../src/**/next/*.mdx',
   '../src/**/*-story.js',
+  '../src/**/*-story.tsx',
   './Preview/Preview.mdx',
 ];
 
