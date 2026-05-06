583f7b0af36a23c57724335e2774b2b43eaf7fd8
Sangeetha Babu <sangeetha9223@gmail.com>
2026-05-06 14:14:14 +0530

    fix(tabs): delete key handling issue

--- packages/web-components/src/components/tabs/tab.ts
+++ packages/web-components/src/components/tabs/tab.ts
@@ -13,6 +13,7 @@ import CDSContentSwitcherItem from '../content-switcher/content-switcher-item';
 import { TABS_ICON_SIZE, TABS_TYPE } from './defs';
 import styles from './tabs.scss?lit';
 import { carbonElement as customElement } from '../../globals/decorators/carbon-element';
+import HostListenerMixin from '../../globals/mixins/host-listener';
 import HostListener from '../../globals/decorators/host-listener';
 import '../button/button';
 import Close16 from '@carbon/icons/es/close/16.js';
@@ -29,7 +30,7 @@ import { classMap } from 'lit/directives/class-map.js';
  * @fires cds-tab-closed - The custom event fired after a a tab is closed upon a user gesture.
  */
 @customElement(`${prefix}-tab`)
-export default class CDSTab extends CDSContentSwitcherItem {
+export default class CDSTab extends HostListenerMixin(CDSContentSwitcherItem) {
   /**
    * `true` if this tab should be highlighted.
    * If `true`, parent `<cds-tabs>` selects/deselects this tab upon keyboard interaction.
@@ -97,8 +98,10 @@ export default class CDSTab extends CDSContentSwitcherItem {
   @HostListener('keydown')
   protected _handleKeydown(event: KeyboardEvent) {
     const { key } = event;
-
-    if (this._dismissable && !this.disabled && key === 'Delete') {
+    if (
+      (this._dismissable && !this.disabled && key === 'Delete') ||
+      key === 'Backspace'
+    ) {
       event.preventDefault();
       this._handleClose(event);
     }
