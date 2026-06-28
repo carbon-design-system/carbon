7bd089a840e6bfca5804ec4fcb8ebe0c11d74b4d
Subhom1 <subhomkundu@gmail.com>
2026-06-28 22:04:14 +0100

    refactor(progress-bar): replace querySelector with query decorator

---
+++ packages/web-components/src/components/progress-bar/__tests__/progress-bar-test.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright IBM Corp. 2026
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { expect, fixture, html } from '@open-wc/testing';
+import '@carbon/web-components/es/components/progress-bar/index.js';
+
+describe('cds-progress-bar', () => {
+  it('should update the bar transform when progress changes', async () => {
+    const el = await fixture(html`
+      <cds-progress-bar max="200" value="50"></cds-progress-bar>
+    `);
+    const bar = el.shadowRoot.querySelector('.cds--progress-bar__bar');
+
+    expect(bar.style.transform).to.equal('scaleX(0.25)');
+
+    el.value = 100;
+    await el.updateComplete;
+    expect(bar.style.transform).to.equal('scaleX(0.5)');
+
+    el.status = 'finished';
+    await el.updateComplete;
+    expect(bar.style.transform).to.equal('none');
+  });
+});
