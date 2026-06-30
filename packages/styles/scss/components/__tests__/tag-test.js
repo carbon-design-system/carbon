09f743e6bd83656bd1b8b2bf0f0f5a6d94f4e4d2
Subhom1 <subhomkundu@gmail.com>
2026-06-30 16:45:12 +0100

    fix(tag): guard hover styles with any-hover

--- packages/styles/scss/components/__tests__/tag-test.js
+++ packages/styles/scss/components/__tests__/tag-test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2018, 2023
+ * Copyright IBM Corp. 2018, 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -9,6 +9,7 @@
 
 'use strict';
 
+const css = require('css');
 const { SassRenderer } = require('@carbon/test-utils/scss');
 
 const { render } = SassRenderer.create(__dirname);
@@ -21,5 +22,65 @@ describe('scss/components/tag', () => {
       $_: get('mixin', meta.mixin-exists('tag', 'tag'));
     `);
     expect(unwrap('mixin')).toBe(true);
+  });
+
+  test('hover styles are only applied on devices that support hover', async () => {
+    const { result } = await render(`
+      @use '../tag';
+    `);
+    const stylesheet = css.parse(result.css.toString()).stylesheet;
+    const hoverRules = [];
+
+    function collectTagHoverRules(rules, media = []) {
+      for (const rule of rules) {
+        if (rule.type === 'media') {
+          collectTagHoverRules(rule.rules, [...media, rule.media]);
+        }
+
+        if (
+          rule.type === 'rule' &&
+          rule.selectors.some(
+            (selector) =>
+              selector.includes('.cds--tag') && selector.includes(':hover')
+          )
+        ) {
+          hoverRules.push({ media, selectors: rule.selectors });
+        }
+      }
+    }
+
+    collectTagHoverRules(stylesheet.rules);
+
+    const interactiveHoverRules = hoverRules
+      .map(({ media, selectors }) => ({
+        media,
+        // Skeleton tags have pointer-events disabled and inherit this reset
+        // from the shared skeleton utility.
+        selectors: selectors.filter(
+          (selector) => !selector.includes('.cds--skeleton:hover')
+        ),
+      }))
+      .filter(({ selectors }) => selectors.length > 0);
+
+    expect(interactiveHoverRules.length).toBeGreaterThan(0);
+    expect(
+      interactiveHoverRules.some(({ selectors }) =>
+        selectors.some((selector) =>
+          selector.includes('.cds--tag--selectable:hover')
+        )
+      )
+    ).toBe(true);
+    expect(
+      interactiveHoverRules
+        .filter(
+          ({ media }) =>
+            !media.some((query) => query.includes('(any-hover: hover)'))
+        )
+        .flatMap(({ selectors }) => selectors)
+        .filter(
+          (selector) =>
+            selector.includes('.cds--tag') && selector.includes(':hover')
+        )
+    ).toEqual([]);
   });
 });
