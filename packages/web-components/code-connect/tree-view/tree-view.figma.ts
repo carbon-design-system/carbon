803ef15a0d1e956765400ed92f18a73c6f20426c
Heloise Lui <71858203+heloiselui@users.noreply.github.com>
2026-07-09 13:48:22 +0100

    feat: add code connect support for web components (#22630)
    * feat: add code connhect support for web components
    
    * fix(web-components): update Code Connect snippets from review
    
    * fix(web-components): align tooltip and toggle mappings
    
    * chore: add tree-view issue note

--- packages/web-components/code-connect/tree-view/tree-view.figma.ts
+++ packages/web-components/code-connect/tree-view/tree-view.figma.ts
@@ -1,17 +1,13 @@
 /**
- * Copyright IBM Corp. 2016, 2026
+ * Copyright IBM Corp. 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
-// @ts-nocheck
-import React from 'react';
-import { TreeView } from '@carbon/react';
-import figma from '@figma/code-connect';
+import figma, { html } from '@figma/code-connect/html';
 
 figma.connect(
-  TreeView,
   'https://www.figma.com/design/YAnB1jKx0yCUL29j6uSLpg/(v11)-All-themes---Carbon-Design-System?node-id=11948-286738&t=aG4cJRjteQHcd71k-4',
   {
     props: {
@@ -23,12 +19,14 @@ figma.connect(
       }),
       children: figma.children(['Branch node item']),
     },
-    example: ({ children, treeNode }) => (
-      <TreeView size={treeNode.size}>
-        {/* TODO: Figma component doesn't currently nest TreeNodes accurately
-      code sample below is incomplete (Issue #17607). */}
-        {children}
-      </TreeView>
-    ),
+    // TODO: Figma component doesn't currently nest TreeNodes accurately,
+    // code sample below is incomplete (Issue #17607).
+    example: (props) =>
+      html`<cds-tree-view label="Tree View" size=${props.treeNode.size}>
+        ${props.children}
+      </cds-tree-view>`,
+    imports: [
+      "import '@carbon/web-components/es/components/tree-view/index.js'",
+    ],
   }
 );
