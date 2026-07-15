3486b79a8d9c2737b493edc4c6ff4b945d2b42f3
Taylor Jones <taylor.jones826@gmail.com>
2026-07-15 10:05:49 -0500

    fix(deps): support chalk 5 in postinstall scripts
    Chalk 5 is ESM-only, so the CommonJS postinstall scripts in
    carbon-components and carbon-components-react cannot use require('chalk').
    Replace the synchronous require with an async function that obtains
    chalk via dynamic import(), which is the supported CJS→ESM bridge.
    
    Also dedupe yarn.lock so all chalk ranges resolve to 5.6.2.
    
    Add postinstall regression tests under each package's __tests__/
    directory. The tests spawn the script in a child Node process with
    FORCE_COLOR=0 and assert: exit 0, package name in stderr, deprecation
    URL in stderr, and no TypeError.

--- packages/carbon-components/scripts/postinstall.js
+++ packages/carbon-components/scripts/postinstall.js
@@ -1,12 +1,23 @@
+/**
+ * Copyright IBM Corp. 2024, 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 'use strict';
 
-const chalk = require('chalk');
+async function main() {
+  const { default: chalk } = await import('chalk');
 
-// eslint-disable-next-line no-console
-console.warn(
-  chalk.yellow('Warning: ') +
-    'the ' +
-    chalk.cyan('carbon-components') +
-    ' package is no longer supported. More info at ' +
-    chalk.dim.underline.italic('https://carbondesignsystem.com/deprecations/')
-);
+  // eslint-disable-next-line no-console
+  console.warn(
+    chalk.yellow('Warning: ') +
+      'the ' +
+      chalk.cyan('carbon-components') +
+      ' package is no longer supported. More info at ' +
+      chalk.dim.underline.italic('https://carbondesignsystem.com/deprecations/')
+  );
+}
+
+main();
