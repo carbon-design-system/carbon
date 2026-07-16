d9d40c610a0efb79b70d7ad10f18510fdd1f2198
Mahmoud <132728978+maradwan26@users.noreply.github.com>
2026-07-16 09:03:01 -0400

    feat(code-connect): add remaining support for web components (#22687)
    * feat(code-connect): remaining web components support
    
    * fix(react): various fixes, typos, etc
    
    * fix: use static IconName
    
    ---------
    
    Co-authored-by: Heloise Lui <71858203+heloiselui@users.noreply.github.com>

--- packages/web-components/code-connect/data-table/table-cell.figma.ts
+++ packages/web-components/code-connect/data-table/table-cell.figma.ts
@@ -1,17 +1,13 @@
 /**
- * Copyright IBM Corp. 2016, 2026
+ * Copyright IBM Corp. 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
-// @ts-nocheck
-import React from 'react';
-import { TableCell } from '@carbon/react';
-import figma from '@figma/code-connect';
+import figma, { html } from '@figma/code-connect/html';
 
 figma.connect(
-  TableCell,
   'https://www.figma.com/design/YAnB1jKx0yCUL29j6uSLpg/(v11)-All-themes---Carbon-Design-System?node-id=6172-291044&t=5C1lVaCoSygb13J1-4',
   {
     props: {
@@ -22,11 +18,10 @@ figma.connect(
         true: figma.string('Cell text'),
       }),
     },
-    example: ({ children, text }) => (
-      <TableCell>
-        {text}
-        {children}
-      </TableCell>
-    ),
+    example: (props) =>
+      html`<cds-table-cell>${props.text}${props.children}</cds-table-cell>`,
+    imports: [
+      "import '@carbon/web-components/es/components/data-table/index.js'",
+    ],
   }
 );
--- packages/web-components/code-connect/ui-shell/side-nav-menu-item.figma.ts
+++ packages/web-components/code-connect/ui-shell/side-nav-menu-item.figma.ts
@@ -1,17 +1,13 @@
 /**
- * Copyright IBM Corp. 2016, 2026
+ * Copyright IBM Corp. 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
-// @ts-nocheck
-import React from 'react';
-import { SideNavLink, SideNavMenu, SideNavMenuItem } from '@carbon/react';
-import figma from '@figma/code-connect';
+import figma, { html } from '@figma/code-connect/html';
 
 figma.connect(
-  SideNavMenuItem,
   'https://www.figma.com/design/YAnB1jKx0yCUL29j6uSLpg/(v11)-All-themes---Carbon-Design-System?node-id=2346-16194&t=wcK3P98b09VsrxXF-4',
   {
     props: {
@@ -20,16 +16,17 @@ figma.connect(
         Active: true,
       }),
     },
-    example: ({ linkText, isActive }) => (
-      <SideNavMenuItem isActive={isActive} href="#">
-        {linkText}
-      </SideNavMenuItem>
-    ),
+    example: (props) =>
+      html`<cds-side-nav-menu-item href="#" active=${props.isActive}>
+        ${props.linkText}
+      </cds-side-nav-menu-item>`,
+    imports: [
+      "import '@carbon/web-components/es/components/ui-shell/index.js'",
+    ],
   }
 );
 
 figma.connect(
-  SideNavLink,
   'https://www.figma.com/design/YAnB1jKx0yCUL29j6uSLpg/(v11)-All-themes---Carbon-Design-System?node-id=2346-16194&t=wcK3P98b09VsrxXF-4',
   {
     variant: { 'Icon left': 'True' },
@@ -39,16 +36,18 @@ figma.connect(
         Active: true,
       }),
     },
-    example: ({ linkText, isActive }) => (
-      <SideNavLink renderIcon={IconName} isActive={isActive} href="#">
-        {linkText}
-      </SideNavLink>
-    ),
+    example: (props) =>
+      html`<cds-side-nav-link href="#" active=${props.isActive}>
+        <span slot="title-icon">IconName</span>
+        ${props.linkText}
+      </cds-side-nav-link>`,
+    imports: [
+      "import '@carbon/web-components/es/components/ui-shell/index.js'",
+    ],
   }
 );
 
 figma.connect(
-  SideNavMenu,
   'https://www.figma.com/design/YAnB1jKx0yCUL29j6uSLpg/(v11)-All-themes---Carbon-Design-System?node-id=2346-16194&t=wcK3P98b09VsrxXF-4',
   {
     variant: { Type: 'Sub-menu' },
@@ -58,16 +57,17 @@ figma.connect(
         Active: true,
       }),
     },
-    example: ({ linkText, isActive }) => (
-      <SideNavMenu title={linkText} isActive={isActive}>
-        <SideNavMenuItem href="#">Nested link</SideNavMenuItem>
-      </SideNavMenu>
-    ),
+    example: (props) =>
+      html`<cds-side-nav-menu title=${props.linkText} active=${props.isActive}>
+        <cds-side-nav-menu-item href="#">Nested link</cds-side-nav-menu-item>
+      </cds-side-nav-menu>`,
+    imports: [
+      "import '@carbon/web-components/es/components/ui-shell/index.js'",
+    ],
   }
 );
 
 figma.connect(
-  SideNavMenu,
   'https://www.figma.com/design/YAnB1jKx0yCUL29j6uSLpg/(v11)-All-themes---Carbon-Design-System?node-id=2346-16194&t=wcK3P98b09VsrxXF-4',
   {
     variant: { Type: 'Sub-menu', 'Icon left': 'True' },
@@ -77,19 +77,13 @@ figma.connect(
         Active: true,
       }),
     },
-    example: ({ linkText, isActive }) => (
-      <SideNavMenu renderIcon={IconName} title={linkText} isActive={isActive}>
-        <SideNavMenuItem href="#">Nested link</SideNavMenuItem>
-      </SideNavMenu>
-    ),
+    example: (props) =>
+      html`<cds-side-nav-menu title=${props.linkText} active=${props.isActive}>
+        <span slot="title-icon">IconName</span>
+        <cds-side-nav-menu-item href="#">Nested link</cds-side-nav-menu-item>
+      </cds-side-nav-menu>`,
+    imports: [
+      "import '@carbon/web-components/es/components/ui-shell/index.js'",
+    ],
   }
 );
-
-// figma.connect(
-//   HeaderMenuItem,
-//   'https://www.figma.com/design/YAnB1jKx0yCUL29j6uSLpg/(v11)-All-themes---Carbon-Design-System?node-id=2346-16194&t=wcK3P98b09VsrxXF-4',
-//   {
-//     variant: { Type: 'Divider' },
-//     example: () => <SwitcherDivider />,
-//   }
-// );
