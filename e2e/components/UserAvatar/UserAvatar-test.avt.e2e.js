9c12fcd4995fd2ba40786f4c01433671131cd5ec
Anamika T S <anamika.t.s@ibm.com>
2026-08-14 11:03:32 +0530

    fix: update copyright

--- e2e/components/UserAvatar/UserAvatar-test.avt.e2e.js
+++ e2e/components/UserAvatar/UserAvatar-test.avt.e2e.js
@@ -1,7 +1,7 @@
 // cspell:words useravatar
 
 /**
- * Copyright IBM Corp. 2024, 2025
+ * Copyright IBM Corp. 2024, 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
