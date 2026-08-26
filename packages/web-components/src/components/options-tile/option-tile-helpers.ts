a68903af39b3b432aa9473fad9a4487f084063fd
Rucha Sohoni <rucha.sohoni@gmail.com>
2026-08-26 11:10:38 +0100

    feat(web-components): migrate options tile initial commit

---
+++ packages/web-components/src/components/options-tile/option-tile-helpers.ts
@@ -0,0 +1,53 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2026
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export const blockClass = 'options-tile-stories';
+
+// spell-checker:disable
+export const languages = [
+  { label: 'English', value: 'en' },
+  { label: '简体中文 - Chinese Simplified', value: 'zh-CN' },
+  { label: '繁體中文 - Chinese Traditional', value: 'zh-TW' },
+  { label: 'Français - French', value: 'fr' },
+  { label: 'Deutsch - German', value: 'de' },
+  { label: 'Italiano - Italian', value: 'it' },
+  { label: '日本語 - Japanese', value: 'ja' },
+  { label: '한국어 - Korean', value: 'ko' },
+  { label: 'Polski - Polish', value: 'pl' },
+  { label: 'Português (brasileiro) - Portuguese (Brazilian)', value: 'pt-BR' },
+  { label: 'Русский - Russian', value: 'ru' },
+  { label: 'Español - Spanish', value: 'es' },
+];
+
+export const locales = [
+  { label: 'English', value: 'en' },
+  { label: 'English-US', value: 'en-US' },
+  { label: 'English-UK', value: 'en-UK' },
+  { label: 'English-Canada', value: 'en-CA' },
+  { label: 'English-Australia', value: 'en-AU' },
+  { label: 'Japanese', value: 'ja' },
+  { label: 'Korean', value: 'ko' },
+  { label: 'Chinese-PRC', value: 'zh-CN' },
+  { label: 'Chinese-Taiwan', value: 'zh-TW' },
+  { label: 'Vietnamese', value: 'vi' },
+  { label: 'Thai', value: 'th' },
+  { label: 'Russian', value: 'ru' },
+  { label: 'Polish', value: 'pl' },
+  { label: 'Greek', value: 'el' },
+  { label: 'Hebrew', value: 'he' },
+  { label: 'Arabic', value: 'ar' },
+  { label: 'Spanish', value: 'es' },
+  { label: 'German', value: 'de' },
+  { label: 'French', value: 'fr' },
+  { label: 'French-Canada', value: 'fr-CA' },
+  { label: 'Italian', value: 'it' },
+  { label: 'Portuguese-Brazil', value: 'pt-BR' },
+  { label: 'Turkish', value: 'tr' },
+];
+// spell-checker:enable
