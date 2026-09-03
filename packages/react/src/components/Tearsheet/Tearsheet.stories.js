d1ce930ad9ffc8b10d7c2310e7c836977d619d12
Amal K Joy <amal.joy.k@gmail.com>
2026-09-03 15:07:32 +0530

    fix: review comments

--- packages/react/src/components/Tearsheet/Tearsheet.stories.js
+++ packages/react/src/components/Tearsheet/Tearsheet.stories.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import './story.scss';
+import './Tearsheet.stories.scss';
 import docs from './Tearsheet.mdx';
 
 import React, { useRef, useState } from 'react';
