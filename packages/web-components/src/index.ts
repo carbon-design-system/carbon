a7a190c16c1060e606e81dae40509bc7e8087037
Anamika T S <47971732+anamikaanu96@users.noreply.github.com>
2026-09-09 04:52:14 +0000

    feat(web-components): v12 migrate user-avatar from carbon-for-ibm-products (#23051)
    * chore: migrate TruncatedText component to core
    
    * feat: migrate ActionSet utility to core
    
    * chore: truncatedText and Resizer story align for v12
    
    * chore: migrate resizer from labs
    
    * fix: exclude migrated components from build and d.ts generation
    
    * feat: migrate sidePanel from products
    
    * feat: exclude migrated style from build until v12
    
    * test: fix failed tests in SidePanel
    
    * fix: restore style exclusions
    
    * fix: corrected styles index to align with standard
    
    * feat: migrate truncated-text component
    
    * feat: migrate action-set utility component
    
    * feat: migrate resizer from labs to core
    
    * feat: migrate sidepanel component
    
    * chore: exclude migrated components build and from v11 stories
    
    * fix: resizer test fix
    
    * fix: update tests
    
    * feat(web-components): migrate user-avatar from carbon-for-ibm-products
    
    * fix: move useravatar to components
    
    * fix: remove @license
    
    * fix: review changes
    
    * feat(web-components): fix review comments in user avatar
    
    * fix: test failure
    
    ---------
    
    Co-authored-by: Amal K Joy <amal.joy.k@gmail.com>

--- packages/web-components/src/index.ts
+++ packages/web-components/src/index.ts
@@ -202,5 +202,6 @@ export { default as CDSLayoutConstraint } from './components/layout/layout-const
 // export { default as CDSActionSet } from './components/action-set/action-set';
 // export { default as CDSEditInPlace } from './components/edit-in-place/edit-in-place';
 // export { default as CDSSidePanel } from './components/side-panel/side-panel';
+// export { default as CDSUserAvatar } from './components/user-avatar/user-avatar';
 // export { default as CDSBigNumber } from './components/big-number/big-number';
 // export { default as CDSOptionsTile } from './components/options-tile/options-tile';
