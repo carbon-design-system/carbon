f9d6c8ff486e2641ff5a090192d6d15458f75f13
Anamika T S <47971732+anamikaanu96@users.noreply.github.com>
2026-09-11 10:43:25 +0000

    feat(web-components): v12 migrate InterstitialScreen from carbon-for-ibm-products (#23258)
    * feat(web-components): v12 migrate interestitial from ibm-products
    
    * feat(web-components): v12 migrate interestitial from ibm-products
    
    * feat(web-components): fix ai review comments in interestitial
    
    * fix: remove @license
    
    * fix: add inline notification
    
    * fix: add dleanup

---
+++ packages/web-components/src/components/interstitial-screen/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Copyright IBM Corp. 2025, 2026
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import './interstitial-screen';
+import './interstitial-screen-header';
+import './interstitial-screen-body';
+import './interstitial-screen-body-item';
+import './interstitial-screen-footer';
