67709317fdc8d3423812ce4f94d8c4b7ffd179d9
Subhom1 <subhomkundu@gmail.com>
2026-09-14 23:09:05 +0100

    fix(overflow-menu-item): add target prop type

--- packages/react/src/components/OverflowMenuItem/OverflowMenuItem-test.js
+++ packages/react/src/components/OverflowMenuItem/OverflowMenuItem-test.js
@@ -80,6 +80,24 @@ describe('OverflowMenuItem - RTL', () => {
       );
     });
 
+    it('should support anchor target and rel props', () => {
+      render(
+        <OverflowMenuItem
+          closeMenu={jest.fn()}
+          href="https://carbondesignsystem.com"
+          itemText="one"
+          rel="external noreferrer"
+          target="_blank"
+        />
+      );
+
+      expect(screen.getByRole('menuitem')).toHaveAttribute('target', '_blank');
+      expect(screen.getByRole('menuitem')).toHaveAttribute(
+        'rel',
+        'external noreferrer'
+      );
+    });
+
     it('should call closeMenu on click', async () => {
       const closeMenu = jest.fn();
       render(<OverflowMenuItem closeMenu={closeMenu} itemText="one" />);
