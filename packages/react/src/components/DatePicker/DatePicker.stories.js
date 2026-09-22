12c3db024ea0977bd7102614ce517182e4239fce
Anamika T S <anamika.t.s@ibm.com>
2026-09-22 16:40:38 +0530

    fix: copyright and remove duplicate code

--- packages/react/src/components/DatePicker/DatePicker.stories.js
+++ packages/react/src/components/DatePicker/DatePicker.stories.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2016, 2023
+ * Copyright IBM Corp. 2016, 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -150,11 +150,6 @@ export const Default = ({ readOnly, ...args }) => {
       )}
     </DatePicker>
   );
-};
-
-Default.args = {
-  invalidText: 'Error message goes here',
-  warnText: 'Warning message goes here',
 };
 
 Default.args = { ...sharedArgs };
