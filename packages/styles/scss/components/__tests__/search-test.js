eeceec048450e2dd6074f06aa0c6e0b0454412e5
Subhom1 <subhomkundu@gmail.com>
2026-09-26 22:39:53 +0100

    fix(search): add expandable search active state

--- packages/styles/scss/components/__tests__/search-test.js
+++ packages/styles/scss/components/__tests__/search-test.js
@@ -9,6 +9,7 @@
 
 'use strict';
 
+const postcss = require('postcss');
 const { SassRenderer } = require('@carbon/test-utils/scss');
 
 const { render } = SassRenderer.create(__dirname);
@@ -22,4 +23,34 @@ describe('scss/components/search', () => {
     `);
     expect(unwrap('mixin')).toBe(true);
   });
+
+  test('expandable search magnifier has an active state', async () => {
+    const { result } = await render(`
+      @use '../search';
+    `);
+
+    const styles = declarationsForSelector(
+      result.css.toString(),
+      '.cds--search--expandable .cds--search-magnifier:active'
+    );
+
+    expect(styles['background-color']).toBe(
+      'var(--cds-background-selected, rgba(141, 141, 141, 0.2))'
+    );
+  });
 });
+
+function declarationsForSelector(css, selector) {
+  const styles = {};
+  postcss.parse(css).walkRules((rule) => {
+    if (!rule.selectors.includes(selector)) {
+      return;
+    }
+    for (const node of rule.nodes) {
+      if (node.type === 'decl') {
+        styles[node.prop] = node.value;
+      }
+    }
+  });
+  return styles;
+}
